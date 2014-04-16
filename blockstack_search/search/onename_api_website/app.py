3f3a0681f5f9f0f61f720f171cf3bf15609430b6
Muhammad Asjad <asjad@halfmoon.io>
2014-04-16 16:00:25 +0500

    updated apidocumentation

--- blockstack_search/search/onename_api_website/app.py
+++ blockstack_search/search/onename_api_website/app.py
@@ -35,5 +35,5 @@ def page_not_found(e):
 
 # server launchpad
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get('PORT', 8080))
+	app.run(host='127.0.0.1', port=port)
