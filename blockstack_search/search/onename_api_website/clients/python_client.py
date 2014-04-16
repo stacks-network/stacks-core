9d013ae109c348329459ab3a016466daab6b9dee
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-04-16 13:16:01 +0500

    improved the python code to make it look like a client library

--- blockstack_search/search/onename_api_website/clients/python_client.py
+++ blockstack_search/search/onename_api_website/clients/python_client.py
@@ -1,18 +1,31 @@
 import json
 import requests
 
-PROFILE_URL = 'http://localhost:5003/v1/people/'
-SEARCH_URL = 'http://localhost:5003/v1/people-search/'
-ACCESS_TOKEN = '<ACCESS_TOKEN>'
+class OneName:
 
-#Profile API
-query = 'muneeb'
-url = PROFILE_URL + "id=" + query 
-resp = requests.get(url)
-print resp.text
+    profile_url = 'http://162.243.106.239:5005/v1/people/'
+    search_url = 'http://162.243.106.239:5005/v1/people-search/'
 
-#SEARCH API
-query = 'john '
-url = SEARCH_URL + ACCESS_TOKEN + "?keywords=" + query
-resp = requests.get(url)
-print resp.text
+    def __init__(self):
+        pass
+
+    def get_profile(self, onename_id):
+        url = self.profile_url + "id=" + onename_id 
+        resp = requests.get(url)
+        return resp.text
+
+    def search(self, query, accesss_token):
+        url = self.search_url + access_token + "?keywords=" + query
+        resp = requests.get(url)
+        return resp.text
+
+#------------------------------
+##############Main#############
+#------------------------------
+
+access_token = '35dd5f7b87ffd7580afd120f90473674'
+api = OneName()
+
+print api.get_profile('muneeb')
+print "\n#-------------------------------------------------------------------"
+print api.search('ryan', access_token)
