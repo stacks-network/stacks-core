794859d1214d012a7788c8f065f08b60993f394f
Muneeb Ali <muneeb@ali.vc>
2014-07-21 02:18:39 -0700

    added default DNS and NMC servers, from Google and OneName respectively

---
+++ config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+	Open Name System
+	~~~~~
+
+	:copyright: (c) 2014 by opennamesystem.org
+	:license: MIT, see LICENSE for more details.
+"""
+
+DNS_SERVERS = ['8.8.8.8','8.8.4.4'] #use Google's public DNS servers as default
+NMC_SERVERS = ['162.243.253.65','107.170.167.141'] #use OneName's public NMC servers as default
+
+NAMECOIND_PORT = 8332
+NAMECOIND_USER = 'opennamesystem'
+NAMECOIND_PASSWD = 'opennamesystem'
+NAMECOIND_USE_HTTPS = True
