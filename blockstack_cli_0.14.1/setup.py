6a8391a38d3448e84fd4994462b216008add5980
Muneeb Ali <muneeb@ali.vc>
2014-07-22 02:27:21 -0700

    updated setup

--- blockstack_cli_0.14.1/setup.py
+++ blockstack_cli_0.14.1/setup.py
@@ -14,17 +14,18 @@ setup(
     version='0.1.0',
     url='https://github.com/opennamesystem/opendig',
     license='MIT',
-    author='Muneeb Ali, Ryan Shea',
+    author='Muneeb Ali (@muneeb), Ryan Shea (@ryaneshea)',
     author_email='hello@halfmoonlabs.com',
     description="Command-line tool for Open Name System (like dig is for DNS)",
     packages=['opendig'],
     scripts=['bin/opendig'],
     zip_safe=False,
+    download_url = 'https://github.com/opennamesystem/opendig/tarball/0.1'
     install_requires=[
         'cement>=2.2.2',
         'dnspython>=1.11.1'
     ],
-    keywords = ['domain', 'name', 'resolve', 'bitcoin', 'address'],
+    keywords = ['domain', 'name', 'resoluton', 'bitcoin', 'address'],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
