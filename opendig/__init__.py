0650c932d78659c3bafa593516ac5bae895f167e
Muneeb Ali <muneeb@ali.vc>
2014-07-22 03:23:09 -0700

    read user ini file, if not go to fallback config file

--- opendig/__init__.py
+++ opendig/__init__.py
@@ -17,7 +17,7 @@ home_dir = os.path.expanduser('~')
 current_dir =  os.path.abspath(os.path.dirname(__file__))
 
 config_local = home_dir + '/.opendig'
-config_default = current_dir + '/config_default'
+config_default = current_dir + '/config_default.py'
 
 config = ConfigParser.ConfigParser()
 
@@ -31,13 +31,7 @@ try:
 	NAMECOIND_PASSWD = config.get('namecoind','passwd')
 	USE_HTTPS = config.get('namecoind','use_https')
 except:
-	config.read(config_default)
-	DNS_SERVER = config.get('dns','server1')
-	ONS_SERVER = config.get('ons','server1')
-	NAMECOIND_PORT = config.get('namecoind','port')
-	NAMECOIND_USER = config.get('namecoind','user')
-	NAMECOIND_PASSWD = config.get('namecoind','passwd')
-	USE_HTTPS = config.get('namecoind','use_https')
-
+	from config_default import * 
+	
 from .dns_resolver import dns_resolver
 from .ons_resolver import ons_resolver
