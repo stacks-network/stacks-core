51235119a72da923e23341cab40323befcd9dcaf
Muneeb Ali <muneeb@ali.vc>
2014-07-22 03:22:09 -0700

    added default fallback options, if a user doesn't specify a local ini file and servers

---
+++ blockstack_cli_0.14.1/opendig/config_default.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+	OpenDig
+	~~~~~
+
+	:copyright: (c) 2014 by OpenNameSystem.org
+	:license: MIT, see LICENSE for more details.
+"""
+
+#these default options are provided only for the convenience of users
+#users should really specify their own servers in ~/.opendig (in ini format)
+
+DNS_SERVER = '8.8.8.8' #use a Google DNS server as default backup 
+ONS_SERVER = '162.243.253.65' #use a OneName ONS server as default backup 
+NAMECOIND_PORT = 8332
+NAMECOIND_USER = 'opennamesystem'
+NAMECOIND_PASSWD = 'opennamesystem'
+USE_HTTPS = True
