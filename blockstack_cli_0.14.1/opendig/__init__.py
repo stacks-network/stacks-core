8b3cb8cdc89e765cfe2c063e6ad2f4429f97794d
Muneeb Ali <muneeb@ali.vc>
2014-07-22 00:29:30 -0700

    enabled proper import and reading from config file

---
+++ blockstack_cli_0.14.1/opendig/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+	Open Name System
+	~~~~~
+
+	:copyright: (c) 2014 by opennamesystem.org
+	:license: MIT, see LICENSE for more details.
+"""
+
+import os 
+import ConfigParser
+
+__version__ = '0.1.0'
+
+home_dir = os.path.expanduser('~')
+current_dir =  os.path.abspath(os.path.dirname(__file__))
+
+config_local = home_dir + '/.opendig'
+config_default = current_dir + '/config_default'
+
+config = ConfigParser.ConfigParser()
+
+#if no local configuration then use the default servers
+try:
+	config.read(config_local)
+	DNS_SERVER = config.get('dns','server1')
+	ONS_SERVER = config.get('ons','server1')
+	NAMECOIND_PORT = config.get('namecoind','port')
+	NAMECOIND_USER = config.get('namecoind','user')
+	NAMECOIND_PASSWD = config.get('namecoind','passwd')
+	USE_HTTPS = config.get('namecoind','use_https')
+except:
+	config.read(config_default)
+	DNS_SERVER = config.get('dns','server1')
+	ONS_SERVER = config.get('ons','server1')
+	NAMECOIND_PORT = config.get('namecoind','port')
+	NAMECOIND_USER = config.get('namecoind','user')
+	NAMECOIND_PASSWD = config.get('namecoind','passwd')
+	USE_HTTPS = config.get('namecoind','use_https')
+
+from .dns_resolver import dns_resolver
+from .ons_resolver import ons_resolver
