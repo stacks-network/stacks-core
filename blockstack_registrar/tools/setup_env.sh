f259ebdc092a54f0d312ebabd6ffb0b92601d326
Muneeb Ali <muneeb@ali.vc>
2014-07-30 16:45:44 -0700

    updated list of env variables

--- blockstack_registrar/tools/setup_env.sh
+++ blockstack_registrar/tools/setup_env.sh
@@ -4,32 +4,16 @@
 # All Rights Reserved
 #-----------------------
 
-echo "Enter NAMECOIND_SERVER:"
-read input
-export NAMECOIND_SERVER=$input
-
-echo "Enter NAMECOIND_PORT:"
-read input
-export NAMECOIND_PORT=$input
-
-echo "Enter NAMECOIND_USER:"
-read input
-export NAMECOIND_USER=$input
-
-echo "Enter NAMECOIND_PASSWD:"
-read input
-export NAMECOIND_PASSWD=$input
-
-echo "Enter WALLET_PASSPHRASE:"
-read input
-export WALLET_PASSPHRASE=$input
-
-echo "Enter LOAD_BALANCER:"
-read input
-export LOAD_BALANCER=$input
-
 echo "Enter MONGODB_URI:"
 read input
 export MONGODB_URI=$input
+
+echo "Enter OLD_DB:"
+read input
+export OLD_DB=$input
+
+echo "Enter FRONTEND_APP_SECRET:"
+read input
+export FRONTEND_APP_SECRET=$input
 
 echo "Done"
