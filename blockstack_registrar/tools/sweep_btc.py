d77977c5ce7a05cb35a0f519e64cca73fc03062e
Muneeb Ali <muneeb@ali.vc>
2014-08-04 23:36:19 -0700

    added support for sweeping btc wallets

--- blockstack_registrar/tools/sweep_btc.py
+++ blockstack_registrar/tools/sweep_btc.py
@@ -11,10 +11,10 @@ import requests
 from config import MONGODB_URI, OLD_DB, FRONTEND_SECRET
 
 from encrypt import bip38_decrypt
-
 from coinkit import BitcoinKeypair, NamecoinKeypair
 
 from commontools import log
+from coinrpc import bitcoind 
 
 #-----------------------------------
 from pymongo import MongoClient
@@ -37,7 +37,11 @@ def sweep_btc(transfer_user):
 	if new_user is None:
 		return		
 	
+
 	old_user = old_users.find_one({'username':new_user['username']})
+	
+	if old_user is None:
+		return
 	
 	new_btc_address = new_user['bitcoin_address']
 	old_btc_address = json.loads(old_user['profile'])['bitcoin']['address']
@@ -49,11 +53,20 @@ def sweep_btc(transfer_user):
 	if old_btc_address == keypair.address():
 		balance = fetch_balance(old_btc_address)
 
+		if balance is None:
+			return True 
+			
 		if balance > float(0):
 			log.debug(new_user['username'])
+			log.debug("old btc address: " + old_btc_address)
+			#print bitcoind.unlock_wallet()
+			#print bitcoind.importprivkey(keypair.wif_pk())
 			log.debug("final balance: %s", balance) 
 			log.debug('-' * 5)
+			return True
 
+	return False
+		
 #-----------------------------------
 def fetch_balance(btc_address):
 
@@ -64,9 +77,3 @@ def fetch_balance(btc_address):
 
 	return r.json()['final_balance'] * 0.00000001 #convert to BTC from Satoshis 
 
-#-----------------------------------
-if __name__ == '__main__':
-
-	for i in transfer.find():
-		sweep_btc(i)
-		
