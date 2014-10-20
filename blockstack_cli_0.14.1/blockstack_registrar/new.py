4a37dcf8b6e873f40b85efc13e4635e9a5e827ee
Muneeb Ali <muneeb@ali.vc>
2014-10-20 11:37:55 -0400

    simplified requirements

--- blockstack_cli_0.14.1/blockstack_registrar/new.py
+++ blockstack_cli_0.14.1/blockstack_registrar/new.py
@@ -6,15 +6,50 @@
 #-----------------------
 
 from blockdata.namecoind_cluster import get_server
+from blockdata.register import update_name
 from commontools import setup_logging
+
+import json 
 
 import logging
 setup_logging()
 log = logging.getLogger()
 
+from pymongo import MongoClient
+client = MongoClient() 
+local_db = client['temp_db']
+expiring_users = local_db.users
+
+from time import sleep
+
+#-----------------------------------
+def send_update():
+
+	for i in expiring_users.find():
+		key = i['name']
+		try:
+			value = json.loads(i['value'])
+
+			value['message'] = value['message'].replace('This OneName username','This username')
+		except:
+			value = i['value']
+			
+		print key
+		print value 
+		print '-' * 5
+
+		try:
+			update_name(key,value)
+		except Exception as e:
+			print e 
+		sleep(5)
+
 #-----------------------------------
 if __name__ == '__main__':
 
-	key = 'u/cameron'
+	#key = 'u/awrightiv'
+	#log.debug(get_server(key))
+	#value = json.loads('{"next":"u/awright"}')
+	#update_name(key,value)
 
-	log.debug(get_server(key))
+	send_update()
