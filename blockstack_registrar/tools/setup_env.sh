10a198dc3df5aa5ccfb54d579dbcba7c7a68f61c
Muneeb Ali <muneeb@ali.vc>
2014-10-29 18:54:38 -0400

    removed unused env variables

--- blockstack_registrar/tools/setup_env.sh
+++ blockstack_registrar/tools/setup_env.sh
@@ -16,4 +16,24 @@ echo "Enter FRONTEND_APP_SECRET:"
 read input
 export FRONTEND_APP_SECRET=$input
 
+echo "Enter NAMECOIND_SERVER:"
+read input
+export NAMECOIND_SERVER=$input
+
+echo "Enter NAMECOIND_PORT:"
+read input
+export NAMECOIND_PORT=$input
+
+echo "Enter NAMECOIND_USER:"
+read input
+export NAMECOIND_USER=$input
+
+echo "Enter NAMECOIND_PASSWD:"
+read input
+export NAMECOIND_PASSWD=$input
+
+echo "Enter NAMECOIND_WALLET_PASSPHRASE:"
+read input
+export NAMECOIND_WALLET_PASSPHRASE=$input
+
 echo "Done"
