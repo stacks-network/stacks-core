16c35411812205521a934ef4dda9d07ba571e02a
Muneeb Ali <muneeb@ali.vc>
2014-12-01 18:51:08 -0500

    added support to force refresh i.e., bypass the cache

--- api/proofs/views.py
+++ api/proofs/views.py
@@ -1,43 +1,28 @@
-import json, traceback
-from flask import jsonify, request
+from flask import jsonify
 
 from . import v1proofs
-from .proofs import profile_to_verifications
-from ..errors import APIError
-from ..parameters import parameters_required
+from .proofs import profile_to_proofs
 from ..crossdomain import crossdomain
-from ..profile import get_blockchain_profile
+from ..profile import get_db_profile
 from ..auth import auth_required
 
 @v1proofs.route('/users/<openname>/verifications')
-@auth_required(exception_paths=['/v1/users/fredwilson/verifications',
-								'/v1/users/example/verifications'])
+@auth_required(exception_paths=['/v1/users/example/verifications'])
 @crossdomain(origin='*')
 def verify_profile(openname):
-	profile = get_blockchain_profile(openname)
 
-	"""if not (request.data or request.form):
-		raise APIError('A payload must be included', status_code=400)
+	try:
+		refresh = int(request.args.get('refresh'))
+	except:
+		refresh = 0
 
-	if request.data:
-		try:
-			data = json.loads(request.data)
-		except ValueError:
-			raise APIError('Data must be in JSON format', status_code=400)
-	elif request.form:
-		try:
-			data = dict(request.form)
-		except:
-			traceback.print_exc()
-			raise APIError('Invalid form data', status_code=400)
-
-	openname = str(data["openname"][0])
-	if "profile" in data:
-		profile = data["profile"]
+	if refresh == 1:
+		refresh = True
 	else:
-		profile = get_blockchain_profile(openname)
-	"""
-	
-	verifications = profile_to_verifications(profile, openname)
+		refresh = False 
+
+	profile = get_db_profile(openname)
+
+	verifications = profile_to_proofs(profile, openname, refresh)
 
 	return jsonify({ "profile": profile, "verifications": verifications }), 200
