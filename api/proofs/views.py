f978019d8d98ed1717da693149597054be554c8e
Muneeb Ali <muneeb@ali.vc>
2014-12-01 19:00:53 -0500

    small fix

--- api/proofs/views.py
+++ api/proofs/views.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from . import v1proofs
 from .proofs import profile_to_proofs
 from ..crossdomain import crossdomain
-from ..profile import get_db_profile
+from ..profile import get_db_profile, get_blockchain_profile
 from ..auth import auth_required
 
 @v1proofs.route('/users/<openname>/verifications')
@@ -22,6 +22,10 @@ def verify_profile(openname):
 		refresh = False 
 
 	profile = get_db_profile(openname)
+
+	#for users registered outside of Onename
+	if profile is None:
+		profile = get_blockchain_profile(openname)
 
 	verifications = profile_to_proofs(profile, openname, refresh)
 
