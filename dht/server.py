975b3f8dcc792842063bcc19b7279b7ce260a1c8
Muneeb Ali <muneeb@ali.vc>
2014-12-18 19:05:54 -0500

    renamed to .py

---
+++ dht/server.py
@@ -0,0 +1,30 @@
+from twisted.application import service, internet
+from twisted.python.log import ILogObserver
+from twisted.internet import reactor, task
+
+import sys 
+import os
+sys.path.append(os.path.dirname(__file__))
+
+# Hack around absolute paths
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(current_dir + "/../")
+
+sys.path.insert(0, parent_dir)
+
+from kademlia.network import Server
+from kademlia import log
+
+from storage import OpennameStorage
+from client import hostname_to_ip
+from opennamelib.config import DEFAULT_DHT_SERVERS, DHT_SERVER_PORT
+
+application = service.Application("kademlia")
+application.setComponent(ILogObserver, log.FileLogObserver(sys.stdout, log.INFO).emit)
+
+kserver = Server(storage=OpennameStorage())
+bootstrap_servers = hostname_to_ip(DEFAULT_DHT_SERVERS)
+kserver.bootstrap(bootstrap_servers)
+
+server = internet.UDPServer(DHT_SERVER_PORT, kserver.protocol)
+server.setServiceParent(application)
