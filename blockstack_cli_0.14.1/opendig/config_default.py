aa5ef3ef713419de987e838128d109671c98f597
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-12-21 13:12:32 +0500

    complying with pep8 standards, fixing issue #5

--- blockstack_cli_0.14.1/opendig/config_default.py
+++ blockstack_cli_0.14.1/opendig/config_default.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-	OpenDig
-	~~~~~
+    OpenDig
+    ~~~~~
 
-	:copyright: (c) 2014 by OpenNameSystem.org
-	:license: MIT, see LICENSE for more details.
+    :copyright: (c) 2014 by OpenNameSystem.org
+    :license: MIT, see LICENSE for more details.
 """
 
-#these default options are provided only for the convenience of users
-#users should really specify their own servers in ~/.opendig (in ini format)
+# these default options are provided only for the convenience of users
+# users should really specify their own servers in ~/.opendig (in ini format)
 
-DNS_SERVERS = ['8.8.8.8','8.8.4.4'] #use a Google DNS servers as default backup 
-ONS_SERVERS = ['162.243.253.65','107.170.167.141'] #use a OneName ONS servers as default backup 
+DNS_SERVERS = ['8.8.8.8', '8.8.4.4']  # use a Google DNS servers as default backup
+ONS_SERVERS = ['162.243.253.65', '107.170.167.141']  # use a OneName ONS servers as default backup
 NAMECOIND_PORT = 8332
 NAMECOIND_USER = 'opennamesystem'
 NAMECOIND_PASSWD = 'opennamesystem'
