ce0b649aa19d1ca0941f5822c2b1da0131e6a1e2
Muneeb Ali <muneeb@ali.vc>
2015-02-04 16:50:14 -0500

    Reorganized files in a package and added periodic blockchain indexing

---
+++ opennamed/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    opennamed
+    ~~~~~
+
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.0'
+
+from opennamed import run_opennamed
+from openname_cli import run_cli
