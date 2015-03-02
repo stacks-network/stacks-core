6d3d0dcbecd06623c81ba3d0de081c730b0bacbf
Muneeb Ali <muneeb@ali.vc>
2015-03-02 16:19:54 -0500

    pep8
    updated config settings
    
    tested bulk profiles
    
    added back option for config_local.py file
    
    docker run file (placing here for convenience)
    
    adding (sample) apache config files
    
    adding a Dockerfile for openname-resolver

--- blockstack_resolver/server/config.py
+++ blockstack_resolver/server/config.py
@@ -1,65 +1,66 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-	Openname-resolver
-	~~~~~
+    Openname-resolver
+    ~~~~~
 
-	:copyright: (c) 2014 by Openname.org
-	:license: MIT, see LICENSE for more details.
+    :copyright: (c) 2015 by Openname.org
+    :license: MIT, see LICENSE for more details.
 """
 
-try: 
-	from config_local import *
+import os
+from commontools import log
+
+DEBUG = True
+
+DEFAULT_PORT = 5000
+DEFAULT_HOST = '0.0.0.0'
+DEFAULT_MEMCACHED_PORT = 11211
+DEFAULT_MEMCACHED_SERVER = '127.0.0.1'
+
+MEMCACHED_TIMEOUT = 15 * 60
+MEMCACHED_ENABLED = True
+
+try:
+    from config_local import *
 except:
 
-	import os
-	from commontools import log
+    log.debug('config_local.py not found, using default settings')
 
-	DEBUG = True
+    try:
+        MEMCACHED_USERNAME = os.environ['MEMCACHEDCLOUD_USERNAME']
+        MEMCACHED_PASSWORD = os.environ['MEMCACHEDCLOUD_PASSWORD']
+    except:
+        try:
+            MEMCACHED_USERNAME = os.environ['MEMCACHIER_USERNAME']
+            MEMCACHED_PASSWORD = os.environ['MEMCACHIER_PASSWORD']
+        except:
+            MEMCACHED_USERNAME = None
+            MEMCACHED_PASSWORD = None
 
-	DEFAULT_PORT =5000
-	DEFAULT_HOST = '0.0.0.0'
+    try:
+        MEMCACHED_SERVERS = os.environ['MEMCACHEDCLOUD_SERVERS'].split(',')
+    except:
+        try:
+            MEMCACHED_SERVERS = os.environ['MEMCACHIER_SERVERS'].split(',')
+        except:
+            memcached_server = DEFAULT_MEMCACHED_SERVER + ':' + str(DEFAULT_MEMCACHED_PORT)
+            MEMCACHED_SERVERS = [memcached_server]
 
-	try:
-		MEMCACHED_USERNAME = os.environ['MEMCACHEDCLOUD_USERNAME']
-		MEMCACHED_PASSWORD = os.environ['MEMCACHEDCLOUD_PASSWORD']
-	except:
-		try:
-			MEMCACHED_USERNAME = os.environ['MEMCACHIER_USERNAME']
-			MEMCACHED_PASSWORD = os.environ['MEMCACHIER_PASSWORD']
-		except:
-			MEMCACHED_USERNAME = None
-			MEMCACHED_PASSWORD = None
+    # --------------------------------------------------
+    NAMECOIND_USE_HTTPS = True
 
-	try:
-		MEMCACHED_SERVERS = os.environ['MEMCACHEDCLOUD_SERVERS'].split(',')
-	except:
-	    try:
-		    MEMCACHED_SERVERS = os.environ['MEMCACHIER_SERVERS'].split(',')
-	    except:
-		    MEMCACHED_SERVERS = ['127.0.0.1:11211']
-        
-	MEMCACHED_TIMEOUT = 15 * 60
-	MEMCACHED_ENABLED = True
+    try:
+        NAMECOIND_SERVER = os.environ['NAMECOIND_SERVER']
+        NAMECOIND_PORT = os.environ['NAMECOIND_PORT']
+        NAMECOIND_USER = os.environ['NAMECOIND_USER']
+        NAMECOIND_PASSWD = os.environ['NAMECOIND_PASSWD']
+    except:
+        log.debug("Namecoind not configured properly")
+        exit(1)
 
-	#--------------------------------------------------
-	NAMECOIND_USE_HTTPS = True
-
-	try:
-		NAMECOIND_SERVER = os.environ['NAMECOIND_SERVER']
-		NAMECOIND_PORT = os.environ['NAMECOIND_PORT']
-		NAMECOIND_USER = os.environ['NAMECOIND_USER']
-		NAMECOIND_PASSWD = os.environ['NAMECOIND_PASSWD']
-	except:
-		#log.debug("Namecoind not configured")
-		NAMECOIND_PORT = 5005
-		NAMECOIND_SERVER = NAMECOIND_USER = NAMECOIND_PASSWD = ''
-
-	#--------------------------------------------------
-	
-	try:
-		API_USERNAME = os.environ['API_USERNAME']
-		API_PASSWORD = os.environ['API_PASSWORD']
-	except: 
-		API_USERNAME = 'opennamesystem'
-		API_PASSWORD = 'opennamesystem'
+    # --------------------------------------------------
+    try:
+        API_USERNAME = os.environ['API_USERNAME']
+        API_PASSWORD = os.environ['API_PASSWORD']
+    except:
+        API_USERNAME = API_PASSWORD = ''
