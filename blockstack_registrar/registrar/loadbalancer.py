75e5107f5082501ca60a11039e7aca74ffda7598
Muneeb Ali <muneeb@ali.vc>
2015-03-30 17:58:29 -0400

    separated out loadbalancer and simplified it

---
+++ blockstack_registrar/registrar/loadbalancer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------
+# Copyright 2015 Halfmoon Labs, Inc.
+# All Rights Reserved
+# -----------------------
+
+from config import LOAD_SERVERS, MAX_PENDING_TX
+
+from coinrpc.namecoind_cluster import pending_transactions
+
+from commontools import log
+
+# -----------------------------------
+def load_balance(current_server):
+
+    log.debug("current server: %s", load_servers[current_server])
+
+    counter = 0
+
+    for server in LOAD_SERVERS:
+        if current_server == server:
+            server_number = counter
+
+        counter += 1
+
+    while(1):
+        if pending_transactions(LOAD_SERVERS[server_number]) > MAX_PENDING_TX:
+
+            if server_number == len(load_servers) - 1:
+                server_number = 0
+            else:
+                server_number += 1
+
+            log.debug("load balancing: switching to %s", load_servers[current_server])
+            sleep(30)
+
+        else:
+            break
+
+    return LOAD_SERVERS[server_number]
+
+# -----------------------------------
+if __name__ == '__main__':
+
+    load_balance('named3')
