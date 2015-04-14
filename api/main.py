804edadfd76c9ffc87bcab64036f832c40a4b18d
Muneeb Ali <muneeb@ali.vc>
2015-04-14 19:55:57 -0400

    This commit adds:
    1) Ability to broadcast transactions to the namecoind network via the API
    2) General fixes/updates/cleanup to get the search and users API endpoints working

--- api/main.py
+++ api/main.py
@@ -6,6 +6,7 @@
 """
 
 import requests
+import json
 
 from flask import redirect, url_for, render_template, request
 from flask import jsonify
@@ -15,6 +16,8 @@ from .errors import APIError
 from .helper import parameters_required
 from .crossdomain import crossdomain
 from .auth import auth_required
+
+from pybitcoin.rpc import namecoind
 
 
 # --------------------------------------
@@ -29,16 +32,16 @@ def index():
 def versions():
     data = {
         'api': '1',
-        'openname_specs': '0.2',
-        'openname_directory': '0.1',
+        'passcard_specs': '0.2',
+        'passcard_directory': '0.1',
     }
 
     return jsonify(data), 200
 
 
 # --------------------------------------
+#@auth_required(exception_queries=['fredwilson'])
 @app.route('/search', methods=['GET'])
-@auth_required(exception_queries=['fredwilson'])
 @parameters_required(parameters=['query'])
 @crossdomain(origin='*')
 def search_people():
@@ -66,32 +69,55 @@ def search_people():
 
 
 # --------------------------------------
-@auth_required(exception_paths=['/v1/user_count/example'])
-@crossdomain(origin='*')
+#@auth_required(exception_paths=['/v1/user_count/example'])
 @app.route('/users')
+@crossdomain(origin='*')
 def user_count():
 
-    BASE_URL = 'https://resolver.onename.com/v1/users/'
+    BASE_URL = 'http://resolver.onename.com/v1/users'
 
     try:
-        reply = requests.get(BASE_URL, timeout=1)
+        reply = requests.get(BASE_URL, timeout=3)
+
     except Exception as e:
         raise APIError(str(e), status_code=404)
 
-    return reply
+    return jsonify(reply.json()), 200
 
 
 # --------------------------------------
+#@auth_required(exception_paths=['/v1/users/example'])
 @app.route('/users/<username>')
-@auth_required(exception_paths=['/v1/users/example'])
 @crossdomain(origin='*')
 def api_user(username):
 
-    BASE_URL = 'https://resolver.onename.com/v1/users/'
+    BASE_URL = 'http://resolver.onename.com/v1/users/'
 
     try:
-        reply = requests.get(BASE_URL + username, timeout=1)
+        reply = requests.get(BASE_URL + username, timeout=3)
     except Exception as e:
         raise APIError(str(e), status_code=404)
 
-    return reply
+    return jsonify(reply.json()), 200
+
+
+# --------------------------------------
+#@auth_required(exception_paths=['/v1/users/example'])
+@app.route('/transactions/send', methods=['POST'])
+@crossdomain(origin='*')
+def broadcast_tx():
+
+    data = json.loads(request.data)
+
+    signed_hex = data['signed_hex']
+
+    reply = {}
+
+    try:
+        tx_hash = namecoind.sendrawtransaction(signed_hex)
+    except Exception as e:
+        raise APIError(str(e), status_code=404)
+
+    reply['transaction_hash'] = tx_hash
+
+    return jsonify(reply), 200
