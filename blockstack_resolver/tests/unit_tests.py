283795e3e0c07171b4d73dde42b59da014294534
Muneeb Ali <muneeb@ali.vc>
2015-07-20 19:42:50 -0400

    updates to license + pep8 changes

--- blockstack_resolver/tests/unit_tests.py
+++ blockstack_resolver/tests/unit_tests.py
@@ -5,6 +5,7 @@ import os
 import sys
 import requests
 import json
+import unittest
 
 # Hack around absolute paths
 current_dir = os.path.abspath(os.path.dirname(__file__))
@@ -12,21 +13,21 @@ parent_dir = os.path.abspath(current_dir + "/../")
 
 sys.path.insert(0, parent_dir)
 
-import unittest
 from server.resolver import app
 
 VERSION = '1'
 
 c = app.test_client()
 
+
 def build_url(api_endpoint):
 
     return '/v' + VERSION + '/' + api_endpoint
 
 
-def get_passcard(passname):
+def get_profile(username):
 
-    url = build_url('users/' + passname)
+    url = build_url('users/' + username)
 
     r = c.get(url)
 
@@ -38,13 +39,10 @@ class ResolverTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def test_connectivity(self):
         """ Check connection to resolver
         """
-        
         html = ''
-
 
         try:
             reply = c.get('/')
@@ -54,52 +52,49 @@ class ResolverTestCase(unittest.TestCase):
 
         self.assertIn('Welcome to this resolver', html, msg="resolver is not online")
 
-
-    def test_valid_passcards(self):
-        """ Check valid passcards
+    def test_valid_profiles(self):
+        """ Check valid profiles
         """
 
-        passnames = ['fredwilson', 'davidlee']
-        
-        for passname in passnames:
-    
-            reply = get_passcard(passname)
+        usernames = ['fredwilson', 'davidlee']
 
-            passcard = reply[passname]['profile']
+        for username in usernames:
 
-            verifications = reply[passname]['verifications']
+            reply = get_profile(username)
 
-            self.assertIsInstance(passcard, dict, msg="data not properly formatted")
+            profile = reply[username]['profile']
 
-            self.assertIn('name', passcard, msg="name not in data")
+            verifications = reply[username]['verifications']
+
+            self.assertIsInstance(profile, dict, msg="data not properly formatted")
+
+            self.assertIn('name', profile, msg="name not in data")
 
             self.assertGreater(len(verifications), 0, msg="no varifications found")
-    
 
-    def test_invalid_passcards(self):
-        """ Check invalid passcards
+    def test_invalid_profiles(self):
+        """ Check invalid profiles
         """
 
-        passnames = ['ben']
+        usernames = ['ben']
 
-        for passname in passnames:
-    
-            reply = get_passcard(passname)[passname]
+        for username in usernames:
 
-            self.assertIn('error', reply, msg="resolver didn't give error on invalid passcard")
+            reply = get_profile(username)[username]
 
+            self.assertIn('error', reply, msg="resolver didn't give error on invalid profile")
 
-    def test_invalid_passcards(self):
-        """ Check unregistered passnames
+    def test_invalid_profiles(self):
+        """ Check unregistered usernames
         """
 
-        passnames = ['gfegef7ev79efv9ev23t4fv']
-   
-        for passname in passnames:
-    
-            reply = get_passcard(passname)
+        usernames = ['gfegef7ev79efv9ev23t4fv']
 
-            self.assertIn('error', reply, msg="resolver didn't give error on unregistered passcard")
+        for username in usernames:
+
+            reply = get_profile(username)
+
+            self.assertIn('error', reply, msg="resolver didn't give error on unregistered profile")
 
 
 if __name__ == '__main__':
