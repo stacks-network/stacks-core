5939e7ad9945d1a9978a2b466aec35d5710cef5c
Muneeb Ali <muneeb@ali.vc>
2015-08-06 16:51:50 -0400

    updated license

--- blockstack_cli/blockstack_registrar/registrar/transfer.py
+++ blockstack_cli/blockstack_registrar/registrar/transfer.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-    registrar
+    Registrar
     ~~~~~
 
     copyright: (c) 2014 by Halfmoon Labs, Inc.
     copyright: (c) 2015 by Blockstack.org
-    license: MIT, see LICENSE for more details.
+
+This file is part of Registrar.
+
+    Registrar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Registrar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resolver. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from .nameops import get_namecoind
