d82ae973d9e80bf95fa42ab55ce0435fa30d487c
Muneeb Ali <muneeb@ali.vc>
2015-08-06 15:20:07 -0400

    updated license

--- blockstack_resolver/runserver.py
+++ blockstack_resolver/runserver.py
@@ -4,8 +4,23 @@
     Resolver
     ~~~~~
 
-    :copyright: (c) 2015 by Blockstack.org
-    :license: MIT, see LICENSE for more details.
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+
+This file is part of Resolver.
+
+    Resolver is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Resolver is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Resolver.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
