52d9a1e8b365d71f9b1418d3719ca2e451cfa087
Jude Nelson <judecn@gmail.com>
2015-08-11 14:17:10 -0400

    License as GPLv3; update copyright notice

--- blockstack_cli_0.14.1/blockstore_client/parsing.py
+++ blockstack_cli_0.14.1/blockstore_client/parsing.py
@@ -1,6 +1,25 @@
-# TODO: this module will become the top-level module for:
-# -- calling into the virtual blockchain's OP_RETURN parsers
-# -- holding any serialization and deserialization logic that is common across this system.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstore-client
+    ~~~~~
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+    
+    This file is part of Blockstore-client.
+    
+    Blockstore-client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Blockstore-client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Blockstore-client.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import json 
 
