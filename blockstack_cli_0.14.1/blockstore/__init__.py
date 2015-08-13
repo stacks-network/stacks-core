49a0aa428fe897d239ffbce8921aaf22aeb45585
Muneeb Ali <muneeb@ali.vc>
2015-08-12 20:07:57 -0400

    PEP8 + error checking
    * makes some PEP8 changes (mostly 4 line spaces instead of 3)
    
    * adds error displays for ping and getinfo command (users generally tend to use these two to test connectivity to the server)

--- blockstack_cli_0.14.1/blockstore/__init__.py
+++ blockstack_cli_0.14.1/blockstore/__init__.py
@@ -5,14 +5,14 @@
     ~~~~~
     copyright: (c) 2014 by Halfmoon Labs, Inc.
     copyright: (c) 2015 by Blockstack.org
-    
+
     This file is part of Blockstore-client.
-    
+
     Blockstore-client is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    
+
     Blockstore-client is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -21,12 +21,16 @@
     along with Blockstore-client.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import client 
-import config 
+import client
+import config
 import schemas
 import parsing
 import user
-import plugins
+import drivers
 
-from client import getinfo, get_user_record, ping, lookup, preorder, update, transfer, renew, revoke, namespace_preorder, namespace_define, namespace_begin, \
-                   get_immutable, get_mutable, put_immutable, put_mutable, delete_immutable, delete_mutable, session, register_storage
+from client import getinfo, get_user_record, ping, lookup
+from client import preorder, update, transfer, renew, revoke
+from client import namespace_preorder, namespace_define, namespace_begin
+from client import get_immutable, get_mutable
+from client import put_immutable, put_mutable, delete_immutable, delete_mutable
+from client import session, register_storage
