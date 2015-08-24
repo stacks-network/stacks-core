b094b3099ba10386212c2867c5df10f02262233d
Jude Nelson <judecn@gmail.com>
2015-08-24 18:11:52 -0400

    Export NAME_IMPORT and NAME_TRANSFER methods

--- blockstore/lib/nameset/__init__.py
+++ blockstore/lib/nameset/__init__.py
@@ -25,6 +25,6 @@ import namedb
 import virtualchain_hooks
 
 from .namedb import BlockstoreDB, BlockstoreDBIterator, get_namespace_from_name, price_name, is_name_mining_fee_sufficient, \
-    is_namespace_mining_fee_sufficient
+    is_namespace_mining_fee_sufficient, get_name_from_fq_name
 from .virtualchain_hooks import get_virtual_chain_name, get_virtual_chain_version, get_first_block_id, get_opcodes, \
     get_op_processing_order, get_magic_bytes, get_db_state, db_parse, db_check, db_commit, db_save, db_iterable
