40be6ad5818ac1ef9915b03ca58b10b399044d06
Jude Nelson <judecn@gmail.com>
2015-09-02 16:17:48 -0400

    Confirm whether or not names have been imported after 10 blocks. Log the set of names confirmed separately from the set of names sent.

--- blockstack_cli/bin/namespace_import.py
+++ blockstack_cli/bin/namespace_import.py
@@ -20,6 +20,8 @@ sys.path.insert(0, parent_dir)
 
 from blockstore import client, config
 
+AVG_BLOCK_TIME = 600    # average number of seconds between blocks 
+CONFIRM_DELAY = 10      # number of blocks to wait to pass before confirming that the name was registered
 
 def pretty_dump(json_str):
     """ pretty dump
@@ -56,6 +58,45 @@ def get_chaincom_api_keys( path="./chaincom.ini" ):
     
     return parser.get("chaincom", "api_key_id"), parser.get("chaincom", "api_key_secret")
 
+
+def confirm_name_imported( client, name ):
+    """
+    See if a name has been imported.
+    """
+    name_info = client.lookup( name )
+    if 'error' in name_info:
+        return False 
+    
+    # must be a full record 
+    name_info = name_info[0]
+    
+    if name_info is None:
+        return False 
+    
+    if 'address' not in name_info:
+        return False 
+    
+    if 'value_hash' not in name_info:
+        return False 
+    
+    if name_info['value_hash'] is None:
+        return False 
+    
+    return True
+    
+
+def find_imported( client, name_list ):
+    """
+    Find the list of names that have been imported
+    """
+    imported = []
+    
+    for name in name_list:
+        if confirm_name_imported( client, name ):
+            imported.append( name )
+            
+    return imported
+        
 
 if __name__ == "__main__":
     
@@ -99,13 +140,17 @@ if __name__ == "__main__":
         print >> sys.stderr, "Invalid JSON file '%s'" % sys.argv[1]
         sys.exit(1)
 
-    # record successfully-imported names 
-    logfile_path = namespace_id + ".log"
+    # recorrd name status
+    logfile_path = namespace_id + ".sent"
     failed_path = namespace_id + ".failed"
+    confirmed_path = namespace_id + ".confirmed"
     
-    processed_so_far_lines = None
-    processed_so_far = []
-    imported_names = {}         # map fully-qualified names to their import records
+    sent_names_json = []
+    sent_names = {}
+    
+    confirmed_names = []
+    
+    unconfirmed_names = {}      # map name to send time
     
     failed_lines = None
     failed = []
@@ -114,16 +159,22 @@ if __name__ == "__main__":
     try:
         # should contain newline-separated list of names we've processed so far
         if not os.path.exists( logfile_path ):
-            log_fd = open( logfile_path, "w+" )
+            sent_fd = open( logfile_path, "w+" )
         else:
-            log_fd = open( logfile_path, "r+" )
+            sent_fd = open( logfile_path, "r+" )
         
         if not os.path.exists( failed_path ):
             failed_fd = open( failed_path, "w+" )
         else:
             failed_fd = open( failed_path, "r+" )
         
-        processed_so_far_lines = log_fd.read()
+        if not os.path.exists( confirmed_path ):
+            confirmed_fd = open( confirmed_path, "w+" )
+        else:
+            confirmed_fd = open( confirmed_path, "r+" )
+            
+        sent_names_json = sent_fd.read().split("\n")
+        confirmed_names = confirmed_fd.read().split("\n")
         failed_fd.seek( 0, 2 )
         
     except Exception, e:
@@ -131,24 +182,54 @@ if __name__ == "__main__":
         print >> sys.stderr, "Failed to open '%s'" % logfile_path
         sys.exit(1)
 
-    processed_so_far = processed_so_far_lines.split("\n")
-    num_processed_so_far = len(processed_so_far)
-    
-    # each line in the log is a JSON object...
-    for imported_name_json in processed_so_far:
-        if len(imported_name_json.strip()) == 0:
+    # get list of sent name records
+    for name_json in sent_names_json:
+        if len(name_json.strip()) == 0:
             continue
         
-        imported_name_rec = json.loads( imported_name_json.strip() )
-        imported_names[ imported_name_rec['name'] ] = imported_name_rec
+        name_rec = json.loads( name_json.strip() )
+        sent_names[ name_rec['name'] ] = name_rec
     
     # connect to blockstore
     conf = config.get_config()
     proxy = client.session(conf['blockstored_server'], conf['blockstored_port'])
     
-    # do all registers
+    time_of_last_confirmation = 0 
+    
+    # find any previously-sent-but-not-confirmed names 
+    for sent_name in sent_names.keys():
+        if sent_name not in confirmed_names:
+            if not sent_names[sent_name].has_key('time') or sent_names[sent_name]['time'] + (AVG_BLOCK_TIME * CONFIRM_DELAY) < time.time():
+                # it's been a while, so let's try to confirm this name
+                t = sent_names[sent_name].get('time', 0)
+                unconfirmed_names[ sent_name ] = t
+    
+    # do all imports
     for name in names:
         
+        # every block (or on start-up), update the list of imported names
+        if time_of_last_confirmation + AVG_BLOCK_TIME < time.time():
+            
+            # get sent names that were sent more than AVG_BLOCK_TIME * CONFIRM_DELAY seconds ago 
+            names_to_check = filter( lambda n: unconfirmed_names[n] + AVG_BLOCK_TIME * CONFIRM_DELAY < time.time(), unconfirmed_names.keys() )
+            
+            # which of our unconfirmed names have been confirmed?
+            new_confirmed = find_imported( client, names_to_check )
+            for confirmed_name in new_confirmed:
+                
+                confirmed_fd.write( "%s\n" % confirmed_name )
+                confirmed_fd.flush()
+                
+                if confirmed_name in sent_names.keys():
+                    del sent_names[confirmed_name]
+                    
+                confirmed_names.append( confirmed_name )
+                
+                del unconfirmed_names[ confirmed_name ]
+                
+            time_of_last_confirmation = time.time()
+            
+                
         username = name['username']
         nmc_address = str(name['nmc_address'])
         update_hash = str(name['hash'])
@@ -157,11 +238,14 @@ if __name__ == "__main__":
         
         address = namecoin_to_bitcoin_address( nmc_address )
         
-        if fqn in imported_names.keys():
+        if fqn in confirmed_names:
+            # already imported
             continue 
-
+        
         print "name_import " + fqn + " " + address + " " + update_hash
 
+        sys.exit(0)
+        
         try:
             result = client.name_import( fqn, address, update_hash, privkey_str )
         except:
@@ -184,25 +268,40 @@ if __name__ == "__main__":
             continue 
         
         # record progress
-        num_processed_so_far += 1
+        num_sent_names += 1
         
         result['name'] = fqn
+        result['time'] = time.time()
         result_str = json.dumps( result )
         
-        log_fd.write( "%s\n" % result_str)
-        log_fd.flush()
+        sent_fd.write( "%s\n" % result_str)
+        sent_fd.flush()
         
         print pretty_dump( result )
         
-        time.sleep(20)
+        unconfirmed_names[ fqn ] = result['time']
         
-        if (num_processed_so_far % 20 == 0):
+        # how long are we doing to wait?
+        delay = 20
+        try:
+            with open("delay.txt", "r") as fd:
+                delay_txt = fd.read()
+            
+            delay = float(delay_txt.strip())
+        except Exception, e:
+            print >> sys.stderr, "failed to read delay.txt; assuming %s" % delay
+            
+        time.sleep(delay)
+        
+        
+        """
+        if (num_sent_names % 20 == 0):
             
             checkpoint_tx = send_checkpoint( chaincom_client, privkey_str )
             print pretty_dump( checkpoint_tx )
             
             time.sleep(20)
-            
+        """
 
     sys.exit(0)
         
