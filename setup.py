2e95cd4d06fcbcb6250bb99dc2bfbec40ee467e2
Muneeb Ali <muneeb@ali.vc>
2015-09-22 14:22:05 -0400

    small fix

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'virtualchain>=0.0.2',
         'kademlia>=0.5',
-        'keychain>=0.1.3'
+        'keychain>=0.1.3',
         'blockstore-client>=0.0.1'
     ],
     classifiers=[
