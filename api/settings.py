f4cb61233d50975d4b43938eb78c537b15b5505b
Ryan Shea <ryan@shea.io>
2015-10-05 18:02:43 -0400

    switch over to using test database locally

--- api/settings.py
+++ api/settings.py
@@ -30,16 +30,12 @@ except:
 # Secret settings
 secrets_list = [
     'INDEX_DB_URI', 'SECRET_KEY', 'MONGODB_PASSWORD',
-    'MAILGUN_API_KEY', 'MONGODB_URI'
+    'MAILGUN_API_KEY', 'MONGODB_URI', 'MONGODB_DB'
 ]
 for env_variable in os.environ:
     if env_variable in secrets_list:
         env_value = os.environ[env_variable]
         exec(env_variable + " = \"\"\"" + env_value + "\"\"\"")
-
-parts = re.split(':|/|@|,|mongodb://', MONGODB_URI)
-(_, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_PORT, _, _,
-    MONGODB_DB) = parts
 
 if 'DYNO' in os.environ:
     DEBUG = False
@@ -48,11 +44,6 @@ else:
     DEBUG = True
     APP_URL = 'localhost:5000'
 
-    """
-    MONGODB_HOST = 'localhost'
-    MONGODB_PORT = 27017
-    MONGODB_DB = 'onename_api8'
-
+    MONGODB_DB = 'onename-api-test'
     MONGODB_URI = 'mongodb://%s:%s/%s' % (
-        MONGODB_HOST, str(MONGODB_PORT), MONGODB_DB)
-    """
+        'localhost', str(27017), MONGODB_DB)
