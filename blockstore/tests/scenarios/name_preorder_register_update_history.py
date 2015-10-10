45993efb4a4ca2ff0264a7c44e684b5c2eb1030d
Jude Nelson <judecn@gmail.com>
2015-10-09 21:57:33 -0400

    Add a bunch of basic functionality unit tests.  The naming is self-explanatory--for example, name_preorder_register_update_cantforce will create a namespace, preorder a name, register it, update it, try to update it with a different key, and verify that the different key can't force the update.
    Each test allows the programmer to define a set of wallets filled with
    btc in advance, create a blockchain by describing the order of
    operations on a block-by-block basis, and then query the resulting
    blockstore database to verify that each operation was processed
    correctly.  The 'testlib.py' file serves as a common interface between
    blockstore's API and the test logic--it will be stable for all tests, so
    blockstore can evolve independently.

---
+++ blockstore/tests/scenarios/name_preorder_register_update_history.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python 
+
+import testlib
+import pybitcoin
+import json
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 ),
+    testlib.Wallet( "5Kg5kJbQHvk1B64rJniEmgbD83FpZpbw2RjdAZEzTefs9ihN3Bz", 100000000000 ),
+    testlib.Wallet( "5JuVsoS9NauksSkqEjbUZxWwgGDQbMwPsEfoRBSpLpgDX1RtLX7", 100000000000 ),
+    testlib.Wallet( "5KEpiSRr1BrT8vRD7LKGCEmudokTh1iMHbiThMQpLdwBwhDJB1T", 100000000000 )
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+
+# map update hash to transaction
+txids = {}
+
+# map update hash to consensus hash 
+consensuses = {}
+
+def scenario( wallets, **kw ):
+
+    global txids
+    global consensuses
+
+    testlib.blockstore_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_namespace_reveal( "test", wallets[1].addr, 52595, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 10, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_namespace_ready( "test", wallets[1].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_name_preorder( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+    
+    testlib.blockstore_name_register( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+
+    # do a sequence of updates, every other block
+    for i in xrange( 0, 20 ):
+
+        if (i % 2) != 0:
+            update_hash = ("%02x" % (i)) * 20
+            resp = testlib.blockstore_name_update( "foo.test", update_hash, wallets[3].privkey )
+
+            txids[ update_hash ] = resp['transaction_hash']
+            consensuses[ update_hash ] = testlib.get_consensus_at( testlib.get_current_block( **kw ), **kw )
+
+        testlib.next_block( **kw )
+
+    testlib.next_block( **kw )
+
+
+def check( state_engine ):
+
+    global txids 
+    global consensus 
+    global consensuses
+
+    # not revealed, but ready 
+    ns = state_engine.get_namespace_reveal( "test" )
+    if ns is not None:
+        return False 
+
+    ns = state_engine.get_namespace( "test" )
+    if ns is None:
+        return False 
+
+    if ns['namespace_id'] != 'test':
+        return False 
+
+    # not preordered
+    preorder = state_engine.get_name_preorder( "foo.test", pybitcoin.make_pay_to_address_script(wallets[2].addr), wallets[3].addr )
+    if preorder is not None:
+        return False
+    
+    # registered 
+    name_rec = state_engine.get_name( "foo.test" )
+    if name_rec is None:
+        return False 
+
+    # updated (latest version: update hash pattern is 0x13)
+    if name_rec['value_hash'] != '13' * 20:
+        return False 
+
+    # name didn't exist before its preorder
+    before_registration = state_engine.get_name_at( "foo.test", name_rec['first_registered'] - 2 )
+    if before_registration is not None:
+        print "before registration"
+        print json.dumps( before_registration, indent=4 )
+        return False 
+
+    # name was preordered just prior to its registration 
+    preorder = state_engine.get_name_at( "foo.test", name_rec['block_number'] )
+    if preorder is None:
+        print "no preorder"
+        return False
+
+    if not preorder.has_key('nameop') or preorder['nameop'] != 'NAME_PREORDER':
+        print "not a preorder"
+        print json.dumps( preorder, indent=4 )
+        print "history"
+        print json.dumps( state_engine.get_name_history( "foo.test", name_rec['block_number'], state_engine.get_current_block() ), indent=4 )
+        return False
+
+    # name existed at the first point of its registration
+    at_registration = state_engine.get_name_at( "foo.test", name_rec['first_registered'] )
+    if at_registration is None:
+        print "at_registration is None"
+        return False 
+
+    # name had null update hash then 
+    if at_registration['value_hash'] is not None:
+        print "at_registration"
+        print json.dumps( at_registration, indent=4 )
+        return False
+
+    # get history...
+    name_history = state_engine.get_name_history( "foo.test", name_rec['first_registered'], state_engine.get_current_block() )
+
+    # did 10 updates and 1 register
+    if len(name_history) != 11:
+        print "history" 
+        print json.dumps(name_history, indent=4 )
+        return False 
+
+    for i in xrange(0, 10):
+        snapshot = name_history[i+1]
+        expected_value_hash = ("%02x" % (2*i + 1)) * 20
+
+        if snapshot['value_hash'] != expected_value_hash:
+            print "Invalid value hash '%s'" % expected_value_hash
+            print json.dumps( name_history, indent=4 )
+            return False 
+
+        if snapshot['txid'] != txids[expected_value_hash]:
+            print "Invalid txid '%s'" % snapshot['txid']
+            print json.dumps( name_history, indent=4 )
+            return False 
+
+
+    return True
--- blockstore/tests/scenarios/testlib.py
+++ blockstore/tests/scenarios/testlib.py
@@ -23,53 +23,53 @@ class Wallet(object):
         self.value = int(value_str)
 
 
-def blockstore_name_preorder( name, privatekey, register_addr, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False ):
-    resp = blockstored.blockstore_name_preorder( name, privatekey, register_addr, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_preorder( name, privatekey, register_addr, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_preorder( name, privatekey, register_addr, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_name_register( name, privatekey, register_addr, renewal_fee=None, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False ):
-    resp = blockstored.blockstore_name_register( name, privatekey, register_addr, renewal_fee=renewal_fee, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_register( name, privatekey, register_addr, renewal_fee=None, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_register( name, privatekey, register_addr, renewal_fee=renewal_fee, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash)
     return resp
 
 
-def blockstore_name_update( name, data_hash, privatekey, tx_only=False, pay_fee=True, public_key=None, subsidy_key=None, testset=False ):
-    resp = blockstored.blockstore_name_update( name, data_hash, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_update( name, data_hash, privatekey, tx_only=False, pay_fee=True, public_key=None, subsidy_key=None, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_update( name, data_hash, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_name_transfer( name, address, keepdata, privatekey, public_key=None, subsidy_key=None, tx_only=False, pay_fee=True, testset=False ):
-    resp = blockstored.blockstore_name_transfer( name, address, keepdata, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_transfer( name, address, keepdata, privatekey, public_key=None, subsidy_key=None, tx_only=False, pay_fee=True, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_transfer( name, address, keepdata, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_name_renew( name, privatekey, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False ):
-    resp = blockstored.blockstore_name_renew( name, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_renew( name, privatekey, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_renew( name, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_name_revoke( name, privatekey, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False ):
-    resp = blockstored.blockstore_name_revoke( name, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset )
+def blockstore_name_revoke( name, privatekey, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_revoke( name, privatekey, tx_only=tx_only, pay_fee=pay_fee, subsidy_key=subsidy_key, public_key=public_key, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_name_import( name, recipient_address, update_hash, privatekey, tx_only=False, testset=False ):
-    resp = blockstored.blockstore_name_import( name, recipient_address, update_hash, privatekey, tx_only=tx_only, testset=testset )
+def blockstore_name_import( name, recipient_address, update_hash, privatekey, tx_only=False, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_name_import( name, recipient_address, update_hash, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_only=False, testset=False ):
-    resp = blockstored.blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_only=tx_only, testset=testset )
+def blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_only=False, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey, tx_only=False, testset=False ):
-    resp = blockstored.blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey, tx_only=tx_only, testset=testset )
+def blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey, tx_only=False, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
-def blockstore_namespace_ready( namespace_id, privatekey, tx_only=False, testset=False ):
-    resp = blockstored.blockstore_namespace_ready( namespace_id, privatekey, tx_only=tx_only, testset=testset )
+def blockstore_namespace_ready( namespace_id, privatekey, tx_only=False, testset=False, consensus_hash=None ):
+    resp = blockstored.blockstore_namespace_ready( namespace_id, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     return resp
 
 
@@ -78,10 +78,28 @@ def next_block( **kw ):
     Advance the mock blockchain by one block.
     Required keyword arguments:
     * bitcoind: the mock bitcoind
-    * block_counter: a dict mapping 'count' to an int
+    * sync_virtualchain_upcall: a no-argument callable that will sync
+    the blockstore db with the virtual chain
     """
-    bitcoind = kw['bitcoind']
-    kw['block_counter']['count'] += 1
-
-    bitcoind.flush_transactions() 
     
+    kw['bitcoind'].flush_transactions() 
+    kw['sync_virtualchain_upcall']()
+
+   
+def get_consensus_at( block_id, **kw ):
+    """
+    Get the consensus hash at a particular block id.
+    Required keyword arguments:
+    * state_engine:  a reference to the virtualchain state engine.
+    """
+
+    return kw['state_engine'].get_consensus_at( block_id )
+
+def get_current_block( **kw ):
+    """
+    Get the current block id.
+    Required keyword arguments:
+    * state_engine:  a reference to the virtualchain state engine.
+    """
+    
+    return kw['state_engine'].get_current_block()
