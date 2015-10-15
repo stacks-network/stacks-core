db73811ec9ece91e8d5d85ea23748d0cee12c80a
Muneeb Ali <muneeb@ali.vc>
2015-10-15 18:34:38 -0400

    moving namecoin functionality (will get deprecated once relevant parts are re-used)

---
+++ blockstack_registrar/registrar/nmc/update.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+    Registrar
+    ~~~~~
+
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+
+This file is part of Registrar.
+
+    Registrar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Registrar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Registrar. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+from pymongo import MongoClient
+from register import process_user, utf8len
+from coinrpc import namecoind
+from config import MONGODB_URI
+
+import os
+
+LOAD_BALANCER = os.environ['LOAD_BALANCER']
+
+remote_client = MongoClient(MONGODB_URI)
+remote_db = remote_client.get_default_database()
+users = remote_db.user
+private_key = remote_client.private_key
+
+from datetime import datetime
+
+
+def update_profile(username, profile):
+
+    #update_name does json.dumps internally
+    process_user(username, profile)
+
+
+def update_profile_from_DB(username):
+
+    entry = users.find_one({'username': username})
+
+    profile = json.loads(entry['profile'])
+
+    update_profile(username, profile)
+
+
+def update_profile_from_file(username,file_name='tools/json_profile.json'):
+
+    json_data = open(file_name)
+
+    try:
+        profile = json.load(json_data)
+    except Exception as e:
+        print e
+        return
+
+    update_profile(username, profile)
+
+
+def private_key_expired(time):
+
+    now = datetime.now()
+    diff = now - time
+    day_diff = diff.days
+
+    if diff.days < -1:
+        return True
+    else:
+        return False
+
+
+def delete_expired_private_keys():
+    print "Private keys: "
+    for user in private_key.find():
+
+        print user['username']
+        if private_key_expired(user['created_at']):
+            private_key.remove(user)
+
+    print '-' * 5
+
+
+def update_profile_from_private_key(username):
+
+    user_private_key = private_key.find_one({"username":username})
+    user = users.find_one({"username":username})
+
+    if ('backend_server' in user) and (user['backend_server'] == int(LOAD_BALANCER)):
+        process_user(user['username'],json.loads(user['profile']))
+        user['profile_update_pending'] = False
+        users.save(user)
+        private_key.remove(user_private_key)
+
+
+def process_profile_updates():
+
+    print "Profile update pending: "
+    for user in users.find():
+
+        if 'name_transferred' in user and user['name_transferred'] is True:
+            #print "name already transferred: " + user['username']
+            continue
+
+        if 'profile_update_pending' in user and user['profile_update_pending']:
+
+            if not namecoind.check_registration('u/' + user['username']):
+                print "Not registered yet: " + user['username']
+                continue
+            else:
+                print "Updating user: " + user['username']
+                print user['backend_server']
+                update_profile_from_private_key(user['username'])
+
+    print '-' * 5
+
+    #delete_expired_private_keys()
+
+
+if __name__ == '__main__':
+
+    username = ''
+    #user = users.find_one({"username":username})
+    #print user['backend_server']
+    #update_profile_from_DB(username)
+    update_profile_from_file(username)
+    #process_profile_updates()
