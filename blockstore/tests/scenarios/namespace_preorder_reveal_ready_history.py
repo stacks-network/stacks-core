a2ab66476e4b0c17453140b8a65a3c7b49bf2607
Jude Nelson <judecn@gmail.com>
2015-10-15 19:59:53 -0400

    Add test to ensure we can get history of a namespace record, and get the namepace record at each point in time.

---
+++ blockstore/tests/scenarios/namespace_preorder_reveal_ready_history.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python 
+
+import testlib 
+import json
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 )
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+
+def scenario( wallets, **kw ):
+
+    testlib.blockstore_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_namespace_reveal( "test", wallets[1].addr, 52595, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 10, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_namespace_ready( "test", wallets[1].privkey )
+    testlib.next_block( **kw )
+
+def check( state_engine ):
+
+    # not revealed, but ready 
+    ns = state_engine.get_namespace_reveal( "test" )
+    if ns is not None:
+        return False 
+
+    ns = state_engine.get_namespace( "test" )
+    if ns is None:
+        return False 
+
+    if ns['namespace_id'] != 'test':
+        return False 
+
+    if ns.get('opcode', None) != 'NAMESPACE_READY':
+        return False 
+
+    ns_preorder = state_engine.get_namespace_at( "test", ns['block_number'] )
+    ns_reveal = state_engine.get_namespace_at( "test", ns['block_number'] + 1 )
+  
+    if ns_preorder is None:
+        return False 
+
+    if ns_preorder.get('opcode', None) != "NAMESPACE_PREORDER":
+        return False 
+
+    if ns_reveal is None:
+        return False 
+
+    if ns_reveal.get('opcode', None) != "NAMESPACE_REVEAL":
+        return False 
+    
+    return True
