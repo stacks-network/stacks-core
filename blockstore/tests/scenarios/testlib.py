7803708fd3b7cd0b3eab9f2063cfbe4daae8a579
Jude Nelson <judecn@gmail.com>
2015-10-15 20:03:59 -0400

    Add logic to next_block() to snapshot the db at each block and save its consensus hash, as well as logic to use these snapshots to verify that we can reconstruct the database at each block.  These tests will be applied systemically.

--- blockstore/tests/scenarios/testlib.py
+++ blockstore/tests/scenarios/testlib.py
@@ -4,6 +4,9 @@
 
 import os
 import sys
+import tempfile
+import errno
+import shutil
 
 # hack around absolute paths
 current_dir =  os.path.abspath(os.path.dirname(__file__) + "/../../..")
@@ -21,6 +24,22 @@ class Wallet(object):
         self.pubkey_hex = pk.public_key().to_hex()
         self.addr = pk.public_key().address()
         self.value = int(value_str)
+
+# store the database after each block, under this directory
+snapshots_dir = None
+
+# consensus hash at each block
+all_consensus_hashes = {}
+
+def log_consensus( **kw ):
+    """
+    Log the consensus hash at the current block.
+    """
+    global all_consensus_hashes 
+
+    block_id = get_current_block( **kw ) 
+    ch = get_consensus_at( block_id, **kw )
+    all_consensus_hashes[ block_id ] = ch
 
 
 def blockstore_name_preorder( name, privatekey, register_addr, tx_only=False, pay_fee=True, subsidy_key=None, public_key=None, testset=False, consensus_hash=None ):
@@ -73,6 +92,30 @@ def blockstore_namespace_ready( namespace_id, privatekey, tx_only=False, testset
     return resp
 
 
+def blockstore_verify_database( consensus_hash, consensus_block_id, db_path, working_db_path=None, start_block=None, testset=False ):
+    return blockstored.verify_database( consensus_hash, consensus_block_id, db_path, working_db_path=working_db_path, start_block=start_block, testset=testset )
+
+
+def blockstore_export_db( path, **kw ):
+    db = kw['state_engine']
+    try:
+        db.export_db( path )
+    except IOError, ie:
+        if ie.errno == errno.ENOENT:
+            pass
+        else:
+            raise
+
+
+def get_all_transactions( **kw ):
+    """
+    Get all bitcoind transactions.
+    Requires:
+    * bitcoind: the mock bitcoind
+    """
+    return kw['bitcoind'].getrawtransactions( 1 )
+
+
 def next_block( **kw ):
     """
     Advance the mock blockchain by one block.
@@ -81,9 +124,19 @@ def next_block( **kw ):
     * sync_virtualchain_upcall: a no-argument callable that will sync
     the blockstore db with the virtual chain
     """
+
+    global snapshots_dir 
     
+    if snapshots_dir is None:
+        snapshots_dir = tempfile.mkdtemp( prefix='blockstore-test-databases-' )
+
+    # flush all transactions
     kw['bitcoind'].flush_transactions() 
     kw['sync_virtualchain_upcall']()
+
+    # snapshot the database
+    blockstore_export_db( os.path.join( snapshots_dir, "blockstore.db.%s" % get_current_block( **kw )), **kw )
+    log_consensus( **kw )
 
    
 def get_consensus_at( block_id, **kw ):
@@ -95,6 +148,7 @@ def get_consensus_at( block_id, **kw ):
 
     return kw['state_engine'].get_consensus_at( block_id )
 
+
 def get_current_block( **kw ):
     """
     Get the current block id.
@@ -103,3 +157,46 @@ def get_current_block( **kw ):
     """
     
     return kw['state_engine'].get_current_block()
+
+
+def cleanup():
+    """
+    Clean up temporary test state.
+    """
+    
+    global snapshots_dir
+    global all_consensus_hashes 
+
+    shutil.rmtree( snapshots_dir )
+    snapshots_dir = None
+    all_consensus_hashes = {}
+    
+
+def check_history( state_engine ):
+    """
+    Verify that the database is reconstructable and 
+    consistent at each point in its history.
+    """
+
+    global all_consensus_hashes
+    global snapshots_dir
+
+    block_ids = sorted( all_consensus_hashes.keys() )
+    db_path = state_engine.get_db_path()
+
+    for block_id in block_ids:
+    
+        state_engine.lastblock = block_ids[0]
+        expected_consensus_hash = all_consensus_hashes[ block_id ]
+        untrusted_db_path = os.path.join( snapshots_dir, "blockstore.db.%s" % block_id )
+        
+        print "\n\nverify %s - %s (%s), expect %s\n\n" % (block_ids[0], block_id+1, untrusted_db_path, expected_consensus_hash)
+
+        valid = blockstore_verify_database( expected_consensus_hash, block_id, untrusted_db_path )
+        if not valid:
+            print "Invalid at block %s" % block_id 
+            return False
+
+    return True
+
+
