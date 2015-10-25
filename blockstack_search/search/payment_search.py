7a1fc1b80b22a6b328c1985fa307b807922ff054
Muneeb Ali <muneeb@ali.vc>
2015-10-24 20:48:47 -0700

    starting verified payment index/search

---
+++ blockstack_search/search/payment_search.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Search
+    ~~~~~
+
+    copyright: (c) 2014 by Halfmoon Labs, Inc.
+    copyright: (c) 2015 by Blockstack.org
+
+This file is part of Search.
+
+    Search is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Search is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Search. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+import requests
+
+from .db import namespace
+
+
+def create_twitter_payment_index():
+
+    return
+
+if __name__ == "__main__":
+
+    create_twitter_payment_index()
