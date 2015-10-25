74dc50c791f0884514320ccc9993900915cfebcb
Jude Nelson <judecn@gmail.com>
2015-10-25 00:29:11 -0400

    Verify that we can reveal multiple namespaces concurrently

---
+++ blockstore/tests/scenarios/namespace_preorder_reveal_multi.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python 
+
+import testlib
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 ),
+    testlib.Wallet( "5Kg5kJbQHvk1B64rJniEmgbD83FpZpbw2RjdAZEzTefs9ihN3Bz", 100000000000 ),
+    testlib.Wallet( "5JuVsoS9NauksSkqEjbUZxWwgGDQbMwPsEfoRBSpLpgDX1RtLX7", 100000000000 ),
+    testlib.Wallet( "5KEpiSRr1BrT8vRD7LKGCEmudokTh1iMHbiThMQpLdwBwhDJB1T", 100000000000 ),
+    testlib.Wallet( "5K5hDuynZ6EQrZ4efrchCwy6DLhdsEzuJtTDAf3hqdsCKbxfoeD", 100000000000 ),
+    testlib.Wallet( "5J39aXEeHh9LwfQ4Gy5Vieo7sbqiUMBXkPH7SaMHixJhSSBpAqz", 100000000000 ),
+    testlib.Wallet( "5K9LmMQskQ9jP1p7dyieLDAeB6vsAj4GK8dmGNJAXS1qHDqnWhP", 100000000000 ),
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+
+def scenario( wallets, **kw ):
+
+    # order 3 namespaces
+    testlib.blockstore_namespace_preorder( "test1", wallets[1].addr, wallets[0].privkey )
+    testlib.blockstore_namespace_preorder( "test2", wallets[3].addr, wallets[2].privkey )
+    testlib.blockstore_namespace_preorder( "test3", wallets[5].addr, wallets[4].privkey )
+    testlib.next_block( **kw )
+
+    # reveal them all
+    testlib.blockstore_namespace_reveal( "test1", wallets[1].addr, 52595, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 11, wallets[0].privkey )
+    testlib.blockstore_namespace_reveal( "test2", wallets[3].addr, 52596, 251, 5, [7,6,5,4,3,2,1,1,1,1,1,1,1,1,1,1], 11, 12, wallets[2].privkey )
+    testlib.blockstore_namespace_reveal( "test3", wallets[5].addr, 52597, 252, 6, [8,7,6,5,4,3,2,2,2,2,2,2,2,2,2,2], 12, 13, wallets[4].privkey )
+    testlib.next_block( **kw )
+
+def check( state_engine ):
+
+    # the namespace has to have been revealed 
+    for i in xrange(1, 4):
+
+        ns = state_engine.get_namespace_reveal( "test%s" % i )
+        if ns is None:
+            return False 
+
+        if ns["namespace_id"] != ("test%s" % i):
+            return False 
+
+        if ns["lifetime"] != 52595 + (i - 1):
+            return False 
+
+        if ns["coeff"] != 250 + (i - 1):
+            return False 
+
+        if ns["base"] != 4 + (i - 1):
+            return False 
+
+        if ns["buckets"] != [x + i - 1 for x in [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0]]:
+            return False 
+
+        if ns["no_vowel_discount"] != 10 + (i):
+            return False
+
+        if ns["nonalpha_discount"] != 10 + (i - 1):
+            return False
+
+    return True
