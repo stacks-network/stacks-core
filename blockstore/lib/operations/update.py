12d7ee2379637540aed08a7f8f1322a04edb0d74
Jude Nelson <judecn@gmail.com>
2015-10-25 19:07:08 -0400

    Make update subsidizable; unify input-checking for parsing and building; document consensus fields

--- blockstore/lib/operations/update.py
+++ blockstore/lib/operations/update.py
@@ -23,7 +23,7 @@
 
 from pybitcoin import embed_data_in_blockchain, make_op_return_tx, BlockchainInfoClient, BitcoinPrivateKey, \
     BitcoinPublicKey, get_unspents, script_hex_to_address, hex_hash160, broadcast_transaction, serialize_transaction, \
-    make_op_return_outputs
+    make_op_return_outputs, make_op_return_script
 
 from utilitybelt import is_hex
 from binascii import hexlify, unhexlify
@@ -35,11 +35,34 @@ from ..hashing import hash256_trunc128
 
 from ..nameset import NAMEREC_FIELDS
 
-# consensus hash fields 
+import virtualchain
+log = virtualchain.session.log
+
+# consensus hash fields (ORDER MATTERS!) 
 FIELDS = NAMEREC_FIELDS + [
-    'name_hash',
-    'consensus_hash'
+    'name_hash',            # hash(name,consensus_hash)
+    'consensus_hash'        # consensus hash when this update was sent
 ]
+
+def update_sanity_test( name, consensus_hash, data_hash ):
+    """
+    Verify the validity of an update's data
+
+    Return True if valid
+    Raise exception if not
+    """
+    
+    if name is not None and (not is_b40( name ) or "+" in name or name.count(".") > 1):
+       raise Exception("Name '%s' has non-base-38 characters" % name)
+   
+    if data_hash is not None and not is_hex( data_hash ):
+       raise Exception("Invalid hex string '%s': not hex" % (data_hash))
+    
+    if len(data_hash) != 2 * LENGTHS['update_hash']:
+       raise Exception("Invalid hex string '%s': bad length" % (data_hash))
+
+    return True
+
 
 def build(name, consensus_hash, data_hash=None, testset=False):
     """
@@ -52,16 +75,11 @@ def build(name, consensus_hash, data_hash=None, testset=False):
     |-----|--|-----------------------------------|-----------------------|
     magic op  hash128(name.ns_id,consensus hash) hash160(data)
     """
+
+    rc = update_sanity_test( name, consensus_hash, data_hash )
+    if not rc:
+        raise Exception("Invalid update data")
     
-    if not is_b40( name ) or "+" in name or name.count(".") > 1:
-       raise Exception("Name '%s' has non-base-38 characters" % name)
-   
-    if not is_hex( data_hash ):
-       raise Exception("Invalid hex string '%s': not hex" % (data_hash))
-    
-    if len(data_hash) != 2 * LENGTHS['update_hash']:
-       raise Exception("Invalid hex string '%s': bad length" % (data_hash))
-       
     hex_name = hash256_trunc128( name + consensus_hash )
     
     readable_script = 'NAME_UPDATE 0x%s 0x%s' % (hex_name, data_hash)
@@ -71,14 +89,50 @@ def build(name, consensus_hash, data_hash=None, testset=False):
     return packaged_script
 
 
-def broadcast(name, data_hash, consensus_hash, private_key, blockchain_client, blockchain_broadcaster=None, pay_fee=True, tx_only=False, public_key=None, testset=False):
+def make_outputs( data, inputs, change_address, pay_fee=True ):
+    """
+    Make outputs for an update.
+    """
+
+    dust_fee = None
+    op_fee = None
+    dust_value = None 
+    
+    if pay_fee:
+        dust_fee = (len(inputs) + 1) * DEFAULT_DUST_FEE + DEFAULT_OP_RETURN_FEE
+        op_fee = DEFAULT_DUST_FEE
+        dust_value = DEFAULT_DUST_FEE
+    
+    else:
+        # will be subsidized
+        dust_fee = 0
+        op_fee = 0
+        dust_value = 0
+   
+    return [
+        # main output
+        {"script_hex": make_op_return_script(data, format='hex'),
+         "value": 0},
+        
+        # change output
+        {"script_hex": make_pay_to_address_script(change_address),
+         "value": calculate_change_amount(inputs, op_fee, dust_fee)}
+    ]
+
+
+def broadcast(name, data_hash, consensus_hash, private_key, blockchain_client, blockchain_broadcaster=None, tx_only=False, user_public_key=None, testset=False):
     """
     Write a name update into the blockchain.
     Returns a JSON object with 'data' set to the nulldata and 'transaction_hash' set to the transaction hash on success.
     """
     
     # sanity check 
-    if public_key is None and private_key is None:
+    pay_fee = True
+    if user_public_key is not None:
+        pay_fee = False
+        tx_only = True
+
+    if user_public_key is None and private_key is None:
         raise Exception("Missing both public and private key")
     
     if not tx_only and private_key is None:
@@ -91,24 +145,24 @@ def broadcast(name, data_hash, consensus_hash, private_key, blockchain_client, b
     inputs = None
     private_key_obj = None
     
-    if private_key is not None:
-        # ordering directly 
+    if user_public_key is not None:
+        # subsidizing 
+        pubk = BitcoinPublicKey( user_public_key )
+        from_address = pubk.address()
+        
+        # get inputs from utxo provider 
+        inputs = get_unspents( from_address, blockchain_client )
+
+    elif private_key is not None:
+        # ordering directly
         pubk = BitcoinPrivateKey( private_key ).public_key()
         public_key = pubk.to_hex()
         
         # get inputs and from address using private key
         private_key_obj, from_address, inputs = analyze_private_key(private_key, blockchain_client)
         
-    elif public_key is not None:
-        # subsidizing 
-        pubk = BitcoinPublicKey( public_key )
-        from_address = pubk.address()
-        
-        # get inputs from utxo provider 
-        inputs = get_unspents( from_address, blockchain_client )
-        
     nulldata = build(name, consensus_hash, data_hash=data_hash, testset=testset)
-    outputs = make_op_return_outputs( nulldata, inputs, from_address, fee=DEFAULT_OP_RETURN_FEE, format='hex' )
+    outputs = make_outputs( nulldata, inputs, from_address, pay_fee=pay_fee )
     
     if tx_only:
        
@@ -128,12 +182,25 @@ def parse(bin_payload):
     Parse a payload to get back the name and update hash.
     NOTE: bin_payload excludes the leading three bytes.
     """
+    
+    if len(bin_payload) != LENGTHS['name_hash'] + LENGTHS['data_hash']:
+        log.error("Invalid update length %s" % len(bin_payload))
+        return None 
+
     name_hash_bin = bin_payload[:LENGTHS['name_hash']]
     update_hash_bin = bin_payload[LENGTHS['name_hash']:]
     
     name_hash = hexlify( name_hash_bin )
     update_hash = hexlify( update_hash_bin )
-    
+  
+    try:
+        rc = update_sanity_test( None, name_hash, update_hash )
+        if not rc:
+            raise Exception("Invalid update data")
+    except Exception, e:
+        log.error("Invalid update data")
+        return None
+
     return {
         'opcode': 'NAME_UPDATE',
         'name_hash': name_hash,
