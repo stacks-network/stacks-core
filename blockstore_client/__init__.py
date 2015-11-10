45d1d0c6db528c05dea2ab05f55d327ca9ee81f0
Jude Nelson <judecn@gmail.com>
2015-11-10 16:36:42 -0500

    Add extra package methods

--- blockstore_client/__init__.py
+++ blockstore_client/__init__.py
@@ -30,8 +30,8 @@ import drivers
 
 from parsing import json_stable_serialize 
 
-from client import getinfo, lookup,get_name_record, ping, get_name_blockchain_record, get_namespace_blockchain_record
-from client import preorder, update, transfer, renew, revoke
+from client import getinfo, lookup, lookup_snv, get_name_record, ping, get_name_blockchain_record, get_namespace_blockchain_record
+from client import preorder, update, transfer, renew, revoke, get_nameops_at
 from client import namespace_preorder, namespace_reveal, namespace_ready
 from client import get_immutable, get_mutable
 from client import put_immutable, put_mutable, delete_immutable, delete_mutable
