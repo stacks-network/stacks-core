adb8ddde9eea846c81f529d5aa6b94b6b15d38a8
Jude Nelson <judecn@gmail.com>
2015-11-10 16:25:29 -0500

    Add regression tests for SNV lookups

---
+++ blockstore/tests/scenarios/name_preorder_register_expire_snv.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python 
+
+import testlib
+import pybitcoin
+import json
+import blockstore_client as snv_client
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 ),
+    testlib.Wallet( "5Kg5kJbQHvk1B64rJniEmgbD83FpZpbw2RjdAZEzTefs9ihN3Bz", 100000000000 ),
+    testlib.Wallet( "5JuVsoS9NauksSkqEjbUZxWwgGDQbMwPsEfoRBSpLpgDX1RtLX7", 100000000000 ),
+    testlib.Wallet( "5KEpiSRr1BrT8vRD7LKGCEmudokTh1iMHbiThMQpLdwBwhDJB1T", 100000000000 )
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+snv_block_id = None
+snv_consensus = None
+
+def scenario( wallets, **kw ):
+
+    testlib.blockstore_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    # NOTE: names expire in 13 blocks
+    testlib.blockstore_namespace_reveal( "test", wallets[1].addr, 13, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 10, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_namespace_ready( "test", wallets[1].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_name_preorder( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+
+    testlib.blockstore_name_register( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+    
+    global snv_block_id, snv_consensus
+    snv_block_id = testlib.get_current_block()
+    snv_consensus = testlib.get_consensus_at( snv_block_id )
+
+    for i in xrange(0, 15):
+        testlib.next_block( **kw )
+
+
+def check( state_engine ):
+
+    # not revealed, but ready 
+    ns = state_engine.get_namespace_reveal( "test" )
+    if ns is not None:
+        return False 
+
+    ns = state_engine.get_namespace( "test" )
+    if ns is None:
+        return False 
+
+    if ns['namespace_id'] != 'test':
+        return False 
+
+    # not preordered
+    preorder = state_engine.get_name_preorder( "foo.test", pybitcoin.make_pay_to_address_script(wallets[2].addr), wallets[3].addr )
+    if preorder is not None:
+        return False
+    
+    # not registered 
+    name_rec = state_engine.get_name( "foo.test" )
+    if name_rec is not None:
+        print "Not expired:"
+        print json.dumps( name_rec, indent=4 )
+        return False 
+
+    #  snv lookup works
+    test_proxy = testlib.TestAPIProxy()
+    snv_rec = snv_client.lookup_snv( "foo.test", snv_block_id, snv_consensus, proxy=test_proxy )
+    if 'error' in snv_rec:
+        print json.dumps(snv_rec, indent=4 )
+        return False
+
+    print snv_rec 
+    return True 
