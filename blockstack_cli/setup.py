f0c9d3da01320fc8a7b0f119bd1496a3e76b5666
Muneeb Ali <muneeb@ali.vc>
2015-11-12 16:35:04 -0500

    bumped version

--- blockstack_cli/setup.py
+++ blockstack_cli/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='blockstore-client',
-    version='0.0.5',
+    version='0.0.6',
     url='https://github.com/blockstack/blockstore-client',
     license='GPLv3',
     author='Blockstack.org',
