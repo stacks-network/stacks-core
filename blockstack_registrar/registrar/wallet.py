df50703922718e466b67cce7d03937425ab4a4d6
Muneeb Ali <muneeb@ali.vc>
2015-11-20 18:19:30 -0500

    added bitcoin keypair generated and secure storage

--- blockstack_registrar/registrar/wallet.py
+++ blockstack_registrar/registrar/wallet.py
@@ -23,13 +23,15 @@ This file is part of Registrar.
     along with Registrar. If not, see <http://www.gnu.org/licenses/>.
 """
 
+from pybitcoin import BitcoinPrivateKey
+
 from tools.crypto_tools import aes_encrypt, aes_decrypt
 from tools.crypto_tools import get_address_from_privkey
 
 from .utils import pretty_print as pprint
 from .utils import config_log
 
-from .db import registrar_users
+from .db import registrar_users, registrar_addresses
 
 from .config import SECRET_KEY
 
@@ -91,7 +93,70 @@ def test_registrar_users():
         else:
             log.debug("ERROR: wrong pvtkey: %s")
 
+
+def get_privkey(address):
+    """ given an address, get decrypted hex private key from DB
+    """
+
+    entry = registrar_addresses.find_one({"address": address})
+
+    if entry is None:
+        log.debug("Address not found in DB, can't fetch privkey")
+        return None
+
+    encrypted_privkey = entry['encrypted_privkey']
+    hex_privkey = aes_decrypt(encrypted_privkey, SECRET_KEY)
+
+    return hex_privkey
+
+
+def get_all_addresses():
+    """ return all bitcoin addresses registrar is using
+    """
+
+    addresses = []
+
+    for entry in registrar_addresses.find():
+
+        addresses.append(entry['address'])
+
+    return addresses
+
+
+def generate_bitcoin_keypairs(number_of_addresses=50):
+    """ This function:
+        1) generates new bitcoin keypairs
+        2) saves encrypted private keys
+        private keys are encrypted with SECRET_KEY
+    """
+
+    if registrar_addresses.find().count() >= number_of_addresses:
+        log.debug("Already have enough addresses")
+        return
+
+    no_of_new_addresses = number_of_addresses - registrar_addresses.find().count()
+
+    for count in range(1, no_of_new_addresses + 1):
+
+        privkey = BitcoinPrivateKey()
+        hex_privkey = privkey.to_hex()
+        encrypted_privkey = aes_encrypt(hex_privkey, SECRET_KEY)
+
+        address = get_address_from_privkey(hex_privkey)
+        log.debug("Creating new address (count, address): (%s, %s):" % (count, address))
+
+        new_entry = {}
+        new_entry['encrypted_privkey'] = encrypted_privkey
+        new_entry['address'] = address
+
+        registrar_addresses.save(new_entry)
+
+    log.debug("")
+
 if __name__ == '__main__':
 
-    get_registrar_users()
+    #get_registrar_users()
     #test_registrar_users()
+    #generate_bitcoin_keypairs()
+    print get_all_addresses()
+    log.debug("see options")
