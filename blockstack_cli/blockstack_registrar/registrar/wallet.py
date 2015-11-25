cf4392be01a7654e6c6a26ea909661b7a8f05a6f
Muneeb Ali <muneeb@ali.vc>
2015-11-25 18:20:22 -0500

    added functionality to make chained payments to wallet addresses, refilling wallet etc

--- blockstack_cli/blockstack_registrar/registrar/wallet.py
+++ blockstack_cli/blockstack_registrar/registrar/wallet.py
@@ -4,8 +4,8 @@
     Registrar
     ~~~~~
 
-    copyright: (c) 2014 by Halfmoon Labs, Inc.
-    copyright: (c) 2015 by Blockstack.org
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
 
 This file is part of Registrar.
 
@@ -23,22 +23,30 @@ This file is part of Registrar.
     along with Registrar. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from pybitcoin import BitcoinPrivateKey
+from pybitcoin import BitcoinPrivateKey, make_send_to_address_tx
+from pybitcoin import BlockcypherClient
 
 from tools.crypto_tools import aes_encrypt, aes_decrypt
 from tools.crypto_tools import get_address_from_privkey
 
 from .utils import pretty_print as pprint
 from .utils import config_log
+from .utils import btc_to_satoshis
 
 from .db import registrar_users, registrar_addresses
 
 from .config import SECRET_KEY, RATE_LIMIT
+from .config import BLOCKCYPHER_TOKEN
+from .config import MINIMUM_BALANCE_PER_ADDRESS, TX_FEE
 
 from .network import bs_client as c
-from .blockchain import get_balance
+from .blockchain import get_balance, dontuseAddress
+
+#from blockcypher import simple_spend_tx
+from blockcypher import pushtx
 
 log = config_log(__name__)
+blockcypher_client = BlockcypherClient(api_key=BLOCKCYPHER_TOKEN)
 
 
 def get_registrar_users():
@@ -117,19 +125,48 @@ def get_addresses(count=50, offset=0):
 
     addresses = []
 
+    ignore_counter = 0
     counter = 0
 
     for entry in registrar_addresses.find():
 
-        if counter < offset:
-            counter += 1
+        if ignore_counter < offset:
+            ignore_counter += 1
             continue
 
         addresses.append(entry['address'])
 
         counter += 1
 
-        if counter >= count:
+        if counter == count:
+            break
+
+    return addresses
+
+
+def get_underfunded_addresses(count=50):
+
+    addresses = []
+
+    counter = 0
+
+    for entry in registrar_addresses.find():
+
+        address = entry['address']
+        balance = get_balance(address)
+
+        if balance <= float(MINIMUM_BALANCE_PER_ADDRESS):
+            log.debug("address %s needs refill: %s"
+                      % (address, balance))
+
+            if dontuseAddress(address):
+                log.debug("address %s has pending tx" % address)
+            else:
+                addresses.append(entry['address'])
+
+        counter += 1
+
+        if counter == count:
             break
 
     return addresses
@@ -163,7 +200,88 @@ def generate_bitcoin_keypairs(number_of_addresses=50):
 
         registrar_addresses.save(new_entry)
 
-    log.debug("")
+
+def send_payment(hex_privkey, to_address, btc_amount):
+
+    to_satoshis = btc_to_satoshis(btc_amount)
+    fee_satoshis = btc_to_satoshis(TX_FEE)
+
+    signed_tx = make_send_to_address_tx(to_address, to_satoshis, hex_privkey,
+                                        blockchain_client=blockcypher_client,
+                                        fee=fee_satoshis)
+
+    resp = pushtx(tx_hex=signed_tx)
+
+    if 'tx' in resp:
+        return resp['tx']['hash']
+    else:
+        log.debug("ERROR: broadcasting tx")
+        return resp
+
+
+def send_chained_payment(btc_per_address, list_of_addresses, list_of_privkeys, 
+                         live=False):
+    """ Sends BTC from head of chain to rest of address list
+
+    @btc_per_address: btc to send per address in list
+    @list_of_addresses: list of addresses in the chain
+                        first address is used to start chained payment
+    @list_of_privkeys: the corresponding priv keys
+
+    Return True/False.
+    """
+
+    no_of_addresses = len(list_of_addresses) - 1
+    index = 0
+
+    starting_amount = no_of_addresses * btc_per_address
+
+    btc_amount = starting_amount
+
+    while(index < no_of_addresses):
+
+        from_address = list_of_addresses[index]
+        hex_privkey = list_of_privkeys[index]
+        to_address = list_of_addresses[index + 1]
+
+        log.debug("sending %s BTC from %s to %s: "
+                  % (btc_amount, from_address, to_address))
+
+        if live:
+            send_payment(hex_privkey, to_address, btc_amount)
+
+        btc_amount -= btc_per_address
+        index += 1
+
+
+def construct_payment_chain(source_address, no_of_addresses, offset=None):
+    """ Makes properly formatted lists for making chained payments
+
+        @source_address: goes at the head of chain
+                         used to fund the entire chained payment
+        @no_of_addresses: no of additional addresses to add to chained payment
+        @offset: the offset for which addresses to pick from DB
+
+        Returns:
+        @list_of_addresses: the chained payment addresses as a list
+        @list_of_privkeys: associated private keys
+    """
+
+    list_of_addresses = []
+    list_of_privkeys = []
+
+    list_of_addresses.append(source_address)
+
+    #send_to_addresses = get_addresses(no_of_addresses, offset)
+    send_to_addresses = get_underfunded_addresses(no_of_addresses)
+
+    for address in send_to_addresses:
+        list_of_addresses.append(str(address))
+
+    for address in list_of_addresses:
+        list_of_privkeys.append(get_privkey(address))
+
+    return list_of_addresses, list_of_privkeys
 
 
 def display_wallet_info(number_of_addresses=RATE_LIMIT):
@@ -171,13 +289,27 @@ def display_wallet_info(number_of_addresses=RATE_LIMIT):
     addresses = get_addresses(count=number_of_addresses)
 
     for address in addresses:
-        log.debug("(%s, %s)" % (address, get_balance(address)))
+        has_pending_tx = dontuseAddress(address)
+        log.debug("(%s, balance %s,\t pending %s)" % (address,
+                                                      get_balance(address),
+                                                      has_pending_tx))
 
     log.debug("Total addresses: %s" % len(addresses))
 
+
+def refill_wallet(source_address, live=False):
+
+    list_of_addresses, list_of_privkeys = construct_payment_chain(source_address, 10)
+    send_chained_payment(0.01, list_of_addresses, list_of_privkeys, live=live)
+
 if __name__ == '__main__':
 
+    # example usage
+
+    #refill_wallet('your_address', live=True)
     #get_registrar_users()
     #test_registrar_users()
     #generate_bitcoin_keypairs()
-    display_wallet_info()
+
+    display_wallet_info(20)
+
