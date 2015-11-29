2f5911dcbfcb4574309e3c35afe4731c26924bd4
Muneeb Ali <muneeb@ali.vc>
2015-11-28 19:48:05 -0500

    separated app-specific drivers

---
+++ blockstack_cli_0.14.1/blockstack_registrar/registrar/drivers/__init__.py
@@ -0,0 +1,25 @@
+"""
+    Registrar
+    ~~~~~
+
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
+
+This file is part of Registrar.
+
+    Registrar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Registrar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Registrar. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from webapp_driver import WebappDriver
+from api_driver import APIDriver
