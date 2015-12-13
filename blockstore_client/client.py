f4971430b385931a075c0aa5e2edd23949eb2fc1
Muneeb Ali <muneeb@ali.vc>
2015-12-12 21:24:50 -0500

    Changed sanity checks before update nameop
    Previous version: checked if value_hash is in blockchain *and* if data is present
    in storage corresponding to value_hash
    
    We don't really need to check storage, because if your previous data is not in storage
    for some reason you should still be able to update your blockchain record and put in
    a new value_hash
    
    This changes the sanity checks to check if record exits on blockchain before sending
    update nameop

--- blockstore_client/client.py
+++ blockstore_client/client.py
@@ -459,7 +459,7 @@ def lookup( name, proxy=None ):
     return result
 
 
-def get_name_record(name, create_if_absent=False):
+def get_name_record(name, create_if_absent=False, check_only_blockchain=False):
     """
     Given the name of the user, look up the user's record hash,
     and then get the record itself from storage.
@@ -490,6 +490,9 @@ def get_name_record(name, create_if_absent=False):
     if 'value_hash' not in name_record:
 
         return {"error": "Name has no user record hash defined"}
+
+    if check_only_blockchain:
+        return name_record
 
     # is there a user record loaded?
     if name_record['value_hash'] in [None, "null", ""]:
@@ -1064,8 +1067,8 @@ def update(name, user_json_or_hash, privatekey, txid=None, proxy=None, tx_only=F
 
         user_record_hash = pybitcoin.hash.hex_hash160(user_db.serialize_user(user_data))
 
-    # go get the current user record
-    current_user_record = get_name_record( name, create_if_absent=True )
+    # go get the current user record (blockchain only)
+    current_user_record = get_name_record(name, check_only_blockchain=True)
     if current_user_record is None:
         return {'error': 'No such user'}
 
