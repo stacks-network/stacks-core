24aefcf720dbbfbe233e99b8a8f2ebc26148f430
Muneeb Ali <muneeb@ali.vc>
2016-01-13 15:59:23 -0500

    implemented and tested send_multi_payment (funding multiple child addresses in a single tx from HD master)

--- blockstack_cli/blockstack_registrar/registrar/wallet.py
+++ blockstack_cli/blockstack_registrar/registrar/wallet.py
@@ -30,7 +30,7 @@ from keychain import PrivateKeychain
 from pybitcoin import make_send_to_address_tx
 from pybitcoin import BlockcypherClient
 
-from crypto.utils import get_address_from_privkey
+from crypto.utils import get_address_from_privkey, get_pubkey_from_privkey
 
 from .utils import pretty_print as pprint
 from .utils import config_log
@@ -44,8 +44,9 @@ from .config import DEFAULT_CHILD_ADDRESSES
 
 from .blockchain import get_balance, dontuseAddress, underfundedAddress
 
-#from blockcypher import simple_spend_tx
 from blockcypher import pushtx
+from blockcypher import create_unsigned_tx, make_tx_signatures
+from blockcypher import broadcast_signed_transaction
 
 log = config_log(__name__)
 blockcypher_client = BlockcypherClient(api_key=BLOCKCYPHER_TOKEN)
@@ -207,21 +208,66 @@ def send_payment(hex_privkey, to_address, btc_amount):
 
 def display_wallet_info(list_of_addresses):
 
+    total_balance = 0
+
     for address in list_of_addresses:
         has_pending_tx = dontuseAddress(address)
+        balance_on_address = get_balance(address)
         log.debug("(%s, balance %s,\t pending %s)" % (address,
-                                                      get_balance(address),
+                                                      balance_on_address,
                                                       has_pending_tx))
+        total_balance += balance_on_address
 
     log.debug("Total addresses: %s" % len(list_of_addresses))
+    log.debug("Total balance: %s" % total_balance)
 
+
+def send_multi_payment(payment_privkey, list_of_addresses, payment_per_address):
+
+    payment_address = get_address_from_privkey(payment_privkey)
+    inputs = [{'address': payment_address}]
+    payment_in_satoshis = btc_to_satoshis(float(payment_per_address))
+    outputs = []
+
+    for address in list_of_addresses:
+        outputs.append({'address': address, 'value': int(payment_in_satoshis)})
+
+    unsigned_tx = create_unsigned_tx(inputs=inputs, outputs=outputs)
+
+    pprint(unsigned_tx)
+
+    # iterate through unsigned_tx['tx']['inputs'] to find each address in order
+    # need to include duplicates as many times as they may appear
+    privkey_list = []
+    pubkey_list = []
+
+    for input in unsigned_tx['tx']['inputs']:
+        privkey_list.append(payment_privkey)
+        pubkey_list.append(get_pubkey_from_privkey(payment_privkey))
+
+    tx_signatures = make_tx_signatures(txs_to_sign=unsigned_tx['tosign'],
+                                       privkey_list=privkey_list,
+                                       pubkey_list=pubkey_list)
+
+    resp = broadcast_signed_transaction(unsigned_tx=unsigned_tx,
+                                        signatures=tx_signatures,
+                                        pubkeys=pubkey_list)
+
+    if 'hash' in resp:
+        return resp['hash']
+    else:
+        return None
 
 if __name__ == '__main__':
 
     HEX_PRIV_KEY = os.environ['HEX_PRIV_KEY']
     wallet = HDWallet(HEX_PRIV_KEY)
 
-    addresses = wallet.get_keypairs(5, include_privkey=False)
-    print get_underfunded_addresses(addresses)
-    print display_wallet_info(addresses)
-    print wallet.get_next_keypair()
+    list_of_addresses = wallet.get_keypairs(10, include_privkey=False)
+    #send_multi_payment(HEX_PRIV_KEY, list_of_addresses, '0.01')
+    addresses = wallet.get_keypairs()
+
+    addresses += list_of_addresses
+    #print get_underfunded_addresses(addresses)
+    display_wallet_info(addresses)
+    #print wallet.get_next_keypair()
