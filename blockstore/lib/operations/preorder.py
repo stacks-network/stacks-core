4235f9a5a7c3d463342785ceea477508e972ebf1
Jude Nelson <judecn@gmail.com>
2016-01-15 18:03:51 -0500

    Add implicit quantity field to preorder (= 1 for single preorder)

--- blockstore/lib/operations/preorder.py
+++ blockstore/lib/operations/preorder.py
@@ -185,7 +185,8 @@ def parse(bin_payload):
     return {
         'opcode': 'NAME_PREORDER',
         'preorder_name_hash': name_hash,
-        'consensus_hash': consensus_hash
+        'consensus_hash': consensus_hash,
+        'quantity': 1
     }
 
 
