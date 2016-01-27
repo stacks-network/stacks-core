749a0be4e972fe4bc9c6d6fda5efbc38c4bb8654
Jude Nelson <judecn@gmail.com>
2016-01-27 12:26:38 -0500

    Add non-automateable test to verify that virtualchain aborts blockstore if it cannot write database state

---
+++ blockstore/tests/scenarios/virtualchain_abort.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstore
+    ~~~~~
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
+
+    This file is part of Blockstore
+
+    Blockstore is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Blockstore is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Blockstore. If not, see <http://www.gnu.org/licenses/>.
+""" 
+
+import testlib 
+import json
+import os
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 )
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+
+def scenario( wallets, **kw ): 
+    resp = testlib.blockstore_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    if 'error' in resp:
+        print json.dumps(resp, indent=4)
+
+    # make the test abort at the next block 
+    wd = testlib.get_working_dir( **kw )
+    os.chmod( wd, 0555 )
+
+    resp = testlib.blockstore_namespace_preorder( "test2", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    if 'error' in resp:
+        print json.dumps(resp, indent=4)
+
+
+
+def check( state_engine ):
+    
+    # should never get here
+    return False
+
