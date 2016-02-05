43e9ec01260f73ee01a386cc47e74b937fa1b1f7
Jude Nelson <judecn@gmail.com>
2016-02-05 15:53:24 -0500

    Test transfer/update/transfer/update loops extensively

--- blockstore/tests/scenarios/testlib.py
+++ blockstore/tests/scenarios/testlib.py
@@ -30,6 +30,8 @@ import errno
 import shutil
 import bitcoin
 import sys
+import copy
+import json
 
 # hack around absolute paths
 current_dir =  os.path.abspath(os.path.dirname(__file__) + "/../../..")
@@ -41,6 +43,11 @@ import blockstore_client
 import blockstore
 import pybitcoin
 
+import virtualchain
+
+if not globals().has_key('log'):
+    log = virtualchain.session.log
+
 class Wallet(object):
     def __init__(self, pk_wif, value_str ):
         pk = pybitcoin.BitcoinPrivateKey( pk_wif )
@@ -50,6 +57,14 @@ class Wallet(object):
         self.pubkey_hex = pk.public_key().to_hex()
         self.addr = pk.public_key().address()
         self.value = int(value_str)
+
+
+class APICallRecord(object):
+    def __init__(self, method, name, result ):
+        self.block_id = max(all_consensus_hashes.keys()) + 1
+        self.name = name
+        self.method = method
+        self.result = result
 
 
 class TestAPIProxy(object):
@@ -63,8 +78,8 @@ class TestAPIProxy(object):
         self.spv_headers_path = utxo_opts['spv_headers_path']
 
     def __getattr__(self, name):
+        
         if hasattr( self.api, "jsonrpc_" + name):
-
             def inner(*args, **kw):
                 c = getattr( self.api, "jsonrpc_" + name)
                 r = c(*args, **kw)
@@ -93,6 +108,10 @@ state_engine = None
 # consensus hash at each block
 all_consensus_hashes = {}
 
+# API call history 
+api_call_history = []
+
+
 def log_consensus( **kw ):
     """
     Log the consensus hash at the current block.
@@ -117,10 +136,11 @@ def make_proxy():
 
 def blockstore_name_preorder( name, privatekey, register_addr, tx_only=False, subsidy_key=None, testset=False, consensus_hash=None ):
 
+    global api_call_history 
+
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_preorder( name, privatekey, register_addr, tx_only=tx_only, subsidy_key=subsidy_key, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.preorder_tx( name, privatekey, register_addr )
     elif subsidy_key is not None:
@@ -128,6 +148,7 @@ def blockstore_name_preorder( name, privatekey, register_addr, tx_only=False, su
     else:
         resp = test_proxy.preorder( name, privatekey, register_addr )
 
+    api_call_history.append( APICallRecord( "preorder", name, resp ) )
     return resp
 
 
@@ -136,7 +157,6 @@ def blockstore_name_preorder_multi( names, privatekey, register_addrs, tx_only=F
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
     
-    # resp = blockstored.blockstore_name_preorder_multi( names, privatekey, register_addrs, tx_only=tx_only, subsidy_key=subsidy_key, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.preorder_multi_tx( names, privatekey, register_addrs )
     elif subsidy_key is not None:
@@ -144,6 +164,7 @@ def blockstore_name_preorder_multi( names, privatekey, register_addrs, tx_only=F
     else:
         resp = test_proxy.preorder_multi( names, privatekey, register_addrs )
 
+    api_call_history.append( APICallRecord( "preorder_multi", name, resp ) )
     return resp
 
 
@@ -152,7 +173,6 @@ def blockstore_name_register( name, privatekey, register_addr, renewal_fee=None,
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_register( name, privatekey, register_addr, renewal_fee=renewal_fee, tx_only=tx_only, subsidy_key=subsidy_key, user_public_key=user_public_key, testset=testset, consensus_hash=consensus_hash)
     if tx_only:
         resp = test_proxy.register_tx( name, privatekey, register_addr )
     elif subsidy_key is not None:
@@ -160,6 +180,7 @@ def blockstore_name_register( name, privatekey, register_addr, renewal_fee=None,
     else:
         resp = test_proxy.register( name, privatekey, register_addr )
 
+    api_call_history.append( APICallRecord( "register", name, resp ) )
     return resp
 
 
@@ -179,6 +200,7 @@ def blockstore_name_update( name, data_hash, privatekey, user_public_key=None, t
         else:
             resp = test_proxy.update( name, data_hash, privatekey )
 
+    api_call_history.append( APICallRecord( "update", name, resp ) )
     return resp
 
 
@@ -187,7 +209,6 @@ def blockstore_name_transfer( name, address, keepdata, privatekey, tx_only=False
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_transfer( name, address, keepdata, privatekey, tx_only=tx_only, subsidy_key=subsidy_key, user_public_key=user_public_key, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.transfer_tx( name, address, keepdata, privatekey )
     elif subsidy_key is not None:
@@ -195,6 +216,7 @@ def blockstore_name_transfer( name, address, keepdata, privatekey, tx_only=False
     else:
         resp = test_proxy.transfer( name, address, keepdata, privatekey )
 
+    api_call_history.append( APICallRecord( "transfer", name, resp ) )
     return resp
 
 
@@ -203,7 +225,6 @@ def blockstore_name_renew( name, privatekey, register_addr=None, tx_only=False, 
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_renew( name, privatekey, register_addr=register_addr, tx_only=tx_only, subsidy_key=subsidy_key, user_public_key=user_public_key, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.renew_tx( name, privatekey )
     elif subsidy_key is not None:
@@ -211,6 +232,7 @@ def blockstore_name_renew( name, privatekey, register_addr=None, tx_only=False, 
     else:
         resp = test_proxy.renew( name, privatekey )
 
+    api_call_history.append( APICallRecord( "renew", name, resp ) )
     return resp
 
 
@@ -219,7 +241,6 @@ def blockstore_name_revoke( name, privatekey, tx_only=False, subsidy_key=None, u
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_revoke( name, privatekey, tx_only=tx_only, subsidy_key=subsidy_key, user_public_key=user_public_key, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.revoke_tx( name, privatekey )
     elif subsidy_key is not None:
@@ -227,6 +248,7 @@ def blockstore_name_revoke( name, privatekey, tx_only=False, subsidy_key=None, u
     else:
         resp = test_proxy.revoke( name, privatekey )
 
+    api_call_history.append( APICallRecord( "revoke", name, resp ) )
     return resp
 
 
@@ -235,12 +257,12 @@ def blockstore_name_import( name, recipient_address, update_hash, privatekey, tx
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_name_import( name, recipient_address, update_hash, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.name_import_tx( name, recipient_address, update_hash, privatekey )
     else:
         resp = test_proxy.name_import( name, recipient_address, update_hash, privatekey )
 
+    api_call_history.append( APICallRecord( "name_import", name, resp ) )
     return resp
 
 
@@ -249,12 +271,12 @@ def blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_o
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_namespace_preorder( namespace_id, register_addr, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.namespace_preorder_tx( namespace_id, register_addr, privatekey )
     else:
         resp = test_proxy.namespace_preorder( namespace_id, register_addr, privatekey )
 
+    api_call_history.append( APICallRecord( "namespace_preorder", namespace_id, resp ) )
     return resp
 
 
@@ -263,12 +285,12 @@ def blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, b
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.namespace_reveal_tx( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey )
     else:
         resp = test_proxy.namespace_reveal( namespace_id, register_addr, lifetime, coeff, base, bucket_exponents, nonalpha_discount, no_vowel_discount, privatekey )
 
+    api_call_history.append( APICallRecord( "namespace_reveal", namespace_id, resp ) )
     return resp
 
 
@@ -277,12 +299,12 @@ def blockstore_namespace_ready( namespace_id, privatekey, tx_only=False, testset
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_namespace_ready( namespace_id, privatekey, tx_only=tx_only, testset=testset, consensus_hash=consensus_hash )
     if tx_only:
         resp = test_proxy.namespace_ready_tx( namespace_id, privatekey )
     else:
         resp = test_proxy.namespace_ready( namespace_id, privatekey )
 
+    api_call_history.append( APICallRecord( "namespace_ready", namespace_id, resp ) )
     return resp
 
 
@@ -291,7 +313,6 @@ def blockstore_announce( message, privatekey, tx_only=False, user_public_key=Non
     test_proxy = make_proxy()
     blockstore_client.client.set_default_proxy( test_proxy )
 
-    # resp = blockstored.blockstore_announce( message, privatekey, testset=testset )
     if tx_only:
         resp = test_proxy.announce_tx( message, privatekey )
     elif subsidy_key is not None:
@@ -299,7 +320,9 @@ def blockstore_announce( message, privatekey, tx_only=False, user_public_key=Non
     else:
         resp = test_proxy.announce( message, privatekey )
 
+    api_call_history.append( APICallRecord( "announce", message, resp ) )
     return resp
+
 
 def blockstore_verify_database( consensus_hash, consensus_block_id, db_path, working_db_path=None, start_block=None, testset=False ):
     return blockstored.verify_database( consensus_hash, consensus_block_id, db_path, working_db_path=working_db_path, start_block=start_block, testset=testset )
@@ -449,6 +472,84 @@ def check_history( state_engine ):
         if not valid:
             print "Invalid at block %s" % block_id 
             return False
+
+    return True
+
+
+def snv_all_names( state_engine ):
+    """
+    Verify that we can use the consensus hash from each consensus-bearing operation
+    to verify all prior name operations.
+    """
+    global all_consensus_hashes 
+    global api_call_history
+
+    test_proxy = TestAPIProxy()
+    blockstore_client.client.set_default_proxy( test_proxy )
+
+    all_names = {}  # map name to {"block_id":..., "consensus_hash":...}
+
+    for api_call in api_call_history:
+
+        log.debug("API call: %s %s" % (api_call.method, api_call.name))
+
+        name = None
+        opcode = None
+
+        if api_call.method.startswith("register") and not api_call.method.startswith("register_multi"):
+            name = api_call.name
+            opcode = "NAME_REGISTRATION"
+
+        elif api_call.method.startswith("name_import"):
+            name = api_call.name
+            opcode = "NAME_IMPORT"
+            
+        if name is not None:
+            block_id = api_call.block_id 
+            consensus_hash = all_consensus_hashes[ block_id ]
+
+            all_names[ name ] = {
+                "block_id": block_id,
+                "consensus_hash": consensus_hash,
+                "opcode": opcode
+            }
+
+            if api_call.result.has_key('transaction_hash'):
+                all_names[name]['txid'] = api_call.result['transaction_hash']
+
+
+    log.debug("SNV verify %s names" % len(all_names.keys()))
+
+    for name in all_names.keys():
+
+        log.debug("SNV verify %s" % name)
+
+        block_id = all_names[name]['block_id']
+        consensus_hash = all_names[name]['consensus_hash']
+        txid = all_names[name].get('txid', None)
+        opcode = all_names[name].get('opcode', None)
+
+        for i in xrange( block_id + 1, max(all_consensus_hashes.keys()) + 1 ):
+
+            trusted_block_id = i
+            trusted_consensus_hash = all_consensus_hashes[i]
+
+            snv_rec = blockstore_client.client.snv_lookup( name, block_id, trusted_consensus_hash, proxy=test_proxy )
+            if 'error' in snv_rec:
+                print json.dumps(snv_rec, indent=4 )
+                return False 
+
+            if snv_rec['name'] != name:
+                print "mismatch name"
+                print json.dumps(snv_rec, indent=4 )
+                return False 
+
+            if opcode is not None and snv_rec['opcode'] != opcode:
+                print "mismatch opcode"
+                print json.dumps(snv_rec, indent=4 )
+                return False 
+
+            log.debug("SNV verified %s with (%s,%s) back to (%s,%s)" % (name, trusted_block_id, trusted_consensus_hash, block_id, consensus_hash ))
 
     return True
 
