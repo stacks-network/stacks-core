0f41a6bd861794a85514ded95ad47a8b5fcffbe7
Jude Nelson <judecn@gmail.com>
2016-03-04 14:55:32 -0500

    virtualchain version bump

--- blockstack_cli_0.14.1/setup.py
+++ blockstack_cli_0.14.1/setup.py
@@ -22,7 +22,7 @@ setup(
     install_requires=[
         'boto==2.38.0',
         'basicrpc==0.0.2',
-        'virtualchain==0.0.6',
+        'virtualchain==0.0.9',
         'protocoin==0.1'
     ],
     classifiers=[
