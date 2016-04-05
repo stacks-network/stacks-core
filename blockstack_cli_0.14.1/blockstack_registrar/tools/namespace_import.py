0c67608be99961d5a122de0010310ab17bd4a4a3
Muneeb Ali <muneeb@ali.vc>
2016-04-05 19:14:07 -0400

    updated license

--- blockstack_cli_0.14.1/blockstack_registrar/tools/namespace_import.py
+++ blockstack_cli_0.14.1/blockstack_registrar/tools/namespace_import.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Registrar
+    ~~~~~
+    :copyright: (c) 2014-2016 by Halfmoon Labs, Inc.
+    :copyright: (c) 2016 blockstack.org
+    :license: MIT, see LICENSE for more details.
+"""
 
 import random
 import json
