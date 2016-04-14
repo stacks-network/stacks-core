7cfd918d4303d0f8fa3a5272b9c2dd0f4a4bdd5e
Ryan Shea <ryan@shea.io>
2016-04-14 15:33:21 -0400

    define the genesis block merkle as a variable

--- blockstack_cli/blockstack_client/spv.py
+++ blockstack_cli/blockstack_client/spv.py
@@ -46,6 +46,7 @@ BLOCK_HEADER_SIZE = 81
 GENESIS_BLOCK_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
 BLOCK_DIFFICULTY_CHUNK_SIZE = 2016
 BLOCK_DIFFICULTY_INTERVAL = 14*24*60*60  # two weeks, in seconds
+GENESIS_BLOCK_MERKLE_ROOT = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
 
 
 class BlockHash(object):
@@ -249,7 +250,7 @@ class SPVClient(object):
             genesis_block_header = BlockHeader()
             genesis_block_header.version = 1
             genesis_block_header.prev_block = 0
-            genesis_block_header.merkle_root = int( "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", 16 )
+            genesis_block_header.merkle_root = int(GENESIS_BLOCK_MERKLE_ROOT, 16 )
             genesis_block_header.timestamp = 1231006505
             genesis_block_header.bits = int( "1d00ffff", 16 )
             genesis_block_header.nonce = 2083236893
