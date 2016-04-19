f70623c9e4b145c02ff4cb47b5580b0b74a6bb51
Jude Nelson <judecn@gmail.com>
2016-04-19 15:32:13 -0400

    module-level methods

---
+++ blockstack_client/backend/utxo/blockstack_utxo/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstack-utxo
+    ~~~~~
+    :copyright: (c) 2014-2016 by Halfmoon Labs, Inc.
+    :copyright: (c) 2016 blockstack.org
+    :license: MIT, see LICENSE for more details.
+"""
+
+from utxo import *
+from version import __version__
