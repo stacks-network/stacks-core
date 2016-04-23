1d817959759ff5cd7cacf4ffa9163dd50bfdc7ec
Jude Nelson <judecn@gmail.com>
2016-04-23 02:19:48 -0400

    test multiple sequential registers

---
+++ integration_tests/blockstack_integration_tests/scenarios/rpc_register_multi_sequential.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstack
+    ~~~~~
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
+
+    This file is part of Blockstack
+
+    Blockstack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Blockstack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
+""" 
+
+import testlib
+import pybitcoin
+import time
+import json
+import sys
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 ),
+    testlib.Wallet( "5Kg5kJbQHvk1B64rJniEmgbD83FpZpbw2RjdAZEzTefs9ihN3Bz", 100000000000 ),
+    testlib.Wallet( "5JuVsoS9NauksSkqEjbUZxWwgGDQbMwPsEfoRBSpLpgDX1RtLX7", 100000000000 ),
+    testlib.Wallet( "5KEpiSRr1BrT8vRD7LKGCEmudokTh1iMHbiThMQpLdwBwhDJB1T", 100000000000 )
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+
+def scenario( wallets, **kw ):
+
+    testlib.blockstack_client_initialize_wallet( wallets[2].privkey, "0123456789abcdef", 50000000000 )
+
+    testlib.blockstack_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstack_namespace_reveal( "test", wallets[1].addr, 52595, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 10, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstack_namespace_ready( "test", wallets[1].privkey )
+    testlib.next_block( **kw )
+
+    for i in xrange(0, 3):
+        resp = testlib.blockstack_rpc_register( "foo_%s.test" % i, "0123456789abcdef" )
+        if 'error' in resp:
+            print >> sys.stderr, json.dumps(resp, indent=4, sort_keys=True)
+            return False
+   
+        # wait for the preorder to get confirmed
+        for i in xrange(0, 12):
+            testlib.next_block( **kw )
+
+        # wait for the poller to pick them up
+        print >> sys.stderr, "Waiting 10 seconds for the backend to submit the registers"
+        time.sleep(10)
+
+
+        # wait for the registers to get confirmed 
+        for i in xrange(0, 12):
+            testlib.next_block( **kw )
+
+        print >> sys.stderr, "Waiting 10 seconds for the backend to acknowledge registrations"
+        time.sleep(10)
+
+
+
+def check( state_engine ):
+
+    # not revealed, but ready 
+    ns = state_engine.get_namespace_reveal( "test" )
+    if ns is not None:
+        print "namespace reveal exists"
+        return False 
+
+    ns = state_engine.get_namespace( "test" )
+    if ns is None:
+        print "no namespace"
+        return False 
+
+    if ns['namespace_id'] != 'test':
+        print "wrong namespace"
+        return False 
+    
+    # owned by the right address 
+    wallet = testlib.blockstack_client_get_wallet()
+    if wallet is None:
+        print "failed to get wallet"
+        return False 
+
+    if 'error' in wallet:
+        print "failed to get wallet: %s" % wallet['error']
+        return False
+
+    for i in xrange(0, 3):
+        # registered 
+        name_rec = state_engine.get_name( "foo_%s.test" % i )
+        if name_rec is None:
+            print "name does not exist"
+            return False 
+
+        # owned by
+        owner_address = str(wallet['owner_address'])
+        if name_rec['address'] != owner_address or name_rec['sender'] != pybitcoin.make_pay_to_address_script(owner_address):
+            print "sender is wrong"
+            return False 
+
+    # all queues are drained 
+    queue_info = testlib.blockstack_client_queue_state()
+    if len(queue_info) > 0:
+        print "Still in queue:\n%s" % json.dumps(queue_info, indent=4, sort_keys=True)
+        return False
+
+    return True
