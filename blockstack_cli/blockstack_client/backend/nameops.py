9996212e31176874de07101d75b3d8dd8b47466b
Jude Nelson <judecn@gmail.com>
2016-04-23 02:21:16 -0400

    return structured data on async nameop; rename backend_{nameop} to async_{nameop} to avoid confusion with the backend_{} namespace in the RPC daemon

--- blockstack_cli/blockstack_client/backend/nameops.py
+++ blockstack_cli/blockstack_client/backend/nameops.py
@@ -22,7 +22,7 @@ from .blockchain import get_tx_confirmations, get_utxos, get_utxo_client
 from .blockchain import dontuseAddress, underfundedAddress
 from .blockchain import recipientNotReady
 
-from crypto.utils import get_address_from_privkey
+from crypto.utils import get_address_from_privkey, get_pubkey_from_privkey
 
 from ..utils import pretty_print as pprint
 from ..utils import pretty_dump
@@ -49,18 +49,19 @@ def send_subsidized(hex_privkey, unsigned_tx_hex):
     reply = {}
 
     # sign all unsigned inputs
-    signed_tx = sign_all_unsigned_inputs(hex_privkey, unsigned_tx_hex)
+    signed_tx = pybitcoin.sign_all_unsigned_inputs(hex_privkey, unsigned_tx_hex)
     utxo_client = get_utxo_client()
     resp = pybitcoin.broadcast_transaction( signed_tx, utxo_client )
 
     if 'transaction_hash' not in resp:
         reply['error'] = "ERROR: broadcasting tx"
         log.debug(pprint(resp))
+        return reply
 
-    return reply
+    return resp
 
 
-def backend_preorder(fqu, paymetn_address, owner_address, payment_privkey=None, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
+def async_preorder(fqu, paymetn_address, owner_address, payment_privkey=None, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
     """
         Preorder a fqu (step #1)
 
@@ -77,33 +78,34 @@ def backend_preorder(fqu, paymetn_address, owner_address, payment_privkey=None, 
     # stale preorder will get removed from preorder_queue
     if in_queue("register", fqu):
         log.debug("Already in register queue: %s" % fqu)
-        return False
+        return {'error': 'Already in register queue'}
 
     if in_queue("preorder", fqu):
         log.debug("Already in preorder queue: %s" % fqu)
-        return False
+        return {'error': 'Already in preorder queue'}
 
     if recipientNotReady(owner_address, proxy=proxy):
         log.debug("Address %s owns too many names already." % owner_address)
-        return False
+        return {'error': 'Address owns too many names'}
 
     payment_address = get_address_from_privkey(payment_privkey)
     if dontuseAddress(payment_address):
         log.debug("Payment address not ready: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is not ready'}
 
     elif underfundedAddress(payment_address):
         log.debug("Payment address under funded: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is underfunded'}
 
     log.debug("Preordering (%s, %s, %s)" % (fqu, payment_address, owner_address))
 
     resp = {}
 
     try:
-        resp = blockstack_preorder(fqu, payment_privkey, owner_address, proxy=proxy,  )
+        resp = blockstack_preorder(fqu, payment_privkey, owner_address, proxy=proxy )
     except Exception as e:
-        log.debug(e)
+        log.exception(e)
+        return {'error': 'Failed to issue preorder'}
 
     if 'transaction_hash' in resp:
         # watch this preorder, and register it when it gets queued
@@ -114,12 +116,12 @@ def backend_preorder(fqu, paymetn_address, owner_address, payment_privkey=None, 
     else:
         log.debug("Error preordering: %s with %s for %s" % (fqu, payment_address, owner_address))
         log.debug("Error below\n%s" % json.dumps(resp, indent=4, sort_keys=True))
-        raise ValueError('Error preordering')
+        return {'error': 'Failed to preorder'}
 
-    return True
+    return resp
 
 
-def backend_register(fqu, payment_address=None, owner_address=None, payment_privkey=None, auto_preorder=True, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
+def async_register(fqu, payment_address=None, owner_address=None, payment_privkey=None, auto_preorder=True, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
     """
         Register a previously preordered fqu (step #2)
 
@@ -130,7 +132,8 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
         @payment_address: used for making the payment
         @owner_address: will own the fqu (must be same as preorder owner_address)
 
-        Returns True/False and stores tx_hash in queue
+        Return {'status': True, ...} on success
+        Return {'error': ...} on error
     """
 
     if proxy is None:
@@ -140,23 +143,23 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
     # stale preorder will get removed from preorder_queue
     if in_queue("register", fqu):
         log.debug("Already in register queue: %s" % fqu)
-        return False
+        return {'error': 'Already in register queue'}
 
     if not in_queue("preorder", fqu):
         if auto_preorder:
-            return preorder(fqu, payment_address, owner_address)
+            return blockstack_preorder(fqu, payment_address, owner_address, proxy=proxy)
         else:
             log.debug("No preorder sent yet: %s" % fqu)
-            return False
+            return {'error': 'No preorder sent yet'}
 
     if is_name_registered(fqu, proxy=proxy):
         log.debug("Already registered %s" % fqu)
-        return False
+        return {'error': 'Already registered'}
 
     preorder_entry = queue_findone( "preorder", fqu, path=queue_path )
     if len(preorder_entry) == 0:
         log.error("No preorder for '%s'" % fqu)
-        return False 
+        return {'error': 'No preorder found'}
 
     preorder_tx = preorder_entry[0]['tx_hash']
     tx_confirmations = get_tx_confirmations(preorder_tx)
@@ -165,7 +168,7 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
         log.debug("Waiting on preorder confirmations: (%s, %s)"
                   % (preorder_tx, tx_confirmations))
 
-        return False
+        return {'error': 'Waiting on preorder confirmations'}
 
     if payment_privkey is None:
         # use the correct owner_address from preorder operation
@@ -175,7 +178,7 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
 
         except:
             log.debug("Error getting preorder addresses")
-            return False
+            return {'error': 'Could not get preorder addresses'}
 
         payment_privkey = get_privkey_from_address(payment_address)
     else:
@@ -183,11 +186,11 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
 
     if dontuseAddress(payment_address):
         log.debug("Payment address not ready: %s" % payment_address)
-        return False
+        return {'error': 'Payment address not ready'}
 
     elif underfundedAddress(payment_address):
         log.debug("Payment address under funded: %s" % payment_address)
-        return False
+        return {'error': 'Payment address underfunded'}
 
     log.debug("Registering (%s, %s, %s)" % (fqu, payment_address, owner_address))
     resp = {}
@@ -195,22 +198,24 @@ def backend_register(fqu, payment_address=None, owner_address=None, payment_priv
     try:
         resp = blockstack_register(fqu, payment_privkey, owner_address, proxy=proxy)
     except Exception as e:
-        log.debug(e)
+        log.exception(e)
+        return {'error': 'Failed to register name'}
 
     if 'transaction_hash' in resp:
         queue_append("register", fqu, resp['transaction_hash'],
                      payment_address=payment_address,
                      owner_address=owner_address,
                      path=queue_path)
+
+        return resp
+
     else:
         log.debug("Error registering: %s" % fqu)
         log.debug(pprint(resp))
-        return False
-
-    return True
+        return {'error': 'Failed to send registration'}
 
 
-def backend_update(fqu, zonefile, owner_privkey, payment_address,
+def async_update(fqu, zonefile, owner_privkey, payment_address,
                       payment_privkey=None, proxy=None, wallet_keys=None, queue_path=DEFAULT_QUEUE_PATH):
     """
         Update a previously registered fqu, using a different payment address
@@ -228,18 +233,18 @@ def backend_update(fqu, zonefile, owner_privkey, payment_address,
 
     if in_queue("update", fqu):
         log.debug("Already in update queue: %s" % fqu)
-        return False
+        return {'error': 'Already in update queue'}
 
     if not is_name_registered(fqu, proxy=proxy):
         log.debug("Not yet registered %s" % fqu)
-        return False
+        return {'error': 'Not yet registered'}
 
     zonefile_hash = hash_zonefile( zonefile )
 
     blockchain_record = blockstack_get_name_blockchain_record( fqu, proxy=proxy )
     if 'error' in blockchain_record:
         log.debug("Failed to read blockchain record for %s" % fqu)
-        return False
+        return {'error': 'Failed to look up name'}
 
     owner_address = blockchain_record['address']
 
@@ -247,15 +252,15 @@ def backend_update(fqu, zonefile, owner_privkey, payment_address,
 
     if check_address != owner_address:
         log.debug("Given privkey/address doesn't own this name.")
-        return False
+        return {'error': 'Name is not owned'}
 
     if dontuseAddress(payment_address):
         log.debug("Payment address not ready: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is not ready'}
 
     elif underfundedAddress(payment_address):
         log.debug("Payment address under funded: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is underfunded'}
 
     owner_public_key = get_pubkey_from_privkey(owner_privkey)
 
@@ -270,18 +275,17 @@ def backend_update(fqu, zonefile, owner_privkey, payment_address,
     try:
         resp = blockstack_update_subsidized(fqu, zonefile_hash,
                                             public_key=owner_public_key,
-                                            subsidy_key=payment_privkey,
-                                            proxy=proxy,
-                                            wallet_keys=wallet_keys)
+                                            subsidy_key=payment_privkey)
     except Exception as e:
-        log.debug(e)
+        log.exception(e)
+        return {'error': 'Failed to make update'}
 
     if 'subsidized_tx' in resp:
         unsigned_tx = resp['subsidized_tx']
     else:
         log.debug("Error updating: %s" % fqu)
         log.debug(resp)
-        return False
+        return {'error': 'Failed to make update'}
 
     broadcast_resp = send_subsidized(owner_privkey, unsigned_tx)
 
@@ -290,15 +294,17 @@ def backend_update(fqu, zonefile, owner_privkey, payment_address,
                      zonefile=zonefile,
                      owner_address=owner_address,
                      path=queue_path)
+
+        broadcast_resp['zonefile_hash'] = zonefile_hash
+        return broadcast_resp
+
     else:
         log.debug("Error updating: %s" % fqu)
         log.debug(broadcast_resp)
-        return False
-
-    return True
+        return {'error': 'Failed to broadcast update'}
 
 
-def backend_transfer(fqu, transfer_address, owner_privkey, payment_address, payment_privkey=None, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
+def async_transfer(fqu, transfer_address, owner_privkey, payment_address, payment_privkey=None, proxy=None, queue_path=DEFAULT_QUEUE_PATH):
     """
         Transfer a previously registered fqu, using a different payment address
 
@@ -307,7 +313,8 @@ def backend_transfer(fqu, transfer_address, owner_privkey, payment_address, paym
         @owner_privkey: privkey of current owner address, to sign tx
         @payment_address: the address which is paying for the cost
 
-        Returns True/False and stores tx_hash in queue
+        Return {'status': True, 'transaction_hash': ...} on success
+        Return {'error': ...} on failure
     """
 
     if proxy is None:
@@ -315,39 +322,39 @@ def backend_transfer(fqu, transfer_address, owner_privkey, payment_address, paym
 
     if in_queue("transfer", fqu):
         log.debug("Already in transfer queue: %s" % fqu)
-        return False
+        return {'error': 'Already in transfer queue'}
 
     if not is_name_registered(fqu, proxy=proxy):
         log.debug("Not yet registered %s" % fqu)
-        return False
+        return {'error': 'Not yet registered'}
 
     if is_name_owner(fqu, transfer_address, proxy=proxy):
         log.debug("Already transferred %s" % fqu)
-        return True
+        return {'error': 'Already transfered'}
 
     if recipientNotReady(transfer_address, proxy=proxy):
         log.debug("Address %s owns too many names already." % transfer_address)
-        return False
+        return {'error': 'Recipient owns too many names'}
 
     blockchain_record = blockstack_get_name_blockchain_record( fqu, proxy=proxy )
     if 'error' in blockchain_record:
         log.debug("Failed to read blockchain record for %s" % fqu)
-        return False
+        return {'error': 'Failed to read blockchain record for name'}
 
     owner_address = blockchain_record['address']
     check_address = get_address_from_privkey(owner_privkey)
 
     if check_address != owner_address:
         log.debug("Given privkey/address doesn't own this name.")
-        return False
+        return {'error': 'Given keypair does not own this name'}
 
     if dontuseAddress(payment_address):
         log.debug("Payment address not ready: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is not ready'}
 
     elif underfundedAddress(payment_address):
         log.debug("Payment address under funded: %s" % payment_address)
-        return False
+        return {'error': 'Payment address is underfunded'}
 
     owner_public_key = get_pubkey_from_privkey(owner_privkey)
     if payment_privkey is None:
@@ -366,14 +373,14 @@ def backend_transfer(fqu, transfer_address, owner_privkey, payment_address, paym
                                               subsidy_key=payment_privkey,
                                               proxy=proxy )
     except Exception as e:
-        log.debug(e)
+        log.exception(e)
 
     if 'subsidized_tx' in resp:
         unsigned_tx = resp['subsidized_tx']
     else:
         log.debug("Error transferring: %s" % fqu)
         log.debug(pprint(resp))
-        return False
+        return resp
 
     broadcast_resp = send_subsidized(owner_privkey, unsigned_tx)
 
@@ -385,6 +392,6 @@ def backend_transfer(fqu, transfer_address, owner_privkey, payment_address, paym
     else:
         log.debug("Error transferring: %s" % fqu)
         log.debug(broadcast_resp)
-        return False
+        return {'error': 'Failed to send transfer'}
 
-    return True
+    return broadcast_resp
