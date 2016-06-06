967400817672b20c99cf3f0168bd41af35577fd6
Ubuntu <blockstack-test@blockstack-test.0dvfhkevvf3e1l0pghozdb4tmd.bx.internal.cloudapp.net>
2016-06-06 21:01:50 +0000

    add dependency_links

--- blockstack_cli/setup.py
+++ blockstack_cli/setup.py
@@ -30,6 +30,12 @@ setup(
         'defusedxml>=0.4.1',
         'keylib>=0.0.2'
     ],
+    dependency_links=[
+        'git://github.com/blockstack/blockstack-virtualchain.git@release-candidate#egg=virtualchain-0.0.9.0',
+        'git://github.com/blockstack/blockstack-utxo.git@release-candidate#egg=blockstack-utxo-0.0.1.0',
+        'git://github.com/blockstack/blockstack-storage-drivers.git@release-candidate#egg=blockstack-storage-drivers-0.0.1.0',
+        'git://github.com/blockstack/dns-zone-file-py@release-candidate#egg=zone_file-0.1.6'
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
