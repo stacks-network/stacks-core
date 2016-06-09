7962704d48ff576f31ed319cbd2d2b2147729f4d
Jude Nelson <judecn@gmail.com>
2016-06-09 03:15:21 -0400

    update package symbols

--- blockstack_cli_0.14.1/blockstack_client/__init__.py
+++ blockstack_cli_0.14.1/blockstack_client/__init__.py
@@ -38,9 +38,8 @@ from proxy import BlockstackRPCClient, get_default_proxy, set_default_proxy, jso
 from proxy import getinfo, ping, get_name_cost, get_namespace_cost, get_all_names, get_names_in_namespace, \
         get_names_owned_by_address, get_consensus_at, get_consensus_range, get_nameops_at, \
         get_nameops_hash_at, get_name_blockchain_record, get_namespace_blockchain_record, \
-        get_namespace_reveal_blockchain_record, preorder, register, update, transfer, revoke, renew, \
         preorder_subsidized, register_subsidized, update_subsidized, transfer_subsidized, revoke_subsidized, renew_subsidized, \
-        send_subsidized, name_import, namespace_preorder, namespace_reveal, namespace_ready 
+        send_subsidized 
         
 from keys import make_wallet_keys, get_owner_keypair, get_data_keypair
 
@@ -50,7 +49,7 @@ from data import get_immutable, get_immutable_by_name, get_mutable, put_immutabl
         delete_mutable, list_mutable_data, list_immutable_data, list_immutable_data_history, list_update_history, list_zonefile_history
 from data import blockstack_url_fetch as fetch_data
 from data import data_get, data_put, data_delete, data_list
-from profile import migrate_profile, get_name_profile, get_name_zonefile, zonefile_publish, hash_zonefile
+from profile import get_name_profile, get_name_zonefile, zonefile_publish, hash_zonefile
 from accounts import list_accounts, get_account, put_account, delete_account
 
 from config import get_logger, get_config, CONFIG_PATH, CONFIG_FILENAME
@@ -62,11 +61,16 @@ from storage import blockstack_immutable_data_url_parse as parse_immutable_data_
 
 from storage import blockstack_data_url_parse as parse_data_url
 from storage import blockstack_data_url as make_data_url
+from storage import get_storage_handlers
 
-from storage import BlockstackURLHandle, BlockstackHandler, get_data_hash, get_blockchain_compat_hash, get_name_zonefile_hash
+from storage import BlockstackURLHandle, BlockstackHandler, get_data_hash, get_blockchain_compat_hash, get_zonefile_data_hash
+from storage import parse_mutable_data as parse_signed_data
 
 from config import USER_ZONEFILE_TTL
 
 from wallet import get_payment_addresses, get_owner_addresses, dump_wallet, load_wallet, get_wallet
 
-from user import is_user_zonefile
+from user import is_user_zonefile, user_zonefile_data_pubkey
+
+from operations import *
+from backend.nameops import *
