144d1dbe1d15e689084ebe4dd2f05b33718bed41
Jude Nelson <judecn@gmail.com>
2016-06-14 11:31:04 -0400

    remove dead code

--- blockstack/lib/operations/nameimport.py
+++ blockstack/lib/operations/nameimport.py
@@ -21,18 +21,11 @@
     along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from pybitcoin import embed_data_in_blockchain, \
-    analyze_private_key, serialize_sign_and_broadcast, make_op_return_script, \
-    make_pay_to_address_script, b58check_encode, b58check_decode, serialize_transaction
- 
-from pybitcoin.transactions.outputs import calculate_change_amount
-from utilitybelt import is_hex
+from pybitcoin import b58check_decode
 from binascii import hexlify, unhexlify
 
-from ..b40 import b40_to_hex, bin_to_b40, is_b40
 from ..config import *
 from ..scripts import *
-from ..hashing import hash256_trunc128
 
 from ..nameset import NAMEREC_FIELDS
 
@@ -75,120 +68,3 @@ def get_import_update_hash_from_outputs( outputs, recipient ):
     
     return ret 
 
-'''
-def build(name, testset=False):
-    """
-    Takes in a name to import.  Name must include the namespace ID.
-    
-    Record format:
-    
-    0    2  3                             39
-    |----|--|-----------------------------|
-    magic op   name.ns_id (37 bytes)
-    
-    The transaction itself will have two outputs:
-    * the recipient
-    * the hash of the name's associated data
-    """
-    
-    if not is_name_valid( name ):
-        raise Exception("Invalid name '%s'" % name)
-
-    readable_script = "NAME_IMPORT 0x%s" % (hexlify(name))
-    hex_script = blockstack_script_to_hex(readable_script)
-    packaged_script = add_magic_bytes(hex_script, testset=testset)
-    
-    return packaged_script
-
-
-def make_outputs( data, inputs, recipient_address, sender_address, update_hash_b58, format='bin'):
-    """
-    Builds the outputs for a name import:
-    * [0] is the OP_RETURN 
-    * [1] is the new owner (recipient)
-    * [2] is the update hash
-    * [3] is the change sent to the original owner
-    """
-    
-    dust_fee = DEFAULT_OP_RETURN_FEE + (len(inputs) + 3) * DEFAULT_DUST_FEE
-    op_fee = 2 * DEFAULT_DUST_FEE
-    dust_value = DEFAULT_DUST_FEE
-    
-    return [
-        # main output
-        {"script_hex": make_op_return_script(data, format=format),
-         "value": 0},
-    
-        # recipient output
-        {"script_hex": make_pay_to_address_script(recipient_address),
-         "value": dust_value},
-        
-        # update hash output
-        {"script_hex": make_pay_to_address_script(update_hash_b58),
-         "value": dust_value},
-        
-        # change output
-        {"script_hex": make_pay_to_address_script(sender_address),
-         "value": calculate_change_amount(inputs, op_fee, dust_fee)}
-    ]
-
-
-def broadcast(name, recipient_address, update_hash, private_key, blockchain_client, blockchain_broadcaster=None, user_public_key=None, testset=False):
-   
-    tx_only = False
-    # TODO: support tx_only
-
-    if blockchain_broadcaster is None:
-        blockchain_broadcaster = blockchain_client 
-    
-    nulldata = build(name, testset=testset)
-    
-    # convert update_hash from a hex string so it looks like an address
-    update_hash_b58 = b58check_encode( unhexlify(update_hash) )
-    
-    # get inputs and from address
-    private_key_obj, from_address, inputs = analyze_private_key(private_key, blockchain_client)
-    
-    # NAME_IMPORT outputs
-    outputs = make_outputs(nulldata, inputs, recipient_address, from_address, update_hash_b58, format='hex')
-    
-    if tx_only:
-        
-        unsigned_tx = serialize_transaction( inputs, outputs )
-        return {"unsigned_tx": unsigned_tx}
-    
-    else:
-        # serialize, sign, and broadcast the tx
-        response = serialize_sign_and_broadcast(inputs, outputs, private_key_obj, blockchain_broadcaster)
-        
-        # response = {'success': True }
-        response.update({'data': nulldata})
-        
-        # return the response
-        return response
-
-
-def parse(bin_payload, recipient, update_hash ):
-    """
-    # NOTE: first three bytes were stripped
-    """
-    
-    fqn = bin_payload
-    if not is_name_valid( fqn ):
-        return None 
-
-    return {
-        'opcode': 'NAME_IMPORT',
-        'name': fqn,
-        'recipient': recipient,
-        'update_hash': update_hash
-    }
-
-
-def get_fees( inputs, outputs ):
-    """
-    Blockstack currently does not allow 
-    the subsidization of namespaces.
-    """
-    return (None, None)
-'''
--- blockstack/lib/operations/namespaceready.py
+++ blockstack/lib/operations/namespaceready.py
@@ -21,14 +21,6 @@
     along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from pybitcoin import embed_data_in_blockchain, BlockchainInfoClient, hex_hash160, make_op_return_tx, serialize_transaction, broadcast_transaction, make_op_return_outputs
-from utilitybelt import is_hex
-from binascii import hexlify, unhexlify
-
-from ..b40 import b40_to_hex, bin_to_b40, is_b40
-from ..config import *
-from ..scripts import *
-
 import virtualchain
 log = virtualchain.get_logger("blockstack-server")
 
@@ -39,94 +31,3 @@ FIELDS = NAMESPACE_REVEAL_FIELDS + [
     'ready_block',      # block number at which the namespace was readied
 ]
 
-'''
-def build( namespace_id, testset=False ):
-   """
-   Record to mark the end of a namespace import in the blockchain.
-   
-   Takes an base40-encoded namespace ID to mark the end.
-   
-   Format:
-   
-   0     2  3  4           23
-   |-----|--|--|------------|
-   magic op  .  ns_id
-   """
-   
-   # sanity check 
-   if not is_b40( namespace_id ) or "+" in namespace_id or namespace_id.count(".") > 0:
-      raise Exception("Namespace ID '%s' has non-base-38 characters" % namespace_id)
-   
-   if len(namespace_id) == 0 or len(namespace_id) > LENGTHS['blockchain_id_namespace_id']:
-      raise Exception("Invalid namespace ID '%s (expected length between 1 and %s)" % (namespace_id, LENGTHS['blockchain_id_namespace_id']))
-   
-   readable_script = "NAMESPACE_READY 0x%s" % (hexlify("." + namespace_id))
-   hex_script = blockstack_script_to_hex(readable_script)
-   packaged_script = add_magic_bytes(hex_script, testset=testset)
-   
-   return packaged_script
-
-
-def broadcast( namespace_id, private_key, blockchain_client, user_public_key=None, testset=False, blockchain_broadcaster=None ):
-   
-   tx_only = False
-   # TODO: support tx_only 
-
-   if blockchain_broadcaster is None:
-       blockchain_broadcaster = blockchain_client 
-    
-   nulldata = build( namespace_id, testset=testset )
-   
-   # get inputs and from address
-   private_key_obj, from_address, inputs = analyze_private_key(private_key, blockchain_client)
-   
-   # OP_RETURN outputs 
-   outputs = make_op_return_outputs( nulldata, inputs, from_address, fee=DEFAULT_OP_RETURN_FEE, format='hex' )
-   
-   if tx_only:
-       
-       unsigned_tx = serialize_transaction( inputs, outputs )
-       return {'unsigned_tx': signed_tx}
-
-   else:
-       
-       signed_tx = tx_serialize_and_sign( inputs, outputs, private_key_obj )
-       response = broadcast_transaction( signed_tx, blockchain_broadcaster )
-       response.update({'data': nulldata})
-       return response
-
-
-def parse( bin_payload ):
-   """
-   NOTE: the first three bytes will be missing
-   NOTE: the first byte in bin_payload is a '.'
-   """
-   
-   if bin_payload[0] != '.':
-       log.error("Missing namespace delimiter .")
-       return None 
-
-   namespace_id = bin_payload[ 1: ]
-   
-   # sanity check
-   if not is_b40( namespace_id ) or "+" in namespace_id or namespace_id.count(".") > 0:
-       log.error("Invalid namespace ID '%s'" % namespace_id)
-       return None
-
-   if len(namespace_id) <= 0 or len(namespace_id) > LENGTHS['blockchain_id_namespace_id']:
-       log.error("Invalid namespace of length %s" % len(namespace_id))
-       return None 
-
-   return {
-      'opcode': 'NAMESPACE_READY',
-      'namespace_id': namespace_id
-   }
-
-
-def get_fees( inputs, outputs ):
-    """
-    Blockstack currently does not allow 
-    the subsidization of namespaces.
-    """
-    return (None, None)
-'''
