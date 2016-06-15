ce7f6c7cf94e6dfad6ed06f5d142f24a91298414
Jude Nelson <judecn@gmail.com>
2016-06-15 11:23:26 -0400

    check for utxo provider by looking for `utxo_provider =` in all ini sections.

--- blockstack_cli/blockstack_client/backend/utxo/blockstack_utxo/utxo.py
+++ blockstack_cli/blockstack_client/backend/utxo/blockstack_utxo/utxo.py
@@ -13,6 +13,7 @@ import sys
 from ConfigParser import SafeConfigParser
 import pybitcoin
 import logging
+import json
 from .blockstack_utxo import BlockstackUTXOClient
 
 DEBUG = True
@@ -52,9 +53,8 @@ def get_logger(name=None):
 log = get_logger("blockstack-utxo")
 
 
-SUPPORTED_UTXO_PROVIDERS = [ "chain_com", "blockcypher", "blockchain_info", "bitcoind_utxo", "blockstack_utxo", "mock_utxo" ]
+SUPPORTED_UTXO_PROVIDERS = [ "blockcypher", "blockchain_info", "bitcoind_utxo", "blockstack_utxo", "mock_utxo" ]
 SUPPORTED_UTXO_PARAMS = {
-    "chain_com": ["api_key_id", "api_key_secret"],
     "blockcypher": ["api_token"],
     "blockchain_info": ["api_token"],
     "bitcoind_utxo": ["rpc_username", "rpc_password", "server", "port", "use_https", "version_byte"],
@@ -63,57 +63,12 @@ SUPPORTED_UTXO_PARAMS = {
 }
 
 
-def default_utxo_provider( config_file=None ):
-   """
-   Get defualt UTXO provider options from a config file.
-   """
-
-   global SUPPORTED_UTXO_PROVIDERS
-
-   if config_file is None:
-       raise Exception("No config file given")
-
-   parser = SafeConfigParser()
-   parser.read( config_file )
-
-   for provider_name in SUPPORTED_UTXO_PROVIDERS:
-       if parser.has_section( provider_name ):
-           return provider_name
-
-   return None
-
-
-def all_utxo_providers( config_file=None ):
-   """
-   Get our defualt UTXO provider options from a config file.
-   """
-
-   global SUPPORTED_UTXO_PROVIDERS
-
-   if config_file is None:
-       raise Exception("No config file given")
-
-   parser = SafeConfigParser()
-   parser.read( config_file )
-
-   provider_names = []
-
-   for provider_name in SUPPORTED_UTXO_PROVIDERS:
-       if parser.has_section( provider_name ):
-           provider_names.append( provider_name )
-
-   return provider_names
-
-
 def default_utxo_provider_opts( utxo_provider, config_file=None ):
    """
    Get the default options for a utxo provider.
    """
 
-   if utxo_provider == "chain_com":
-       return default_chaincom_opts( config_file=config_file )
-
-   elif utxo_provider == "blockcypher":
+   if utxo_provider == "blockcypher":
        return default_blockcypher_opts( config_file=config_file )
 
    elif utxo_provider == "blockchain_info":
@@ -132,48 +87,25 @@ def default_utxo_provider_opts( utxo_provider, config_file=None ):
        raise Exception("Unsupported UTXO provider '%s'" % utxo_provider)
 
 
-def default_chaincom_opts( config_file=None ):
-   """
-   Get our default chain.com options from a config file.
-   """
-   
-   if config_file is None:
-       raise Exception("No config file given")
+def find_service_provider_sections( config_file, utxo_provider_name ):
+    """
+    Find the section of the config file with 'utxo_provider = ' set
+    """
+    parser = SafeConfigParser()
+    parser.read( config_file )
+    secs = []
 
-   parser = SafeConfigParser()
-   parser.read( config_file )
+    for sec in parser.sections():
+        if parser.has_option(sec, 'utxo_provider') and parser.get(sec, 'utxo_provider') == utxo_provider_name:
+            secs.append( sec )
 
-   chaincom_opts = {}
-
-   api_key_id = None
-   api_key_secret = None
-
-   if parser.has_section('chain_com'):
-
-      if parser.has_option('chain_com', 'api_key_id'):
-         api_key_id = parser.get('chain_com', 'api_key_id')
-
-      if parser.has_option('chain_com', 'api_key_secret'):
-         api_key_secret = parser.get('chain_com', 'api_key_secret')
-
-   chaincom_opts = {
-       'utxo_provider': "chain_com",
-       'api_key_id': api_key_id,
-       'api_key_secret': api_key_secret
-   }
-
-
-   # strip Nones
-   for (k, v) in chaincom_opts.items():
-      if v is None:
-         del chaincom_opts[k]
-
-   return chaincom_opts
+    return secs
 
 
 def default_blockcypher_opts( config_file=None ):
    """
    Get our default blockcypher.com options from a config file.
+   Selects options from the first such section.
    """
 
    if config_file is None:
@@ -185,23 +117,27 @@ def default_blockcypher_opts( config_file=None ):
    blockcypher_opts = {}
 
    api_token = None
+   provider_secs = find_service_provider_sections(config_file, 'blockcypher')
+   if len(provider_secs) > 0:
+       provider_sec = provider_secs[0]
 
-   if parser.has_section('blockcypher'):
-
-      if parser.has_option('blockcypher', 'api_token'):
-         api_token = parser.get('blockcypher', 'api_token')
+       if parser.has_option(provider_sec, 'api_token'):
+          api_token = parser.get(provider_sec, 'api_token')
 
    blockcypher_opts = {
-       'utxo_provider': "blockcypher",
        'api_token': api_token
    }
-
 
    # strip Nones
    for (k, v) in blockcypher_opts.items():
       if v is None:
-         del blockcypher_opts[k]
+         # token is optional 
+         if k == 'api_token':
+             blockcypher_opts[k] = ''
+         else:
+             del blockcypher_opts[k]
 
+   blockcypher_opts['utxo_provider'] = 'blockcypher'
    return blockcypher_opts
 
 
@@ -219,14 +155,14 @@ def default_blockchain_info_opts( config_file=None ):
    blockchain_info_opts = {}
 
    api_token = None
+   provider_secs = find_service_provider_sections(config_file, 'blockchain_info')
+   if len(provider_secs) > 0:
+       provider_sec = provider_secs[0]
 
-   if parser.has_section("blockchain_info"):
-
-       if parser.has_option("blockchain_info", "api_token"):
-           api_token = parser.get("blockchain_info", "api_token")
+       if parser.has_option(provider_sec, "api_token"):
+           api_token = parser.get(provider_sec, "api_token")
 
    blockchain_info_opts = {
-       "utxo_provider": "blockchain_info",
        "api_token": api_token
    }
 
@@ -235,6 +171,7 @@ def default_blockchain_info_opts( config_file=None ):
       if v is None:
          del blockchain_info_opts[k]
 
+   blockchain_info_opts['utxo_provider'] = 'blockchain_info'
    return blockchain_info_opts
 
 
@@ -257,30 +194,32 @@ def default_bitcoind_utxo_opts( config_file=None ):
    rpc_password = None
    use_https = None
    version_byte = None
+   
+   provider_secs = find_service_provider_sections(config_file, 'bitcoind_utxo')
+   if len(provider_secs) > 0:
+       provider_sec = provider_secs[0]
 
-   if parser.has_section("bitcoind_utxo"):
+       if parser.has_option(provider_sec, "server"):
+           server = parser.get(provider_sec, "server")
 
-       if parser.has_option("bitcoind_utxo", "server"):
-           server = parser.get("bitcoind_utxo", "server")
+       if parser.has_option(provider_sec, "port"):
+           port = int( parser.get(provider_sec, "port") )
 
-       if parser.has_option("bitcoind_utxo", "port"):
-           port = int( parser.get("bitcoind_utxo", "port") )
+       if parser.has_option(provider_sec, "rpc_username"):
+           rpc_username = parser.get(provider_sec, "rpc_username")
 
-       if parser.has_option("bitcoind_utxo", "rpc_username"):
-           rpc_username = parser.get("bitcoind_utxo", "rpc_username")
+       if parser.has_option(provider_sec, "rpc_password"):
+           rpc_password = parser.get(provider_sec, "rpc_password")
 
-       if parser.has_option("bitcoind_utxo", "rpc_password"):
-           rpc_password = parser.get("bitcoind_utxo", "rpc_password")
+       if parser.has_option(provider_sec, "use_https"):
 
-       if parser.has_option("bitcoind_utxo", "use_https"):
-
-            if parser.get("bitcoind_utxo", "use_https").lower() in ["y", "yes", "true"]:
+            if parser.get(provider_sec, "use_https").lower() in ["y", "yes", "true"]:
                 use_https = True
             else:
                 use_https = False
 
-       if parser.has_option("bitcoind_utxo", "version_byte"):
-           version_byte = int(parser.get("bitcoind_utxo", "version_byte"))
+       if parser.has_option(provider_sec, "version_byte"):
+           version_byte = int(parser.get(provider_sec, "version_byte"))
 
 
    if use_https is None:
@@ -296,7 +235,6 @@ def default_bitcoind_utxo_opts( config_file=None ):
        port = 8332
 
    bitcoind_utxo_opts = {
-       "utxo_provider": "bitcoind_utxo",
        "rpc_username": rpc_username,
        "rpc_password": rpc_password,
        "server": server,
@@ -310,6 +248,7 @@ def default_bitcoind_utxo_opts( config_file=None ):
       if v is None:
          del bitcoind_utxo_opts[k]
 
+   bitcoind_utxo_opts['utxo_provider'] = 'bitcoind_utxo'
    return bitcoind_utxo_opts
 
 
@@ -329,16 +268,17 @@ def default_blockstack_utxo_opts( config_file=None ):
    server = None
    port = None
 
-   if parser.has_section("blockstack_utxo"):
+   provider_secs = find_service_provider_sections(config_file, 'blockstack_utxo')
+   if len(provider_secs) > 0:
+       provider_sec = provider_secs[0]
 
-       if parser.has_option("blockstack_utxo", "server"):
+       if parser.has_option(provider_sec, "server"):
            server = parser.get('blockstack_utxo', 'server')
 
-       if parser.has_option("blockstack_utxo", "port"):
+       if parser.has_option(provider_sec, "port"):
            port = int(parser.get("blockstack_utxo", "port"))
 
    blockstack_utxo_opts = {
-       "utxo_provider": "blockstack_utxo",
        "server": server,
        "port": port
    }
@@ -348,6 +288,7 @@ def default_blockstack_utxo_opts( config_file=None ):
       if v is None:
          del blockstack_utxo_opts[k]
 
+   blockstack_utxo_opts['utxo_provider'] = 'blockstack_utxo'
    return blockstack_utxo_opts
 
 
@@ -366,59 +307,61 @@ def default_mock_utxo_opts( config_file=None ):
 
    if config_file is not None:
 
-      parser = SafeConfigParser()
-      parser.read(config_file)
+      provider_secs = find_service_provider_sections(config_file, 'mock_utxo')
+      if len(provider_secs) > 0:
+         provider_sec = provider_secs[0]
 
-      if parser.has_section("mock_utxo"):
+         parser = SafeConfigParser()
+         parser.read(config_file)
 
-         if parser.has_option('mock_utxo', 'tx_list'):
+         if parser.has_option(provider_sec, 'tx_list'):
             # should be a csv of raw transactions
-            mock_tx_list = parser.get('mock_utxo', 'tx_list').split(',')
+            mock_tx_list = parser.get(provider_sec, 'tx_list').split(',')
 
-         if parser.has_option('mock_utxo', 'tx_file'):
+         if parser.has_option(provider_sec, 'tx_file'):
             # should be a path
-            mock_tx_file = parser.get('mock_utxo', 'tx_file')
+            mock_tx_file = parser.get(provider_sec, 'tx_file')
 
-         if parser.has_option('mock_utxo', 'start_block'):
+         if parser.has_option(provider_sec, 'start_block'):
             # should be an int
             try:
-                mock_start_block = int( parser.get('mock_utxo', 'start_block') )
+                mock_start_block = int( parser.get(provider_sec, 'start_block') )
             except:
                 print >> sys.stderr, "Invalid 'start_block' value: expected int"
                 return None
 
-         if parser.has_option('mock_utxo', 'difficulty'):
+         if parser.has_option(provider_sec, 'difficulty'):
             # should be a float
             try:
-                mock_difficulty = float( parser.get('mock_utxo', 'difficulty') )
+                mock_difficulty = float( parser.get(provider_sec, 'difficulty') )
             except:
                 print >> sys.stderr, "Invalid 'difficulty' value: expected float"
                 return None
 
-         if parser.has_option('mock_utxo', 'start_block'):
+         if parser.has_option(provider_sec, 'start_block'):
             # should be an int
             try:
-                mock_start_block = int( parser.get('mock_utxo', 'start_block'))
+                mock_start_block = int( parser.get(provider_sec, 'start_block'))
             except:
                 print >> sys.stderr, "Invalid 'start_block' value: expected int"
                 return None
 
-         if parser.has_option('mock_utxo', 'save_file'):
+         if parser.has_option(provider_sec, 'save_file'):
              # should be a path 
-             mock_save_file = parser.get('mock_utxo', 'save_file')
+             mock_save_file = parser.get(provider_sec, 'save_file')
 
-         if parser.has_option('mock_utxo', 'initial_utxos'):
+         if parser.has_option(provider_sec, 'initial_utxos'):
             # should be a csv of privatekey:int
             try:
                 # verify that we can parse this
-                wallet_info = parser.get('mock_utxo', 'initial_utxos').split(',')
+                wallet_info = parser.get(provider_sec, 'initial_utxos').split(',')
                 wallets = {}
                 for wi in wallet_info:
                     privkey, value = wi.split(':')
                     wallets[ privkey ] = int(value)
 
                 #mock_initial_utxos = wallets
-                mock_initial_utxos = parser.get('mock_utxo', 'initial_utxos')
+                mock_initial_utxos = parser.get(provider_sec, 'initial_utxos')
 
             except:
                 print >> sys.stderr, "Invalid 'mock_initial_utxos' value: expected CSV of wif_private_key:int"
@@ -426,7 +369,6 @@ def default_mock_utxo_opts( config_file=None ):
 
 
    default_mock_utxo_opts = {
-      "utxo_provider": "mock_utxo",
       "tx_list": mock_tx_list,
       "tx_file": mock_tx_file,
       "start_block": mock_start_block,
@@ -441,6 +383,7 @@ def default_mock_utxo_opts( config_file=None ):
       if v is None:
          del default_mock_utxo_opts[k]
 
+   default_mock_utxo_opts['utxo_provider'] = 'mock_utxo'
    return default_mock_utxo_opts
 
 
@@ -457,9 +400,6 @@ def connect_utxo_provider( utxo_opts ):
    utxo_provider = utxo_opts['utxo_provider']
    if not utxo_provider in SUPPORTED_UTXO_PROVIDERS:
        raise Exception("Unsupported UTXO provider '%s'" % utxo_provider)
-
-   if utxo_provider == "chain_com":
-       return pybitcoin.ChainComClient( utxo_opts['api_key_id'], utxo_opts['api_key_secret'] )
 
    elif utxo_provider == "blockcypher":
        return pybitcoin.BlockcypherClient( utxo_opts['api_token'] )
@@ -478,11 +418,7 @@ def connect_utxo_provider( utxo_opts ):
        try:
            from blockstack_integration_tests import connect_mock_utxo_provider
        except:
-           # maybe legacy
-           try:
-               from blockstack.tests import connect_mock_utxo_provider
-           except ImportError:
-               raise Exception("Mock UTXO provider requires blockstack_integration_tests to be installed")
+           raise Exception("Mock UTXO provider requires blockstack_integration_tests to be installed")
 
        return connect_mock_utxo_provider( utxo_opts )
 
