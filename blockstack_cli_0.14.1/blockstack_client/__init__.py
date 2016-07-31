0de358c17c078a9114bfc2480d5c9b251c4d454e
Jude Nelson <judecn@gmail.com>
2016-07-31 19:35:44 -0400

    add app register/unregister and wallet management

--- blockstack_cli_0.14.1/blockstack_client/__init__.py
+++ blockstack_cli_0.14.1/blockstack_client/__init__.py
@@ -21,6 +21,7 @@
     along with Blockstack-client.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import app
 import accounts
 import client
 import config
@@ -47,20 +48,20 @@ from client import session, get_default_proxy, set_default_proxy, register_stora
 
 from snv import snv_lookup, lookup_snv
 from data import get_immutable, get_immutable_by_name, get_mutable, put_immutable, put_mutable, delete_immutable, \
-        delete_mutable, list_mutable_data, list_immutable_data, list_immutable_data_history, list_update_history, list_zonefile_history
+        delete_mutable, list_mutable_data, list_immutable_data, list_immutable_data_history, list_update_history, list_zonefile_history, \
+        get_app_data, put_app_data, delete_app_data
+
 from data import blockstack_url_fetch as fetch_data
 from data import data_get, data_put, data_delete, data_list
 from data import set_data_pubkey
 from storage import get_announcement, put_announcement
 from profile import get_name_profile, get_name_zonefile, zonefile_publish, get_and_migrate_profile
-from accounts import list_accounts, get_account, put_account, delete_account
+from accounts import list_accounts, get_account, put_account, delete_account, create_app_account
 
 from config import get_logger, get_config, CONFIG_PATH, CONFIG_FILENAME, get_utxo_provider_client, get_tx_broadcaster, default_bitcoind_opts
 
 from storage import blockstack_mutable_data_url as make_mutable_data_url
-from storage import blockstack_mutable_data_url_parse as parse_mutable_data_url
 from storage import blockstack_immutable_data_url as make_immutable_data_url 
-from storage import blockstack_immutable_data_url_parse as parse_immutable_data_url
 
 from storage import blockstack_data_url_parse as parse_data_url
 from storage import blockstack_data_url as make_data_url
@@ -78,3 +79,4 @@ from user import is_user_zonefile, user_zonefile_data_pubkey
 from operations import *
 from backend.nameops import *
 
+from app import app_register, app_unregister, app_get_wallet, app_wallet_path
