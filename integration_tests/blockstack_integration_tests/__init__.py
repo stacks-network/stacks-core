783b6900cdd6a50b6a4db978b41917e9a6350f45
Jude Nelson <judecn@gmail.com>
2016-08-04 15:59:37 -0400

    remove old home-grown mock bitcoind and UTXO provider.  We're using bitcoind with regtest instead.

--- integration_tests/blockstack_integration_tests/__init__.py
+++ integration_tests/blockstack_integration_tests/__init__.py
@@ -1,7 +1,1 @@
 #!/usr/bin/env python
-
-import mock_utxo_provider
-import mock_bitcoind
-
-from mock_utxo_provider import MockUTXOProvider, connect_mock_utxo_provider
-from mock_bitcoind import MockBitcoindConnection, connect_mock_bitcoind, get_mock_bitcoind
