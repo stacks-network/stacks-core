2e143f9cca05b43dac4ff0e85ab0b38c678dd485
Jude Nelson <judecn@gmail.com>
2016-08-04 15:47:25 -0400

    make our SPV client compatible with both mainnet and testnet; use protocoin >= 0.2

--- blockstack_client/spv.py
+++ blockstack_client/spv.py
@@ -33,7 +33,10 @@ import socket
 import os
 import sys
 import time
+import logging
 from config import get_logger
+
+import protocoin
 from protocoin.clients import *
 from protocoin.serializers import *
 from protocoin.fields import *
@@ -42,34 +45,62 @@ import virtualchain
 import pybitcoin
 import bitcoin
 
-log = get_logger()
+log = get_logger("blockstack-client")
 
 BLOCK_HEADER_SIZE = 81
-GENESIS_BLOCK_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+
+GENESIS_BLOCK_HASH = None
+GENESIS_BLOCK_MERKLE_ROOT = None
+USE_MAINNET = False
+USE_TESTNET = False
+
+GENESIS_BLOCK_HASH_MAINNET = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"
+GENESIS_BLOCK_MERKLE_ROOT_MAINNET = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+
+GENESIS_BLOCK_HASH_TESTNET = "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"
+GENESIS_BLOCK_MERKLE_ROOT_TESTNET = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+
+if virtualchain.version_byte == 0:
+    # mainnet
+    log.debug("Using mainnet")
+    USE_MAINNET = True
+    GENESIS_BLOCK_HASH = GENESIS_BLOCK_HASH_MAINNET
+    GENESIS_BLOCK_MERKLE_ROOT = GENESIS_BLOCK_MERKLE_ROOT_MAINNET
+
+elif virtualchain.version_byte == 111:
+    # testnet
+    log.debug("Using testnet/regtest")
+    USE_TESTNET = True
+    GENESIS_BLOCK_HASH = GENESIS_BLOCK_HASH_TESTNET
+    GENESIS_BLOCK_MERKLE_ROOT = GENESIS_BLOCK_HASH_TESTNET
+else:
+    raise Exception("Unknown version byte %s" % virtualchain.version_byte)
+
 BLOCK_DIFFICULTY_CHUNK_SIZE = 2016
 BLOCK_DIFFICULTY_INTERVAL = 14*24*60*60  # two weeks, in seconds
-GENESIS_BLOCK_MERKLE_ROOT = "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
 
 
-class BlockHash(object):
+class BlockHash(SerializableMessage):
     """
     Block hash to request
     """
-    def __init__(self, block_hash ):
-        self.block_hash = block_hash 
+    def __init__(self):
+        self.block_hash = None 
 
     def __repr__(self):
         return "<%s %s>" % (self.__class__.__name__, "%064x" % self.block_hash)
 
 
-class GetHeaders(object):
+class GetHeaders(SerializableMessage):
     """
     getheaders message
     """
+    command = "getheaders"
+
     def __init__(self):
+        self.version = PROTOCOL_VERSION
         self.block_hashes = []
-        self.hash_stop = None
-        self.command = "getheaders"
+        self.hash_stop = 0
 
     def add_block_hash( self, block_hash ):
         """
@@ -79,7 +110,11 @@ class GetHeaders(object):
             raise Exception("A getheaders request cannot have over 2000 block hashes")
 
         hash_num = int("0x" + block_hash, 16)
-        self.block_hashes.append( BlockHash(hash_num) )
+        
+        bh = BlockHash()
+        bh.block_hash = hash_num
+
+        self.block_hashes.append( bh )
         self.hash_stop = hash_num
 
     def num_block_hashes( self ):
@@ -110,15 +145,59 @@ class GetHeadersSerializer( Serializer ):
     hash_stop = Hash()
 
 
+# monkey-patch
+protocoin.serializers.MESSAGE_MAPPING['getheaders'] = GetHeadersSerializer
+
+
 class BlockHeaderClient( BitcoinBasicClient ):
     """
     Client to fetch and store block headers.
     """
 
-    def __init__(self, socket, headers_path ):
+    coin = None
+
+    def __init__(self, socket, headers_path, first_block_hash, last_block_id ):
+
+        if virtualchain.version_byte == 0:
+            self.coin = "bitcoin"
+        else:
+            self.coin = "bitcoin_testnet"
+
         super(BlockHeaderClient, self).__init__(socket)
         self.path = headers_path
-   
+        self.last_block_id = last_block_id
+        self.finished = False
+        self.verack = False
+        self.first_block_hash = first_block_hash
+
+    
+    def loop_exit( self ):
+        """
+        Stop the loop
+        """
+        self.finished = True
+        self.close_stream()
+
+
+    def run( self ):
+        """
+        Interact with the blockchain peer,
+        until we get a socket error or we
+        exit the loop explicitly.
+        Return True on success
+        Raise on error
+        """
+
+        self.handshake()
+
+        try:
+            self.loop()
+        except socket.error, se:
+            if self.finished:
+                return True
+            else:
+                raise
+
 
     def hash_to_string( self, hash_int ):
         return "%064x" % hash_int 
@@ -130,27 +209,68 @@ class BlockHeaderClient( BitcoinBasicClient ):
         NOTE: we request headers in order, so we will expect to receive them in order here.
         Verify that we do so.
         """
+        log.debug("handle headers (%s)" % len(block_headers_message.headers))
+
         block_headers = block_headers_message.headers
-        current_height = SPVClient.height( self.path )
-        log.debug("Receive %s headers (%s to %s)" % (len(block_headers), current_height, current_height + len(block_headers)))
 
         serializer = BlockHeaderSerializer()
 
         # verify that the local header chain connects to this sequence
-        last_header = SPVClient.read_header( self.path, current_height )
+        current_height = SPVClient.height( self.path )
+        if current_height is None:
+            assert USE_TESTNET
+            current_height = -1
 
-        if last_header['hash'] != self.hash_to_string(block_headers[0].prev_block):
-            raise Exception("Received discontinuous block header '%s' (expected '%s')" % \
-                    (self.hash_to_string(block_headers[0].prev_block),
+        assert (current_height >= 0 and USE_MAINNET) or USE_TESTNET, "Invalid height %s" % current_height
+    
+        last_header = None
+
+        if current_height >= 0:
+            last_header = SPVClient.read_header( self.path, current_height )
+            log.debug("Receive %s headers (%s to %s)" % (len(block_headers), current_height, current_height + len(block_headers)))
+
+        else:
+            # first testnet header
+            log.debug("Receive %s testnet headers (%s to %s)" % (len(block_headers), current_height + 1, current_height + 1 + len(block_headers)))
+            last_header = {
+                "version": block_headers[0].version,
+                "prev_block_hash": "%064x" % block_headers[0].prev_block,
+                "merkle_root": "%064x" % block_headers[0].merkle_root,
+                "timestamp": block_headers[0].timestamp,
+                "bits": block_headers[0].bits,
+                "nonce": block_headers[0].nonce,
+                "hash": block_headers[0].calculate_hash()
+            }
+
+        if (USE_MAINNET or (USE_TESTNET and current_height >= 0)) and last_header['hash'] != self.hash_to_string(block_headers[0].prev_block):
+            raise Exception("Received discontinuous block header at height %s: hash '%s' (expected '%s')" % \
+                    (current_height,
+                    self.hash_to_string(block_headers[0].prev_block),
                     last_header['hash'] ))
 
+        header_start = 1
+        if USE_TESTNET and current_height < 0:
+            # save initial header
+            header_start = 0
+
         # verify that this sequence of headers constitutes a hash chain 
-        for i in xrange(1, len(block_headers)):
+        for i in xrange(header_start, len(block_headers)):
             prev_block_hash = self.hash_to_string(block_headers[i].prev_block)
-            if prev_block_hash != block_headers[i-1].calculate_hash():
+            if i > 0 and prev_block_hash != block_headers[i-1].calculate_hash():
                 raise Exception("Block '%s' is not continuous with block '%s'" % \
                         prev_block_hash,
                         block_headers[i-1].calculate_hash())
+
+        if current_height < 0:
+            # save the first header 
+            if not os.path.exists(self.path):
+                with open(self.path, "wb") as f:
+                    block_header_serializer = BlockHeaderSerializer()
+                    bin_data = block_header_serializer.serialize( block_headers[0] )
+                    f.write( bin_data )
+
+            # got all headers, including the first
+            current_height = 0
 
         # insert into to local headers database
         next_block_id = current_height + 1
@@ -172,30 +292,43 @@ class BlockHeaderClient( BitcoinBasicClient ):
 
                 next_block_id += 1
             
-        
+        current_block_id = SPVClient.height( self.path )
+        if current_block_id >= self.last_block_id:
+            # got all headers
+            self.loop_exit()
+            return
+
+        prev_block_header = SPVClient.read_header( self.path, current_block_id )
+        prev_block_hash = prev_block_header['hash']
+        self.send_getheaders( prev_block_hash )
+
+    
     def send_getheaders( self, prev_block_hash ):
         """
         Request block headers from a particular block hash.
         Will receive up to 2000 blocks, starting with the block *after*
         the given block hash (prev_block_hash)
         """
+
         getheaders = GetHeaders()
-        getheaders_serial = GetHeadersSerializer()
 
         getheaders.add_block_hash( prev_block_hash )
-        getheaders.version = PROTOCOL_VERSION
 
-        self.send_message( getheaders, getheaders_serial )
+        log.debug("send getheaders")
+        self.send_message( getheaders )
 
 
     def handshake(self):
         """
         This method will implement the handshake of the
-        Bitcoin protocol. It will send the Version message.
+        Bitcoin protocol. It will send the Version message,
+        and block until it receives a VerAck
         """
+        log.debug("handshake (version %s)" % PROTOCOL_VERSION)
         version = Version()
-        version_serial = VersionSerializer()
-        self.send_message(version, version_serial)
+        version.services = 0    # can't send blocks
+        log.debug("send Version")
+        self.send_message(version)
 
 
     def handle_version(self, message_header, message):
@@ -207,9 +340,14 @@ class BlockHeaderClient( BitcoinBasicClient ):
         :param message_header: The Version message header
         :param message: The Version message
         """
+        log.debug("handle version")
         verack = VerAck()
-        verack_serial = VerAckSerializer()
-        self.send_message(verack, verack_serial)
+        log.debug("send VerAck")
+        self.send_message(verack)
+        self.verack = True
+
+        # begin!
+        self.send_getheaders( self.first_block_hash )
 
 
     def handle_ping(self, message_header, message):
@@ -221,11 +359,12 @@ class BlockHeaderClient( BitcoinBasicClient ):
         :param message_header: The header of the Ping message
         :param message: The Ping message
         """
+        log.debug("handle ping")
         pong = Pong()
         pong_serial = PongSerializer()
         pong.nonce = message.nonce
-        self.send_message(pong, pong_serial)    
-
+        log.debug("send pong")
+        self.send_message(pong)
 
 
 class SPVClient(object):
@@ -250,17 +389,21 @@ class SPVClient(object):
 
             block_header_serializer = BlockHeaderSerializer()
             genesis_block_header = BlockHeader()
-            genesis_block_header.version = 1
-            genesis_block_header.prev_block = 0
-            genesis_block_header.merkle_root = int(GENESIS_BLOCK_MERKLE_ROOT, 16 )
-            genesis_block_header.timestamp = 1231006505
-            genesis_block_header.bits = int( "1d00ffff", 16 )
-            genesis_block_header.nonce = 2083236893
-            genesis_block_header.txns_count = 0
 
-            with open(path, "wb") as f:
-                bin_data = block_header_serializer.serialize( genesis_block_header )
-                f.write( bin_data )
+            if USE_MAINNET:
+                # we know the mainnet block header
+                # but we don't know the testnet/regtest block header
+                genesis_block_header.version = 1
+                genesis_block_header.prev_block = 0
+                genesis_block_header.merkle_root = int(GENESIS_BLOCK_MERKLE_ROOT, 16 )
+                genesis_block_header.timestamp = 1231006505
+                genesis_block_header.bits = int( "1d00ffff", 16 )
+                genesis_block_header.nonce = 2083236893
+                genesis_block_header.txns_count = 0
+
+                with open(path, "wb") as f:
+                    bin_data = block_header_serializer.serialize( genesis_block_header )
+                    f.write( bin_data )
             
 
     @classmethod
@@ -543,55 +686,55 @@ class SPVClient(object):
     def sync_header_chain(cls, path, bitcoind_server, last_block_id ):
         """
         Synchronize our local block headers up to the last block ID given.
+        @bitcoind_server is host:port or just host
         """
         current_block_id = SPVClient.height( path )
+        if current_block_id is None:
+            assert USE_TESTNET
+            current_block_id = -1
+
+        assert (current_block_id >= 0 and USE_MAINNET) or USE_TESTNET
+
         if current_block_id < last_block_id:
-           
-            log.debug("Synchronize %s to %s" % (current_block_id, last_block_id))
+          
+            if USE_MAINNET:
+                log.debug("Synchronize %s to %s" % (current_block_id, last_block_id))
+            else:
+                log.debug("Synchronize testnet %s to %s" % (current_block_id + 1, last_block_id ))
 
             # need to sync
-            prev_block_header = SPVClient.read_header( path, current_block_id )
-            prev_block_hash = prev_block_header['hash']
+            if current_block_id >= 0:
+                prev_block_header = SPVClient.read_header( path, current_block_id )
+                prev_block_hash = prev_block_header['hash']
+
+            else:
+                # can only happen when in testnet
+                prev_block_hash = GENESIS_BLOCK_HASH_TESTNET
 
             # connect 
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect( (bitcoind_server, 8333) )
-            client = BlockHeaderClient( sock, path )
-            client.handshake()
 
-            # request first batch 
-            client.send_getheaders( prev_block_hash )
+            bitcoind_port = 8333
+            if ":" in bitcoind_server:
+                p = bitcoind_server.split(":")
+                bitcoind_server = p[0]
+                bitcoind_port = int(p[1])
 
-            while True:
+            log.debug("connect to %s:%s" % (bitcoind_server, bitcoind_port))
+            sock.connect( (bitcoind_server, bitcoind_port) )
 
-                # next message
-                message_header, message = client.receive_message()
+            client = BlockHeaderClient( sock, path, prev_block_hash, last_block_id )
 
-                if not message:
-                    continue
+            # get headers
+            client.run()
 
-                # dispatch message
-                handle_func_name = "handle_" + message_header.command
-                handle_func = getattr(client, handle_func_name, None)
-                if handle_func:
-                    handle_func(message_header, message)
-               
-                if message_header.command == "headers":
-                    # got reply to our getheaders request.
-                    # pipe the next one in
-                    current_block_id = SPVClient.height( path )
-                    prev_block_header = SPVClient.read_header( path, current_block_id )
-                    prev_block_hash = prev_block_header['hash']
-                    client.send_getheaders( prev_block_hash )
+            # verify headers
+            if SPVClient.height(path) < last_block_id:
+                raise Exception("Did not receive all headers up to %s (only got %s)" % (last_block_id, SPVClient.height(path)))
 
-                # synchronized?
-                if SPVClient.height( path ) >= last_block_id:
-                    break
-
-            # verify headers 
             rc = SPVClient.verify_header_chain( path )
             if not rc:
-               raise Exception("Failed to verify headers (stored in '%s')" % path)
+                raise Exception("Failed to verify headers (stored in '%s')" % path)
 
         return True
 
@@ -606,6 +749,7 @@ if __name__ == "__main__":
         print >> sys.stderr, "Usage: %s bitcoind_server headers_path blockchain_height" % sys.argv[0]
         sys.exit(0)
 
+    log.setLevel(logging.DEBUG)
     SPVClient.init( headers_path )
     rc = SPVClient.sync_header_chain( headers_path, bitcoind_server, height )
     if rc:
