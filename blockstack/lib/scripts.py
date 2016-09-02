acb0eb51048819769ba1605c3b61122504a110b9
Jude Nelson <judecn@gmail.com>
2016-09-02 00:13:51 -0400

    refactoring: move name/namespace validation and pricing to lib/scripts; move common transaction extraction logic to lib/scripts

--- blockstack/lib/scripts.py
+++ blockstack/lib/scripts.py
@@ -64,12 +64,11 @@ def is_name_valid( fqn ):
 
     if not is_b40( name ) or "+" in name or "." in name:
         return False 
-
-    if not is_b40( namespace_id ) or "+" in namespace_id or "." in namespace_id:
+   
+    if not is_namespace_valid( namespace_id ):
         return False
-    
-    name_hex = hexlify(name)
-    if len(name_hex) > LENGTHS['blockchain_id_name'] * 2:
+
+    if len(fqn) > LENGTHS['blockchain_id_name']:
        # too long
        return False 
 
@@ -89,4 +88,176 @@ def is_namespace_valid( namespace_id ):
         return False
 
     return True
+
+
+def get_namespace_from_name( name ):
+   """
+   Get a fully-qualified name's namespace, if it has one.
+   It's the sequence of characters after the last "." in the name.
+   If there is no "." in the name, then it belongs to the null
+   namespace (i.e. the empty string will be returned)
+   """
+   if "." not in name:
+      # empty namespace
+      return ""
+
+   return name.split(".")[-1]
+
+
+def get_name_from_fq_name( name ):
+   """
+   Given a fully-qualified name, get the name part.
+   It's the sequence of characters before the last "." in the name.
+
+   Return None if malformed
+   """
+   if "." not in name:
+      # malformed
+      return None
+
+   return name.split(".")[0]
+
+
+def price_name( name, namespace ):
+   """
+   Calculate the price of a name (without its namespace ID), given the
+   namespace parameters.
+
+   The minimum price is 1 satoshi
+   """
+
+   base = namespace['base']
+   coeff = namespace['coeff']
+   buckets = namespace['buckets']
+
+   bucket_exponent = 0
+   discount = 1.0
+
+   if len(name) < len(buckets):
+       bucket_exponent = buckets[len(name)-1]
+   else:
+       bucket_exponent = buckets[-1]
+
+   # no vowel discount?
+   if sum( [name.lower().count(v) for v in ["a", "e", "i", "o", "u", "y"]] ) == 0:
+       # no vowels!
+       discount = max( discount, namespace['no_vowel_discount'] )
+
+   # non-alpha discount?
+   if sum( [name.lower().count(v) for v in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "_"]] ) > 0:
+       # non-alpha!
+       discount = max( discount, namespace['nonalpha_discount'] )
+
+   price = (float(coeff * (base ** bucket_exponent)) / float(discount)) * NAME_COST_UNIT
+   if price < NAME_COST_UNIT:
+       price = NAME_COST_UNIT
+
+   return price
+
+
+def price_namespace( namespace_id ):
+   """
+   Calculate the cost of a namespace.
+   """
+
+   if len(namespace_id) == 1:
+       return NAMESPACE_1_CHAR_COST
+
+   elif len(namespace_id) in [2, 3]:
+       return NAMESPACE_23_CHAR_COST
+
+   elif len(namespace_id) in [4, 5, 6, 7]:
+       return NAMESPACE_4567_CHAR_COST
+
+   else:
+       return NAMESPACE_8UP_CHAR_COST
+
+
+def find_by_opcode( checked_ops, opcode ):
+    """
+    Given all previously-accepted operations in this block,
+    find the ones that are of a particular opcode.
+
+    @opcode can be one opcode, or a list of opcodes
+    """
+
+    if type(opcode) != list:
+        opcode = [opcode]
+
+    ret = []
+    for opdata in checked_ops:
+        if op_get_opcode_name(opdata['op']) in opcode:
+            ret.append(opdata)
+
+    return ret 
+
+
+def get_burn_fee_from_outputs( outputs ):
+    """
+    Given the set of outputs, find the fee sent 
+    to our burn address.
+    
+    Return the fee on success
+    Return None if not found
+    """
+    
+    ret = None
+    for output in outputs:
+       
+        output_script = output['scriptPubKey']
+        output_asm = output_script.get('asm')
+        output_hex = output_script.get('hex')
+        output_addresses = output_script.get('addresses')
+        
+        if output_asm[0:9] != 'OP_RETURN' and BLOCKSTACK_BURN_ADDRESS == output_addresses[0]:
+            
+            # recipient's script_pubkey and address
+            ret = int(output['value']*(10**8))
+            if os.environ.get("BLOCKSTACK_TEST") == "1" and ret > 1000 * (10**8):
+                raise Exception("Absurdly high burn output\n%s" % simplejson.dumps(outputs, indent=4, sort_keys=True))
+
+            break
+    
+    return ret 
+    
+
+def get_public_key_hex_from_tx( inputs, address ):
+    """
+    Given a list of inputs and the address of one of the inputs,
+    find the public key.
+
+    This only works for p2sh and p2pkh scripts.
+    """
+    
+    ret = None 
+    
+    for inp in inputs:
+        
+        input_scriptsig = inp.get('scriptSig', None )
+        if input_scriptsig is None:
+            continue 
+        
+        input_asm = input_scriptsig.get("asm")
+        
+        if len(input_asm.split(" ")) >= 2:
+            
+            # public key is the second hex string.  verify it matches the address
+            pubkey_hex = input_asm.split(" ")[1]
+            pubkey = None 
+            
+            try:
+                pubkey = virtualchain.BitcoinPublicKey( str(pubkey_hex) ) 
+            except Exception, e: 
+                traceback.print_exc()
+                log.warning("Invalid public key '%s'" % pubkey_hex)
+                continue 
+            
+            if address != pubkey.address():
+                continue 
+            
+            ret = pubkey_hex
+            break
+        
+    return ret 
+
 
