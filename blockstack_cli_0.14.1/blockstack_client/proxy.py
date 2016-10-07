1d8ef3f44d220607c986988ab2b151858a785d1e
Pandu Rao <pandu.rao@socketek.com>
2016-10-06 17:12:36 -0700

    streamline proxy to be pythonic

--- blockstack_cli_0.14.1/blockstack_client/proxy.py
+++ blockstack_cli_0.14.1/blockstack_client/proxy.py
@@ -21,49 +21,34 @@
     along with Blockstack-client. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import argparse
-import sys
 import json
 import traceback
-import types
-import socket
-import uuid
 import os
-import importlib
-import pprint
 import random
-import time
-import copy
-import blockstack_profiles
-import blockstack_zones
-import urllib
 from xmlrpclib import ServerProxy, Transport
 from defusedxml import xmlrpc
 import httplib
 
 # prevent the usual XML attacks
-xmlrpc.MAX_DATA = 10 * 1024 * 1024      # 10MB
+xmlrpc.MAX_DATA = 10 * 1024 * 1024  # 10MiB
 xmlrpc.monkey_patch()
 
 import storage
 
-import pybitcoin
-import bitcoin
-import binascii
-from utilitybelt import is_hex
-
 import config
-from config import get_logger, DEBUG, MAX_RPC_LEN, find_missing, BLOCKSTACKD_SERVER, \
-    BLOCKSTACKD_PORT, BLOCKSTACK_METADATA_DIR, BLOCKSTACK_DEFAULT_STORAGE_DRIVERS, \
-    FIRST_BLOCK_MAINNET, NAME_OPCODES, OPFIELDS, CONFIG_DIR, SPV_HEADERS_PATH, BLOCKCHAIN_ID_MAGIC, \
-    NAME_PREORDER, NAME_REGISTRATION, NAME_UPDATE, NAME_TRANSFER, NAMESPACE_PREORDER, NAME_IMPORT, \
-    USER_ZONEFILE_TTL, CONFIG_PATH
+from config import get_logger, MAX_RPC_LEN, CONFIG_PATH, BLOCKSTACK_TEST
 
-log = get_logger("blockstack-client")
 
-# borrowed with gratitude from Justin Cappos
-# https://seattle.poly.edu/browser/seattle/trunk/demokit/timeout_xmlrpclib.py?rev=692
+log = get_logger('blockstack-client')
+
+BLOCKSTACK_CLIENT_TEST_ALTERNATIVE_CONFIG = os.environ.get('BLOCKSTACK_CLIENT_TEST_ALTERNATIVE_CONFIG', None)
+
+
 class TimeoutHTTPConnection(httplib.HTTPConnection):
+    """
+    borrowed with gratitude from Justin Cappos
+    https://seattle.poly.edu/browser/seattle/trunk/demokit/timeout_xmlrpclib.py?rev=692
+    """
     def connect(self):
         httplib.HTTPConnection.connect(self)
         self.sock.settimeout(self.timeout)
@@ -81,35 +66,37 @@ class TimeoutHTTP(httplib.HTTP):
 
 class TimeoutTransport(Transport):
     def __init__(self, *l, **kw):
-        self.timeout = kw.get('timeout', 10)
-        if 'timeout' in kw.keys():
-            del kw['timeout']
-
+        self.timeout = kw.pop('timeout', 10)
         Transport.__init__(self, *l, **kw)
 
     def make_connection(self, host):
         conn = TimeoutHTTP(host)
         conn.set_timeout(self.timeout)
+
         return conn
+
 
 class TimeoutServerProxy(ServerProxy):
     def __init__(self, uri, *l, **kw):
-        kw['transport'] = TimeoutTransport(timeout=kw.get('timeout',10), use_datetime=kw.get('use_datetime', 0))
-        if 'timeout' in kw.keys():
-            del kw['timeout']
-        
+        timeout = kw.pop('timeout', 10)
+        use_datetime = kw.get('use_datetime', 0)
+        kw['transport'] = TimeoutTransport(timeout=timeout, use_datetime=use_datetime)
         ServerProxy.__init__(self, uri, *l, **kw)
 
 
 # default API endpoint proxy to blockstackd
 default_proxy = None
 
+
 class BlockstackRPCClient(object):
+
     """
     RPC client for the blockstack server
     """
-    def __init__(self, server, port, max_rpc_len=MAX_RPC_LEN, timeout=config.DEFAULT_TIMEOUT, debug_timeline=False, **kw ):
-        self.srv = TimeoutServerProxy( "http://%s:%s" % (server, port), timeout=timeout, allow_none=True )
+
+    def __init__(self, server, port, max_rpc_len=MAX_RPC_LEN, timeout=config.DEFAULT_TIMEOUT, debug_timeline=False, **kw):
+        self.url = 'http://{}:{}'.format(server, port)
+        self.srv = TimeoutServerProxy(self.url, timeout=timeout, allow_none=True)
         self.server = server
         self.port = port
         self.debug_timeline = debug_timeline
@@ -117,12 +104,11 @@ class BlockstackRPCClient(object):
     def __getattr__(self, key):
         try:
             return object.__getattr__(self, key)
-        except AttributeError, ae:
-
-            # random ID to match in logs
+        except AttributeError:
+            r = -1  # random ID to match in logs
             if self.debug_timeline:
-                r = random.randint(0, 2**16)
-                log.debug("RPC(%s) begin http://%s:%s %s" % (r, self.server, self.port, key))
+                r = random.randint(0, 2 ** 16)
+                log.debug('RPC({}) begin {} {}'.format(r, self.url, key))
 
             def inner(*args, **kw):
                 func = getattr(self.srv, key)
@@ -132,14 +118,14 @@ class BlockstackRPCClient(object):
                     try:
                         res = json.loads(res)
                     except (ValueError, TypeError):
-                        if os.environ.get("BLOCKSTACK_TEST") == "1":
-                            log.debug("Server replied invalid JSON: %s" % res)
+                        if BLOCKSTACK_TEST is not None:
+                            log.debug('Server replied invalid JSON: {}'.format(res))
 
-                        log.error("Server replied invalid JSON")
+                        log.error('Server replied invalid JSON')
                         res = {'error': 'Server replied invalid JSON'}
 
                 if self.debug_timeline:
-                    log.debug("RPC(%s) end http://%s:%s %s" % (r, self.server, self.port, key))
+                    log.debug('RPC({}) end {} {}'.format(r, self.url, key))
 
                 return res
 
@@ -151,30 +137,28 @@ def get_default_proxy(config_path=CONFIG_PATH):
     Get the default API proxy to blockstack.
     """
     global default_proxy
-    if default_proxy is None:
-
-        import client
-
-        if os.environ.get("BLOCKSTACK_CLIENT_TEST_ALTERNATIVE_CONFIG", None) == "1":
-            # feature test: make sure alternative config paths get propagated
-            if config_path.startswith("/home"):
-                print config_path
-                traceback.print_stack()
-                os.abort()
-
-        # load     
-        conf = config.get_config(config_path)
-        assert conf is not None, "Failed to get config from '%s'" % config_path
-        blockstack_server = conf['server']
-        blockstack_port = conf['port']
-
-        log.debug("Default proxy to %s:%s" % (blockstack_server, blockstack_port))
-        proxy = client.session(conf=conf, server_host=blockstack_server, server_port=blockstack_port)
-
-        return proxy
-
-    else:
+    if default_proxy is not None:
         return default_proxy
+
+    import client
+
+    if BLOCKSTACK_CLIENT_TEST_ALTERNATIVE_CONFIG is not None:
+        # feature test: make sure alternative config paths get propagated
+        if config_path.startswith('/home'):
+            print(config_path)
+            traceback.print_stack()
+            os.abort()
+
+    # load
+    conf = config.get_config(config_path)
+    assert conf is not None, 'Failed to get config from "{}"'.format(config_path)
+
+    blockstack_server, blockstack_port = conf['server'], conf['port']
+
+    log.debug('Default proxy to {}:{}'.format(blockstack_server, blockstack_port))
+    proxy = client.session(conf=conf, server_host=blockstack_server, server_port=blockstack_port)
+
+    return proxy
 
 
 def set_default_proxy(proxy):
@@ -188,8 +172,8 @@ def set_default_proxy(proxy):
 def json_traceback():
     exception_data = traceback.format_exc().splitlines()
     return {
-        "error": exception_data[-1],
-        "traceback": exception_data
+        'error': exception_data[-1],
+        'traceback': exception_data
     }
 
 
@@ -200,18 +184,13 @@ def getinfo(proxy=None):
 
     resp = {}
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     try:
         resp = proxy.getinfo()
-        if type(resp) == list:
-            if len(resp) == 0:
-                resp = {'error': 'No data returned'}
-            else:
-                resp = resp[0]
-
-    except Exception as e:
+        if isinstance(resp, list):
+            resp = resp[0] if resp else {'error': 'No data returned'}
+    except Exception:
         resp = json_traceback()
 
     return resp
@@ -224,17 +203,12 @@ def ping(proxy=None):
 
     resp = {}
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     try:
         resp = proxy.ping()
-        if type(resp) == list:
-            if len(resp) == 0:
-                resp = {'error': 'No data returned'}
-            else:
-                resp = resp[0]
-
+        if isinstance(resp, list):
+            resp = resp[0] if resp else {'error': 'No data returned'}
     except Exception as e:
         resp['error'] = str(e)
 
@@ -245,15 +219,11 @@ def get_name_cost(name, proxy=None):
     """
     name_cost
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_name_cost(name)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     return resp
 
@@ -262,15 +232,11 @@ def get_namespace_cost(namespace_id, proxy=None):
     """
     namespace_cost
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_namespace_cost(namespace_id)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     return resp
 
@@ -279,8 +245,7 @@ def get_all_names(offset, count, proxy=None):
     """
     get all names
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     return proxy.get_all_names(offset, count)
 
@@ -289,8 +254,7 @@ def get_names_in_namespace(namespace_id, offset, count, proxy=None):
     """
     Get names in a namespace
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     return proxy.get_names_in_namespace(namespace_id, offset, count)
 
@@ -300,8 +264,7 @@ def get_names_owned_by_address(address, proxy=None):
     Get the names owned by an address.
     Only works for p2pkh scripts.
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     return proxy.get_names_owned_by_address(address)
 
@@ -310,15 +273,11 @@ def get_consensus_at(block_height, proxy=None):
     """
     Get consensus at a block
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_consensus_at(block_height)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     return resp
 
@@ -329,42 +288,32 @@ def get_consensus_hashes(block_heights, proxy=None):
     NOTE: returns {block_height (int): consensus_hash (str)}
     (coerces the key to an int)
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_consensus_hashes(block_heights)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     if 'error' in resp:
         return resp
 
-    else:
-        if type(resp) != dict:
-            return {'error': 'Invalid data: expected dict'}
+    if not isinstance(resp, dict):
+        return {'error': 'Invalid data: expected dict'}
 
-        ret = {}
-        for h in resp.keys():
-            try:
-                hint = int(h)
-                ret[hint] = resp[h]
-            except:
-                return {'error': 'Invalid data: expected int'}
-            
-        return ret
+    try:
+        return {int(k): v for k, v in resp.items()}
+    except ValueError:
+        return {'error': 'Invalid data: expected int'}
 
 
 def get_consensus_range(block_id_start, block_id_end, proxy=None):
     """
     Get a range of consensus hashes.  The range is inclusive.
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_consensus_range(block_id_start, block_id_end)
+
     return resp
 
 
@@ -372,10 +321,10 @@ def get_name_blockchain_history(name, start_block, end_block, proxy=None):
     """
     Get the name's historical blockchain records
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_name_blockchain_history(name, start_block, end_block)
+
     return resp
 
 
@@ -383,10 +332,10 @@ def get_records_at(block_id, proxy=None):
     """
     Get the set of records as they were at a particular block.
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_records_at(block_id)
+
     return resp
 
 
@@ -394,15 +343,11 @@ def get_records_hash_at(block_id, proxy=None):
     """
     Get the hash of a set of records as they were at a particular block.
     """
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_records_hash_at(block_id)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     return resp
 
@@ -412,15 +357,11 @@ def get_name_blockchain_record(name, proxy=None):
     get_name_blockchain_record
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     resp = proxy.get_name_blockchain_record(name)
-    if type(resp) == list:
-        if len(resp) == 0:
-            resp = {'error': 'No data returned'}
-        else:
-            resp = resp[0]
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
     return resp
 
@@ -430,23 +371,17 @@ def get_namespace_blockchain_record(namespace_id, proxy=None):
     get_namespace_blockchain_record
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
-    ret = proxy.get_namespace_blockchain_record(namespace_id)
-    if type(ret) == list:
-        if len(ret) == 0:
-            ret = {'error': 'No data returned'}
-            return ret
-        else:
-            ret = ret[0]
+    resp = proxy.get_namespace_blockchain_record(namespace_id)
+    if isinstance(resp, list):
+        resp = resp[0] if resp else {'error': 'No data returned'}
 
-    if ret is not None:
+    if resp is not None:
         # this isn't needed
-        if 'opcode' in ret:
-            del ret['opcode']
+        resp.pop('opcode', None)
 
-    return ret
+    return resp
 
 
 def is_name_registered(fqu, proxy=None):
@@ -454,72 +389,59 @@ def is_name_registered(fqu, proxy=None):
     Return True if @fqu registered on blockchain
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
-    blockchain_record = get_name_blockchain_record( fqu, proxy=proxy )
+    blockchain_record = get_name_blockchain_record(fqu, proxy=proxy)
     if 'error' in blockchain_record:
-        log.debug("Failed to read blockchain record for %s" % fqu)
+        log.debug('Failed to read blockchain record for {}'.format(fqu))
         return False
 
-    if blockchain_record.has_key('revoked') and blockchain_record['revoked']:
+    if blockchain_record.get('revoked', None) is not None:
         return False
 
-    if "first_registered" in blockchain_record:
-        return True
-    else:
-        return False
+    return 'first_registered' in blockchain_record
 
 
-def has_zonefile_hash(fqu, proxy=None ):
+def has_zonefile_hash(fqu, proxy=None):
     """
     Return True if @fqu has a zonefile hash on the blockchain
     """
-    
-    if proxy is None:
-        proxy = get_default_proxy()
 
-    blockchain_record = get_name_blockchain_record(fqu, proxy=proxy )
+    proxy = get_default_proxy() if proxy is None else proxy
+
+    blockchain_record = get_name_blockchain_record(fqu, proxy=proxy)
     if 'error' in blockchain_record:
-        log.debug("Failed to read blockchain record for %s" % fqu)
+        log.debug('Failed to read blockchain record for {}'.format(fqu))
         return False
 
-    if 'value_hash' in blockchain_record and blockchain_record['value_hash'] is not None:
-        return True
-    else:
-        return False
+    return blockchain_record.get('value_hash', None) is not None
 
 
 def is_zonefile_current(fqu, zonefile_json, proxy=None):
-    """ 
+    """
     Return True if hash(@zonefile_json) published on blockchain
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
     zonefile_hash = storage.hash_zonefile(zonefile_json)
-    return is_zonefile_hash_current( fqu, zonefile_hash, proxy=proxy )
+
+    return is_zonefile_hash_current(fqu, zonefile_hash, proxy=proxy)
 
 
 def is_zonefile_hash_current(fqu, zonefile_hash, proxy=None):
-    """ 
+    """
     Return True if hash(@zonefile_json) published on blockchain
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
-    blockchain_record = get_name_blockchain_record( fqu, proxy=proxy )
+    blockchain_record = get_name_blockchain_record(fqu, proxy=proxy)
     if 'error' in blockchain_record:
-        log.debug("Failed to read blockchain record for %s" % fqu)
+        log.debug('Failed to read blockchain record for {}'.format(fqu))
         return False
 
-    if 'value_hash' in blockchain_record and blockchain_record['value_hash'] == zonefile_hash:
-        # if hash of profile is in correct
-        return True
-
-    return False
+    return zonefile_hash == blockchain_record.get('value_hash', '')
 
 
 def is_name_owner(fqu, address, proxy=None):
@@ -527,17 +449,11 @@ def is_name_owner(fqu, address, proxy=None):
     return True if @btc_address owns @fqu
     """
 
-    if proxy is None:
-        proxy = get_default_proxy()
+    proxy = get_default_proxy() if proxy is None else proxy
 
-    blockchain_record = get_name_blockchain_record( fqu, proxy=proxy )
+    blockchain_record = get_name_blockchain_record(fqu, proxy=proxy)
     if 'error' in blockchain_record:
-        log.debug("Failed to read blockchain record for %s" % fqu)
+        log.debug('Failed to read blockchain record for {}'.format(fqu))
         return False
 
-    if 'address' in blockchain_record and blockchain_record['address'] == address:
-        return True
-    else:
-        return False
-
-
+    return address == blockchain_record.get('address', '')
