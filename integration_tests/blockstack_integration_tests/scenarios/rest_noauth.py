c8ad8a820ce31d7977af77c4dc110929a79b9192
Jude Nelson <judecn@gmail.com>
2017-02-07 15:46:33 -0500

    add test for authentication-disabled REST calls

---
+++ integration_tests/blockstack_integration_tests/scenarios/rest_noauth.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Blockstack
+    ~~~~~
+    copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    copyright: (c) 2016 by Blockstack.org
+
+    This file is part of Blockstack
+
+    Blockstack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Blockstack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
+""" 
+import os
+import testlib
+import pybitcoin
+import urllib2
+import json
+import blockstack_client
+import blockstack_profiles
+import blockstack_gpg
+import sys
+import keylib
+import time
+from keylib import ECPrivateKey, ECPublicKey
+
+wallets = [
+    testlib.Wallet( "5JesPiN68qt44Hc2nT8qmyZ1JDwHebfoh9KQ52Lazb1m1LaKNj9", 100000000000 ),
+    testlib.Wallet( "5KHqsiU9qa77frZb6hQy9ocV7Sus9RWJcQGYYBJJBb2Efj1o77e", 100000000000 ),
+    testlib.Wallet( "5Kg5kJbQHvk1B64rJniEmgbD83FpZpbw2RjdAZEzTefs9ihN3Bz", 100000000000 ),
+    testlib.Wallet( "5JuVsoS9NauksSkqEjbUZxWwgGDQbMwPsEfoRBSpLpgDX1RtLX7", 100000000000 ),
+    testlib.Wallet( "5KEpiSRr1BrT8vRD7LKGCEmudokTh1iMHbiThMQpLdwBwhDJB1T", 100000000000 ),
+    testlib.Wallet( "5K5hDuynZ6EQrZ4efrchCwy6DLhdsEzuJtTDAf3hqdsCKbxfoeD", 100000000000 ),
+    testlib.Wallet( "5J39aXEeHh9LwfQ4Gy5Vieo7sbqiUMBXkPH7SaMHixJhSSBpAqz", 100000000000 ),
+    testlib.Wallet( "5K9LmMQskQ9jP1p7dyieLDAeB6vsAj4GK8dmGNJAXS1qHDqnWhP", 100000000000 ),
+    testlib.Wallet( "5KcNen67ERBuvz2f649t9F2o1ddTjC5pVUEqcMtbxNgHqgxG2gZ", 100000000000 ),
+    testlib.Wallet( "5KMbNjgZt29V6VNbcAmebaUT2CZMxqSridtM46jv4NkKTP8DHdV", 100000000000 ),
+]
+
+consensus = "17ac43c1d8549c3181b200f1bf97eb7d"
+wallet_keys = None
+wallet_keys_2 = None
+error = False
+
+index_file_data = "<html><head></head><body>foo.test hello world</body></html>"
+resource_data = "hello world"
+
+def scenario( wallets, **kw ):
+
+    global wallet_keys, wallet_keys_2, error, index_file_data, resource_data
+
+    wallet_keys = testlib.blockstack_client_initialize_wallet( "0123456789abcdef", wallets[5].privkey, wallets[3].privkey, wallets[4].privkey )
+    test_proxy = testlib.TestAPIProxy()
+    blockstack_client.set_default_proxy( test_proxy )
+
+    testlib.blockstack_namespace_preorder( "test", wallets[1].addr, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstack_namespace_reveal( "test", wallets[1].addr, 52595, 250, 4, [6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0], 10, 10, wallets[0].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstack_namespace_ready( "test", wallets[1].privkey )
+    testlib.next_block( **kw )
+
+    testlib.blockstack_name_preorder( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+    
+    testlib.blockstack_name_register( "foo.test", wallets[2].privkey, wallets[3].addr )
+    testlib.next_block( **kw )
+    
+    testlib.blockstack_client_set_wallet( "0123456789abcdef", wallets[5].privkey, wallets[3].privkey, wallets[4].privkey )
+    
+    # migrate profiles 
+    res = testlib.migrate_profile( "foo.test", proxy=test_proxy, wallet_keys=wallet_keys )
+    if 'error' in res:
+        res['test'] = 'Failed to initialize foo.test profile'
+        print json.dumps(res, indent=4, sort_keys=True)
+        error = True
+        return 
+
+    # bootstrap storage for this wallet
+    res = testlib.blockstack_cli_upgrade_storage("foo.test", password="0123456789abcdef")
+    if 'error' in res:
+        print 'failed to bootstrap storage for foo.test'
+        print json.dumps(res, indent=4, sort_keys=True)
+        return False
+
+    if not blockstack_client.check_storage_setup():
+        print "storage is not set up"
+        return False
+
+    # tell serialization-checker that value_hash can be ignored here
+    print "BLOCKSTACK_SERIALIZATION_CHECK_IGNORE value_hash"
+    sys.stdout.flush()
+    
+    testlib.next_block( **kw )
+    
+    config_path = os.environ.get("BLOCKSTACK_CLIENT_CONFIG", None)
+    config_dir = os.path.dirname(config_path)
+
+    # deactivate authentication 
+    blockstack_client.config.write_config_field(config_path, 'blockstack-client', 'authenticate_api', 'False')
+    
+    blockstack_client.rpc.local_rpc_stop(config_dir=config_dir)
+    res = blockstack_client.rpc.local_rpc_ensure_running(config_dir=config_dir, password="0123456789abcdef")
+    if not res:
+        print 'failed to restart RPC daemon'
+        return False
+
+    testlib.blockstack_client_set_wallet( "0123456789abcdef", wallets[5].privkey, wallets[3].privkey, wallets[4].privkey )
+
+    # make sure we can do REST calls
+    res = testlib.blockstack_REST_call('GET', '/api/v1/blockchains/bitcoin/pending', None, name='foo.test', appname='register', user_id='fake_user')
+    if 'error' in res:
+        res['test'] = 'Failed to get queues'
+        print json.dumps(res)
+        return False
+
+    # make sure we can do REST calls with different app names and user names
+    res = testlib.blockstack_REST_call('GET', '/api/v1/blockchains/bitcoin/pending', None, name='foo.test', appname='fake', user_id='fake_user2')
+    if 'error' in res:
+        res['test'] = 'Failed to get queues'
+        print json.dumps(res)
+        return False
+
+    # register the name bar.test 
+    res = testlib.blockstack_REST_call('POST', '/api/v1/names', None, name="foo.test", appname="also_fake", data={'name': 'bar.test'})
+    if 'error' in res:
+        res['test'] = 'Failed to register user'
+        print json.dumps(res)
+        error = True
+        return False
+
+    # wait for update to get confirmed 
+    for i in xrange(0, 48):
+        if i % 12 == 0:
+            print "waiting for RPC daemon to catch up"
+            time.sleep(10)
+
+        testlib.next_block( **kw )
+
+    res = testlib.blockstack_REST_call("GET", "/api/v1/names/bar.test", None, name="foo.test", appname="register")
+    if 'error' in res:
+        res['test'] = 'Failed to query name'
+        print json.dumps(res)
+        error = True
+        return False
+
+    if res['http_status'] != 200:
+        res['test'] = 'HTTP status {}, response = {}'.format(res['http_status'], res['response'])
+        print json.dumps(res)
+        error = True
+        return False
+
+    # should now be registered 
+    if res['response']['status'] != 'registered':
+        print "register not complete"
+        print json.dumps(res)
+        return False
+   
+
+
+def check( state_engine ):
+
+    global wallet_keys, error, index_file_data, resource_data
+    
+    config_path = os.environ.get("BLOCKSTACK_CLIENT_CONFIG")
+    assert config_path
+
+    if error:
+        print "Key operation failed."
+        return False
+
+    # not revealed, but ready 
+    ns = state_engine.get_namespace_reveal( "test" )
+    if ns is not None:
+        print "namespace not ready"
+        return False 
+
+    ns = state_engine.get_namespace( "test" )
+    if ns is None:
+        print "no namespace"
+        return False 
+
+    if ns['namespace_id'] != 'test':
+        print "wrong namespace"
+        return False 
+
+    names = ['foo.test', 'bar.test']
+    wallet_keys_list = [wallet_keys]
+    test_proxy = testlib.TestAPIProxy()
+
+    for i in xrange(0, len(names)):
+        name = names[i]
+        wallet_payer = 5
+        wallet_owner = 3
+        wallet_data_pubkey = 4
+
+        # not preordered
+        preorder = state_engine.get_name_preorder( name, pybitcoin.make_pay_to_address_script(wallets[wallet_payer].addr), wallets[wallet_owner].addr )
+        if preorder is not None:
+            print "still have preorder"
+            return False
+    
+        # registered 
+        name_rec = state_engine.get_name( name )
+        if name_rec is None:
+            print "name does not exist"
+            return False 
+
+        # owned 
+        if name_rec['address'] != wallets[wallet_owner].addr or name_rec['sender'] != pybitcoin.make_pay_to_address_script(wallets[wallet_owner].addr):
+            print "name {} has wrong owner".format(name)
+            return False 
+
+    return True
