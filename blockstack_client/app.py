1a05a8b57b4a72d43474c336e6c6c760f810e7f7
Jude Nelson <judecn@gmail.com>
2017-03-15 18:11:24 -0400

    rework signin so as to authorize an externally-given public key

--- blockstack_client/app.py
+++ blockstack_client/app.py
@@ -50,7 +50,7 @@ from .schemas import *
 from keys import HDWallet, get_pubkey_hex
 
 
-def app_make_session( app_domain, methods, app_public_key, master_data_privkey_hex, app_user_id=None, session_lifetime=None, blockchain_ids=None, config_path=CONFIG_PATH ):
+def app_make_session( app_public_key, app_domain, methods, master_data_privkey, app_user_id=None, session_lifetime=None, blockchain_ids=None, config_path=CONFIG_PATH ):
     """
     Make a session JWT for this application.
     Verify with user private key
@@ -81,7 +81,7 @@ def app_make_session( app_domain, methods, app_public_key, master_data_privkey_h
     jsonschema.validate(ses, APP_SESSION_SCHEMA)
 
     signer = jsontokens.TokenSigner()
-    session_token = signer.sign( ses, master_data_privkey_hex )
+    session_token = signer.sign( ses, master_data_privkey )
     session = jsontokens.decode_token(session_token)
 
     return {'session': session, 'session_token': session_token}
