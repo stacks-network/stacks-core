9f33ada97ab6ce110b0effdaef55a97a832d12a0
Muneeb Ali <muneeb@ali.vc>
2017-03-17 11:13:47 -0400

    removed config_local dependency

--- api/resolver/config.py
+++ api/resolver/config.py
@@ -31,48 +31,21 @@ CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 NAMES_FILE = os.path.join(CURRENT_DIR, NAMES_FILENAME)
 NEW_NAMES_FILE = os.path.join(CURRENT_DIR, NEW_NAMES_FILENAME)
 
-try:
-    from config_local import *
-except:
+DEBUG = True
 
-    log.debug('config_local.py not found, using default settings')
+MEMCACHED_USERNAME = None
+MEMCACHED_PASSWORD = None
 
-    MEMCACHED_TIMEOUT = 12 * 60 * 60
-    USERSTATS_TIMEOUT = 60 * 60
-    MEMCACHED_ENABLED = False
+BLOCKSTACKD_IP = 'localhost'
+BLOCKSTACKD_PORT = 6264
+DHT_MIRROR_IP = '52.20.98.85'
+DHT_MIRROR_PORT = 6266
 
-    try:
-        MEMCACHED_USERNAME = os.environ['MEMCACHEDCLOUD_USERNAME']
-        MEMCACHED_PASSWORD = os.environ['MEMCACHEDCLOUD_PASSWORD']
-    except:
-        try:
-            MEMCACHED_USERNAME = os.environ['MEMCACHIER_USERNAME']
-            MEMCACHED_PASSWORD = os.environ['MEMCACHIER_PASSWORD']
-        except:
-            MEMCACHED_USERNAME = None
-            MEMCACHED_PASSWORD = None
+MEMCACHED_TIMEOUT = 12 * 60 * 60
+USERSTATS_TIMEOUT = 60 * 60
+MEMCACHED_ENABLED = False
 
-    try:
-        MEMCACHED_SERVERS = os.environ['MEMCACHEDCLOUD_SERVERS'].split(',')
-    except:
-        try:
-            MEMCACHED_SERVERS = os.environ['MEMCACHIER_SERVERS'].split(',')
-        except:
-            memcached_server = MEMCACHED_SERVER + ':' + str(MEMCACHED_PORT)
-            MEMCACHED_SERVERS = [memcached_server]
+MEMCACHED_SERVERS = ['127.0.0.1:11211']
 
-    try:
-        BLOCKSTACKD_IP = os.environ['BLOCKSTACKD_IP']
-        BLOCKSTACKD_PORT = os.environ['BLOCKSTACKD_PORT']
-        DHT_MIRROR_IP = os.environ['DHT_MIRROR_IP']
-        DHT_MIRROR_PORT = os.environ['DHT_MIRROR_PORT']
-    except:
-        log.debug("Blockstack-server or DHT-mirror not configured properly")
-        exit(1)
-
-    # if password protecting the resolver
-    try:
-        API_USERNAME = os.environ['API_USERNAME']
-        API_PASSWORD = os.environ['API_PASSWORD']
-    except:
-        API_USERNAME = API_PASSWORD = ''
+API_USERNAME = 'default'
+API_PASSWORD = 'default'
