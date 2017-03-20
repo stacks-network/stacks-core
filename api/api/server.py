2a82995ac1025b2c38c0d5e38a656833204e1538
Muneeb Ali <muneeb@ali.vc>
2017-03-19 23:40:29 -0400

    added catch all for GET and POST calls, and disabled POST calls for public servers

--- api/api/server.py
+++ api/api/server.py
@@ -33,6 +33,7 @@ from flask_crossdomain import crossdomain
 from flask import render_template, send_from_directory
 
 from .utils import get_api_calls
+from .settings import PUBLIC_NODE, SERVER_URL, BASE_API_URL
 
 app = Flask(__name__)
 
@@ -42,43 +43,38 @@ parent_dir = os.path.abspath(current_dir + "/../")
 
 sys.path.insert(0, parent_dir)
 
+import blockstack_client.config as blockstack_config
 import blockstack_client.config as blockstack_constants
 
-from blockstack_client.rpc import BlockstackAPIEndpoint
 from blockstack_client.rpc import local_api_connect, local_api_start, local_api_action
 from blockstack_client.wallet import make_wallet
+from blockstack_client.proxy import getinfo
 
-wallet_keys = make_wallet('password', encrypt=False)
+log = blockstack_config.get_logger()
+
+#wallet_keys = make_wallet('password', encrypt=False)
 api_password = "password"
 port = 6269
 host = 'localhost'
 config_path = blockstack_constants.CONFIG_PATH
 
-#local_api_action("start", password="temptemptemp", api_pass=api_password)
+#local_api_start(password='temptemptemp')
 
-local_api_start(password='password')
+"""
+Check first if API daemon is running
+"""
+status = local_api_action('status')
 
-#api_server = local_api_connect(api_host=host, api_port=port)
-print local_api_action('status')
+if(status):
+    log.debug("API daemon is running")
+else:
+    log.debug("Start API daemon first")
+    exit(0)
 
-#BlockstackAPIEndpoint(api_password, wallet_keys, host=host, port=port, config_path=config_path)
-
-BASE_API_URL = "http://localhost:6270"
-
-@app.route('/v1/node/ping', methods=['GET'])
-@crossdomain(origin='*')
-def api_ping():
-
-    API_URL = BASE_API_URL + '/v1/node/ping'
-
-    resp = requests.get(API_URL)
-
-    return jsonify(resp.json()), 200
 
 @app.route('/v1/names/<name>', methods=['GET'])
 @crossdomain(origin='*')
 def api_names(name):
-
 
     API_URL = BASE_API_URL + '/v1/names/' + name
 
@@ -87,10 +83,37 @@ def api_names(name):
     return jsonify(resp.json()), 200
 
 
+@app.route('/<path:path>', methods=['GET'])
+def catch_all_get(path):
+
+    API_URL = BASE_API_URL + '/' + path
+
+    resp = requests.get(API_URL)
+
+    return jsonify(resp.json()), 200
+
+
+@app.route('/<path:path>', methods=['POST'])
+def catch_all_post(path):
+
+    if PUBLIC_NODE:
+        return render_template('403.html'), 403
+
+    API_URL = BASE_API_URL + '/' + path
+
+    resp = requests.post(API_URL, data=requests.data)
+
+    return jsonify(resp.json()), 200
+
+
 @app.route('/')
 def index():
     api_calls = get_api_calls('api/api_v1.md')
-    return render_template('index.html', api_calls=api_calls)
+    server_info = getinfo()
+
+    return render_template('index.html', api_calls=api_calls,
+                                         server_info=server_info,
+                                         server_url=SERVER_URL)
 
 
 @app.route('/favicon.ico')
