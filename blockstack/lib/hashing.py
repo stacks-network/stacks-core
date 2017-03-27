58ce4ba8cc3c0e8480ac58ab7f369fcffe28f291
Jude Nelson <judecn@gmail.com>
2017-03-27 15:12:23 -0400

    clean up imports

--- blockstack/lib/hashing.py
+++ blockstack/lib/hashing.py
@@ -21,8 +21,10 @@
     along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from utilitybelt import dev_urandom_entropy, is_hex
+from utilitybelt import is_hex
 from binascii import hexlify, unhexlify
+import hashlib
+
 from pybitcoin.hash import hex_hash160, bin_hash160, bin_sha256, bin_double_sha256, hex_to_bin_reversed, bin_to_hex_reversed
 
 from .b40 import b40_to_bin
@@ -47,4 +49,5 @@ def hash256_trunc128( data ):
    Hash a string of data by taking its 256-bit sha256 and truncating it to 128 bits.
    """
    return hexlify( bin_sha256( data )[0:16] )
-   
+
+
