580b5cbb118c8e310664ce05992c26cb84add12e
Jude Nelson <judecn@gmail.com>
2017-04-10 11:48:44 -0400

    Renamed blockstack_utxo to refer to the Insight API endpoint we'll be running; consolidated code for the explorer UTXO endpoint and the dedicated UTXO endpoint we maintain (both use insight API)

--- blockstack_client/backend/utxo/blockstack_explorer.py
+++ blockstack_client/backend/utxo/blockstack_explorer.py
@@ -27,100 +27,14 @@ import requests
 import json
 import traceback
 
+from .insight_api import _get_unspents as get_unspents
+from .insight_api import _broadcast_transaction as broadcast_transaction
+from .insight_api import InsightClient
+
 BLOCKSTACK_EXPLORER_URL = "https://explorer.blockstack.org"
 
-class BlockstackExplorerClient(object):
+class BlockstackExplorerClient(InsightClient):
     def __init__(self, url=BLOCKSTACK_EXPLORER_URL):
-        self.url = url
+        super(BlockstackExplorerClient, self).__init__(url)
 
-    def get_unspents(self, address):
-        url = self.url + '/insight-api/addr/{}/utxo'.format(address)
-        resp = None
-        try:
-            req = requests.get(url)
-            resp = req.json()
-        except:
-            raise Exception("Failed to query UTXOs")
-
-        # format...
-        try:
-            return format_unspents(resp)
-        except Exception as e:
-            traceback.print_exc()
-            raise ValueError("Invalid UTXO response")
-        
-
-    def broadcast_transaction(self, rawtx):
-        url = self.url + '/insight-api/tx/send'
-        req = None
-        
-        data = json.dumps({'rawtx': rawtx})
-        headers = {'content-type': 'application/json', 'accept': 'application/json'}
-        
-        req = None
-        resp = None
-
-        try:
-            req = requests.post(url, data=data, headers=headers)
-        except:
-            raise Exception("Failed to send transaction")
-
-        try:
-            resp = req.json()
-            return {'success': True, 'tx_hash': resp['txid']}
-        except:
-            raise Exception("Failed to broadcast transaction, got response: {}".format(req.text))
-
-
-def format_unspents(unspents):
-    return [{
-        'transaction_hash': s['txid'],
-        'output_index': s['vout'],
-        'value': s['satoshis'],
-        'script_hex': s['scriptPubKey'],
-        'confirmations': s['confirmations']
-    } for s in unspents]
-
-
-def get_unspents( address, client=BlockstackExplorerClient() ):
-    """
-    Get unspent outputs from a Blockstack server.
-    """
-
-    if not isinstance(client, BlockstackExplorerClient):
-        raise Exception("Not a Blockstack Explorer client")
-
-    unspents = client.get_unspents( address )
-    return unspents
-
-
-def broadcast_transaction( txdata, client=BlockstackExplorerClient() ):
-    """
-    Send a transaction through a Blockstack server
-    """
-
-    if not isinstance(client, BlockstackExplorerClient):
-        raise Exception("Not a Blockstack Explorer client")
-
-    res = client.broadcast_transaction( txdata )
-    return res
-
-
-if __name__ == '__main__':
-    import sys
-    op = sys.argv[1]
-    arg = sys.argv[2]
-
-    if op == 'get_unspents':
-        res = get_unspents(arg)
-        print json.dumps(res, indent=4, sort_keys=True)
-        sys.exit(0)
-
-    elif op == 'broadcast_transaction':
-        res = broadcast_transaction(arg)
-        print json.dumps(res, indent=4, sort_keys=True)
-        sys.exit(0)
-
-    else:
-        raise Exception("invalid argument")
 
