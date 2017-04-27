f7fa4d85f71c43a38b08978ea874dc886b318da7
Jude Nelson <judecn@gmail.com>
2017-04-27 19:35:15 -0400

    clean up import symbols

--- blockstack_client/backend/__init__.py
+++ blockstack_client/backend/__init__.py
@@ -18,8 +18,15 @@
     You should have received a copy of the GNU General Public License
     along with Blockstack-client. If not, see <http://www.gnu.org/licenses/>.
 """
+
+import utxo
+import drivers
+
+"""
 import registrar
 import safety
+import drivers
+import utxo
 
 from nameops import do_preorder, do_register, do_update, do_transfer, do_revoke, do_renewal, \
         do_namespace_preorder, do_namespace_reveal, do_namespace_ready, do_announce, do_name_import
@@ -28,4 +35,4 @@ from nameops import estimate_preorder_tx_fee, estimate_register_tx_fee, estimate
         estimate_transfer_tx_fee, estimate_revoke_tx_fee, estimate_namespace_preorder_tx_fee, \
         estimate_namespace_reveal_tx_fee, estimate_namespace_ready_tx_fee, estimate_announce_tx_fee, \
         estimate_name_import_tx_fee
-
+"""
