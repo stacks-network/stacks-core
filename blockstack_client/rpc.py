3dcf4585632cc22fbeb7da5768e1fa17d39060bc
Jude Nelson <judecn@gmail.com>
2017-07-06 18:48:39 -0400

    warn if we see a legacy authToken

--- blockstack_client/rpc.py
+++ blockstack_client/rpc.py
@@ -463,6 +463,7 @@ class BlockstackAPIEndpointHandler(SimpleHTTPRequestHandler):
 
         if legacy:
             # legacy; fill in defaults
+            log.warning("Legacy authToken")
             app_public_key = str(decoded_token['payload']['app_public_key'])
             app_private_key = '0000000000000000000000000000000000000000000000000000000000000001'
             app_public_keys = []
