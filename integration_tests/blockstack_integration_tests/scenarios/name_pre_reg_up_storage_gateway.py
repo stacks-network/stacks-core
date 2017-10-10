2cbd672472ef7d08a05e9ab73b8c267745408f41
Jude Nelson <judecn@gmail.com>
2017-10-10 11:24:54 -0400

    remove references to rpc_get_zonefiles_by_names

--- integration_tests/blockstack_integration_tests/scenarios/name_pre_reg_up_storage_gateway.py
+++ integration_tests/blockstack_integration_tests/scenarios/name_pre_reg_up_storage_gateway.py
@@ -253,30 +253,18 @@ def check( state_engine ):
 
     # zonefile and profile replicated to blockstack server 
     try:
-        zonefile_by_name_str = srv.get_zonefiles_by_names(['foo.test'])
         zonefile_by_hash_str = srv.get_zonefiles([name_rec['value_hash']])
-       
-        zonefile_by_name = json.loads(zonefile_by_name_str)
         zonefile_by_hash = json.loads(zonefile_by_hash_str)
 
-        assert 'error' not in zonefile_by_name, json.dumps(zonefile_by_name, indent=4, sort_keys=True)
         assert 'error' not in zonefile_by_hash, json.dumps(zonefile_by_hash, indent=4, sort_keys=True)
 
         zf1 = None
-        zf2 = None
         try:
-            zf1 = base64.b64decode( zonefile_by_name['zonefiles']['foo.test'] )
-        except:
-            print zonefile_by_name
-            raise
-
-        try:
-            zf2 = base64.b64decode( zonefile_by_hash['zonefiles'][name_rec['value_hash']] )
+            zf1 = base64.b64decode( zonefile_by_hash['zonefiles'][name_rec['value_hash']] )
         except:
             print zonefile_by_hash
             raise
         
-        assert zf1 == zf2
         zonefile = blockstack_zones.parse_zone_file( zf1 )
 
         user_pubkey = blockstack_client.user.user_zonefile_data_pubkey( zonefile )
