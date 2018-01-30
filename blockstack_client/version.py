e1c8fef0702eba1286195fe5ea8d0fa00a8c7f94
Jude Nelson <judecn@gmail.com>
2018-01-29 19:11:16 -0500

    version bump to 0.18

--- blockstack_client/version.py
+++ blockstack_client/version.py
@@ -22,6 +22,6 @@
 
 # this is the only place where version should be updated
 __version_major__ = '0'
-__version_minor__ = '17'
+__version_minor__ = '18'
 __version_patch__ = '0'
-__version__ = '{}.{}.{}.16'.format(__version_major__, __version_minor__, __version_patch__)
+__version__ = '{}.{}.{}.0'.format(__version_major__, __version_minor__, __version_patch__)
