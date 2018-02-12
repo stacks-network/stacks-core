38d8ff40466a7e802a8194d62a0a0617816f2601
Jude Nelson <judecn@gmail.com>
2018-02-12 18:38:54 -0500

    guard db_restore exception

--- blockstack/lib/fast_sync.py
+++ blockstack/lib/fast_sync.py
@@ -637,7 +637,12 @@ def blockstack_backup_restore(working_dir, block_number):
     Raise an exception on error
     """
     db = BlockstackDB.get_readwrite_instance(working_dir, restore=True, restore_block_height=block_number)
-    db.db_restore(block_number=block_number)
-    db.close()
+    try:
+        db.db_restore(block_number=block_number)
+    except Exception as e:
+        raise e
+    finally:
+        db.close()
+
     return True
 
