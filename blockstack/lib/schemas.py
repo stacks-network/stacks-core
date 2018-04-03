66d541041f5ff5ad0e8d42589262ed45d0213740
Jude Nelson <judecn@gmail.com>
2018-04-03 18:01:05 -0400

    fix copyright statement

--- blockstack/lib/schemas.py
+++ blockstack/lib/schemas.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-    Blockstack-client
+    Blockstack
     ~~~~~
     copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
     copyright: (c) 2016-2018 by Blockstack.org
 
-    This file is part of Blockstack-client.
+    This file is part of Blockstack.
 
-    Blockstack-client is free software: you can redistribute it and/or modify
+    Blockstack is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Blockstack-client is distributed in the hope that it will be useful,
+    Blockstack is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with Blockstack-client. If not, see <http://www.gnu.org/licenses/>.
+    along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from .config import LENGTHS, NAME_OPCODES, NAME_TRANSFER, TRANSFER_KEEP_DATA, TRANSFER_REMOVE_DATA, NAME_REGISTRATION
