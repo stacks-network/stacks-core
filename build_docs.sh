ed7d6c32623dbd1ff6a4e15301a488bb35cb7d33
Jude Nelson <judecn@gmail.com>
2018-04-05 17:32:04 -0400

    use blockstack/version.py

--- build_docs.sh
+++ build_docs.sh
@@ -4,7 +4,7 @@ set -e
 COMMAND=$1
 
 if [ "$1" = "public_api" ]; then
-    version=$(python2 -c 'exec(open("blockstack_client/version.py").read()) ; print "{}.{}.{}".format(__version_major__, __version_minor__, __version_patch__)')
+    version=$(python2 -c 'exec(open("blockstack/version.py").read()) ; print "{}.{}.{}".format(__version_major__, __version_minor__, __version_patch__)')
     cp docs/api-specs.md /tmp/
     sed -i "s/###version###/$version/g" /tmp/api-specs.md
     aglio -i /tmp/api-specs.md --theme-template docs/aglio_templates/public.jade -o api/templates/index.html
