30d100f9c717ad2c8dc8db1e3c1bcf4d0479344b
Jude Nelson <judecn@gmail.com>
2018-04-30 10:36:45 -0400

    describe environs

--- bin/blockstack-public-testnet.sh
+++ bin/blockstack-public-testnet.sh
@@ -8,4 +8,13 @@ function exit_error() {
 which blockstack-test-scenario 2>&1 > /dev/null || exit_error 'You must install the integration test suite first'
 which blockstack-core 2>&1 > /dev/null || exit_error 'You must install Blockstack Core first'
 
-BLOCKSTACK_TEST_CLIENT_BIND=0.0.0.0 BLOCKSTACK_PUBLIC_TESTNET=1 BLOCKSTACK_CORE_VERSION="0.0.0.1" blockstack-test-scenario blockstack_integration_tests.scenarios.testnet_public
+# take all inbound requests
+export BLOCKSTACK_TEST_CLIENT_BIND="0.0.0.0"
+
+# activate public testnet features (i.e. make Atlas run at the normal speed)
+export BLOCKSTACK_PUBLIC_TESTNET="1"
+
+# make ourselves look like we're an old Core node, so mainnet nodes don't add us as peers
+export BLOCKSTACK_CORE_VERSION="0.0.0.1"
+
+blockstack-test-scenario blockstack_integration_tests.scenarios.testnet_public
