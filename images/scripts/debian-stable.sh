0b447add17eb949c70d0557e0e6a087a7cb55c58
Jude Nelson <judecn@gmail.com>
2018-08-21 15:43:02 -0400

    remove deprecated scripts

--- images/scripts/debian-stable.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get update && sudo apt-get install -y python-pip python-dev libssl-dev libffi-dev rng-tools libgmp3-dev
-sudo pip install git+https://github.com/blockstack/virtualchain.git --upgrade
-sudo pip install git+https://github.com/blockstack/dns-zone-file-py.git --upgrade
-sudo pip install git+https://github.com/blockstack/blockstack-profiles-py.git --upgrade
-sudo pip install fastecdsa
-sudo pip install git+https://github.com/blockstack/blockstack-core.git --upgrade
