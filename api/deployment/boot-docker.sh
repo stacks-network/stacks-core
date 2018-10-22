d2eea7c060dbcb3c9990064bcb341ecbaf2ef9bc
Jude Nelson <judecn@gmail.com>
2018-10-22 12:03:55 -0400

    update boot-docker.sh script to use latest fast-sync mirror

--- api/deployment/boot-docker.sh
+++ api/deployment/boot-docker.sh
@@ -17,8 +17,7 @@ which blockstack-core >/dev/null || exit_error "blockstack-core not found"
 
 if ! [ -f "$STATE_DIR/blockstack-server.db" ]; then 
    # no state yet
-   # TODO: remove arguments once in master
-   blockstack-core --debug fast_sync http://testnet.blockstack.org/snapshot.bsk 04874e5e95e9da7662de351c967c1edcaccf902536cbb0d4e00e6d22038034628700ae6b360c2cf10c3c834863c9c98f33e9bd07fc0c349e7c14f07f8b0e5a7421
+   blockstack-core --debug fast_sync
    sed -i -e 's/api_host = localhost/api_host = 0.0.0.0/' "$STATE_DIR/blockstack-server.ini"
 fi
 
