2126d0458422fa78189bfbefe6cbf911b591bd09
Jude Nelson <judecn@gmail.com>
2018-10-26 10:22:07 -0400

    sync with new epoch API

--- integration_tests/blockstack_integration_tests/scenarios/name_pre_reg_expire_nextepoch_reregister.py
+++ integration_tests/blockstack_integration_tests/scenarios/name_pre_reg_expire_nextepoch_reregister.py
@@ -96,7 +96,7 @@ def scenario( wallets, **kw ):
         return False
 
     # actually expire
-    for i in xrange(0, 5 * blockstack_server.config.get_epoch_namespace_lifetime_multiplier( testlib.get_current_block(**kw), "test", "BTC" ) - 5 - 3):
+    for i in xrange(0, 5 * blockstack_server.config.get_epoch_namespace_lifetime_multiplier( testlib.get_current_block(**kw), "test") - 5 - 3):
         testlib.next_block( **kw )
 
     # should work
