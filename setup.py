ff86948ed2f720824cd5e6ece6a63aaaf2bf81ff
Jude Nelson <judecn@gmail.com>
2018-10-29 14:56:01 -0400

    version bump on virtualchain

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        'virtualchain>=0.20.0',
+        'virtualchain>=20.0.0.0',
         'keychain>=0.14.2.0',
         'protocoin>=0.2',
         'blockstack-zones>=0.19.0',
