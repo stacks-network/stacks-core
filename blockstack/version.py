3f57145896f9193bc5324eb731a4a15a1b5da7fd
Jude Nelson <judecn@gmail.com>
2018-11-01 17:19:28 -0400

    version bump

--- blockstack/version.py
+++ blockstack/version.py
@@ -2,4 +2,4 @@
 __version_major__ = '20'
 __version_minor__ = '0'
 __version_patch__ = '8'
-__version__ = '{}.{}.{}.6'.format(__version_major__, __version_minor__, __version_patch__)
+__version__ = '{}.{}.{}.7'.format(__version_major__, __version_minor__, __version_patch__)
