6f07533f7f1db9fac8bcc4f2d554d0b2e3083d0c
Jude Nelson <judecn@gmail.com>
2018-11-26 16:27:53 -0500

    beginnings of a thread-to-thread pipeline trait

---
+++ src/util/pipeline.rs
@@ -0,0 +1,34 @@
+/*
+ copyright: (c) 2013-2018 by Blockstack PBC, a public benefit corporation.
+
+ This file is part of Blockstack.
+
+ Blockstack is free software. You may redistribute or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ Blockstack is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY, including without the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use std::fs;
+use std::cmp;
+use std::sync::Arc;
+use std::sync::mpsc::{SyncSender, sync_channel};
+use std::thread;
+use std::thread::JoinHandle;
+
+/*
+pub trait ThreadPipelineStage {
+    pub fn get_input(&self) -> Result<SyncSender, Error>;
+    pub fn get_output(&self) -> Result<
+
+    // pub fn connect<T: ThreadPipelineStage>(&next_stage: T) -> Result<(), &'static str>;
+}
+*/
