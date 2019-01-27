4df9a5eeea8a3950db295ca5ce61343c3f16d364
Jude Nelson <judecn@gmail.com>
2019-01-27 18:14:13 -0500

    flesh out pipeline traits and impls

--- src/util/pipeline.rs
+++ src/util/pipeline.rs
@@ -17,18 +17,128 @@
  along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use std::fs;
-use std::cmp;
+
 use std::sync::Arc;
-use std::sync::mpsc::{SyncSender, sync_channel};
-use std::thread;
-use std::thread::JoinHandle;
+use std::sync::mpsc::{SyncSender, Receiver};
+use std::sync::mpsc::sync_channel;
 
-/*
-pub trait ThreadPipelineStage {
-    pub fn get_input(&self) -> Result<SyncSender, Error>;
-    pub fn get_output(&self) -> Result<
+use util::Error as util_error;
 
-    // pub fn connect<T: ThreadPipelineStage>(&next_stage: T) -> Result<(), &'static str>;
+pub trait PipelineProcessor<I, O, C> 
+where
+    I: Sync + Send,
+    O: Sync + Send
+{
+    fn process(&mut self, item: &I, context: &mut C) -> Result<O, String>;
 }
-*/
+
+pub struct PipelineStage<I, O>
+where
+    I: Sync + Send,
+    O: Sync + Send
+{
+    chan_in: Option<Receiver<Arc<I>>>,
+    chan_out: Option<SyncSender<Arc<O>>>,
+
+    source: Option<Vec<I>>,
+    sink: bool
+}
+
+impl<I, O> PipelineStage<I, O> 
+where
+    I: Sync + Send,
+    O: Sync + Send
+{
+    pub fn new() -> PipelineStage<I, O> {
+        PipelineStage {
+            chan_in: None,
+            chan_out: None,
+            source: None,
+            sink: false
+        }
+    }
+
+    pub fn new_source(inputs: Vec<I>) -> PipelineStage<I, O> {
+        PipelineStage {
+            chan_in: None,
+            chan_out: None,
+            source: Some(inputs.into_iter().rev().collect()),
+            sink: false
+        }
+    }
+
+    pub fn new_sink() -> PipelineStage<I, O> {
+        PipelineStage {
+            chan_in: None,
+            chan_out: None,
+            source: None,
+            sink: true
+        }
+    }
+
+    fn recv(&mut self) -> Result<Arc<I>, util_error> {
+        match self.source {
+            None => {
+                match self.chan_in {
+                    Some(ref mut channel) => {
+                        channel.recv()
+                            .map_err(|e| util_error::RecvError(e))
+                    },
+                    None => {
+                        Err(util_error::ChannelNotConnected)
+                    }
+                }
+            },
+            Some(ref mut inputs) => {
+                let next_opt = inputs.pop();
+                match next_opt {
+                    Some(input) => {
+                        Ok(Arc::new(input))
+                    },
+                    None => {
+                        Err(util_error::ChannelSourceDrained)
+                    }
+                }
+            }
+        }
+    }
+
+    fn send(&mut self, output: O) -> Result<(), util_error> {
+        if !self.sink {
+            match self.chan_out {
+                Some(ref mut channel) => {
+                    channel.send(Arc::new(output))
+                        .map_err(|e| util_error::SendError)
+                },
+                None => Err(util_error::ChannelNotConnected)
+            }
+        }
+        else {
+            // no-op
+            Ok(())
+        }
+    }
+
+    pub fn next<P, C>(&mut self, mut p: P, ctx: &mut C) -> Result<(), util_error>
+    where
+        P: PipelineProcessor<I, O, C>
+    {
+        let arc_i = self.recv()?;
+        let out = p.process(&arc_i, ctx)
+            .map_err(|se| util_error::ProcessError(se))?;
+
+        self.send(out)?;
+        Ok(())
+    }
+
+    pub fn connect<A, T, B>(stage1: &mut PipelineStage<A, T>, stage2: &mut PipelineStage<T, B>, bufsize: usize)
+    where
+        A: Send + Sync,
+        T: Send + Sync,
+        B: Send + Sync
+    {
+        let (sender, receiver) = sync_channel::<Arc<T>>(bufsize);
+        stage1.chan_out = Some(sender);
+        stage2.chan_in = Some(receiver);
+    }
+}
