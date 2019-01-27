c9fe11d61b60419ecd972fdd0ae1bc88431781ea
Jude Nelson <judecn@gmail.com>
2019-01-27 18:15:09 -0500

    add util error types in preparation for pipeline implementations

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -17,10 +17,77 @@
  along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#[macro_use] pub mod macros;
 pub mod hash;
 pub mod log;
-#[macro_use] pub mod macros;
 pub mod pair;
 pub mod pipeline;
 pub mod vrf;
 
+use std::io::Error as io_error;
+use std::sync::mpsc::RecvError as recv_error;
+use std::fmt;
+use std::error;
+
+#[derive(Debug)]
+pub enum Error {
+    /// Not implemented 
+    NotImplemented,
+    /// Failed to start a thread 
+    ThreadStartFailure(io_error),
+    /// Failed to join a thread 
+    ThreadJoinFailure(io_error),
+    /// Failed to receive data 
+    RecvError(recv_error),
+    /// Failed to send data 
+    SendError,
+    /// Channel not connected 
+    ChannelNotConnected,
+    /// Channel source drained 
+    ChannelSourceDrained,
+    /// Pipeline stage error 
+    ProcessError(String)
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::NotImplemented => f.write_str(error::Error::description(self)),
+            Error::ThreadStartFailure(ref e) => fmt::Display::fmt(e, f),
+            Error::ThreadJoinFailure(ref e) => fmt::Display::fmt(e, f),
+            Error::RecvError(ref e) => fmt::Display::fmt(e, f),
+            Error::SendError => f.write_str(error::Error::description(self)),
+            Error::ChannelNotConnected => f.write_str(error::Error::description(self)),
+            Error::ChannelSourceDrained => f.write_str(error::Error::description(self)),
+            Error::ProcessError(ref e) => fmt::Display::fmt(e, f),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::NotImplemented => None,
+            Error::ThreadStartFailure(ref e) => Some(e),
+            Error::ThreadJoinFailure(ref e) => Some(e),
+            Error::RecvError(ref e) => Some(e),
+            Error::SendError => None,
+            Error::ChannelNotConnected => None,
+            Error::ChannelSourceDrained => None,
+            Error::ProcessError(ref e) => None,
+        }
+    }
+
+    fn description(&self) -> &str {
+        match *self {
+            Error::NotImplemented => "Not implemented",
+            Error::ThreadStartFailure(ref e) => e.description(),
+            Error::ThreadJoinFailure(ref e) => e.description(),
+            Error::RecvError(ref e) => e.description(),
+            Error::SendError => "Failed to send",
+            Error::ChannelNotConnected => "Channel not connected",
+            Error::ChannelSourceDrained => "Channel source drained",
+            Error::ProcessError(ref e) => e.as_str()
+        }
+    }
+}
