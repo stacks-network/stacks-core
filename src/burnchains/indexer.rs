78ea465b09d5b57e7037489b43b2d71af27a6490
Jude Nelson <judecn@gmail.com>
2019-01-27 18:20:42 -0500

    extend indexer trait to deal with reorgs and to fetch headers and blocks

--- src/burnchains/indexer.rs
+++ src/burnchains/indexer.rs
@@ -18,10 +18,28 @@
 */
 
 use burnchains::*;
+use burnchains::Error as burnchain_error;
+use burnchains::bitcoin::BlockSender;
+
+use burnchains::BurnchainHeaderHash;
+use burnchains::BurnchainBlock;
+
+use chainstate::burn::db::burndb::BurnDB;
+
+use std::sync::Arc;
+use std::sync::mpsc::SyncSender;
+
+pub type BlockChannel<A, K> = SyncSender<Arc<BurnchainBlock<A, K>>>;
 
 pub trait BurnchainIndexer<A: Address, K: PublicKey> {
-    fn setup(&mut self, working_directory: &str) -> Result<(), &'static str>;
-    fn connect(&mut self, &str) -> Result<(), &'static str>;
-    fn get_block_hash(&mut self, block_height: u64) -> Result<String, &'static str>;
-    fn get_block_txs(&mut self, block_hash: &str) -> Result<Box<Vec<BurnchainTransaction<A, K>>>, &'static str>;
+    fn init(network_name: &String, working_directory: &String) -> Result<Self, burnchain_error>
+        where Self : Sized;
+    fn connect(&mut self) -> Result<(), burnchain_error>;
+    fn get_blockchain_height(&self) -> Result<u64, burnchain_error>;
+    fn get_headers_path(&self) -> String;
+    fn get_headers_height(&self, headers_path: &String) -> Result<u64, burnchain_error>;
+    fn find_chain_reorg(&mut self, headers_path: &String, start_height: u64) -> Result<(u64, Vec<BurnchainHeaderHash>), burnchain_error>;
+    fn sync_headers(&mut self, headers_path: &String, start_height: u64, end_height: u64) -> Result<(), burnchain_error>;
+    fn drop_headers(&mut self, headers_path: &String, new_height: u64) -> Result<(), burnchain_error>;
+    fn sync_blocks(&mut self, headers_path: &String, start_height: u64, end_height: u64, block_channel: &BlockChannel<A, K>) -> Result<(), burnchain_error>;
 }
