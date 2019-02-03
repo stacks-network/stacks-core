54e94ad436ee55fda881219f3dc8d13d323805df
Jude Nelson <judecn@gmail.com>
2019-02-02 23:55:14 -0500

    prototype working main loop

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -21,6 +21,7 @@
 use std::path::PathBuf;
 use std::sync::mpsc::sync_channel;
 use std::thread;
+use std::fs;
 
 use chainstate::burn::db::burndb::BurnDB;
 
@@ -118,6 +119,8 @@ where
     let db_path = db_pathbuf.to_str().unwrap().to_string();
 
     let mut indexer : I = BurnchainIndexer::init(network_name, working_dir)?;
+
+    // careful -- need to use the address and public key types that the indexer uses
     let mut burndb = BurnDB::<<<I as BurnchainIndexer>::P as BurnchainBlockParser>::A, <<I as BurnchainIndexer>::P as BurnchainBlockParser>::K>::connect(&db_path, first_block, first_block_hash, true)
         .map_err(burnchain_error::DBError)?;
 
@@ -137,11 +140,15 @@ where
     }
 
     // get latest headers 
+    let header_height = indexer.get_headers_height(&headers_path)?;
+    
     // TODO: do this atomically -- write to headers_path.new, do the sync, and then merge the files
-    // atomically.
-    indexer.sync_headers(&headers_path, sync_height, end_block)?;
+    // and rename the merged file over the headers file (atomic)
+    debug!("Sync headers from {} - {}", header_height, end_block);
+    indexer.sync_headers(&headers_path, header_height, end_block)?;
 
-    // initial inputs 
+    // initial inputs
+    // TODO: stream this -- don't need to load them all into RAM
     let input_headers = indexer.read_headers(&headers_path, sync_height, end_block)?;
 
     // synchronize 
@@ -154,6 +161,7 @@ where
 
     let download_thread : thread::JoinHandle<Result<(), burnchain_error>> = thread::spawn(move || {
         loop {
+            debug!("Try recv next header");
             let header : <<<I as BurnchainIndexer>::P as BurnchainBlockParser>::D as BurnchainBlockDownloader>::H = downloader_recv.recv()
                 .map_err(|_e| burnchain_error::ThreadChannelError)?;
 
@@ -167,6 +175,7 @@ where
 
     let parse_thread : thread::JoinHandle<Result<(), burnchain_error>> = thread::spawn(move || {
         loop {
+            debug!("Try recv next block");
             let block : <<<I as BurnchainIndexer>::P as BurnchainBlockParser>::D as BurnchainBlockDownloader>::B = parser_recv.recv()
                 .map_err(|_e| burnchain_error::ThreadChannelError)?;
 
@@ -180,6 +189,7 @@ where
 
     let db_thread : thread::JoinHandle<Result<(), burnchain_error>> = thread::spawn(move || {
         loop {
+            debug!("Try recv next parsed block");
             let burnchain_block : BurnchainBlock<<<I as BurnchainIndexer>::P as BurnchainBlockParser>::A, <<I as BurnchainIndexer>::P as BurnchainBlockParser>::K> = db_recv.recv()
                 .map_err(|_e| burnchain_error::ThreadChannelError)?;
 
@@ -196,18 +206,72 @@ where
     }
 
     // join up 
-    download_thread.join();
-    parse_thread.join();
-    db_thread.join();
+    let download_res = download_thread.join();
+    let parser_res = parse_thread.join();
+    let db_res = db_thread.join();
+
+    debug!("Download res: {:?}", download_res);
+    debug!("Parser res: {:?}", parser_res);
+    debug!("DB res: {:?}", db_res);
 
     Ok(end_block)
 }
 
+/// Instantiate Bitcoin-specific state 
+fn setup_burnchain_bitcoin(network_name: &String, working_dir: &String) -> Result<(), burnchain_error> {
+    use burnchains::bitcoin::indexer::BitcoinIndexerConfig;
+    use burnchains::bitcoin::indexer::BitcoinIndexer;
+    
+    // working dir
+    let working_dir_path = PathBuf::from(working_dir);
+    if !working_dir_path.exists() {
+       fs::create_dir_all(&working_dir_path)
+           .map_err(burnchain_error::FSError)?;
+    }
+
+    // bitcoin.ini
+    let mut bitcoin_conf_path = working_dir_path.clone();
+    bitcoin_conf_path.push("bitcoin.ini");
+
+    if !bitcoin_conf_path.exists() {
+        let bitcoin_conf_path_str = bitcoin_conf_path.to_str().unwrap().to_string();
+        let default_config = BitcoinIndexerConfig::default(working_dir);
+        default_config.to_file(&bitcoin_conf_path_str)
+            .map_err(burnchain_error::bitcoin)?;
+    }
+
+    let bitcoin_conf_path_str = bitcoin_conf_path.to_str().unwrap();
+    let mut bitcoin_indexer = BitcoinIndexer::init(network_name, working_dir)?;
+    let spv_headers_pathbuf = PathBuf::from(bitcoin_indexer.config.spv_headers_path.clone());
+    
+    // initial spv headers 
+    if !spv_headers_pathbuf.exists() {
+        // sync headers 
+        let bitcoin_blockchain_height = bitcoin_indexer.get_blockchain_height()?;
+        bitcoin_indexer.sync_all_headers(bitcoin_blockchain_height)
+            .map_err(burnchain_error::bitcoin)?;
+    }
+    
+    Ok(())
+}
+
 /// Synchronize burn transactions from the Bitcoin blockchain 
 pub fn sync_burnchain_bitcoin(network_name: &String, working_dir: &String) -> Result<u64, burnchain_error> {
-    use burnchains::bitcoin::indexer::FIRST_BLOCK_MAINNET;
-    use burnchains::bitcoin::indexer::FIRST_BLOCK_MAINNET_HASH;
     use burnchains::bitcoin::indexer::BitcoinIndexer;
+    use bitcoin::network::serialize::BitcoinHash;
 
-    sync_burnchain::<BitcoinIndexer>(network_name, working_dir, FIRST_BLOCK_MAINNET, &FIRST_BLOCK_MAINNET_HASH)
+    setup_burnchain_bitcoin(network_name, working_dir)?;
+
+    let bitcoin_indexer = BitcoinIndexer::init(network_name, working_dir)?;
+    let first_block_height = bitcoin_indexer.get_first_block_height();
+    let headers_path = bitcoin_indexer.get_headers_path();
+    let initial_headers_list = bitcoin_indexer.read_headers(&headers_path, first_block_height, first_block_height+1)?;
+    
+    if initial_headers_list.len() == 0 {
+        return Err(burnchain_error::MissingHeaders);
+    }
+
+    let first_block_header_hash = &BurnchainHeaderHash::from_bitcoin_hash(&initial_headers_list[0].block_header.header.bitcoin_hash());
+
+    sync_burnchain::<BitcoinIndexer>(network_name, working_dir, first_block_height, first_block_header_hash)
 }
