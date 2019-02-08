388743c40642934c249a3fe1d46af796d5764e5b
Aaron Blankstein <aaron@blockstack.com>
2019-02-08 14:15:16 -0600

    update tests to work with env constructions

--- src/vm/tests/simple_apply_eval.rs
+++ src/vm/tests/simple_apply_eval.rs
@@ -1,4 +1,4 @@
-use vm::eval;
+use vm::{eval, execute};
 use vm::database::MemoryContractDatabase;
 use vm::errors::Error;
 use vm::{Value, Context, Environment};
@@ -25,15 +25,16 @@ fn test_simple_user_function() {
                    SymbolicExpression::Atom("x".to_string())]));
 
     let func_args = vec!["x".to_string()];
-    let user_function = DefinedFunction { body: func_body,
-                                          arguments: func_args };
+    let user_function = DefinedFunction::new(func_args, func_body, false);
 
     let context = Context::new();
-    let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
+    let mut global_context = Context::new();
+    let mut db = MemoryContractDatabase::new();
 
-    env.global_context.variables.insert("a".to_string(), Value::Int(59));
-    env.global_context.functions.insert("do_work".to_string(), user_function);
+    global_context.variables.insert("a".to_string(), Value::Int(59));
+    global_context.functions.insert("do_work".to_string(), user_function);
 
+    let mut env = Environment::new(&global_context, &mut db);
     assert_eq!(Ok(Value::Int(64)), eval(&content[0], &mut env, &context));
 }
 
@@ -56,7 +57,9 @@ fn test_simple_let() {
 
     if let Ok(parsed_program) = parse(&program) {
         let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
+        let global_context = Context::new();
+        let mut db = MemoryContractDatabase::new();
+        let mut env = Environment::new(&global_context, &mut db);
 
         assert_eq!(Ok(Value::Int(7)), eval(&parsed_program[0], &mut env, &context));        
     } else {
@@ -67,25 +70,19 @@ fn test_simple_let() {
 
 #[test]
 fn test_buffer_equality() {
-    let tests = parse(
-        &"(eq? \"a b c\" \"a b c\")
-          (eq? \"\\\" a b d\"
-               \"\\\" a b d\")
-          (not (eq? \"\\\" a b d\"
-                    \" a b d\"))");
+    let tests = [
+        "(eq? \"a b c\" \"a b c\")",
+        "(eq? \"\\\" a b d\"
+               \"\\\" a b d\")",
+        "(not (eq? \"\\\" a b d\"
+                    \" a b d\"))"];
     let expectations = [
         Value::Bool(true),
         Value::Bool(true),
         Value::Bool(true)];
 
-    if let Ok(to_eval) = tests {
-        let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
-        to_eval.iter().zip(expectations.iter())
-            .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), eval(program, &mut env, &context)));
-    } else {
-        assert!(false, "Failed to parse function bodies.");
-    }
+    tests.iter().zip(expectations.iter())
+        .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), execute(program)));
 }
 
 #[test]
@@ -109,15 +106,17 @@ fn test_simple_if_functions() {
     if let Ok(parsed_bodies) = function_bodies {
         let func_args1 = vec!["x".to_string()];
         let func_args2 = vec!["x".to_string()];
-        let user_function1 = DefinedFunction { body: parsed_bodies[0].clone(),
-                                               arguments: func_args1 };
-        let user_function2 = DefinedFunction { body: parsed_bodies[1].clone(),
-                                               arguments: func_args2 };
-        let mut context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
+        let user_function1 = DefinedFunction::new(func_args1, parsed_bodies[0].clone(), false);
+        let user_function2 = DefinedFunction::new(func_args2, parsed_bodies[1].clone(), false);
 
-        env.global_context.functions.insert("with_else".to_string(), user_function1);
-        env.global_context.functions.insert("without_else".to_string(), user_function2);
+        let context = Context::new();
+        let mut global_context = Context::new();
+        let mut db = MemoryContractDatabase::new();
+
+        global_context.functions.insert("with_else".to_string(), user_function1);
+        global_context.functions.insert("without_else".to_string(), user_function2);
+
+        let mut env = Environment::new(&global_context, &mut db);
 
         if let Ok(tests) = evals {
             assert_eq!(Ok(Value::Int(1)), eval(&tests[0], &mut env, &context));
@@ -133,25 +132,24 @@ fn test_simple_if_functions() {
 
 #[test]
 fn test_simple_arithmetic_functions() {
-    let tests = parse(&
-        "(* 52314 414)
-         (/ 52314 414)
-         (* 2 3 4 5)
-         (/ 10 13)
-         (mod 51 2)
-         (- 5 4 1)
-         (+ 5 4 1)
-         (eq? (* 2 3)
-              (+ 2 2 2))
-         (> 1 2)
-         (< 1 2)
-         (<= 1 1)
-         (>= 2 1)
-         (>= 1 1)
-         (pow 2 16)
-         (pow 2 32)
-         (- (pow 2 32))
-");
+    let tests = [
+        "(* 52314 414)",
+         "(/ 52314 414)",
+         "(* 2 3 4 5)",
+         "(/ 10 13)",
+         "(mod 51 2)",
+         "(- 5 4 1)",
+         "(+ 5 4 1)",
+         "(eq? (* 2 3)
+              (+ 2 2 2))",
+         "(> 1 2)",
+         "(< 1 2)",
+         "(<= 1 1)",
+         "(>= 2 1)",
+         "(>= 1 1)",
+         "(pow 2 16)",
+         "(pow 2 32)",
+         "(- (pow 2 32))"];
 
     let expectations = [
         Value::Int(21657996),
@@ -170,33 +168,29 @@ fn test_simple_arithmetic_functions() {
         Value::Int(65536),
         Value::Int(u32::max_value() as i128 + 1),
         Value::Int(-1 * (u32::max_value() as i128 + 1)),
-];
+    ];
 
-    if let Ok(to_eval) = tests {
-        let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
-        to_eval.iter().zip(expectations.iter())
-            .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), eval(program, &mut env, &context)));
-    } else {
-        assert!(false, "Failed to parse function bodies.");
-    }
+    tests.iter().zip(expectations.iter())
+        .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), execute(program)));
 }
 
 #[test]
 fn test_arithmetic_errors() {
-    let tests = parse(&
-        "(>= 1)
-         (+ 1 'true)
-         (/ 10 0)
-         (mod 10 0)
-         (pow 2 128)
-         (* 10 (pow 2 126))
-         (+ (pow 2 126) (pow 2 126))
-         (- 0 (pow 2 126) (pow 2 126) 1)
-         (-) (/) (mod 1) (pow 1)
-         (pow 2 (pow 2 32))
-         (pow 2 (- 1))
-");
+    let tests = [
+        "(>= 1)",
+        "(+ 1 'true)",
+        "(/ 10 0)",
+        "(mod 10 0)",
+        "(pow 2 128)",
+        "(* 10 (pow 2 126))",
+        "(+ (pow 2 126) (pow 2 126))",
+        "(- 0 (pow 2 126) (pow 2 126) 1)",
+        "(-)",
+        "(/)",
+        "(mod 1)",
+        "(pow 1)",
+         "(pow 2 (pow 2 32))",
+         "(pow 2 (- 1))"];
 
     let expectations = [
         Err(Error::InvalidArguments("Binary comparison must be called with exactly 2 arguments".to_string())),
@@ -215,27 +209,21 @@ fn test_arithmetic_errors() {
         Err(Error::Arithmetic("Power argument to (pow ...) must be a u32 integer".to_string()))
     ];
 
-    if let Ok(to_eval) = tests {
-        let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
-        for (program, expectation) in to_eval.iter().zip(expectations.iter()) {
-            assert_eq!(*expectation, eval(program, &mut env, &context));
-        }
-    } else {
-        assert!(false, "Failed to parse function bodies.");
+    for (program, expectation) in tests.iter().zip(expectations.iter()) {
+        assert_eq!(*expectation, execute(program));
     }
 }
 
 #[test]
 fn test_bool_functions() {
-    let tests = parse(&
-        "'true
-         (and 'true 'true 'true)
-         (and 'false 'true 'true)
-         (and 'false (> 1 (/ 10 0)))
-         (or 'true (> 1 (/ 10 0)))
-         (or 'false 'false 'false)
-         (not 'true)");
+    let tests = [
+        "'true",
+         "(and 'true 'true 'true)",
+         "(and 'false 'true 'true)",
+         "(and 'false (> 1 (/ 10 0)))",
+         "(or 'true (> 1 (/ 10 0)))",
+         "(or 'false 'false 'false)",
+         "(not 'true)"];
 
     let expectations = [
         Value::Bool(true),
@@ -246,34 +234,22 @@ fn test_bool_functions() {
         Value::Bool(false),
         Value::Bool(false)];
 
-    if let Ok(to_eval) = tests {
-        let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
-        to_eval.iter().zip(expectations.iter())
-            .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), eval(program, &mut env, &context)));
-    } else {
-        assert!(false, "Failed to parse function bodies.");
-    }
+    tests.iter().zip(expectations.iter())
+        .for_each(|(program, expectation)| assert_eq!(Ok(expectation.clone()), execute(program)));
 }
 
 #[test]
 fn test_bad_lets() {
-    let tests = parse(&
-        "(let ((tx-sender 1)) (+ tx-sender tx-sender))
-         (let ((* 1)) (+ * *))
-         (let ((a 1) (a 2)) (+ a a))");
+    let tests = [
+        "(let ((tx-sender 1)) (+ tx-sender tx-sender))",
+        "(let ((* 1)) (+ * *))",
+        "(let ((a 1) (a 2)) (+ a a))"];
 
     let expectations: &[Result<Value, Error>] = &[
         Err(Error::ReservedName("tx-sender".to_string())),
         Err(Error::ReservedName("*".to_string())),
         Err(Error::MultiplyDefined("a".to_string()))];
 
-    if let Ok(to_eval) = tests {
-        let context = Context::new();
-        let mut env = Environment::new(Box::new(MemoryContractDatabase::new()));
-        to_eval.iter().zip(expectations.iter())
-            .for_each(|(program, expectation)| assert_eq!(*expectation, eval(program, &mut env, &context)));
-    } else {
-        assert!(false, "Failed to parse function bodies.");
-    }
+    tests.iter().zip(expectations.iter())
+        .for_each(|(program, expectation)| assert_eq!(*expectation, execute(program)));
 }
