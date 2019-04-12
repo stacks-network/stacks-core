7432d225e070c9cd021b6e60fe2efca5db2dc533
Jude Nelson <judecn@gmail.com>
2019-04-12 17:19:56 -0400

    mio-based poll loop for reading/writing sockets

---
+++ src/net/poll.rs
@@ -0,0 +1,158 @@
+/*
+ copyright: (c) 2013-2019 by Blockstack PBC, a public benefit corporation.
+
+ This file is part of Blockstack.
+
+ Blockstack is free software. You may redistribute or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ Blockstack is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY, including without the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use net::PeerAddress;
+use net::Neighbor;
+use net::NeighborKey;
+use net::Error as net_error;
+
+use util::db::Error as db_error;
+use util::db::DBConn;
+
+use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::time::Duration;
+use std::io::Read;
+use std::io::Write;
+
+use util::log;
+
+use mio;
+use mio::net as mio_net;
+use mio::Ready;
+use mio::Token;
+use mio::PollOpt;
+
+pub const NUM_NEIGHBORS : u32 = 32;
+
+const SERVER : Token = mio::Token(0);
+
+pub struct NetworkPollState {
+    pub new: HashMap<usize, mio_net::TcpStream>,
+    pub ready: Vec<usize>
+}
+
+impl NetworkPollState {
+    pub fn new() -> NetworkPollState {
+        NetworkPollState {
+            new: HashMap::new(),
+            ready: vec![]
+        }
+    }
+}
+
+pub struct NetworkState {
+    poll: mio::Poll,
+    server: mio_net::TcpListener,
+    events: mio::Events,
+    count: usize,
+}
+
+impl NetworkState {
+    pub fn bind(addr: &SocketAddr, capacity: usize) -> Result<NetworkState, net_error> {
+        let server = mio_net::TcpListener::bind(addr)
+            .map_err(|e| {
+                error!("Failed to bind to {:?}: {:?}", addr, e);
+                net_error::BindError
+            })?;
+
+        let poll = mio::Poll::new()
+            .map_err(|e| {
+                error!("Failed to initialize poller: {:?}", e);
+                net_error::BindError
+            })?;
+
+        let events = mio::Events::with_capacity(capacity);
+
+        poll.register(&server, SERVER, mio::Ready::readable(), mio::PollOpt::edge())
+            .map_err(|e| {
+                error!("Failed to register server socket: {:?}", &e);
+                net_error::BindError
+            })?;
+
+        Ok(NetworkState {
+            poll: poll,
+            server: server,
+            events: events,
+            count: 1
+        })
+    }
+
+    /// next event ID
+    pub fn next_event_id(&mut self) -> usize {
+        let ret = self.count;
+        self.count += 1;
+        ret
+    }
+
+    /// Register a socket for read/write notifications with this poller
+    pub fn register(&mut self, event_id: usize, sock: &mio_net::TcpStream) -> Result<(), net_error> {
+        self.poll.register(sock, mio::Token(event_id), Ready::all(), PollOpt::edge())
+            .map_err(|e| {
+                error!("Failed to register socket: {:?}", &e);
+                net_error::RegisterError
+            })
+    }
+
+    /// Deregister a socket event
+    pub fn deregister(&mut self, sock: &mio_net::TcpStream) -> Result<(), net_error> {
+        self.poll.deregister(sock)
+            .map_err(|e| {
+                error!("Failed to deregister socket: {:?}", &e);
+                net_error::RegisterError
+            })
+    }
+
+    /// Connect to a remote peer, but don't register it with the poll handle.
+    pub fn connect(&mut self, addr: &SocketAddr) -> Result<mio_net::TcpStream, net_error> {
+        mio_net::TcpStream::connect(addr)
+            .map_err(|e| net_error::ConnectionError)
+    }
+
+    /// Poll socket states
+    pub fn poll(&mut self, timeout: u64) -> Result<NetworkPollState, net_error> {
+        self.poll.poll(&mut self.events, Some(Duration::from_millis(timeout)))
+            .map_err(|e| {
+                error!("Failed to poll: {:?}", &e);
+                net_error::PollError
+            })?;
+       
+        let mut poll_state = NetworkPollState::new();
+        for event in self.events.iter() {
+            match event.token() {
+                SERVER => {
+                    // new inbound connection 
+                    let (client_sock, client_addr) = match self.server.accept() {
+                        Ok((client_sock, client_addr)) => (client_sock, client_addr),
+                        Err(e) => return Err(net_error::AcceptError)
+                    };
+
+                    self.count += 1;
+                    poll_state.new.insert(self.count, client_sock);
+                },
+                mio::Token(event_id) => {
+                    // I/O available 
+                    poll_state.ready.push(event_id);
+                }
+            }
+        }
+
+        Ok(poll_state)
+    }
+}
