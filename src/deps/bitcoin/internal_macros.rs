b4b059810c320d1a09cf6b9cd6b9bbe70da0e649
Jude Nelson <judecn@gmail.com>
2019-04-12 17:15:57 -0400

    remove dead macro code

--- src/deps/bitcoin/internal_macros.rs
+++ src/deps/bitcoin/internal_macros.rs
@@ -53,70 +53,6 @@ macro_rules! impl_newtype_consensus_encoding {
     );
 }
 
-macro_rules! nu_select {
-    ($($name:pat = $rx:expr => $code:expr),+) => ({
-        nu_select!{ $($name = $rx, recv => $code),+ }
-    });
-    ($($name:pat = $rx:expr, $meth:ident => $code:expr),+) => ({
-        use rustrt::local::Local;
-        use rustrt::task::Task;
-        use sync::comm::Packet;
-
-        // Is anything already ready to receive? Grab it without waiting.
-        $(
-            if (&$rx as &Packet).can_recv() {
-                let $name = $rx.$meth();
-                $code
-            }
-        )else+
-        else {
-            // Start selecting on as many as we need to before getting a bite.
-            // Keep count of how many, since we need to abort every selection
-            // that we started.
-            let mut started_count = 0;
-            // Restrict lifetime of borrows in `packets`
-            {
-                let packets = [ $( &$rx as &Packet, )+ ];
-
-                let task: Box<Task> = Local::take();
-                task.deschedule(packets.len(), |task| {
-                    match packets[started_count].start_selection(task) {
-                        Ok(()) => {
-                            started_count += 1;
-                            Ok(())
-                        }
-                        Err(task) => Err(task)
-                    }
-                });
-            }
-
-            let mut i = 0;
-            let ret = $(
-                // Abort the receivers, stopping at the first ready one to get its data.
-                if { i += 1; i <= started_count } &&
-                     // If start_selection() failed, abort_selection() will fail too,
-                     // but it still counts as "data available".
-                     ($rx.abort_selection() || i == started_count) {
-                    // React to the first
-                    let $name = $rx.$meth();
-                    $code
-                })else+
-                else {
-                    fail!("we didn't find the ready receiver, but we should have had one");
-                };
-            // At this point, the first i receivers have been aborted. We need to abort the rest:
-            $(if i > 0 {
-                i -= 1;
-            } else {
-                $rx.abort_selection();
-            })+
-            let _ = i; // Shut up `i -= 1 but i is never read` warning
-            // Return
-            ret
-        }
-    })
-}
-
 macro_rules! user_enum {
     (
         $(#[$attr:meta])*
@@ -221,57 +157,6 @@ macro_rules! user_enum {
     );
 }
 
-macro_rules! impl_array_newtype_encodable {
-    ($thing:ident, $ty:ty, $len:expr) => {
-        #[cfg(feature = "serde")]
-        impl<'de> $crate::serde::Deserialize<'de> for $thing {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-            where
-                D: $crate::serde::Deserializer<'de>,
-            {
-                use $crate::std::fmt::{self, Formatter};
-
-                struct Visitor;
-                impl<'de> $crate::serde::de::Visitor<'de> for Visitor {
-                    type Value = $thing;
-
-                    fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-                        formatter.write_str("a fixed size array")
-                    }
-
-                    #[inline]
-                    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-                    where
-                        A: $crate::serde::de::SeqAccess<'de>,
-                    {
-                        let mut ret: [$ty; $len] = [0; $len];
-                        for item in ret.iter_mut() {
-                            *item = match seq.next_element()? {
-                                Some(c) => c,
-                                None => return Err($crate::serde::de::Error::custom("end of stream"))
-                            };
-                        }
-                        Ok($thing(ret))
-                    }
-                }
-
-                deserializer.deserialize_seq(Visitor)
-            }
-        }
-
-        #[cfg(feature = "serde")]
-        impl $crate::serde::Serialize for $thing {
-            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-            where
-                S: $crate::serde::Serializer,
-            {
-                let &$thing(ref dat) = self;
-                (&dat[..]).serialize(serializer)
-            }
-        }
-    }
-}
-
 macro_rules! impl_index_newtype {
     ($thing:ident, $ty:ty) => {
         impl ::std::ops::Index<::std::ops::Range<usize>> for $thing {
@@ -325,9 +210,6 @@ macro_rules! display_from_debug {
 
 #[cfg(test)]
 macro_rules! hex_script (($s:expr) => (::deps::bitcoin::blockdata::script::Script::from(::util::hash::hex_bytes($s).unwrap())));
-
-#[cfg(test)]
-macro_rules! hex_hash (($s:expr) => (::deps::bitcoin::util::hash::Sha256dHash::from(&::util::hash::hex_bytes($s).unwrap()[..])));
 
 macro_rules! serde_struct_impl {
     ($name:ident, $($fe:ident),*) => (
