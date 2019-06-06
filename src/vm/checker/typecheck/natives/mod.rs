55cf56440c6effabe6cee4602e10bb5ac5670876
Aaron Blankstein <aaron@blockstack.com>
2019-06-06 14:47:39 -0500

    implement filter function

--- src/vm/checker/typecheck/natives/mod.rs
+++ src/vm/checker/typecheck/natives/mod.rs
@@ -5,6 +5,7 @@ use vm::types::{TypeSignature, AtomTypeIdentifier, TupleTypeSignature, BlockInfo
 use super::{TypeChecker, TypingContext, TypeResult, FunctionType, no_type, check_atomic_type}; 
 use vm::checker::errors::{CheckError, CheckErrors, CheckResult};
 
+mod lists;
 mod maps;
 mod options;
 
@@ -143,82 +144,6 @@ fn check_special_tuple_cons(checker: &mut TypeChecker, args: &[SymbolicExpressio
         AtomTypeIdentifier::TupleType(tuple_signature)))
 }
 
-fn get_simple_native_or_user_define(function_name: &str, checker: &TypeChecker) -> CheckResult<FunctionType> {
-    if let Some(ref native_function) = NativeFunctions::lookup_by_name(function_name) {
-        if let TypedNativeFunction::Simple(SimpleNativeFunction(function_type)) = TypedNativeFunction::type_native_function(native_function) {
-            Ok(function_type)
-        } else {
-            Err(CheckError::new(CheckErrors::IllegalOrUnknownFunctionApplication(function_name.to_string())))
-        }
-    } else {
-        checker.get_function_type(function_name)
-            .ok_or(CheckError::new(CheckErrors::IllegalOrUnknownFunctionApplication(function_name.to_string())))
-    }
-}
-
-fn check_special_map(checker: &mut TypeChecker, args: &[SymbolicExpression], context: &TypingContext) -> TypeResult {
-    if args.len() != 2 {
-        return Err(CheckError::new(CheckErrors::IncorrectArgumentCount(2, args.len())))
-    }
-    
-    let function_name = args[0].match_atom()
-        .ok_or(CheckError::new(CheckErrors::NonFunctionApplication))?;
-    // we will only lookup native or defined functions here.
-    //   you _cannot_ map a special function.
-    let function_type = get_simple_native_or_user_define(function_name, checker)?;
-    
-    checker.type_map.set_type(&args[0], no_type())?;
-    
-    let argument_type = checker.type_check(&args[1], context)?;
-    
-    let argument_length = argument_type.list_max_len()
-        .ok_or(CheckError::new(CheckErrors::ExpectedListApplication))?;
-    
-    let argument_items_type = argument_type.get_list_item_type()
-        .ok_or(CheckError::new(CheckErrors::ExpectedListApplication))?;
-    
-    function_type.check_args(&[argument_items_type])?;
-    
-    let mapped_type = function_type.return_type();
-    
-    TypeSignature::list_of(mapped_type, argument_length)
-        .map_err(|_| CheckError::new(CheckErrors::ConstructedListTooLarge))
-}
-
-fn check_special_fold(checker: &mut TypeChecker, args: &[SymbolicExpression], context: &TypingContext) -> TypeResult {
-    if args.len() != 3 {
-        return Err(CheckError::new(CheckErrors::IncorrectArgumentCount(3, args.len())))
-    }
-    
-    let function_name = args[0].match_atom()
-        .ok_or(CheckError::new(CheckErrors::NonFunctionApplication))?;
-    // we will only lookup native or defined functions here.
-    //   you _cannot_ fold a special function.
-    let function_type = get_simple_native_or_user_define(function_name, checker)?;
-    
-    checker.type_map.set_type(&args[0], no_type())?;
-    
-    let list_argument_type = checker.type_check(&args[1], context)?;
-
-    let list_items_type = list_argument_type.get_list_item_type()
-        .ok_or(CheckError::new(CheckErrors::ExpectedListApplication))?;
-        
-    let initial_value_type = checker.type_check(&args[2], context)?;
-    let return_type = function_type.return_type();
-
-    // fold: f(A, B) -> A
-    //     where A = initial_value_type
-    //           B = list items type
-    
-    // f must accept the initial value and the list items type
-    function_type.check_args(&[initial_value_type.clone(), list_items_type.clone()])?;
-    // f must _also_ accepts its own return type!
-    function_type.check_args(&[return_type.clone(), list_items_type.clone()])?;
-    // TODO: those clones _should_ be removed.
-    
-    Ok(return_type)
-}
-
 fn check_special_let(checker: &mut TypeChecker, args: &[SymbolicExpression], context: &TypingContext) -> TypeResult {
     if args.len() != 2 {
         return Err(CheckError::new(CheckErrors::IncorrectArgumentCount(2, args.len())))
@@ -337,8 +262,9 @@ impl TypedNativeFunction {
                                                                    TypeSignature::new_atom( AtomTypeIdentifier::BoolType )))),
             If => Special(SpecialNativeFunction(&check_special_if)),
             Let => Special(SpecialNativeFunction(&check_special_let)),
-            Map => Special(SpecialNativeFunction(&check_special_map)),
-            Fold => Special(SpecialNativeFunction(&check_special_fold)),
+            Map => Special(SpecialNativeFunction(&lists::check_special_map)),
+            Filter => Special(SpecialNativeFunction(&lists::check_special_filter)),
+            Fold => Special(SpecialNativeFunction(&lists::check_special_fold)),
             ListCons => Special(SpecialNativeFunction(&check_special_list_cons)),
             FetchEntry => Special(SpecialNativeFunction(&maps::check_special_fetch_entry)),
             FetchContractEntry => Special(SpecialNativeFunction(&maps::check_special_fetch_contract_entry)),
