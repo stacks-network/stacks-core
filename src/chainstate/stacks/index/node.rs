7113b3d9607412931ac5ecfe2703b26da0df2f69
Jude Nelson <judecn@gmail.com>
2019-08-04 00:07:58 -0400

    Modify the TrieCursor structure to return an error indicating why it couldn't finish walking to the next node (call this a CursorError).  Use this for following backptrs and COW'ing nodes instead of trying to deduce what happened.

--- src/chainstate/stacks/index/node.rs
+++ src/chainstate/stacks/index/node.rs
@@ -61,6 +61,37 @@ use chainstate::stacks::index::Error as Error;
 use util::hash::to_hex;
 use util::log;
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum CursorError {
+    PathDiverged,
+    BackptrEncountered(TriePtr),
+    ChrNotFound
+}
+
+impl fmt::Display for CursorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CursorError::PathDiverged => f.write_str(error::Error::description(self)),
+            CursorError::BackptrEncountered(_) => f.write_str(error::Error::description(self)),
+            CursorError::ChrNotFound => f.write_str(error::Error::description(self)),
+        }
+    }
+}
+
+impl error::Error for CursorError {
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        match *self {
+            CursorError::PathDiverged => "Path diverged",
+            CursorError::BackptrEncountered(_) => "Back-pointer encountered",
+            CursorError::ChrNotFound => "Node child not found",
+        }
+    }
+}
+
 /// All numeric values of a Trie node when encoded.
 /// They are all 7-bit numbers -- the 8th bit is used to indicate whether or not the value
 /// identifies a back-pointer to be followed.
@@ -297,19 +328,19 @@ pub struct TrieCursor {
     pub nodes: Vec<TrieNodeType>,   // list of nodes this cursor visits
     pub node_ptrs: Vec<TriePtr>,    // list of ptr branches this cursor has taken
     pub block_hashes: Vec<BlockHeaderHash>,  // list of Tries we've visited.  block_hashes[i] corresponds to node_ptrs[i]
-    pub diverged: bool              // flag as to whether or not the path we're walking differs from a node's compressed path
+    pub last_error: Option<CursorError>,    // last error encountered while walking (used to make sure the client calls the right "recovery" method)
 }
 
 impl TrieCursor {
-    pub fn new(path: &TriePath, root_ptr: u32) -> TrieCursor {
+    pub fn new(path: &TriePath, root_ptr: TriePtr) -> TrieCursor {
         TrieCursor {
             path: path.clone(),
             index: 0,
             node_path_index: 0,
             nodes: vec![],
-            node_ptrs: vec![TriePtr::new(TrieNodeID::Node256, 0, root_ptr)],
+            node_ptrs: vec![root_ptr],
             block_hashes: vec![],
-            diverged: false
+            last_error: None
         }
     }
 
@@ -339,11 +370,6 @@ impl TrieCursor {
         self.index == self.path.len()
     }
 
-    /// Did we reach a node where we could not walk any further?
-    pub fn div(&self) -> bool {
-        self.diverged
-    }
-
     /// last ptr visited
     pub fn ptr(&self) -> TriePtr {
         // should always be true by construction
@@ -370,35 +396,27 @@ impl TrieCursor {
             TrieNodeType::Node256(ref data) => self.node_path_index == data.path.len(),
         }
     }
-
-    /// Replace the last-visited node and ptr within this trie.  Used when doing a copy-on-write or
-    /// promoting a node, so the cursor state accurately reflects the nodes and tries visited.
-    pub fn retarget(&mut self, node: &TrieNodeType, ptr: &TriePtr, hash: &BlockHeaderHash) -> () {
-        self.nodes.pop();
-        self.node_ptrs.pop();
-        self.block_hashes.pop();
-
-        self.nodes.push(node.clone());
-        self.node_ptrs.push(ptr.clone());
-        self.block_hashes.push(hash.clone());
-    }
-
+    
     /// Walk to the next node, following its compressed path as far as we can and then walking to
     /// its child pointer.  If we successfully follow the path, then return the pointer we reached.
-    /// Otherwise return None, and possibly set the 'diverged' flag (if applicable).
+    /// Otherwise, if we reach the end of the path, return None.  If the path diverges or a node
+    /// cannot be found, then return an Err.
     ///
-    /// This method does not follow back-pointers, and will return None if a back-pointer is
+    /// This method does not follow back-pointers, and will return Err if a back-pointer is
     /// reached.  The caller will need to manually call walk() on the last node visited to get the
     /// back-pointer, shunt to the node it points to, and then call walk_backptr_step_backptr() to
     /// record the back-pointer that was followed.  Once the back-pointer has been followed,
     /// caller should call walk_backptr_step_finish().  This is specifically relevant to the MARF,
     /// not to the individual tries.
-    pub fn walk(&mut self, node: &TrieNodeType, block_hash: &BlockHeaderHash) -> Option<TriePtr> {
+    pub fn walk(&mut self, node: &TrieNodeType, block_hash: &BlockHeaderHash) -> Result<Option<TriePtr>, CursorError> {
+        // can only be called if we called the appropriate "repair" method or if there is no error
+        assert!(self.last_error.is_none());
+
         trace!("cursor: walk: node = {:?} block = {:?}", node, block_hash);
 
         if self.index >= self.path.len() {
             trace!("cursor: out of path");
-            return None;
+            return Ok(None);
         }
 
         let node_path = match node {
@@ -422,8 +440,8 @@ impl TrieCursor {
             if node_path[i] != path_bytes[self.index] {
                 // diverged
                 trace!("cursor: diverged({:?} != {:?}): i = {}, self.index = {}, self.node_path_index = {}", &node_path, &path_bytes, i, self.index, self.node_path_index);
-                self.diverged = true;
-                return None;
+                self.last_error = Some(CursorError::PathDiverged);
+                return Err(CursorError::PathDiverged);
             }
             self.index += 1;
             self.node_path_index += 1;
@@ -446,17 +464,20 @@ impl TrieCursor {
                 Some(ref ptr) => {
                     if !is_backptr(ptr.id()) {
                         // not going to follow a back-pointer
-                        self.walk_backptr_finish(&ptr, block_hash);
+                        self.node_ptrs.push(ptr.clone());
+                        self.block_hashes.push(block_hash.clone());
                         true
                     }
                     else {
                         // the caller will need to follow the backptr, and call
-                        // walk_backptr_step_backptr() for each node visited, and then walk_backptr_finish()
+                        // repair_backptr_step_backptr() for each node visited, and then repair_backptr_finish()
                         // once the final ptr and block_hash are discovered.
+                        self.last_error = Some(CursorError::BackptrEncountered(ptr.clone()));
                         false
                     }
                 },
                 None => {
+                    self.last_error = Some(CursorError::ChrNotFound);
                     false
                 }
             };
@@ -466,38 +487,79 @@ impl TrieCursor {
             }
 
             if ptr_opt.is_none() {
+                assert!(self.last_error.is_some());
+
                 trace!("cursor: not found: chr = 0x{:02x}, self.index = {}, self.path = {:?}", chr, self.index-1, &path_bytes);
+                return Err(self.last_error.clone().unwrap());
             }
-            ptr_opt
+            else {
+                return Ok(ptr_opt);
+            }
         }
         else {
             trace!("cursor: now out of path");
-            None
+            return Ok(None);
         }
+    }
+
+    /// Replace the last-visited node and ptr within this trie.  Used when doing a copy-on-write or
+    /// promoting a node, so the cursor state accurately reflects the nodes and tries visited.
+    pub fn repair_retarget(&mut self, node: &TrieNodeType, ptr: &TriePtr, hash: &BlockHeaderHash) -> () {
+        // this can only be called if we failed to walk to a node (this method _should not_ be
+        // called if we walked to a backptr).
+        if Some(CursorError::ChrNotFound) != self.last_error && Some(CursorError::PathDiverged) != self.last_error {
+            eprintln!("{:?}", &self.last_error);
+            panic!();
+        }
+
+        self.nodes.pop();
+        self.node_ptrs.pop();
+        self.block_hashes.pop();
+
+        self.nodes.push(node.clone());
+        self.node_ptrs.push(ptr.clone());
+        self.block_hashes.push(hash.clone());
+
+        self.last_error = None;
     }
 
     /// Record that a node was walked to by way of a back-pointer.
     /// next_node should be the node walked to.
     /// ptr is the ptr we'll be walking from, off of next_node.
     /// block_hash is the block where next_node came from.
-    pub fn walk_backptr_step_backptr(&mut self, next_node: &TrieNodeType, ptr: &TriePtr, block_hash: &BlockHeaderHash) -> () {
+    pub fn repair_backptr_step_backptr(&mut self, next_node: &TrieNodeType, ptr: &TriePtr, block_hash: &BlockHeaderHash) -> () {
+        // this can only be called if we walked to a backptr.
+        // If it's anything else, we're in trouble.
+        if Some(CursorError::ChrNotFound) == self.last_error || Some(CursorError::PathDiverged) == self.last_error {
+            eprintln!("{:?}", &self.last_error);
+            panic!();
+        }
+
         let backptr = TriePtr::new(set_backptr(ptr.id()), ptr.chr(), ptr.ptr());        // set_backptr() informs update_root_hash() to skip this node
         self.node_ptrs.push(backptr);
         self.block_hashes.push(block_hash.clone());
         
         self.nodes.push(next_node.clone());
-        trace!("Cursor: walk_backptr_step_backptr ptr={:?} block_hash={:?} next_node={:?}", ptr, block_hash, next_node);
+        trace!("Cursor: repair_backptr_step_backptr ptr={:?} block_hash={:?} next_node={:?}", ptr, block_hash, next_node);
     }
     
     /// Record that we landed on a non-backptr from a backptr.
     /// ptr is a non-backptr that refers to the node we landed on.
-    pub fn walk_backptr_finish(&mut self, ptr: &TriePtr, block_hash: &BlockHeaderHash) -> () {
+    pub fn repair_backptr_finish(&mut self, ptr: &TriePtr, block_hash: &BlockHeaderHash) -> () {
+        // this can only be called if we walked to a backptr.
+        // If it's anything else, we're in trouble.
+        if Some(CursorError::ChrNotFound) == self.last_error || Some(CursorError::PathDiverged) == self.last_error {
+            eprintln!("{:?}", &self.last_error);
+            panic!();
+        }
         assert!(!is_backptr(ptr.id()));
 
         self.node_ptrs.push(ptr.clone());
         self.block_hashes.push(block_hash.clone());
         
-        trace!("Cursor: walk_backptr_finish ptr={:?} block_hash={:?}", ptr, block_hash);
+        self.last_error = None;
+
+        trace!("Cursor: repair_backptr_finish ptr={:?} block_hash={:?}", ptr, block_hash);
     }
 }
 
@@ -2046,7 +2108,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..31 {
@@ -2130,7 +2192,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..15 {
@@ -2209,7 +2271,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..10 {
@@ -2285,7 +2347,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..7 {
@@ -2360,7 +2422,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..6 {
@@ -2434,7 +2496,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..5 {
@@ -2507,7 +2569,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..4 {
@@ -2578,7 +2640,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..2 {
@@ -2648,7 +2710,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let mut walk_point = nodes[0].clone();
 
         for i in 0..1 {
@@ -2717,7 +2779,7 @@ mod test {
         assert_eq!(node_ptrs[node_ptrs.len()-1].id, TrieNodeID::Leaf);
 
         // walk down the trie
-        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_ptr());
+        let mut c = TrieCursor::new(&TriePath::from_bytes(&path).unwrap(), trie_io.root_trieptr());
         let walk_point = nodes[0].clone();
 
         // walk to the leaf
