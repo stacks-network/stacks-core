3c3f6cc044d1b77e2a8e82947ecc5b39f246d520
Jude Nelson <judecn@gmail.com>
2019-08-20 17:58:38 -0400

    update the block codecs to be in sync with sip 005

--- src/chainstate/stacks/block.rs
+++ src/chainstate/stacks/block.rs
@@ -17,15 +17,9 @@
  along with Blockstack. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use chainstate::stacks::TransactionAnchorMode;
-use chainstate::stacks::TransactionPayloadID;
-use chainstate::stacks::StacksBlockHeader;
-use chainstate::stacks::StacksBlock;
-use chainstate::stacks::StacksMicroblockHeader;
-use chainstate::stacks::StacksMicroblock;
-use chainstate::stacks::StacksTransaction;
-use chainstate::stacks::MAX_BLOCK_SIZE;
-use chainstate::stacks::MAX_MICROBLOCK_SIZE;
+use chainstate::stacks::*;
+use chainstate::stacks::index::TrieHash;
+use net::StacksPublicKeyBuffer;
 
 use chainstate::burn::BlockHeaderHash;
 
@@ -33,10 +27,14 @@ use net::StacksMessageCodec;
 use net::Error as net_error;
 use net::codec::{read_next, write_next};
 
-use util::vrf::ECVRF_Proof;
+use util::vrf::{
+    ECVRF_Proof,
+    ECVRF_PROOF_ENCODED_SIZE
+};
+
 use util::hash::MerkleTree;
-use util::hash::Sha256Sum;
-use util::hash::DoubleSha256;
+use util::hash::Sha512_256;
+use util::secp256k1::MessageSignature;
 
 impl StacksMessageCodec for ECVRF_Proof {
     fn serialize(&self) -> Vec<u8> {
@@ -45,55 +43,82 @@ impl StacksMessageCodec for ECVRF_Proof {
 
     fn deserialize(buf: &Vec<u8>, index_ptr: &mut u32, max_size: u32) -> Result<ECVRF_Proof, net_error> {
         let index = *index_ptr;
-        if index > u32::max_value() - 80 {
+        if index > u32::max_value() - ECVRF_PROOF_ENCODED_SIZE {
             return Err(net_error::OverflowError);
         }
-        if index + 80 < max_size {
+        if index + ECVRF_PROOF_ENCODED_SIZE < max_size {
             return Err(net_error::OverflowError);
         }
 
-        if (buf.len() as u32) < index + 80 {
+        if (buf.len() as u32) < index + ECVRF_PROOF_ENCODED_SIZE {
             return Err(net_error::UnderflowError);
         }
-        let res = ECVRF_Proof::from_slice(&buf[(index as usize)..((index+80) as usize)])
+        let res = ECVRF_Proof::from_slice(&buf[(index as usize)..((index+ECVRF_PROOF_ENCODED_SIZE) as usize)])
             .map_err(|_e| net_error::DeserializeError)?;
             
-        *index_ptr += 80;
+        *index_ptr += ECVRF_PROOF_ENCODED_SIZE;
         Ok(res)
     }
 }
-            
+
+impl StacksMessageCodec for StacksWorkScore {
+    fn serialize(&self) -> Vec<u8> {
+        let mut ret = vec![];
+        write_next(&mut ret, &self.burn);
+        write_next(&mut ret, &self.work);
+        ret
+    }
+
+    fn deserialize(buf: &Vec<u8>, index: &mut u32, max_size: u32) -> Result<StacksWorkScore, net_error> {
+        let burn = read_next(buf, index, max_size)?;
+        let work = read_next(buf, index, max_size)?;
+        Ok(StacksWorkScore {
+            burn,
+            work
+        })
+    }
+}
 
 impl StacksMessageCodec for StacksBlockHeader {
     fn serialize(&self) -> Vec<u8> {
         let mut ret = vec![];
         write_next(&mut ret, &self.version);
-        write_next(&mut ret, &self.parent_block);
-        write_next(&mut ret, &self.last_microblock);
+        write_next(&mut ret, &self.total_work);
         write_next(&mut ret, &self.proof);
-        write_next(&mut ret, &self.merkle_root);
+        write_next(&mut ret, &self.parent_block);
+        write_next(&mut ret, &self.parent_microblock);
+        write_next(&mut ret, &self.tx_merkle_root);
+        write_next(&mut ret, &self.state_index_root);
+        write_next(&mut ret, &StacksPublicKeyBuffer::from_public_key(&self.microblock_pubkey));
         ret
     }
 
     fn deserialize(buf: &Vec<u8>, index: &mut u32, max_size: u32) -> Result<StacksBlockHeader, net_error> {
-        let version: u8                         = read_next(&buf, index, max_size)?;
-        let parent_block: BlockHeaderHash       = read_next(&buf, index, max_size)?;
-        let last_microblock: BlockHeaderHash    = read_next(&buf, index, max_size)?;
-        let proof : ECVRF_Proof                 = read_next(&buf, index, max_size)?;
-        let merkle_root: DoubleSha256           = read_next(&buf, index, max_size)?;
+        let version: u8                         = read_next(buf, index, max_size)?;
+        let total_work : StacksWorkScore        = read_next(buf, index, max_size)?;
+        let proof : ECVRF_Proof                 = read_next(buf, index, max_size)?;
+        let parent_block: BlockHeaderHash       = read_next(buf, index, max_size)?;
+        let parent_microblock: BlockHeaderHash  = read_next(buf, index, max_size)?;
+        let tx_merkle_root: Sha512_256          = read_next(buf, index, max_size)?;
+        let state_index_root: TrieHash          = read_next(buf, index, max_size)?;
+        let pubkey_buf : StacksPublicKeyBuffer  = read_next(buf, index, max_size)?;
+
+        let microblock_pubkey = pubkey_buf.to_public_key()?;
 
         Ok(StacksBlockHeader {
             version,
-            parent_block,
-            last_microblock,
+            total_work,
             proof,
-            merkle_root
+            parent_block,
+            parent_microblock,
+            tx_merkle_root,
+            state_index_root,
+            microblock_pubkey
         })
     }
 }
 
 impl StacksMessageCodec for StacksBlock {
-    /// NOTE: the merkle root will _not_ be checked against the transactions!
     fn serialize(&self) -> Vec<u8> {
         let mut ret = vec![];
         write_next(&mut ret, &self.header);
@@ -116,12 +141,10 @@ impl StacksMessageCodec for StacksBlock {
         // (no OffChainOnly allowed)
         for i in 0..txs.len() {
             match txs[i].anchor_mode {
-                TransactionAnchorMode::OnChainOnly | TransactionAnchorMode::Any => {
-                    continue;
-                }
-                _ => {
+                TransactionAnchorMode::OffChainOnly => {
                     return Err(net_error::DeserializeError);
-                }
+                },
+                _ => {}
             };
         }
 
@@ -131,10 +154,10 @@ impl StacksMessageCodec for StacksBlock {
             .map(|tx| tx.txid().as_bytes().to_vec())
             .collect();
 
-        let merkle_tree = MerkleTree::<DoubleSha256>::new(&txid_vecs);
-        let merkle_root = merkle_tree.root();
+        let merkle_tree = MerkleTree::<Sha512_256>::new(&txid_vecs);
+        let tx_merkle_root = merkle_tree.root();
         
-        if merkle_root != header.merkle_root {
+        if tx_merkle_root != header.tx_merkle_root {
             return Err(net_error::DeserializeError);
         }
 
@@ -149,26 +172,35 @@ impl StacksMessageCodec for StacksMicroblockHeader {
     fn serialize(&self) -> Vec<u8> {
         let mut ret = vec![];
         write_next(&mut ret, &self.version);
+        write_next(&mut ret, &self.sequence);
         write_next(&mut ret, &self.prev_block);
-        write_next(&mut ret, &self.merkle_root);
+        write_next(&mut ret, &self.tx_merkle_root);
+        write_next(&mut ret, &self.signature);
         ret
     }
 
     fn deserialize(buf: &Vec<u8>, index_ptr: &mut u32, max_size: u32) -> Result<StacksMicroblockHeader, net_error> {
         let version : u8                    = read_next(buf, index_ptr, max_size)?;
+        let sequence : u32                  = read_next(buf, index_ptr, max_size)?;
         let prev_block : BlockHeaderHash    = read_next(buf, index_ptr, max_size)?;
-        let merkle_root : DoubleSha256      = read_next(buf, index_ptr, max_size)?;
+        let tx_merkle_root : Sha512_256     = read_next(buf, index_ptr, max_size)?;
+        let signature : MessageSignature    = read_next(buf, index_ptr, max_size)?;
+
+        let _ = signature.to_secp256k1_recoverable()
+            .ok_or(net_error::DeserializeError)?;
+        
         Ok(StacksMicroblockHeader {
             version,
+            sequence,
             prev_block,
-            merkle_root
+            tx_merkle_root,
+            signature
         })
     }
 }
 
 
 impl StacksMessageCodec for StacksMicroblock {
-    /// NOTE: the merkle root will _not_ be checked against the transactions!
     fn serialize(&self) -> Vec<u8> {
         let mut ret = vec![];
         write_next(&mut ret, &self.header);
@@ -206,10 +238,10 @@ impl StacksMessageCodec for StacksMicroblock {
             .map(|tx| tx.txid().as_bytes().to_vec())
             .collect();
 
-        let merkle_tree = MerkleTree::<DoubleSha256>::new(&txid_vecs);
-        let merkle_root = merkle_tree.root();
+        let merkle_tree = MerkleTree::<Sha512_256>::new(&txid_vecs);
+        let tx_merkle_root = merkle_tree.root();
         
-        if merkle_root != header.merkle_root {
+        if tx_merkle_root != header.tx_merkle_root {
             return Err(net_error::DeserializeError);
         }
 
