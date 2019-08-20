ca054861c22b00065a89fe4daf9b84a0e8992e01
Jude Nelson <judecn@gmail.com>
2019-08-20 17:55:39 -0400

    separate out address encoding and hashing into the address module -- it is not specific to bitcoin anymore

--- src/address/mod.rs
+++ src/address/mod.rs
@@ -20,6 +20,16 @@
 use std::error;
 use std::fmt;
 
+use burnchains::PublicKey;
+
+use deps::bitcoin::blockdata::script::{Script, Instruction, Builder};
+use deps::bitcoin::blockdata::opcodes::All as btc_opcodes;
+
+use util::hash::Hash160;
+
+use sha2::Sha256;
+use sha2::Digest;
+
 pub mod b58;
 pub mod c32;
 
@@ -69,6 +79,185 @@ impl error::Error for Error {
             Error::InvalidLength(_) => "invalid length for b58 type",
             Error::TooShort(_) => "b58ck data less than 4 bytes",
             Error::Other(_) => "unknown b58 error"
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, PartialEq, Copy, )]
+pub enum AddressHashMode {
+    // serialization modes for public keys to addresses.
+    // We support four different modes due to legacy compatibility with Stacks v1 addresses:
+    SerializeP2PKH = 0x00,          // hash160(public-key), same as bitcoin's p2pkh
+    SerializeP2SH = 0x01,           // hash160(multisig-redeem-script), same as bitcoin's multisig p2sh
+    SerializeP2WPKH = 0x02,         // hash160(segwit-program-00(p2pkh)), same as bitcoin's p2sh-p2wpkh
+    SerializeP2WSH = 0x03,          // hash160(segwit-program-00(public-keys)), same as bitcoin's p2sh-p2wsh
+}
+
+/// Given the u8 of an AddressHashMode, deduce the AddressHashNode
+pub fn address_serialization_flag(flags: u8) -> AddressHashMode {
+    match flags & 0x03 {
+        x if x == AddressHashMode::SerializeP2PKH as u8 => AddressHashMode::SerializeP2PKH,
+        x if x == AddressHashMode::SerializeP2SH as u8 => AddressHashMode::SerializeP2SH,
+        x if x == AddressHashMode::SerializeP2WPKH as u8 => AddressHashMode::SerializeP2WPKH,
+        x if x == AddressHashMode::SerializeP2WSH as u8 => AddressHashMode::SerializeP2WSH,
+        _ => unreachable!()
+    }
+}
+
+/// Internally, the Stacks blockchain encodes address the same as Bitcoin
+/// single-sig address (p2pkh)
+/// Get back the hash of the address
+fn to_bits_p2pkh<K: PublicKey>(pubk: &K) -> Hash160 {
+    let key_hash = Hash160::from_data(&pubk.to_bytes());
+    key_hash
+}
+
+/// Internally, the Stacks blockchain encodes address the same as Bitcoin
+/// multi-sig address (p2sh)
+fn to_bits_p2sh<K: PublicKey>(num_sigs: usize, pubkeys: &Vec<K>) -> Hash160 {
+    let mut bldr = Builder::new();
+    bldr = bldr.push_int(num_sigs as i64);
+    for pubk in pubkeys {
+        bldr = bldr.push_slice(&pubk.to_bytes());
+    }
+    bldr = bldr.push_int(pubkeys.len() as i64);
+    bldr = bldr.push_opcode(btc_opcodes::OP_CHECKMULTISIG);
+    
+    let script = bldr.into_script();
+    let script_hash = Hash160::from_data(&script.as_bytes());
+    script_hash
+}
+
+/// Internally, the Stacks blockchain encodes address the same as Bitcoin
+/// single-sig address over p2sh (p2h-p2wpkh)
+fn to_bits_p2sh_p2wpkh<K: PublicKey>(pubk: &K) -> Hash160 {
+    let key_hash = Hash160::from_data(&pubk.to_bytes());
+
+    let bldr = Builder::new()
+        .push_int(0)
+        .push_slice(key_hash.as_bytes());
+
+    let script = bldr.into_script();
+    let script_hash = Hash160::from_data(&script.as_bytes());
+    script_hash
+}
+
+/// Internally, the Stacks blockchain encodes address the same as Bitcoin
+/// multisig address over p2sh (p2sh-p2wsh)
+fn to_bits_p2sh_p2wsh<K: PublicKey>(num_sigs: usize, pubkeys: &Vec<K>) -> Hash160 {
+    let mut bldr = Builder::new();
+    bldr = bldr.push_int(num_sigs as i64);
+    for pubk in pubkeys {
+        bldr = bldr.push_slice(&pubk.to_bytes());
+    }
+    bldr = bldr.push_int(pubkeys.len() as i64);
+    bldr = bldr.push_opcode(btc_opcodes::OP_CHECKMULTISIG);
+
+    let mut digest = Sha256::new();
+    let mut d = [0u8; 32];
+
+    digest.input(bldr.into_script().as_bytes());
+    d.copy_from_slice(digest.result().as_slice());
+
+    let ws = Builder::new().push_int(0).push_slice(&d).into_script();
+    let ws_hash = Hash160::from_data(&ws.as_bytes());
+    ws_hash
+}
+
+/// Convert a number of required signatures and a list of public keys into a byte-vec to hash to an
+/// address.  Validity of the hash_flag vis a vis the num_sigs and pubkeys will _NOT_ be checked.
+/// This is a low-level method.  Consider using StacksAdress::from_public_keys() if you can.
+pub fn public_keys_to_address_hash<K: PublicKey>(hash_flag: &AddressHashMode, num_sigs: usize, pubkeys: &Vec<K>) -> Hash160 {
+    match *hash_flag {
+        AddressHashMode::SerializeP2PKH => to_bits_p2pkh(&pubkeys[0]),
+        AddressHashMode::SerializeP2SH => to_bits_p2sh(num_sigs, pubkeys),
+        AddressHashMode::SerializeP2WPKH => to_bits_p2sh_p2wpkh(&pubkeys[0]),
+        AddressHashMode::SerializeP2WSH => to_bits_p2sh_p2wsh(num_sigs, pubkeys)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use burnchains::PublicKey;
+    use util::log;
+    use util::hash::*;
+    use util::secp256k1::Secp256k1PublicKey as PubKey;
+
+    struct PubkeyFixture {
+        keys: Vec<PubKey>,
+        num_required: usize,
+        segwit: bool,
+        result: Vec<u8>
+    }
+
+    #[test]
+    fn test_public_keys_to_address_hash() {
+        let pubkey_fixtures = vec![
+            PubkeyFixture {
+                // script pubkey for p2pkh
+                keys: vec![
+                    PubKey::from_hex("040fadbbcea0ff3b05f03195b41cd991d7a0af8bd38559943aec99cbdaf0b22cc806b9a4f07579934774cc0c155e781d45c989f94336765e88a66d91cfb9f060b0").unwrap(),
+                ],
+                num_required: 1,
+                segwit: false,
+                result: hex_bytes("395f3643cea07ec4eec73b4d9a973dcce56b9bf1").unwrap().to_vec()
+            },
+            PubkeyFixture {
+                // script pubkey for multisig p2sh
+                keys: vec![
+                    PubKey::from_hex("040fadbbcea0ff3b05f03195b41cd991d7a0af8bd38559943aec99cbdaf0b22cc806b9a4f07579934774cc0c155e781d45c989f94336765e88a66d91cfb9f060b0").unwrap(),
+                    PubKey::from_hex("04c77f262dda02580d65c9069a8a34c56bd77325bba4110b693b90216f5a3edc0bebc8ce28d61aa86b414aa91ecb29823b11aeed06098fcd97fee4bc73d54b1e96").unwrap(),
+                ],
+                num_required: 2,
+                segwit: false,
+                result: hex_bytes("fd3a5e9f5ba311ce6122765f0af8da7488e25d3a").unwrap().to_vec(),
+            },
+            PubkeyFixture {
+                // script pubkey for p2sh-p2wpkh
+                keys: vec![
+                    PubKey::from_hex("020fadbbcea0ff3b05f03195b41cd991d7a0af8bd38559943aec99cbdaf0b22cc8").unwrap(),
+                ],
+                num_required: 1,
+                segwit: true,
+                result: hex_bytes("0ac7ad046fe22c794dd923b3be14b2e668e50c42").unwrap().to_vec(),
+            },
+            PubkeyFixture {
+                // script pubkey for multisig p2sh-p2wsh
+                keys: vec![
+                    PubKey::from_hex("020fadbbcea0ff3b05f03195b41cd991d7a0af8bd38559943aec99cbdaf0b22cc8").unwrap(),
+                    PubKey::from_hex("02c77f262dda02580d65c9069a8a34c56bd77325bba4110b693b90216f5a3edc0b").unwrap(),
+                ],
+                num_required: 2,
+                segwit: true,
+                result: hex_bytes("3e02fa83ac2fae11fd6703b91e7c94ad393052e2").unwrap().to_vec(),
+            },
+        ];
+
+        for pubkey_fixture in pubkey_fixtures {
+            let hash_mode = 
+                if !pubkey_fixture.segwit {
+                    if pubkey_fixture.num_required == 1 {
+                        AddressHashMode::SerializeP2PKH
+                    }
+                    else {
+                        AddressHashMode::SerializeP2SH
+                    }
+                }
+                else {
+                    if pubkey_fixture.num_required == 1 {
+                        AddressHashMode::SerializeP2WPKH
+                    }
+                    else {
+                        AddressHashMode::SerializeP2WSH
+                    }
+                };
+
+            let result_hash = public_keys_to_address_hash(&hash_mode, pubkey_fixture.num_required, &pubkey_fixture.keys);
+            let result = result_hash.as_bytes().to_vec();
+
+            assert_eq!(result, pubkey_fixture.result);
         }
     }
 }
