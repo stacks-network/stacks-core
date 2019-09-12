df2cecca9f4580a8eb62afbbd4266dc5021cb63b
Ludovic Galabru <ludovic@blockstack.com>
2019-09-12 16:57:34 -0400

    Introduce PreSymbolicExpressionType

--- src/vm/representations.rs
+++ src/vm/representations.rs
@@ -6,29 +6,6 @@ use regex::{Regex};
 use vm::types::{Value};
 use vm::errors::{RuntimeErrorType};
 
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub enum SymbolicExpressionType {
-    AtomValue(Value),
-    Atom(ClarityName),
-    List(Box<[SymbolicExpression]>),
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct SymbolicExpression {
-    pub expr: SymbolicExpressionType,
-    // this id field is used by compiler passes to store information in
-    //  maps.
-    // first pass       -> fill out unique ids
-    // ...typing passes -> store information in hashmap according to id.
-    // 
-    // this is a fairly standard technique in compiler passes
-    pub id: u64,
-
-    #[cfg(feature = "developer-mode")]
-    pub span: Span,
-}
-
 macro_rules! guarded_string {
     ($Name:ident, $Label:literal, $Regex:expr) => {
         #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
@@ -78,6 +55,111 @@ macro_rules! guarded_string {
 guarded_string!(ClarityName, "ClarityName", Regex::new("^[a-zA-Z]([a-zA-Z0-9]|[-_!?+<>=/*])*$|^[-+=/*]$|^[<>]=?$"));
 guarded_string!(ContractName, "ContractName", Regex::new("^[a-zA-Z]([a-zA-Z0-9]|[-_])*$|^__transient$"));
 
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub enum PreSymbolicExpressionType {
+    AtomValue(Value),
+    Atom(ClarityName),
+    List(Box<[PreSymbolicExpression]>),
+    UnexpandedContractName(ContractName)
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct PreSymbolicExpression {
+    pub pre_expr: PreSymbolicExpressionType,
+
+    #[cfg(feature = "developer-mode")]
+    pub span: Span,
+}
+
+impl PreSymbolicExpression {
+    #[cfg(feature = "developer-mode")]
+    fn cons() -> Self {
+        Self {
+            span: Span::zero(),
+            pre_expr: PreSymbolicExpressionType::AtomValue(Value::Bool(false))
+        }
+    }
+    #[cfg(not(feature = "developer-mode"))]
+    fn cons() -> Self {
+        Self {
+            pre_expr: PreSymbolicExpressionType::AtomValue(Value::Bool(false))
+        }
+    }
+
+    #[cfg(feature = "developer-mode")]
+    pub fn set_span(&mut self, start_line: u32, start_column: u32, end_line: u32, end_column: u32) {
+        self.span = Span {
+            start_line,
+            start_column,
+            end_line,
+            end_column
+        }
+    }
+
+    #[cfg(not(feature = "developer-mode"))]
+    pub fn set_span(&mut self, _start_line: u32, _start_column: u32, _end_line: u32, _end_column: u32) {
+    }
+
+    pub fn unexpanded_contract_name(val: ContractName) -> PreSymbolicExpression {
+        PreSymbolicExpression {
+            pre_expr: PreSymbolicExpressionType::UnexpandedContractName(val),
+            .. PreSymbolicExpression::cons()
+        }
+    }
+
+    pub fn atom_value(val: Value) -> PreSymbolicExpression {
+        PreSymbolicExpression {
+            pre_expr: PreSymbolicExpressionType::AtomValue(val),
+            .. PreSymbolicExpression::cons()
+        }
+    }
+
+    pub fn atom(val: ClarityName) -> PreSymbolicExpression {
+        PreSymbolicExpression {
+            pre_expr: PreSymbolicExpressionType::Atom(val),
+            .. PreSymbolicExpression::cons()
+        }
+    }
+
+    pub fn list(val: Box<[PreSymbolicExpression]>) -> PreSymbolicExpression {
+        PreSymbolicExpression {
+            pre_expr: PreSymbolicExpressionType::List(val),
+            .. PreSymbolicExpression::cons()
+        }
+    }
+
+    pub fn match_atom_value(&self) -> Option<&Value> {
+        if let PreSymbolicExpressionType::AtomValue(ref value) = self.pre_expr {
+            Some(value)
+        } else {
+            None
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub enum SymbolicExpressionType {
+    AtomValue(Value),
+    Atom(ClarityName),
+    List(Box<[SymbolicExpression]>),
+    LiteralValue(Value),
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct SymbolicExpression {
+    pub expr: SymbolicExpressionType,
+    // this id field is used by compiler passes to store information in
+    //  maps.
+    // first pass       -> fill out unique ids
+    // ...typing passes -> store information in hashmap according to id.
+    // 
+    // this is a fairly standard technique in compiler passes
+    pub id: u64,
+
+    #[cfg(feature = "developer-mode")]
+    pub span: Span,
+}
+
 impl SymbolicExpression {
     #[cfg(feature = "developer-mode")]
     fn cons() -> Self {
@@ -119,6 +201,13 @@ impl SymbolicExpression {
     pub fn atom(val: ClarityName) -> SymbolicExpression {
         SymbolicExpression {
             expr: SymbolicExpressionType::Atom(val),
+            .. SymbolicExpression::cons()
+        }
+    }
+
+    pub fn literal_value(val: Value) -> SymbolicExpression {
+        SymbolicExpression {
+            expr: SymbolicExpressionType::LiteralValue(val),
             .. SymbolicExpression::cons()
         }
     }
@@ -173,7 +262,7 @@ impl fmt::Display for SymbolicExpression {
             SymbolicExpressionType::Atom(ref value) => {
                 write!(f, "{}", &**value)?;
             },
-            SymbolicExpressionType::AtomValue(ref value) => {
+            SymbolicExpressionType::AtomValue(ref value) | SymbolicExpressionType::LiteralValue(ref value) => {
                 write!(f, "{}", value)?;
             }
         };
