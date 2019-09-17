fd249be6683ab9f81cea89c31c9a96a1290dc430
Ludovic Galabru <ludovic@blockstack.com>
2019-09-17 14:59:36 -0400

    Fix log_asset_transfer signature

--- src/vm/contexts.rs
+++ src/vm/contexts.rs
@@ -441,10 +441,9 @@ impl <'a> GlobalContext<'a> {
         self.asset_maps.len() == 0
     }
 
-    pub fn log_asset_transfer(&mut self, sender: &PrincipalData, contract_identifier: &QualifiedContractIdentifier, asset_name: &str, transfered: Value) {
-        let asset_name = String::from(asset_name).try_into().expect("Failed to get asset name");
+    pub fn log_asset_transfer(&mut self, sender: &PrincipalData, contract_identifier: &QualifiedContractIdentifier, asset_name: &ClarityName, transfered: Value) {
         let asset_identifier = AssetIdentifier { contract_identifier: contract_identifier.clone(),
-                                                 asset_name };
+                                                 asset_name: asset_name.clone() };
         self.asset_maps.last_mut()
             .expect("Failed to obtain asset map")
             .add_asset_transfer(sender, asset_identifier, transfered)
