b079333c64fb9d831e2bb2806244de143af0e7cc
Ludovic Galabru <ludovic@blockstack.com>
2019-09-23 17:51:57 -0400

    Add dummy client

---
+++ core-contracts/src/bns-client.ts
@@ -0,0 +1,25 @@
+import { Client, Provider, Receipt, Result } from "@blockstack/clarity";
+
+export class BNSClient extends Client {
+  constructor(provider: Provider) {
+    super("bns", "bns", provider);
+  }
+
+  async preorderNamespace(hashedNamespace: string, params: { sender: string }): Promise<Receipt> {
+    const tx = this.createTransaction({
+      method: { name: "preorder-namespace", args: [`"${hashedNamespace}"`] }
+    });
+    await tx.sign(params.sender);
+    const res = await this.submitTransaction(tx);
+    return res;
+  }
+
+  async revealNamespace(namespace: string, params: { sender: string }): Promise<Receipt> {
+    const tx = this.createTransaction({
+      method: { name: "reveal-namespace", args: [`${namespace}`] }
+    });
+    await tx.sign(params.sender);
+    const res = await this.submitTransaction(tx);
+    return res;
+  }
+}
