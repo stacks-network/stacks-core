b7e37b858c630fb904c1928094b5a82ddef11026
Jude Nelson <judecn@gmail.com>
2019-11-04 14:27:28 -0500

    get_root_hash_at() method

--- src/chainstate/stacks/index/marf.rs
+++ src/chainstate/stacks/index/marf.rs
@@ -38,6 +38,7 @@ use chainstate::burn::BlockHeaderHash;
 use chainstate::stacks::index::bits::{
     get_leaf_hash,
     get_node_hash,
+    read_root_hash,
 };
 
 use chainstate::stacks::index::node::{
@@ -521,7 +522,6 @@ impl MARF {
             }
         }
 
-
         MARF::get_by_key(storage, current_block_hash, &hash_key)
             .map(|option_result| {
                 option_result.map( |marf_value| { u32::from(marf_value) } )
@@ -551,6 +551,9 @@ impl MARF {
     pub fn set_block_height(&mut self, block_hash: &BlockHeaderHash, height: u32) -> Result<(), Error> {
         let height_key = format!("{}::{}", BLOCK_HEIGHT_TO_HASH_MAPPING_KEY, height);
         let hash_key = format!("{}::{}", BLOCK_HASH_TO_HEIGHT_MAPPING_KEY, block_hash);
+
+        test_debug!("Set {}::{} = {}", BLOCK_HEIGHT_TO_HASH_MAPPING_KEY, height, block_hash.to_hex());
+        test_debug!("Set {}::{} = {}", BLOCK_HASH_TO_HEIGHT_MAPPING_KEY, block_hash.to_hex(), height);
 
         self.insert(&hash_key, MARFValue::from(height))?;
         self.insert(&height_key, MARFValue::from(block_hash.clone()))
@@ -726,7 +729,6 @@ impl MARF {
     }
 
     /// Access internal storage
-    #[cfg(test)]
     pub fn borrow_storage_backend(&mut self) -> &mut TrieFileStorage {
         &mut self.storage
     }
@@ -739,14 +741,13 @@ impl MARF {
     /// Get the root trie hash at a particular block
     pub fn get_root_hash_at(&mut self, block_hash: &BlockHeaderHash) -> Result<TrieHash, Error> {
         let cur_block_hash = self.storage.get_cur_block();
-        let cur_block_rw = self.storage.readwrite();
 
-        self.storage.open_block(block_hash, false)?;
-        let root_hash = read_root_hash(&mut self.storage)?;
+        self.storage.open_block(block_hash)?;
+        let root_hash_res = read_root_hash(&mut self.storage);
 
         // restore
-        self.storage.open_block(&cur_block_hash, cur_block_rw)?;
-        Ok(root_hash)
+        self.storage.open_block(&cur_block_hash)?;
+        root_hash_res
     }
 }
 
