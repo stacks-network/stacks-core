f1f74834a30f9cb6163479a9ec1b8fbabe24f552
Ludovic Galabru <ludovic@blockstack.com>
2019-12-17 01:27:30 -0500

    Read and decode StacksTransaction from disk

--- src/testnet/mem_pool.rs
+++ src/testnet/mem_pool.rs
@@ -1,21 +1,25 @@
 use std::thread;
 use std::time;
 use std::fs;
+use std::io::Read;
 
 use burnchains::{Txid};
+use chainstate::stacks::{StacksTransaction};
+use net::StacksMessageCodec;
 
 pub trait MemPool {
+    fn poll(&mut self) -> Vec<StacksTransaction>;
     fn start(&mut self);
     fn stop(&mut self);
-    fn reset(&mut self);
     fn handle_incoming_tx(&mut self, tx: Txid);
     fn archive_tx(&mut self, tx: Txid);
 }
 
+#[derive(Clone)]
 pub struct MemPoolFS {
     path: String,
-    pending_txs: Vec<Txid>,
-    archived_txs: Vec<Txid>,
+    pending_txs: Vec<StacksTransaction>,
+    archived_txs: Vec<StacksTransaction>,
 }
 
 impl MemPoolFS {
@@ -29,28 +33,38 @@ impl MemPoolFS {
 }
 
 impl MemPool for MemPoolFS {
-    fn start(&mut self) {
-        loop {
-            let block_time = time::Duration::from_millis(10000);
-            let now = time::Instant::now();
-            thread::sleep(block_time);
-            println!("Tick");
 
-            let mempool = fs::read_dir(self.path.clone()).unwrap();
-            for tx in mempool {
-                // De-serialize tx
-                let txid = Txid([0u8; 32]);
-                self.handle_incoming_tx(txid);
+    fn poll(&mut self) -> Vec<StacksTransaction> {
+        let txs_paths = fs::read_dir(self.path.clone()).unwrap();
+        let mut decoded_txs = vec![];
+
+        for tx in txs_paths {
+            if let Ok(tx) = tx {
+                let path = tx.path();
+                if path.is_dir() {
+                    continue;
+                }
+                
+                let mut file = fs::File::open(path).unwrap();
+                let mut encoded_tx = vec![];
+                file.read(&mut encoded_tx[..]).unwrap();
+
+                let mut index = 0;
+                let tx = StacksTransaction::deserialize(&encoded_tx, &mut index, encoded_tx.len() as u32).map_err(|_e| {
+                    eprintln!("Failed to decode transaction");
+                    panic!();
+                }).unwrap();
+                
+                decoded_txs.push(tx)
             }
         }
+        decoded_txs
+    }
+
+    fn start(&mut self) {
     }
 
     fn stop(&mut self) {
-
-    }
-
-    fn reset(&mut self) {
-
     }
 
     fn handle_incoming_tx(&mut self, tx: Txid) {
