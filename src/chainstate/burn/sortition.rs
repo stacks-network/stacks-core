7fa7b9b5dbc3adae058f42f244ef64ef1f7f3a91
Jude Nelson <judecn@gmail.com>
2019-12-19 19:12:48 -0500

    unused variable

--- src/chainstate/burn/sortition.rs
+++ src/chainstate/burn/sortition.rs
@@ -210,7 +210,6 @@ impl BlockSnapshot {
 
         let last_sortition_hash = parent_snapshot.sortition_hash.clone();
         let last_burn_total = parent_snapshot.total_burn;
-        let last_sortition = parent_snapshot.sortition;
         
         // next sortition hash
         let next_sortition_hash = last_sortition_hash.mix_burn_header(&block_hash);
