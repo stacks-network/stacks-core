e9df959bba081b5d00939b1abc84e7fa0e75ec01
Ludovic Galabru <ludovic@blockstack.com>
2020-02-03 11:19:08 -0500

    Add more tests

--- src/vm/analysis/trait_checker/tests.rs
+++ src/vm/analysis/trait_checker/tests.rs
@@ -4,7 +4,7 @@ use vm::analysis::{AnalysisDatabase, contract_interface_builder::build_contract_
 use vm::database::MemoryBackingStore;
 use vm::analysis::mem_type_check;
 use vm::analysis::type_check;
-use vm::types::QualifiedContractIdentifier;
+use vm::types::{TypeSignature, QualifiedContractIdentifier};
 
 #[test]
 fn test_dynamic_dispatch_by_defining_trait() {
@@ -28,6 +28,96 @@ fn test_dynamic_dispatch_by_defining_trait() {
         type_check(&dispatching_contract_id, &mut dispatching_contract, db, true)?;
         type_check(&target_contract_id, &mut target_contract, db, true)
     }).unwrap();
+}
+
+#[test]
+fn test_dynamic_dispatch_unknown_method() {
+    let dispatching_contract_src =
+        "(define-trait trait-1 (
+            (get-1 (uint) (response uint uint))))
+        (define-public (wrapped-get-1 (contract <trait-1>)) 
+            (contract-call? contract get-2 u0))";
+    let target_contract_src =
+        "(define-public (get-1 (x uint)) (ok u1))";
+
+    let dispatching_contract_id = QualifiedContractIdentifier::local("dispatching-contract").unwrap();
+    let target_contract_id = QualifiedContractIdentifier::local("target-contract").unwrap();
+
+    let mut dispatching_contract = parse(&dispatching_contract_id, dispatching_contract_src).unwrap();
+    let mut target_contract = parse(&target_contract_id, target_contract_src).unwrap();
+    let mut marf = MemoryBackingStore::new();
+    let mut db = marf.as_analysis_db();
+
+    let err = db.execute(|db| {
+        type_check(&dispatching_contract_id, &mut dispatching_contract, db, true)?;
+        type_check(&target_contract_id, &mut target_contract, db, true)
+    }).unwrap_err();
+    match err.err {
+        CheckErrors::TraitMethodUnknown(_, _) => {},
+        _ => {
+            println!("{:?}", err);
+            panic!("Attempt to call init-factorial should fail!")
+        }
+    }
+}
+
+#[test]
+fn test_semi_dynamic_looping_methods() {
+    let dispatching_contract_src =
+        "(define-trait trait-1 (
+            (get-1 (uint) (response uint uint))))
+        (define-public (wrapped-get-1 (contract <trait-1>)) 
+            (contract-call? contract get-1 u0))";
+    let target_contract_src =
+        "(define-public (get-1 (x uint)) (contract-call? .dispatching-contract wrapped-get-1 .target-contract))";
+
+    let dispatching_contract_id = QualifiedContractIdentifier::local("dispatching-contract").unwrap();
+    let target_contract_id = QualifiedContractIdentifier::local("target-contract").unwrap();
+
+    let mut dispatching_contract = parse(&dispatching_contract_id, dispatching_contract_src).unwrap();
+    let mut target_contract = parse(&target_contract_id, target_contract_src).unwrap();
+    let mut marf = MemoryBackingStore::new();
+    let mut db = marf.as_analysis_db();
+
+    let err = db.execute(|db| {
+        type_check(&dispatching_contract_id, &mut dispatching_contract, db, true)?;
+        type_check(&target_contract_id, &mut target_contract, db, true)
+    }).unwrap_err();
+    match err.err {
+        CheckErrors::TypeError(TypeSignature::TraitReferenceType(_), TypeSignature::PrincipalType) => {},
+        _ => {
+            println!("{:?}", err);
+            panic!("Attempt to call init-factorial should fail!")
+        }
+    }
+}
+
+#[test]
+fn test_passing_trait_reference_instances() {
+    let dispatching_contract_src =
+        "(define-trait trait-1 (
+            (get-1 (uint) (response uint uint))))
+        (define-public (wrapped-get-1 (contract <trait-1>)) 
+            (internal-get-1 contract))
+        (define-public (internal-get-1 (contract <trait-1>))
+            (ok u1))";
+
+    let dispatching_contract_id = QualifiedContractIdentifier::local("dispatching-contract").unwrap();
+
+    let mut dispatching_contract = parse(&dispatching_contract_id, dispatching_contract_src).unwrap();
+    let mut marf = MemoryBackingStore::new();
+    let mut db = marf.as_analysis_db();
+
+    let err = db.execute(|db| {
+        type_check(&dispatching_contract_id, &mut dispatching_contract, db, true)
+    }).unwrap_err();
+    match err.err {
+        CheckErrors::UndefinedVariable(_) => {},
+        _ => {
+            println!("{:?}", err);
+            panic!("Attempt to call init-factorial should fail!")
+        }
+    }
 }
 
 #[test]
@@ -173,6 +263,57 @@ fn test_dynamic_dispatch_including_nested_trait() {
         type_check(&target_nested_contract_id, &mut target_nested_contract, db, true)
     }).unwrap();
     
+}
+
+#[test]
+fn test_dynamic_dispatch_including_wrong_nested_trait() {
+    let contract_defining_nested_trait_src = 
+    "(define-trait trait-a (
+        (get-a (uint) (response uint uint))))";
+    let contract_defining_trait_src = 
+        "(use-trait trait-a .contract-defining-nested-trait.trait-a)
+        (define-trait trait-1 (
+            (get-1 (<trait-a>) (response uint uint))))";
+    let dispatching_contract_src =
+        "(use-trait trait-1 .contract-defining-trait.trait-1)
+         (use-trait trait-a .contract-defining-trait.trait-a)
+         (define-public (wrapped-get-1 (contract <trait-1>) (nested-contract <trait-1>)) 
+            (contract-call? contract get-1 nested-contract))";
+    let target_contract_src =
+        "(use-trait trait-a .contract-defining-nested-trait.trait-a)
+        (define-public (get-1 (nested-contract <trait-a>))
+            (contract-call? nested-contract get-a u0))";
+    let target_nested_contract_src =
+        "(define-public (get-a (x uint)) (ok u99))";
+
+    let contract_defining_trait_id = QualifiedContractIdentifier::local("contract-defining-trait").unwrap();
+    let dispatching_contract_id = QualifiedContractIdentifier::local("dispatching-contract").unwrap();
+    let target_contract_id = QualifiedContractIdentifier::local("target-contract").unwrap();
+    let contract_defining_nested_trait_id = QualifiedContractIdentifier::local("contract-defining-nested-trait").unwrap();
+    let target_nested_contract_id = QualifiedContractIdentifier::local("target-nested-contract").unwrap();
+
+    let mut contract_defining_trait = parse(&contract_defining_trait_id, contract_defining_trait_src).unwrap();
+    let mut dispatching_contract = parse(&dispatching_contract_id, dispatching_contract_src).unwrap();
+    let mut target_contract = parse(&target_contract_id, target_contract_src).unwrap();
+    let mut contract_defining_nested_trait = parse(&contract_defining_nested_trait_id, contract_defining_nested_trait_src).unwrap();
+    let mut target_nested_contract = parse(&target_nested_contract_id, target_nested_contract_src).unwrap();
+    let mut marf = MemoryBackingStore::new();
+    let mut db = marf.as_analysis_db();
+
+    let err = db.execute(|db| {
+        type_check(&contract_defining_nested_trait_id, &mut contract_defining_nested_trait, db, true)?;
+        type_check(&contract_defining_trait_id, &mut contract_defining_trait, db, true)?;
+        type_check(&dispatching_contract_id, &mut dispatching_contract, db, true)?;
+        type_check(&target_contract_id, &mut target_contract, db, true)?;
+        type_check(&target_nested_contract_id, &mut target_nested_contract, db, true)
+    }).unwrap_err();
+    match err.err {
+        CheckErrors::TypeError(TypeSignature::TraitReferenceType(_), TypeSignature::TraitReferenceType(_)) => {},
+        _ => {
+            println!("{:?}", err);
+            panic!("Attempt to call init-factorial should fail!")
+        }
+    }
 }
 
 #[test]
