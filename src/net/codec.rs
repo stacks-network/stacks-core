daa90990ecadecf6f1266a48b9180555cf8745cf
Jude Nelson <judecn@gmail.com>
2020-03-25 15:14:09 -0400

    has_ith_block and has_ith_microblock_stream are only used in tests

--- src/net/codec.rs
+++ src/net/codec.rs
@@ -409,6 +409,7 @@ impl BlocksInvData {
         bitvec
     }
 
+    #[cfg(test)]
     pub fn has_ith_block(&self, block_index: u16) -> bool {
         if block_index >= self.bitlen {
             return false;
@@ -419,6 +420,7 @@ impl BlocksInvData {
         (self.block_bitvec[idx as usize] & (1 << bit)) != 0
     }
     
+    #[cfg(test)]
     pub fn has_ith_microblock_stream(&self, block_index: u16) -> bool {
         if block_index >= self.bitlen {
             return false;
