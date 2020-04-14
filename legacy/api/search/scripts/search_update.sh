be8169127d6a6fc788811412e692cca16dbfdea7
Matthew Little <zone117x@gmail.com>
2020-04-14 09:24:03 +0200

    chore: delete legacy directory

--- legacy/api/search/scripts/search_update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Refreshing mongodb index at $(date)" >> /tmp/update_index_out
-python -m api.search.basic_index --refresh  >> /tmp/update_index_out
-echo "Finished indexing at $(date)" >> /tmp/update_index_out
--- legacy/docs/faq_technical.md
+++
@@ -1,1 +0,0 @@
-The documentation has been moved to [docs.blockstack.org](https://docs.blockstack.org/faqs/allfaqs), please update your bookmarks.
