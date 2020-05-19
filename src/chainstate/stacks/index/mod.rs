0f8412915c8edfa9fd0172f02ae46d13c68f9f15
Aaron Blankstein <aaron@blockstack.com>
2020-05-19 13:48:22 -0500

    lint: macro indentation

--- src/chainstate/stacks/index/mod.rs
+++ src/chainstate/stacks/index/mod.rs
@@ -77,33 +77,33 @@ pub const SENTINEL_ARRAY: [u8; 32] = [255u8; 32];
 
 macro_rules! impl_marf_trie_id {
     ($thing:ident) => {
-impl MarfTrieId for $thing {
-    fn as_bytes(&self) -> &[u8] {
-        self.as_ref()
-    }
-    fn to_bytes(self) -> [u8; 32] {
-        self.0
-    }
-    fn sentinel() -> Self {
-        Self(SENTINEL_ARRAY.clone())
-    }
-}
-
-impl From<MARFValue> for $thing {
-    fn from(m: MARFValue) -> Self {
-        let h = m.0;
-        let mut d = [0u8; 32];
-        for i in 0..32 {
-            d[i] = h[i];
-        }
-        for i in 32..h.len() {
-            if h[i] != 0 {
-                panic!("Failed to convert MARF value into BHH: data stored after 32nd byte");
+        impl MarfTrieId for $thing {
+            fn as_bytes(&self) -> &[u8] {
+                self.as_ref()
+            }
+            fn to_bytes(self) -> [u8; 32] {
+                self.0
+            }
+            fn sentinel() -> Self {
+                Self(SENTINEL_ARRAY.clone())
             }
         }
-        Self(d)
-    }
-}
+
+        impl From<MARFValue> for $thing {
+            fn from(m: MARFValue) -> Self {
+                let h = m.0;
+                let mut d = [0u8; 32];
+                for i in 0..32 {
+                    d[i] = h[i];
+                }
+                for i in 32..h.len() {
+                    if h[i] != 0 {
+                        panic!("Failed to convert MARF value into BHH: data stored after 32nd byte");
+                    }
+                }
+                Self(d)
+            }
+        }
     }
 }
 
