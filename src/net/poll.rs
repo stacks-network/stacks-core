9096052ba7c98e2c65f4db6b7c6da8d17a00a55c
Jude Nelson <judecn@gmail.com>
2020-06-04 18:51:45 -0400

    enhance debug output

--- src/net/poll.rs
+++ src/net/poll.rs
@@ -352,7 +352,7 @@ impl NetworkState {
                             Ok(eid) => eid,
                             Err(_e) => {
                                 // no poll slots available. Close the socket and carry on.
-                                info!("Too many peers, closing {:?} ({:?})", &_client_addr, &client_sock);
+                                info!("Too many peers, closing {:?} (events: {}, in-flight: {}, capacity: {})", &client_sock, self.event_map.len(), new_events.len(), self.event_capacity);
                                 let _ = client_sock.shutdown(Shutdown::Both);
                                 continue;
                             }
@@ -496,7 +496,7 @@ mod test {
     #[test]
     fn test_register_deregister_stress() {
         let mut ns = NetworkState::new(20).unwrap();
-        let mut count = 10;
+        let mut count = 0;
         let mut in_use = HashSet::new();
         let mut events_in = vec![];
 
@@ -504,7 +504,6 @@ mod test {
             let next_eid = ns.make_next_event_id(count, &in_use).unwrap();
             ns.event_map.insert(next_eid, 0);
             events_in.push(next_eid);
-            count = (next_eid + 1) % 20;
         }
 
         assert_eq!(ns.event_map.len(), 20);
@@ -523,7 +522,6 @@ mod test {
             let next_eid = ns.make_next_event_id(count, &in_use).unwrap();
             events_in.push(next_eid);
             in_use.insert(next_eid);
-            count = (next_eid + 1) % 20;
         }
         
         assert_eq!(ns.event_map.len(), 0);
