a9c9cfa7104a6a5a7028c9da487d42132eacdd6b
Jude Nelson <judecn@gmail.com>
2020-07-09 14:00:01 -0400

    fix test build

--- testnet/stacks-node/src/tests/integrations.rs
+++ testnet/stacks-node/src/tests/integrations.rs
@@ -7,7 +7,7 @@ use stacks::vm::{
     analysis::{mem_type_check, contract_interface_builder::{build_contract_interface, ContractInterface}},
     Value };
 use stacks::chainstate::stacks::{
-    db::StacksChainState, StacksPrivateKey, StacksTransaction, StacksAddress };
+    db::StacksChainState, StacksPrivateKey, StacksTransaction, StacksAddress, StacksBlockHeader };
 use stacks::chainstate::burn::VRFSeed;
 use stacks::burnchains::Address;
 use stacks::net::{AccountEntryResponse, ContractSrcResponse, CallReadOnlyRequestBody};
@@ -657,7 +657,7 @@ fn contract_stx_transfer() {
                 let cur_tip = (chain_tip.metadata.burn_header_hash.clone(), chain_tip.metadata.anchored_header.block_hash());
                 // check that 1000 stx _was_ transfered to the contract principal
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&contract_identifier.clone().into())
                         })
@@ -667,7 +667,7 @@ fn contract_stx_transfer() {
                 let sk_3 = StacksPrivateKey::from_hex(SK_3).unwrap();
                 let addr_3 = to_addr(&sk_3).into();
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&addr_3)
                         })
@@ -691,7 +691,7 @@ fn contract_stx_transfer() {
                 let sk_2 = StacksPrivateKey::from_hex(SK_2).unwrap();
                 let addr_2 = to_addr(&sk_2).into();
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&addr_2)
                         })
@@ -699,7 +699,7 @@ fn contract_stx_transfer() {
                     1);
 
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&contract_identifier.clone().into())
                         })
@@ -714,7 +714,7 @@ fn contract_stx_transfer() {
 
                 // check that 1000 stx were sent to the contract
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&contract_identifier.clone().into())
                         })
@@ -724,7 +724,7 @@ fn contract_stx_transfer() {
                 let sk_3 = StacksPrivateKey::from_hex(SK_3).unwrap();
                 let addr_3 = to_addr(&sk_3).into();
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&addr_3)
                         })
@@ -775,7 +775,7 @@ fn mine_contract_twice() {
             let cur_tip = (chain_tip.metadata.burn_header_hash.clone(), chain_tip.metadata.anchored_header.block_hash());
             // check that the contract published!
             assert_eq!(
-                &chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                &chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                     conn.with_clarity_db_readonly(|db| {
                         db.get_contract_src(&contract_identifier).unwrap()
                     })
@@ -850,7 +850,7 @@ fn bad_contract_tx_rollback() {
                 let cur_tip = (chain_tip.metadata.burn_header_hash.clone(), chain_tip.metadata.anchored_header.block_hash());
                 // check that 1000 stx _was_ transfered to the contract principal
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&contract_identifier.clone().into())
                         })
@@ -860,7 +860,7 @@ fn bad_contract_tx_rollback() {
                 let sk_3 = StacksPrivateKey::from_hex(SK_3).unwrap();
                 let addr_3 = to_addr(&sk_3).into();
                 assert_eq!(
-                    chain_state.with_read_only_clarity_tx(&cur_tip.0, &cur_tip.1, |conn| {
+                    chain_state.with_read_only_clarity_tx(&StacksBlockHeader::make_index_block_hash(&cur_tip.0, &cur_tip.1), |conn| {
                         conn.with_clarity_db_readonly(|db| {
                             db.get_account_stx_balance(&addr_3)
                         })
