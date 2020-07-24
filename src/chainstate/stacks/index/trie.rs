a67b764a55a9ceeca8bbb4d01776d803513de701
Jude Nelson <judecn@gmail.com>
2020-07-24 03:28:24 -0400

    fix tracer

--- src/chainstate/stacks/index/trie.rs
+++ src/chainstate/stacks/index/trie.rs
@@ -680,6 +680,7 @@ impl Trie {
                 let node_hash = my_hash.clone();
                 let _ = Trie::get_trie_root_ancestor_hashes_bytes(storage, &node_hash)
                     .and_then(|_hs| {
+                        storage.clear_cached_ancestor_hashes_bytes();
                         trace!("update_root_hash: Updated {:?} with {:?} from {} to {} + {:?} = {} (fixed root)", &node, &child_ptr, &_cur_hash, &node_hash, &_hs[1..].to_vec(), &h);
                         Ok(())
                     });
@@ -735,6 +736,7 @@ impl Trie {
                                 if is_trace() {
                                     let _ = Trie::get_trie_root_ancestor_hashes_bytes(storage, &content_hash)
                                         .and_then(|_hs| {
+                                            storage.clear_cached_ancestor_hashes_bytes();
                                             trace!("update_root_hash: Updated {:?} with {:?} from {:?} to {:?} + {:?} = {:?}", &node, &child_ptr, &_cur_hash, &content_hash, &_hs[1..].to_vec(), &h);
                                             Ok(())
                                         });
