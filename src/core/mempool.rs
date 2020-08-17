5c2c4bc3ad2d1ef639a8672c1e303b83bef2623f
Jude Nelson <judecn@gmail.com>
2020-08-17 00:00:34 -0400

    pox --> burn

--- src/core/mempool.rs
+++ src/core/mempool.rs
@@ -810,7 +810,7 @@ mod tests {
 
     use vm::{
         database::HeadersDB,
-        database::NULL_POX_STATE_DB,
+        database::NULL_BURN_STATE_DB,
         types::{QualifiedContractIdentifier, PrincipalData},
         Value, ClarityName, ContractName, errors::RuntimeErrorType, errors::Error as ClarityError };
     use chainstate::burn::{VRFSeed, BlockHeaderHash};
@@ -868,7 +868,7 @@ mod tests {
 
         {
             let (chainstate_tx, clar_tx) = chainstate.chainstate_tx_begin().unwrap();
-            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_POX_STATE_DB,
+            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_BURN_STATE_DB,
                                                                 &ConsensusHash::empty(),
                                                                 &BlockHeaderHash::sentinel(),
                                                                 &b_1.0,
@@ -878,7 +878,7 @@ mod tests {
 
         {
             let (chainstate_tx, clar_tx) = chainstate.chainstate_tx_begin().unwrap();
-            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_POX_STATE_DB,
+            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_BURN_STATE_DB,
                                                                 &ConsensusHash::empty(),
                                                                 &BlockHeaderHash::sentinel(),
                                                                 &b_3.0,
@@ -888,7 +888,7 @@ mod tests {
 
         {
             let (chainstate_tx, clar_tx) = chainstate.chainstate_tx_begin().unwrap();
-            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_POX_STATE_DB,
+            let c_tx = StacksChainState::chainstate_block_begin(&chainstate_tx, clar_tx, &NULL_BURN_STATE_DB,
                                                                 &b_1.0,
                                                                 &b_1.1,
                                                                 &b_2.0,
