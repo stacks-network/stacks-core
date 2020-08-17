2447afda7f5a4500e6a9fb283b1db301ad5c7e5d
Jude Nelson <judecn@gmail.com>
2020-08-17 00:00:55 -0400

    add test-specific boot code in network tests

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1541,6 +1541,8 @@ pub mod test {
     use chainstate::stacks::miner::*;
     use chainstate::stacks::miner::test::*;
     use chainstate::stacks::*;
+    use chainstate::stacks::db::*;
+    use chainstate::stacks::boot::*;
 
     use chainstate::stacks::db::StacksChainState;
 
@@ -1580,7 +1582,10 @@ pub mod test {
 
     use mio;
 
+    use util::strings::*;
     use util::vrf::*;
+
+    use vm::types::*;
 
     // emulate a socket
     pub struct NetCursor<T> {
@@ -1768,6 +1773,7 @@ pub mod test {
         pub test_name: String,
         pub initial_balances: Vec<(PrincipalData, u64)>,
         pub spending_account: TestMiner,
+        pub setup_code: String,
     }
 
     impl TestPeerConfig {
@@ -1796,7 +1802,8 @@ pub mod test {
                 data_url: "".into(),
                 test_name: "".into(),
                 initial_balances: vec![],
-                spending_account: spending_account
+                spending_account: spending_account,
+                setup_code: "".into()
             }
         }
 
@@ -1900,7 +1907,43 @@ pub mod test {
                                              &config.asn4_entries, Some(&config.initial_neighbors)).unwrap();
 
             let mut sortdb = SortitionDB::connect(&sortdb_path, config.burnchain.first_block_height, &config.burnchain.first_block_hash, get_epoch_time_secs(), true).unwrap();
-            let chainstate = StacksChainState::open_and_exec(false, config.network_id, &chainstate_path, Some(config.initial_balances.clone()), |_| {}, ExecutionCost::max_value()).unwrap();
+            let init_code = config.setup_code.clone();
+            let chainstate = StacksChainState::open_and_exec(false, config.network_id, &chainstate_path, Some(config.initial_balances.clone()), 
+                |ref mut clarity_tx| {
+                    if init_code.len() > 0 {
+                        clarity_tx.connection().as_transaction(|clarity| {
+                            let boot_code_address = StacksAddress::from_string(&STACKS_BOOT_CODE_CONTRACT_ADDRESS.to_string()).unwrap();
+                            let boot_code_account = StacksAccount {
+                                principal: PrincipalData::Standard(StandardPrincipalData::from(boot_code_address.clone())),
+                                nonce: 0,
+                                stx_balance: 0,
+                                stx_locked: 0,
+                                unlock_height: 0
+                            };
+                            let boot_code_auth = TransactionAuth::Standard(TransactionSpendingCondition::Singlesig(SinglesigSpendingCondition {
+                                signer: boot_code_address.bytes.clone(),
+                                hash_mode: SinglesigHashMode::P2PKH,
+                                key_encoding: TransactionPublicKeyEncoding::Uncompressed,
+                                nonce: 0,
+                                fee_rate: 0,
+                                signature: MessageSignature::empty()
+                            }));
+
+                            debug!("Instantiate test-specific boot code contract '{}.{}' ({} bytes)...", &STACKS_BOOT_CODE_CONTRACT_ADDRESS, &config.test_name, init_code.len());
+                            
+                            let smart_contract = TransactionPayload::SmartContract(
+                                TransactionSmartContract {
+                                    name: ContractName::try_from(config.test_name.as_str()).expect("FATAL: invalid boot-code contract name"),
+                                    code_body: StacksString::from_str(&init_code).expect("FATAL: invalid boot code body"),
+                                }
+                            );
+
+                            let boot_code_smart_contract = StacksTransaction::new(TransactionVersion::Testnet, boot_code_auth.clone(), smart_contract);
+                            StacksChainState::process_transaction_payload(clarity, &boot_code_smart_contract, &boot_code_account).unwrap();
+                        });
+                    }
+                },
+                ExecutionCost::max_value()).unwrap();
             
             let mut stacks_node = TestStacksNode::from_chainstate(chainstate);
 
