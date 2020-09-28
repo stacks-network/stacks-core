66202808619e904ffe4868a67e62d620aff22240
Jude Nelson <judecn@gmail.com>
2020-09-28 14:26:57 -0400

    WIP: refactor run loop watchdog code into its own struct

--- testnet/stacks-node/src/run_loop/neon.rs
+++ testnet/stacks-node/src/run_loop/neon.rs
@@ -17,6 +17,7 @@ use super::RunLoopCallbacks;
 
 use crate::monitoring::start_serving_monitoring_metrics;
 use crate::neon_node::TESTNET_CHAIN_ID;
+use crate::burnchains::BurnchainTip;
 
 /// Coordinating a node running in neon mode.
 #[cfg(test)]
@@ -32,6 +33,147 @@ pub struct RunLoop {
     config: Config,
     pub callbacks: RunLoopCallbacks,
     coordinator_channels: Option<(CoordinatorReceivers, CoordinatorChannels)>
+}
+
+/// Monitor the state of the Stacks blockchain as the peer network and relay threads download and
+/// proces Stacks blocks.  Don't allow the node to process the next PoX reward cycle's sortitions
+/// unless it's reasonably sure that it has processed all Stacks blocks for this reward cycle.
+/// This struct monitors the Stacks chainstate to make this determination.
+pub struct PoxSyncWatchdog {
+    chainstate_path: String,
+    unprocessed_block_samples: VecDeque<i64>,
+    max_samples: u64,
+    max_staging: u64,
+    delay_heuristic: u64,
+    sync_frequency: u64,
+    sync_deadline: u64,
+    initial_block_download: bool,
+}
+
+impl PoxSyncWatchdog {
+    pub fn new(chainstate_path: String) -> PoxSyncWatchdog {
+        PoxSyncWatchdog {
+            chainstate_path: chainstate_path,
+            unprocessed_block_samples: VecDeque::new(),
+            max_samples: 30,
+            max_staging: 10,
+            delay_heuristic: 60_000,        // 1 minute,
+            sync_frequency: 10,
+            sync_deadline: 0,
+            initial_block_download: true
+        }
+    }
+
+    /// How many recently-added Stacks blocks are in an attachable state, up to $max_staging, and
+    /// no more recent than $now - $delay_heuristic seconds?
+    /// ($max_staging is necessary to limit the runtime of this method, since the underlying SQL
+    /// uses COUNT(*), which in Sqlite is a _O(n)_ operation for _n_ rows)
+    fn count_attachable_stacks_blocks(&self) -> Result<u64, String> {
+        let chainstate = match StacksChainState::open(false, TESTNET_CHAIN_ID, &self.chainstate_path) {
+            Ok(cs) => cs,
+            Err(e) => {
+                return Err(format!("Failed to open chainstate at '{}': {:?}", &self.chainstate_path, &e));
+            },
+        };
+
+        let cnt = StacksChainState::count_attachable_staging_blocks(&chainstate.blocks_db, self.max_staging, get_epoch_time_secs() - (self.delay_heuristic / 1000))
+            .map_err(|e| format!("Failed to count attachable staging blocks: {:?}", &e))?;
+
+        Ok(cnt)
+    }
+
+    /// Are we still in the initial block download?  Infer this by checking to see how many reward
+    /// cycles have been processed, compared to how many reward cycles exist.  If they're equal,
+    /// then we're ready to start mining
+    pub fn infer_initial_block_download(&mut self, burnchain_ctl: &BitcoinRegtestController, burnchain_tip: &BurnchainTip, burnchain_height: u64) -> bool {
+        let highest_reward_cycle = match burnchain_ctl.get_burnchain().block_height_to_reward_cycle(burnchain_height) {
+            Some(x) => x,
+            None => {
+                return false;
+            }
+        };
+
+        let highest_available_reward_cycle = match burnchain_ctl.get_burnchain().block_height_to_reward_cycle(burnchain_tip.block_snapshot.block_height) {
+            Some(x) => x,
+            None => {
+                return false;
+            }
+        };
+
+        self.initial_block_download = highest_available_reward_cycle <= highest_reward_cycle;
+        self.initial_block_download
+    }
+
+    /// Wait until all of the Stacks blocks for the given reward cycle are seemingly downloaded and
+    /// processed.  Do so by watching the _rate_ at which attachable Stacks blocks arrive.  If the
+    /// rate goes to 0, and the number goes to 0 over $delay_heuristic milliseconds, then infer
+    /// that all Stacks blocks have been downloaded.
+    pub fn pox_sync_wait(&mut self, burnchain_ctl: &BitcoinRegtestController, burnchain_tip: &BurnchainTip, burnchain_height: u64) {
+        self.infer_initial_block_download(burnchain_ctl, burnchain_tip, burnchain_height);
+        loop {
+            let mut total_blocks_seen = 0;
+            let mut total_recent_changes = 0;
+            let do_sync = match self.count_attachable_stacks_blocks() {
+                Ok(num_available) => {
+                    self.unprocessed_block_samples.push_back(num_available as i64);
+                    if (self.unprocessed_block_samples.len() as u64) > self.max_samples {
+                        self.unprocessed_block_samples.pop_front();
+                    }
+
+                    if num_available > 0 || (self.unprocessed_block_samples.len() as u64) < self.max_samples {
+                        // wait for a bit before asking again (up to a minute total)
+                        sleep_ms((self.delay_heuristic / self.max_samples) as u64);
+                    }
+
+                    // take first derivative of samples
+                    let mut deltas = vec![];
+                    let mut prev = *self.unprocessed_block_samples.front().unwrap_or(&(self.max_staging as i64));
+                    for (i, sample) in self.unprocessed_block_samples.iter().enumerate() {
+                        total_blocks_seen += sample;
+                        if i == 0 {
+                            continue;
+                        }
+                        let delta = sample - prev;
+                        prev = *sample;
+                        deltas.push(delta);
+                    }
+
+                    // if first-derivative is `y = 0` for the last $delay_heuristic millis, then we can infer
+                    // that we've processed all of the burnchain blocks -- no more are likely to
+                    // arrive late (but we can recover via the PoX unhappy path if that happens)
+                    let mut flatlined = true;
+                    for d in deltas.iter() {
+                        if *d != 0 {
+                            total_recent_changes += 1;
+                            flatlined = false;
+                        }
+                    }
+
+                    flatlined == 0
+                },
+                Err(msg) => {
+                    warn!("{}", msg);
+                    false
+                }
+            };
+
+            if !do_sync {
+                // still working on blocks
+                debug!("Still downloading Stacks blocks; will not sync burnchain yet (total blocks seen in sample: {}; total number of changes in sample: {})", total_blocks_seen, total_recent_changes);
+                sleep_ms((self.delay_heuristic / self.max_samples) as u64);
+                continue;
+            }
+
+            let now = get_epoch_time_secs();
+            if now < self.sync_deadline {
+                debug!("Wait until {} to sync with the burnchain", self.sync_deadline);
+                sleep_ms((self.sync_deadline - now) * 1000);
+            }
+
+            self.sync_deadline = get_epoch_time_secs() + self.sync_frequency;
+            break;
+        }
+    }
 }
 
 impl RunLoop {
@@ -80,20 +222,6 @@ impl RunLoop {
     fn bump_blocks_processed(&self) {
     }
 
-    fn count_attachable_stacks_blocks(&self, chainstate_path: &String, limit: u64) -> Result<u64, String> {
-        let chainstate = match StacksChainState::open(false, TESTNET_CHAIN_ID, &chainstate_path) {
-            Ok(cs) => cs,
-            Err(e) => {
-                return Err(format!("Failed to open chainstate at '{}': {:?}", chainstate_path, &e));
-            },
-        };
-
-        let cnt = StacksChainState::count_attachable_staging_blocks(&chainstate.blocks_db, limit)
-            .map_err(|e| format!("Failed to count attachable staging blocks: {:?}", &e))?;
-
-        Ok(cnt)
-    }
-
     /// Starts the testnet runloop.
     /// 
     /// This function will block by looping infinitely.
@@ -107,6 +235,7 @@ impl RunLoop {
 
         // Initialize and start the burnchain.
         let mut burnchain = BitcoinRegtestController::new(self.config.clone(), Some(coordinator_senders.clone()));
+        let pox_constants = burnchain.get_pox_constants();
 
         let is_miner = if self.config.node.miner {
             let keychain = Keychain::default(self.config.node.seed.clone());
@@ -131,7 +260,8 @@ impl RunLoop {
             false
         };
 
-        match burnchain.start() {
+        let mut target_burnchain_block_height = 1;
+        match burnchain.start(Some(target_burnchain_block_height)) {
             Ok(_) => {},
             Err(e) => {
                 warn!("Burnchain controller stopped: {}", e);
@@ -194,78 +324,17 @@ impl RunLoop {
         }
         
         let chainstate_path = self.config.get_chainstate_path();
-        let mut unprocessed_block_samples : VecDeque<i64> = VecDeque::new();
-        let max_samples = 30;
-        let max_staging = 10;
-        let delay_heuristic = 60_000;       // 1 minute
+        let mut pox_watchdog = PoxSyncWatchdog::new(chainstate_path);
+        let mut burnchain_height = 1;
 
-        // how often to sync
-        let sync_frequency = 10;
-        let mut sync_deadline = get_epoch_time_secs() + sync_frequency;
+        // prepare to fetch the first reward cycle!
+        target_burnchain_block_height = pox_constants.reward_cycle_length as u64;
 
         loop {
-            let mut total_blocks_seen = 0;
-            let mut total_recent_changes = 0;
-            let do_sync = match self.count_attachable_stacks_blocks(&chainstate_path, max_staging) {
-                Ok(num_available) => {
-                    unprocessed_block_samples.push_back(num_available as i64);
-                    if unprocessed_block_samples.len() > max_samples {
-                        unprocessed_block_samples.pop_front();
-                    }
+            // wait until it's okay to process the next sortitions
+            pox_watchdog.pox_sync_wait(&burnchain, &burnchain_tip, burnchain_height); 
 
-                    if num_available > 0 || unprocessed_block_samples.len() < max_samples {
-                        // wait for a bit before asking again (up to a minute total)
-                        sleep_ms((delay_heuristic / max_samples) as u64);
-                    }
-
-                    // take first derivative of samples
-                    let mut deltas = vec![];
-                    let mut prev = *unprocessed_block_samples.front().unwrap_or(&(max_staging as i64));
-                    for (i, sample) in unprocessed_block_samples.iter().enumerate() {
-                        total_blocks_seen += sample;
-                        if i == 0 {
-                            continue;
-                        }
-                        let delta = sample - prev;
-                        prev = *sample;
-                        deltas.push(delta);
-                    }
-
-                    // if first-derivative is `y = 0` for the last 60 seconds, then we can infer
-                    // that we've processed all of the burnchain blocks -- no more are likely to
-                    // arrive late (but we can recover via the PoX unhappy path if that happens)
-                    let mut flatlined = true;
-                    for d in deltas.iter() {
-                        if *d != 0 {
-                            total_recent_changes += 1;
-                            flatlined = false;
-                        }
-                    }
-
-                    flatlined && total_blocks_seen == 0
-                },
-                Err(msg) => {
-                    warn!("{}", msg);
-                    false
-                }
-            };
-
-            if !do_sync {
-                // still working on blocks
-                debug!("Still downloading blocks; will not sync burnchain yet (total blocks seen in sample: {}; total number of changes in sample: {})", total_blocks_seen, total_recent_changes);
-                sleep_ms((delay_heuristic / max_samples) as u64);
-                continue;
-            }
-
-            let now = get_epoch_time_secs();
-            if now < sync_deadline {
-                debug!("Wait until {} to sync with the burnchain", sync_deadline);
-                sleep_ms((sync_deadline - now) * 1000);
-            }
-
-            sync_deadline = get_epoch_time_secs() + sync_frequency;
-
-            let (next_burnchain_tip, burnchain_height) = match burnchain.sync() {
+            let (next_burnchain_tip, next_burnchain_height) = match burnchain.sync(Some(target_burnchain_block_height)) {
                 Ok(x) => x,
                 Err(e) => {
                     warn!("Burnchain controller stopped: {}", e);
@@ -273,7 +342,9 @@ impl RunLoop {
                 }
             };
 
+            target_burnchain_block_height += pox_constants.reward_cycle_length as u64;
             burnchain_tip = next_burnchain_tip;
+            burnchain_height = next_burnchain_height;
 
             let sortition_tip = &burnchain_tip.block_snapshot.sortition_id;
             let next_height = burnchain_tip.block_snapshot.block_height;
