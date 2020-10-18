244ddbc42a46d6cd0ce5dd39acd956721c3e6721
Jude Nelson <judecn@gmail.com>
2020-10-18 16:32:13 -0400

    make faucet port configurable, fix typo

--- net-test/bin/config.sh
+++ net-test/bin/config.sh
@@ -1,7 +1,7 @@
 # local config
 __ROOT="$(realpath "$(pwd)"/..)"
-__ETC="$_ROOT/etc"
-__MNT="$_ROOT/mnt"
+__ETC="$__ROOT/etc"
+__MNT="$__ROOT/mnt"
 
 BITCOIN_CONF="$__ETC/bitcoin.conf"
 BITCOIN_CONTROLLER_CONF="$__ETC/bitcoin-neon-controller.toml"
@@ -18,6 +18,8 @@ STACKS_FOLLOWER_LOGFILE="$__MNT/stacks-node-follower.log"
 FAUCET_LOGFILE="$__MNT/faucet.log"
 
 STACKS_MASTER_PUBLIC_IP="127.0.0.1"
+
+FAUCET_PORT=8080
 
 __NOW="$(date +%s)"
 STACKS_CHAINSTATE_DIR="$__MNT/stacks-chainstate-$__NOW"
