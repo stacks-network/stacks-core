e29668cde4d182cd41573b1f81f2284c0a325d8d
CharlieC3 <2747302+CharlieC3@users.noreply.github.com>
2020-11-20 11:42:57 -0500

    chore: fix rustfmt in gh action

--- src/util/vrf.rs
+++ src/util/vrf.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
