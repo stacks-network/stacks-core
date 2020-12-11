e9c6e23494b8b4f0199bab2a0f0ae0c51503d189
Matthew Little <zone117x@gmail.com>
2020-12-11 23:36:11 +0100

    fix: rustfmt

--- testnet/stacks-node/src/genesis_data.rs
+++ testnet/stacks-node/src/genesis_data.rs
@@ -2,7 +2,7 @@ use stx_genesis::GenesisData;
 
 // Uses the full production genesis chainstate.txt data when compiled regularly, .e.g. `cargo build`.
 // Uses a small test chainstate.txt file when tests are ran regularly, .e.g. `cargo test`.
-// The production file can be used in tests by specifying the `prod-genesis-chainstate` feature 
+// The production file can be used in tests by specifying the `prod-genesis-chainstate` feature
 // flag, .e.g. `cargo test --features prod-genesis-chainstate`
 
 #[cfg(any(not(test), feature = "prod-genesis-chainstate"))]
