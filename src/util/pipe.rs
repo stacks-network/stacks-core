d773f1a6b26dcab3df2d6a68cfa00b930961b4a4
Aaron Blankstein <aaron@blockstack.com>
2020-12-31 11:24:58 -0600

    fix pipe unit test

--- src/util/pipe.rs
+++ src/util/pipe.rs
@@ -412,7 +412,8 @@ mod test {
 
     #[test]
     fn test_connection_pipe_producer_consumer() {
-        let mut buf = Box::new([0u8; 1048576]); // 1 MB
+        let mut buf = vec![0u8; 1048576]; // 1 MB
+
         let mut rng = rand::thread_rng();
         rng.fill_bytes(&mut *buf);
 
