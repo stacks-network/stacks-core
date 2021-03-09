db500a90eda71c35c4af9c82a3e62d70af3f4435
wileyj <wileyj@users.noreply.github.com>
2021-03-09 16:49:39 -0500

    rustfmt

--- src/monitoring/prometheus.rs
+++ src/monitoring/prometheus.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 use prometheus::{Histogram, IntCounter, IntCounterVec, IntGauge, IntGaugeVec};
 
 lazy_static! {
