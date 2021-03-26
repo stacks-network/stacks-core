3922e7dd5f3e6611c5c28725b95ae1247287218e
Jude Nelson <judecn@gmail.com>
2021-03-26 01:59:19 -0400

    cargo fmt

--- src/net/download.rs
+++ src/net/download.rs
@@ -2079,7 +2079,8 @@ impl PeerNetwork {
             debug!("{:?}: Scheduling full downloader re-scan", &self.local_peer);
             0
         } else {
-            let start_reward_cycle = (self.pox_id().num_inventory_reward_cycles() as u64).saturating_sub(self.connection_opts.inv_reward_cycles);
+            let start_reward_cycle = (self.pox_id().num_inventory_reward_cycles() as u64)
+                .saturating_sub(self.connection_opts.inv_reward_cycles);
             let start_sortition_height = self
                 .burnchain
                 .reward_cycle_to_block_height(start_reward_cycle)
