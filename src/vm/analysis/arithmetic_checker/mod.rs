d6bab83a3e1f72f8fe7f858deea19b52196521cb
Greg Coppola <greg@hiro.so>
2021-06-09 13:28:39 -0500

    Expand a single-line case to whole block for visibility

--- src/vm/analysis/arithmetic_checker/mod.rs
+++ src/vm/analysis/arithmetic_checker/mod.rs
@@ -190,7 +190,10 @@ impl<'a> ArithmeticOnlyChecker<'a> {
             | ConsSome | ConsOkay | ConsError | DefaultTo | UnwrapRet | UnwrapErrRet | IsOkay
             | IsNone | Asserts | Unwrap | UnwrapErr | IsErr | IsSome | TryRet | ToUInt | ToInt
             | BuffToIntLe | BuffToUIntLe | BuffToIntBe | BuffToUIntBe | Len | Begin
-            | TupleMerge => self.check_all(args),
+            | TupleMerge => {
+                // Check all arguments.
+                self.check_all(args)
+            }
             // we need to treat all the remaining functions specially, because these
             //   do not eval all of their arguments (rather, one or more of their arguments
             //   is a name)
