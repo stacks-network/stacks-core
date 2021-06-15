a6ff07bd3d20c5cc97c5ba8f1fcee400b59a58b9
Greg Coppola <greg@hiro.so>
2021-06-15 17:30:53 -0500

    test is running

--- src/vm/analysis/arithmetic_checker/mod.rs
+++ src/vm/analysis/arithmetic_checker/mod.rs
@@ -181,7 +181,7 @@ impl<'a> ArithmeticOnlyChecker<'a> {
             | AsContract | ElementAt | IndexOf | Map | Filter | Fold => {
                 return Err(Error::FunctionNotPermitted(function));
             }
-            BuffToIntLe | BuffToUIntLe | BuffToIntBe | BuffToUIntBe => {
+            BuffToIntLe | BuffToUIntLe | BuffToIntBe | BuffToUIntBe | PrincipalMatches => {
                 return Err(Error::FunctionNotPermitted(function));
             }
             Sha512 | Sha512Trunc256 | Secp256k1Recover | Secp256k1Verify | Hash160 | Sha256
