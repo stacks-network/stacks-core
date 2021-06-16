a621b20c4ba936e34c029cde5b34b578580967ab
Greg Coppola <greg@hiro.so>
2021-06-16 09:22:15 -0500

    Added skeleton for new functions

--- src/vm/tests/principals.rs
+++ src/vm/tests/principals.rs
@@ -9,8 +9,8 @@ use vm::ClarityVersion;
 
 #[test]
 fn test_simple_principal_one() {
-	// For little endian, 0001 at the beginning should be interpreted as the least significant bit.
-	let good1_test = "(principal-matches 'SZ2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKQ9H6DPR)";
-	let good1_expected = Value::UInt(256);
-	assert_eq!(good1_expected, execute_v2(good1_test).unwrap().unwrap());
+    // For little endian, 0001 at the beginning should be interpreted as the least significant bit.
+    let good1_test = "(principal-matches 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6)";
+    let good1_expected = Value::UInt(256);
+    assert_eq!(good1_expected, execute_v2(good1_test).unwrap().unwrap());
 }
