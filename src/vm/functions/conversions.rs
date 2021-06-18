dd674a26c6ac4c197658a8f1cf5ee3ee67f20729
Greg Coppola <greg@hiro.so>
2021-06-18 10:50:41 -0500

    Initial import of string/int features

--- src/vm/functions/conversions.rs
+++ src/vm/functions/conversions.rs
@@ -113,3 +113,107 @@ pub fn native_buff_to_uint_be(value: Value) -> Result<Value> {
     }
     return buff_to_int_generic(value, EndianDirection::BigEndian, convert_to_uint_be);
 }
+
+pub fn native_string_to_int_generic(
+    value: Value,
+    conversion_fn: fn(String) -> Result<Value>,
+) -> Result<Value> {
+    match value {
+        Value::Sequence(SequenceData::String(CharType::ASCII(ASCIIData { data }))) => {
+            let as_string = String::from_utf8(data).unwrap();
+            return conversion_fn(as_string);
+        }
+        Value::Sequence(SequenceData::String(CharType::UTF8(UTF8Data { data }))) => {
+            let flat = data.into_iter().flatten().collect();
+            let as_string = String::from_utf8(flat).unwrap();
+            return conversion_fn(as_string);
+        }
+        _ => {
+            return Err(CheckErrors::UnionTypeError(
+                vec![
+                    TypeSignature::max_string_ascii(),
+                    TypeSignature::max_string_utf8(),
+                ],
+                TypeSignature::type_of(&value),
+            )
+            .into())
+        }
+    };
+}
+
+pub fn native_string_to_int(value: Value) -> Result<Value> {
+    fn convert_to_int(raw_string: String) -> Result<Value> {
+        let possible_int = raw_string.parse::<i128>();
+        match possible_int {
+            Ok(val) => return Ok(Value::Int(val)),
+            Err(error) => return Err(CheckErrors::InvalidCharactersDetected.into()),
+        }
+    }
+    return native_string_to_int_generic(value, convert_to_int);
+}
+
+pub fn native_string_to_uint(value: Value) -> Result<Value> {
+    fn convert_to_uint(raw_string: String) -> Result<Value> {
+        let possible_int = raw_string.parse::<u128>();
+        match possible_int {
+            Ok(val) => return Ok(Value::UInt(val)),
+            Err(error) => return Err(CheckErrors::InvalidCharactersDetected.into()),
+        }
+    }
+    return native_string_to_int_generic(value, convert_to_uint);
+}
+
+pub fn native_int_to_string_generic(
+    value: Value,
+    conversion_fn: fn(String) -> Value,
+) -> Result<Value> {
+    match value {
+        Value::Int(ref int_value) => {
+            let as_string = int_value.to_string();
+            let value = conversion_fn(as_string);
+            return Ok(value);
+        }
+        Value::UInt(ref uint_value) => {
+            let as_string = uint_value.to_string();
+            let value = conversion_fn(as_string);
+            return Ok(value);
+        }
+        _ => {
+            return Err(CheckErrors::UnionTypeError(
+                vec![TypeSignature::IntType, TypeSignature::UIntType],
+                TypeSignature::type_of(&value),
+            )
+            .into())
+        }
+    };
+}
+
+pub fn native_int_to_ascii(value: Value) -> Result<Value> {
+    // Given a string representing an integer, convert this to Clarity ASCII value.
+    fn convert_to_ascii(raw_string: String) -> Value {
+        let data_vec = raw_string.as_bytes().to_vec();
+        return Value::Sequence(SequenceData::String(CharType::ASCII(ASCIIData {
+            data: data_vec,
+        })));
+    }
+    return native_int_to_string_generic(value, convert_to_ascii);
+}
+
+pub fn native_int_to_utf8(value: Value) -> Result<Value> {
+    // Given a string representing an integer, convert this to Clarity UTF8 value.
+    // The conversion of an integer into a string is going to be ASCII-compliant, therefor, we just need
+    // to wrap each individual character in another vector.
+    fn convert_to_utf8(raw_string: String) -> Value {
+        let data_vec = raw_string.as_bytes().to_vec();
+        let mut wrapped_vec = Vec::new();
+        for i in data_vec {
+            let mut inner_vec = Vec::new();
+            inner_vec.push(i);
+            wrapped_vec.push(inner_vec);
+        }
+        return Value::Sequence(SequenceData::String(CharType::UTF8(UTF8Data {
+            data: wrapped_vec,
+        })));
+    }
+    return native_int_to_string_generic(value, convert_to_utf8);
+}
