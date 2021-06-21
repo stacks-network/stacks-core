6fca774bfd24a85e5aed468f45c86f1ae0c77ae0
Greg Coppola <greg@hiro.so>
2021-06-21 18:31:44 -0500

    piped in clarity version to type checker

--- src/vm/analysis/type_checker/natives/mod.rs
+++ src/vm/analysis/type_checker/natives/mod.rs
@@ -529,7 +529,7 @@ impl TypedNativeFunction {
         match self {
             Special(SpecialNativeFunction(check)) => check(checker, args, context),
             Simple(SimpleNativeFunction(function_type)) => {
-                checker.type_check_function_type(function_type, args, context)
+                checker.type_check_function_type(function_type, args, context, checker.clarity_version.clone())
             }
         }
     }
