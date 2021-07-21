1fefdbba5256be5f78dece3f84838ce690379e1c
Gregory Coppola <60008382+gregorycoppola@users.noreply.github.com>
2021-07-21 14:46:21 -0500

    fix build

--- src/vm/database/clarity_db.rs
+++ src/vm/database/clarity_db.rs
@@ -705,7 +705,7 @@ impl<'a> ClarityDatabase<'a> {
     ) -> BurnchainHeaderHash {
         self.burn_state_db
             .get_burn_header_hash_using_consensus_hash(burnchain_block_height)
-            .expect("Failed to get block data.");
+            .expect("Failed to get block data.")
     }
 
     pub fn get_burnchain_block_height(&mut self, id_bhh: &StacksBlockId) -> Option<u32> {
