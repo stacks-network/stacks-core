daa9176567ba8d5a6ee3eeb7f10d27c9f2cc0895
Gregory Coppola <60008382+gregorycoppola@users.noreply.github.com>
2021-09-13 20:16:48 -0500

    removed events

--- src/chainstate/stacks/db/blocks.rs
+++ src/chainstate/stacks/db/blocks.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use chainstate::stacks::events::*;
 use std::cmp;
 use std::collections::{HashMap, HashSet};
 use std::convert::From;
