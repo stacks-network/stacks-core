f689ee062573147b385ba5bc7da28e692858fcd1
Pavitthra Pandurangan <paviusa23@gmail.com>
2021-10-07 14:34:26 -0400

    Added logging for data

--- parse_profile.py
+++ parse_profile.py
@@ -1,5 +1,6 @@
-import json
 from collections import Counter, defaultdict
+import csv
+import json
 import numpy as np
 import sys
 
@@ -81,7 +82,7 @@ def process_events(events):
             tx_costs[tx_id] = tx_cost
         # Q4
         elif name == "Frequencies of all events for a block":
-            burn_height = event["details"]["burn_header_height"],
+            burn_height = event["details"]["burn_header_height"]
             data = BlockTxData(
                 event["details"]["stacks_block_id"],
                 event["details"]["burn_header_height"],
@@ -109,16 +110,16 @@ def process_events(events):
         # Q6
         elif name == "End of processing block":
             elapsed = event["details"]["timestamp"] - event["details"]["start_timestamp"]
-            block_processing_times.append(elapsed)
+            block_processing_times.append((event["details"]["timestamp"], event["details"]["start_timestamp"], elapsed))
         # Q7
         elif name == "Node starting up":
             node_start_time = event["details"]["timestamp"]
         elif name == "Node fully caught up":
             if node_startup_time == None and node_start_time != None:
-                node_startup_time = event["details"]["timestamp"] - node_start_time
+                node_startup_time = (node_start_time, event["details"]["timestamp"], event["details"]["timestamp"] - node_start_time)
         # Q8
         elif name == "Miner assembled block":
-            mining_times.append(event["details"]["mining_time"])
+            mining_times.append((event["details"]["block_header_hash"], event["details"]["mining_time"]))
 
     return (block_tenure_stats, unmatched_block_tenure, tx_costs, block_limit, block_tx_data, microblock_limit_hit,
             anchored_block_limit_hit, block_processing_times, node_startup_time, mining_times)
@@ -187,7 +188,9 @@ def compute_stats_for_block_commitment(commitment_map):
     if num_values == 0:
         print("No values...")
         return
-    values = np.array(list(commitment_map.values()))
+    elapsed_times = [elapsed for  (_, _, elapsed) in list(commitment_map.values())]
+    # values = np.array(list(commitment_map.values()))
+    values = np.array(elapsed_times)
     f_perc = np.percentile(values, 5)
     avg = np.percentile(values, 50)
     nf_perc = np.percentile(values, 95)
@@ -205,17 +208,17 @@ def compute_stats_for_block_commitment(commitment_map):
 #   --> some start_times may not be associated with any end_time (for example, for the blocks downloaded during ibd)
 # canonical_chain - map of height to BHH of canonical chain
 # QUESTION: how to get the canonical block chain?
-def compute_q1_and_q2(data):
+def compute_q1_and_q2(data, raw_data_dir):
     fastest_block_tracker = {}
     good_block_tracker = {}
 
     for block_data in data:
         block_prod_time = block_data.end_time - block_data.start_time
-        if block_data.height not in fastest_block_tracker or block_prod_time < fastest_block_tracker[block_data.height]:
-            fastest_block_tracker[block_data.height] = block_prod_time
+        if block_data.height not in fastest_block_tracker or block_prod_time < fastest_block_tracker[block_data.height][2]:
+            fastest_block_tracker[block_data.height] = (block_data.start_time, block_data.end_time, block_prod_time)
 
-        if block_data.is_good_commitment == True and (block_data.height not in good_block_tracker or block_prod_time < good_block_tracker[block_data.height]):
-            good_block_tracker[block_data.height] = block_prod_time
+        if block_data.is_good_commitment == True and (block_data.height not in good_block_tracker or block_prod_time < good_block_tracker[block_data.height][2]):
+            good_block_tracker[block_data.height] = (block_data.start_time, block_data.end_time, block_prod_time)
 
     # Q1
     print("\nAnswering Q1: What is the average time (in ms) a synchronized miner takes to broadcast a block commitment?")
@@ -224,6 +227,23 @@ def compute_q1_and_q2(data):
     # Q2
     print("\nAnswering Q2: What is the average time (in ms) a synchronized miner takes to broadcast their first good block commitment?")
     compute_stats_for_block_commitment(good_block_tracker)
+
+    file_path = raw_data_dir + "/" +"q1.csv"
+    headers = ["start_time (ms)", "end_time (ms)", "elapsed_time (ms)"]
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for data in fastest_block_tracker.values():
+            row = [data[0], data[1], data[2]]
+            writer.writerow(row)
+
+    file_path = raw_data_dir + "/" +"q2.csv"
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        for data in good_block_tracker.values():
+            row = [data[0], data[1], data[2]]
+            writer.writerow(row)
 
 ## Test for Q1/ Q2
 ## test case:
@@ -248,16 +268,22 @@ def compute_q1_and_q2(data):
 # Q3:  What distribution of transactions are limited by runtime, read count, write count, etc.?
 # tx_cost_data - a list of ExecutionCost structs (each represents the cost of one transaction
 # block_limit - an ExecutionCost struct representing the block limit
-def compute_q3(tx_cost_data, block_limit):
+def compute_q3(tx_cost_data, block_limit, raw_data_dir):
+    log_data = []
     max_dim_map = defaultdict(int)
     max_dim_counter = 0
     num_txs = len(tx_cost_data)
     for _txid, cost_struct in tx_cost_data.items():
+        curr_row_data = [cost_struct.runtime, cost_struct.read_count, cost_struct.read_length, cost_struct.write_count, cost_struct.write_length]
         cost_percent_struct = cost_struct.get_percent_struct(block_limit)
+        curr_row_data.extend([cost_percent_struct.runtime, cost_percent_struct.read_count, cost_percent_struct.read_length, cost_percent_struct.write_count, cost_percent_struct.write_length])
         max_dim = cost_percent_struct.max()
+        curr_row_data.append(max_dim)
         if max_dim != None:
             max_dim_map[max_dim] += 1
             max_dim_counter += 1
+
+        log_data.append(curr_row_data)
 
     print("\nAnswering Q3: Determine distribution of transactions limited by each cost dimension.")
     total = sum(max_dim_map.values())
@@ -265,7 +291,14 @@ def compute_q3(tx_cost_data, block_limit):
         max_dim_map[k] = max_dim_map[k]*100/total
         print(k,  max_dim_map[k])
     print(max_dim_counter, "out of", num_txs, "transactions had singularly large cost dimension.", max_dim_counter/num_txs * 100, "%.")
-    return max_dim_map
+
+    file_path = raw_data_dir + "/" +"q3.csv"
+    headers = ["runtime", "read_count", "read_length", "write_count", "write_length", "runtime (%)", "read_count (%)", "read_length (%)", "write_count (%)", "write_length (%)", "max_dimension"]
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+
+        writer.writerows(log_data)
 
 ## test case:
 ## runtime, read_count, read_length, write_count, write_length
@@ -296,9 +329,10 @@ class BlockTxData:
 # Q4: What are the relative frequencies of different event types? (this breaks down by contract)
 # block_tx_data - map of burn block height to BlockTxData
 # min_burn_height - only compute stats for blocks with a height greater than or equal to this value
-def compute_q4(block_tx_data, min_burn_height=None):
+def compute_q4(block_tx_data, raw_data_dir, min_burn_height=None):
     all_block_num_events = []
     all_block_event_counter = defaultdict(int)
+    log_dict = {}
     for burn_height, curr_block_data in block_tx_data.items():
         # If min_burn_height is set, don't consider blocks with lesser heights
         if min_burn_height != None and burn_height < min_burn_height:
@@ -311,11 +345,16 @@ def compute_q4(block_tx_data, min_burn_height=None):
             total_event_count += event_count
             all_block_event_counter[event_id] += event_count
         all_block_num_events.append(total_event_count)
+        log_dict[burn_height] = curr_block_data.event_frequency_map
 
     print("\nAnswering Q4: stats on all blocks")
     avg_events_per_block =  np.average(np.asarray(all_block_num_events))
     print("Average number of events per block: ", avg_events_per_block)
     print("Total event map: ", all_block_event_counter)
+
+    file_path = raw_data_dir + "/" +"q4.json"
+    with open(file_path, 'w') as f:
+        json.dump(log_dict, f)
 
 
 class BlockLimitHit:
@@ -330,7 +369,7 @@ class BlockLimitHit:
 # { "exceeded_dimensions": _,
 #  "parent_tip_height": _,
 #  "block_limit_hit": _ }
-def compute_q5a(block_limit_hit_data, total_blocks_mined):
+def compute_q5a(block_limit_hit_data, total_blocks_mined, raw_data_dir):
     exceeded_dimension_counter = defaultdict(int)
     total_limits_hit = 0
     for lim_hit in block_limit_hit_data:
@@ -347,6 +386,10 @@ def compute_q5a(block_limit_hit_data, total_blocks_mined):
     print("Num of total blocks:", total_blocks_mined)
     print("Percent breakdown of which limit is hit:", percent_per_dim)
 
+    file_path = raw_data_dir + "/" +"q5.json"
+    with open(file_path, 'w') as f:
+        json.dump(block_limit_hit_data, f)
+
 class MicroblockLimitHit:
     def __init__(self, exceeded_dimensions, anchor_block_tip_height, sequence_number):
         self.exceeded_dimensions = exceeded_dimensions
@@ -359,7 +402,7 @@ class MicroblockLimitHit:
 # { "exceeded_dimensions": _,
 # "anchor_block_tip_height": _,
 # "sequence_number": _ }
-def compute_q5b(microblock_limit_hit_data):
+def compute_q5b(microblock_limit_hit_data, raw_data_dir):
     exceeded_dimension_counter = defaultdict(int)
     total_limits_hit = 0
     for lim_hit in microblock_limit_hit_data:
@@ -375,10 +418,14 @@ def compute_q5b(microblock_limit_hit_data):
     print("Num of full microblocks:", len(microblock_limit_hit_data))
     print("Percent breakdown of which limit is hit:", percent_per_dim)
 
+    file_path = raw_data_dir + "/" +"q5b.json"
+    with open(file_path, 'w') as f:
+        json.dump(microblock_limit_hit_data, f)
 
 # Q6: How long does it take a follower to process an epoch? (logs time in `append_block`)
-def compute_q6(timestamps):
-    data = np.array(timestamps)
+def compute_q6(processing_times, raw_data_dir):
+    elapsed_times = [elapsed for (_, _, elapsed) in processing_times]
+    data = np.array(elapsed_times)
     f_perc = np.percentile(data, 5)
     avg = np.percentile(data, 50)
     nf_perc = np.percentile(data, 95)
@@ -387,15 +434,31 @@ def compute_q6(timestamps):
     print("5th percentile:", f_perc)
     print("50th percentile:", avg)
     print("95th percentile:", nf_perc)
-    print("Num values:", len(timestamps))
+    print("Num values:", len(elapsed_times))
+
+    file_path = raw_data_dir + "/" +"q6.csv"
+    headers = ["start_time (ms)", "end_time (ms)", "elapsed_time (ms)"]
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(processing_times)
+
 
 # Q7: No logging - how long does it take to start a non-sidecar follower from genesis?
-def compute_q7(node_startup_time):
+def compute_q7(node_startup_data, raw_data_dir):
     print("\nAnswering Q7: amount of time (in secs) to start a node from genesis")
-    print("Elapsed time (secs): ", node_startup_time)
+    print("Elapsed time (secs): ", node_startup_data[2])
+
+    file_path = raw_data_dir + "/" +"q7.csv"
+    headers = ["start_time (ms)", "end_time (ms)", "elapsed_time (ms)"]
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerow(node_startup_data)
 
 # Q8: How long does it take a miner to assemble a block?
-def compute_q8(mining_times):
+def compute_q8(mining_data, raw_data_dir):
+    mining_times = [mining_time for (_, mining_time) in mining_data]
     data = np.array(mining_times)
     f_perc = np.percentile(data, 5)
     avg = np.percentile(data, 50)
@@ -407,7 +470,15 @@ def compute_q8(mining_times):
     print("95th percentile:", nf_perc)
     print("Num values:", len(mining_times))
 
-def compute_stats_from_log_file(file_name="sample_logs.txt"):
+    file_path = raw_data_dir + "/" +"q8.csv"
+    headers = ["block_header_hash", "mining_time (ms)"]
+    with open(file_path, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+        writer.writerows(mining_data)
+
+
+def compute_stats_from_log_file(file_name="sample_logs.txt", raw_data_dir="raw_data"):
     events = parse_logs(file_name)
     (block_tenure_stats, unmatched_block_tenure, tx_costs, block_limit, block_tx_data, microblock_limit_hit,
      anchored_block_limit_hit, block_processing_times, node_startup_time, mining_times) = process_events(events)
@@ -416,14 +487,14 @@ def compute_stats_from_log_file(file_name="sample_logs.txt"):
 
     print("Starting computation of block stats...")
 
-    compute_q1_and_q2(block_tenure_stats)
-    compute_q3(tx_costs, block_limit)
-    compute_q4(block_tx_data)
-    compute_q5a(anchored_block_limit_hit, len(mining_times))
-    compute_q5b(microblock_limit_hit)
-    compute_q6(block_processing_times)
+    compute_q1_and_q2(block_tenure_stats, raw_data_dir)
+    compute_q3(tx_costs, block_limit, raw_data_dir)
+    compute_q4(block_tx_data, raw_data_dir)
+    compute_q5a(anchored_block_limit_hit, len(mining_times), raw_data_dir)
+    compute_q5b(microblock_limit_hit, raw_data_dir)
+    compute_q6(block_processing_times, raw_data_dir)
     compute_q7(node_startup_time)
-    compute_q8(mining_times)
+    compute_q8(mining_times, raw_data_dir)
 
 ##### Call computation functions
 ## JSON data for transactions
