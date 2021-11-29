4a6d371c3f55cec4f93be79848634194ae479728
Greg Coppola <greg@hiro.so>
2021-11-29 19:52:18 +0000

    added execute_with_parameters

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -383,8 +383,8 @@ pub fn eval_all(
 /// that the result is the same before returning the result
 #[cfg(test)]
 pub fn execute_on_network(program: &str, use_mainnet: bool) -> Result<Option<Value>> {
-    let epoch_200_result = execute_in_epoch(program, StacksEpochId::Epoch20, use_mainnet);
-    let epoch_205_result = execute_in_epoch(program, StacksEpochId::Epoch2_05, use_mainnet);
+    let epoch_200_result = execute_with_parameters(program, ClarityVersion::Clarity2, StacksEpochId::Epoch20, use_mainnet);
+    let epoch_205_result = execute_with_parameters(program, ClarityVersion::Clarity2, StacksEpochId::Epoch2_05, use_mainnet);
     assert_eq!(
         epoch_200_result, epoch_205_result,
         "Epoch 2.0 and 2.05 should have same execution result, but did not for program `{}`",
@@ -400,16 +400,17 @@ pub fn execute(program: &str) -> Result<Option<Value>> {
 }
 
 #[cfg(test)]
-pub fn execute_in_epoch(
+pub fn execute_with_parameters(
     program: &str,
+    clarity_version:ClarityVersion,
     epoch: StacksEpochId,
     use_mainnet: bool,
 ) -> Result<Option<Value>> {
     let contract_id = QualifiedContractIdentifier::transient();
-    let mut contract_context = ContractContext::new(contract_id.clone());
+    let mut contract_context = ContractContext::new(contract_id.clone(), clarity_version);
     let mut marf = MemoryBackingStore::new();
     let conn = marf.as_clarity_db();
-    let mut global_context = GlobalContext::new(false, conn, LimitedCostTracker::new_free(), epoch);
+    let mut global_context = GlobalContext::new(use_mainnet, conn, LimitedCostTracker::new_free(), epoch);
     global_context.execute(|g| {
         let parsed = ast::build_ast(&contract_id, program, &mut ())?.expressions;
         eval_all(&parsed, &mut contract_context, g)
--- src/vm/tests/principals.rs
+++ src/vm/tests/principals.rs
@@ -1,4 +1,4 @@
-use vm::execute_against_version_and_network;
+use vm::execute_with_parameters;
 use vm::types::BufferLength;
 use vm::types::SequenceSubtype::{BufferType, StringType};
 use vm::types::StringSubtype::ASCII;
@@ -19,12 +19,13 @@ use vm::types::{QualifiedContractIdentifier, TypeSignature};
 use vm::{
     CallStack, ContractContext, Environment, GlobalContext, LocalContext, SymbolicExpression,
 };
+use crate::core::StacksEpochId;
 
 #[test]
 fn test_simple_is_standard_check_inputs() {
     let wrong_type_test = "(is-standard u10)";
     assert_eq!(
-        execute_against_version_and_network(wrong_type_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(wrong_type_test, ClarityVersion::Clarity2, StacksEpochId::Epoch21, true)
             .unwrap_err(),
         CheckErrors::TypeValueError(PrincipalType, Value::UInt(10)).into()
     );
@@ -35,13 +36,13 @@ fn test_simple_is_standard_testnet_cases() {
     let testnet_addr_test = "(is-standard 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
@@ -49,13 +50,13 @@ fn test_simple_is_standard_testnet_cases() {
     let testnet_addr_test = "(is-standard 'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6.tokens)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
@@ -63,13 +64,13 @@ fn test_simple_is_standard_testnet_cases() {
     let testnet_addr_test = "(is-standard 'SN2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKP6D2ZK9)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
@@ -77,13 +78,13 @@ fn test_simple_is_standard_testnet_cases() {
     let testnet_addr_test = "(is-standard 'SN2J6ZY48GV1EZ5V2V5RB9MP66SW86PYKKP6D2ZK9.tokens)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(testnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(testnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
@@ -93,13 +94,13 @@ fn test_simple_is_standard_mainnet_cases() {
     let mainnet_addr_test = "(is-standard 'SP3X6QWWETNBZWGBK6DRGTR1KX50S74D3433WDGJY)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
@@ -107,13 +108,13 @@ fn test_simple_is_standard_mainnet_cases() {
     let mainnet_addr_test = "(is-standard 'SP3X6QWWETNBZWGBK6DRGTR1KX50S74D3433WDGJY.tokens)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
@@ -121,13 +122,13 @@ fn test_simple_is_standard_mainnet_cases() {
     let mainnet_addr_test = "(is-standard 'SM3X6QWWETNBZWGBK6DRGTR1KX50S74D3433WDGJY)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
@@ -135,13 +136,13 @@ fn test_simple_is_standard_mainnet_cases() {
     let mainnet_addr_test = "(is-standard 'SM3X6QWWETNBZWGBK6DRGTR1KX50S74D3433WDGJY.tokens)";
     assert_eq!(
         Value::Bool(true),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(mainnet_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(mainnet_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
@@ -153,13 +154,13 @@ fn test_simple_is_standard_undefined_cases() {
     let invalid_addr_test = "(is-standard 'S1G2081040G2081040G2081040G208105NK8PE5)";
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(invalid_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(invalid_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(invalid_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(invalid_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
@@ -167,13 +168,13 @@ fn test_simple_is_standard_undefined_cases() {
     let invalid_addr_test = "(is-standard 'S1G2081040G2081040G2081040G208105NK8PE5.tokens)";
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(invalid_addr_test, ClarityVersion::Clarity2, true)
+        execute_with_parameters(invalid_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,true)
             .unwrap()
             .unwrap()
     );
     assert_eq!(
         Value::Bool(false),
-        execute_against_version_and_network(invalid_addr_test, ClarityVersion::Clarity2, false)
+        execute_with_parameters(invalid_addr_test, ClarityVersion::Clarity2,  StacksEpochId::Epoch21,false)
             .unwrap()
             .unwrap()
     );
