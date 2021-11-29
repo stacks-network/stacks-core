a9d6e50c4e659a578e1b0d0979cbb50e48cb8354
Greg Coppola <greg@hiro.so>
2021-11-29 15:46:53 +0000

    cleaning up test code

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -383,8 +383,8 @@ pub fn eval_all(
 /// that the result is the same before returning the result
 #[cfg(test)]
 pub fn execute_on_network(program: &str, use_mainnet: bool) -> Result<Option<Value>> {
-    let epoch_200_result = execute_in_epoch(program, StacksEpochId::Epoch20, use_mainnet);
-    let epoch_205_result = execute_in_epoch(program, StacksEpochId::Epoch2_05, use_mainnet);
+    let epoch_200_result = execute_against_version_and_network(program, StacksEpochId::Epoch20, use_mainnet);
+    let epoch_205_result = execute_against_version_and_network(program, StacksEpochId::Epoch2_05, use_mainnet);
     assert_eq!(
         epoch_200_result, epoch_205_result,
         "Epoch 2.0 and 2.05 should have same execution result, but did not for program `{}`",
@@ -400,7 +400,7 @@ pub fn execute(program: &str) -> Result<Option<Value>> {
 }
 
 #[cfg(test)]
-pub fn execute_in_epoch(
+pub fn execute_against_version_and_network(
     program: &str,
     epoch: StacksEpochId,
     use_mainnet: bool,
@@ -416,27 +416,18 @@ pub fn execute_in_epoch(
     })
 }
 
-/* Run provided program in a brand new environment, with a transient, empty
- *  database. Only used by CLI and unit tests.
- */
+/// Run provided program in a brand new environment, with a transient, empty
+/// database. Only used by CLI and unit tests.
+#[cfg(test)]
 pub fn execute_against_version(program: &str, version: ClarityVersion) -> Result<Option<Value>> {
     execute_against_version_and_network(program, version, false)
 }
 
-/* Run provided program in a brand new environment, with a transient, empty
- *  database. Only used by CLI and unit tests.
- *
- * This version of the function assumes that the ClarityVersion is Clarity1.
- */
-pub fn execute(program: &str) -> Result<Option<Value>> {
-    execute_against_version(program, ClarityVersion::Clarity1)
-}
-
-/* Run provided program in a brand new environment, with a transient, empty
- *  database. Only used by CLI and unit tests.
- *
- * This version of the function assumes that the ClarityVersion is Clarity2.
- */
+/// Run provided program in a brand new environment, with a transient, empty
+/// database. Only used by CLI and unit tests.
+///
+/// This version of the function assumes that the ClarityVersion is Clarity2.
+#[cfg(test)]
 pub fn execute_v2(program: &str) -> Result<Option<Value>> {
     execute_against_version(program, ClarityVersion::Clarity2)
 }
