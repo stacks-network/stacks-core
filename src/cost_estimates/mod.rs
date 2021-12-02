e5c2f7af63dee520a9591c0a6789df4ef63166d4
Aaron Blankstein <aaron@blockstack.com>
2021-12-02 10:21:36 -0600

    initial implementation of proposals in #2954

--- src/cost_estimates/mod.rs
+++ src/cost_estimates/mod.rs
@@ -13,6 +13,7 @@ use vm::costs::ExecutionCost;
 use burnchains::Txid;
 use chainstate::stacks::db::StacksEpochReceipt;
 
+pub mod fee_medians;
 pub mod fee_scalar;
 pub mod metrics;
 pub mod pessimistic;
