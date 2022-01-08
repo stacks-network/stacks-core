b9fea19d1a25ab9b1d72e0319c78ee2d87f800c0
Greg Coppola <greg@hiro.so>
2022-01-08 18:06:54 +0000

    changed run structure

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 export STACKS_LOG_DEBUG=1
+cd testnet/stacks-node && \
+cargo build --release && \
+cd ../.. && \
 target/release/stacks-node start --config=./testnet/stacks-node/conf/mainnet-miner-conf.toml
