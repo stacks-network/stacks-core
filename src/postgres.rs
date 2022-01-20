17707236ec0032620d9568a99a7b65278a623763
Greg Coppola <greg@hiro.so>
2022-01-20 05:29:54 +0000

    postgres demo

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,14 +1,16 @@
 extern crate postgres;
-use postgres::{Client, NoTls, Error};
+use postgres::{Client, Error, NoTls};
 
 fn main() -> Result<(), Error> {
-    let mut client = Client::connect("postgresql://postgres:postgres@localhost/temp_database", NoTls)?;
-    
+    let mut client = Client::connect(
+        "postgresql://postgres:postgres@localhost/temp_database",
+        NoTls,
+    )?;
+
     client.execute(
         "INSERT INTO mempool_tx_attempt (tx_id, status, comment) VALUES ($1, $2, $3)",
         &[&"id3", &"status3", &"comment3"],
     )?;
 
     Ok(())
-
 }
