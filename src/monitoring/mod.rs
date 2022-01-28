6c41bfe7de25cf4a0b346fcce0ba5bba68a83c99
gregorycoppola <greg@hiro.so>
2022-01-28 16:13:29 +0000

    forgot to format

--- src/monitoring/mod.rs
+++ src/monitoring/mod.rs
@@ -28,6 +28,7 @@ use crate::{
     },
 };
 use burnchains::BurnchainSigner;
+use std::convert::TryInto;
 use std::error::Error;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Mutex;
@@ -35,7 +36,6 @@ use util::db::sqlite_open;
 use util::db::Error as DatabaseError;
 use util::uint::{Uint256, Uint512};
 use vm::costs::ExecutionCost;
-use std::convert::TryInto;
 
 #[cfg(feature = "monitoring_prom")]
 mod prometheus;
