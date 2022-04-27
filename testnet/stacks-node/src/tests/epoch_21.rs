433d54691c799e1f63c9596cd497c77fc98ea49d
Jude Nelson <judecn@gmail.com>
2022-04-27 13:36:54 -0400

    feat: add integration test for get-burn-block-info?

--- testnet/stacks-node/src/tests/epoch_21.rs
+++ testnet/stacks-node/src/tests/epoch_21.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::thread;
 
@@ -5,10 +6,17 @@ use stacks::burnchains::Burnchain;
 use stacks::chainstate::stacks::db::StacksChainState;
 use stacks::chainstate::stacks::StacksBlockHeader;
 use stacks::types::chainstate::StacksAddress;
+use stacks::vm::types::PrincipalData;
 
+use crate::config::Config;
+use crate::config::EventKeyType;
+use crate::config::EventObserverConfig;
+use crate::config::InitialBalance;
 use crate::neon;
+use crate::neon::RunLoopCounter;
 use crate::tests::bitcoin_regtest::BitcoinCoreController;
 use crate::tests::neon_integrations::*;
+use crate::tests::*;
 use crate::BitcoinRegtestController;
 use crate::BurnchainController;
 use stacks::core;
@@ -19,23 +27,34 @@ use stacks::chainstate::burn::distribution::BurnSamplePoint;
 use stacks::burnchains::PoxConstants;
 
 use crate::stacks_common::types::Address;
+use crate::stacks_common::util::hash::hex_bytes;
 
+use stacks_common::types::chainstate::BurnchainHeaderHash;
 use stacks_common::util::secp256k1::Secp256k1PublicKey;
 
-#[test]
-#[ignore]
-fn transition_fixes_utxo_chaining() {
-    // very simple test to verify that the miner will keep making valid (empty) blocks after the
-    // transition.  Really tests that the block-commits are well-formed before and after the epoch
-    // transition.
-    if env::var("BITCOIND_TEST") != Ok("1".into()) {
-        return;
-    }
+use stacks::chainstate::coordinator::comm::CoordinatorChannels;
 
+fn advance_to_2_1(
+    mut initial_balances: Vec<InitialBalance>,
+) -> (
+    Config,
+    BitcoinCoreController,
+    BitcoinRegtestController,
+    RunLoopCounter,
+    CoordinatorChannels,
+) {
     let epoch_2_05 = 210;
     let epoch_2_1 = 215;
 
+    test_observer::spawn();
+
     let (mut conf, miner_account) = neon_integration_test_conf();
+
+    conf.initial_balances.append(&mut initial_balances);
+    conf.events_observers.push(EventObserverConfig {
+        endpoint: format!("localhost:{}", test_observer::EVENT_OBSERVER_PORT),
+        events_keys: vec![EventKeyType::AnyEvent],
+    });
 
     let mut epochs = core::STACKS_EPOCHS_REGTEST.to_vec();
     epochs[1].end_height = epoch_2_05;
@@ -192,7 +211,28 @@ fn transition_fixes_utxo_chaining() {
     let account = get_account(&http_origin, &miner_account);
     assert_eq!(account.nonce, 9);
 
-    eprintln!("Begin epoch 2.1");
+    eprintln!("Begin Stacks 2.1");
+    return (
+        conf,
+        btcd_controller,
+        btc_regtest_controller,
+        blocks_processed,
+        channel,
+    );
+}
+
+#[test]
+#[ignore]
+fn transition_fixes_utxo_chaining() {
+    if env::var("BITCOIND_TEST") != Ok("1".into()) {
+        return;
+    }
+
+    // very simple test to verify that the miner will keep making valid (empty) blocks after the
+    // transition.  Really tests that the block-commits are well-formed before and after the epoch
+    // transition.
+    let (conf, _btcd_controller, mut btc_regtest_controller, blocks_processed, coord_channel) =
+        advance_to_2_1(vec![]);
 
     // post epoch 2.1 -- UTXO chaining should be fixed
     for i in 0..10 {
@@ -227,5 +267,194 @@ fn transition_fixes_utxo_chaining() {
     assert_eq!(burn_sample.len(), 1);
     assert_eq!(burn_sample[0].burns, 10_000);
 
-    channel.stop_chains_coordinator();
+    test_observer::clear();
+    coord_channel.stop_chains_coordinator();
+}
+
+#[test]
+#[ignore]
+fn transition_adds_burn_block_height() {
+    if env::var("BITCOIND_TEST") != Ok("1".into()) {
+        return;
+    }
+
+    // very simple test to verify that after the 2.1 transition, get-burn-block-info? works as
+    // expected
+
+    let spender_sk = StacksPrivateKey::new();
+    let spender_addr = PrincipalData::from(to_addr(&spender_sk));
+    let spender_addr_c32 = StacksAddress::from(to_addr(&spender_sk));
+
+    let (conf, _btcd_controller, mut btc_regtest_controller, blocks_processed, coord_channel) =
+        advance_to_2_1(vec![InitialBalance {
+            address: spender_addr.clone(),
+            amount: 200_000_000,
+        }]);
+    let http_origin = format!("http://{}", &conf.node.rpc_bind);
+
+    // post epoch 2.1 -- we should be able to query any/all burnchain headers after the first
+    // burnchain block height
+    let contract = "
+    (define-private (test-burn-headers-cls (height uint) (base uint))
+        (begin
+            (print { height: (+ base height), hash: (get-burn-block-info? header-hash (+ base height)) })
+            base
+        )
+    )
+    (define-public (test-burn-headers)
+        (begin
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u0
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u25
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u50
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u75
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u100
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u125
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u150
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u175
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u200
+            )
+            (fold test-burn-headers-cls
+                (list u0 u1 u2 u3 u4 u5 u6 u7 u8 u9 u10 u11 u12 u13 u14 u15 u16 u17 u18 u19 u20 u21 u22 u23 u24)
+                u225
+            )
+            (ok u0)
+        )
+    )
+    ";
+
+    let tx = make_contract_publish(
+        &spender_sk,
+        0,
+        (2 * contract.len()) as u64,
+        "test-burn-headers",
+        contract,
+    );
+    submit_tx(&http_origin, &tx);
+
+    // mine it
+    next_block_and_wait(&mut btc_regtest_controller, &blocks_processed);
+    next_block_and_wait(&mut btc_regtest_controller, &blocks_processed);
+
+    let tx = make_contract_call(
+        &spender_sk,
+        1,
+        (2 * contract.len()) as u64,
+        &spender_addr_c32,
+        "test-burn-headers",
+        "test-burn-headers",
+        &[],
+    );
+    submit_tx(&http_origin, &tx);
+
+    let cc_txid = StacksTransaction::consensus_deserialize(&mut &tx[..])
+        .unwrap()
+        .txid();
+
+    // mine it
+    next_block_and_wait(&mut btc_regtest_controller, &blocks_processed);
+    next_block_and_wait(&mut btc_regtest_controller, &blocks_processed);
+
+    // check it
+    let mut header_hashes: HashMap<u64, Option<BurnchainHeaderHash>> = HashMap::new();
+    let blocks = test_observer::get_blocks();
+    for block in blocks {
+        let transactions = block.get("transactions").unwrap().as_array().unwrap();
+        let events = block.get("events").unwrap().as_array().unwrap();
+
+        for tx in transactions.iter() {
+            let raw_tx = tx.get("raw_tx").unwrap().as_str().unwrap();
+            if raw_tx == "0x00" {
+                continue;
+            }
+            let tx_bytes = hex_bytes(&raw_tx[2..]).unwrap();
+            let parsed = StacksTransaction::consensus_deserialize(&mut &tx_bytes[..]).unwrap();
+            if parsed.txid() == cc_txid {
+                // check events for this block
+                for event in events.iter() {
+                    if let Some(cev) = event.get("contract_event") {
+                        // strip leading `0x`
+                        eprintln!("{:#?}", &cev);
+                        let clarity_serialized_value = hex_bytes(
+                            &String::from_utf8(
+                                cev.get("raw_value").unwrap().as_str().unwrap().as_bytes()[2..]
+                                    .to_vec(),
+                            )
+                            .unwrap(),
+                        )
+                        .unwrap();
+                        let clarity_value =
+                            Value::deserialize_read(&mut &clarity_serialized_value[..], None)
+                                .unwrap();
+                        let pair = clarity_value.expect_tuple();
+                        let height = pair.get("height").unwrap().clone().expect_u128() as u64;
+                        let bhh_opt =
+                            pair.get("hash")
+                                .unwrap()
+                                .clone()
+                                .expect_optional()
+                                .map(|inner_buff| {
+                                    let buff_bytes_vec = inner_buff.expect_buff(32);
+                                    let mut buff_bytes = [0u8; 32];
+                                    buff_bytes.copy_from_slice(&buff_bytes_vec[0..32]);
+                                    BurnchainHeaderHash(buff_bytes)
+                                });
+
+                        header_hashes.insert(height, bhh_opt);
+                    }
+                }
+            }
+        }
+    }
+
+    let sortdb = btc_regtest_controller.sortdb_mut();
+    let all_snapshots = sortdb.get_all_snapshots().unwrap();
+    let tip = SortitionDB::get_canonical_burn_chain_tip(sortdb.conn()).unwrap();
+
+    // all block headers are accounted for
+    for sn in all_snapshots.iter() {
+        match header_hashes.get(&sn.block_height) {
+            Some(Some(bhh)) => {
+                assert_eq!(&sn.burn_header_hash, bhh);
+
+                // only defined up to the tip
+                assert!(sn.block_height < tip.block_height);
+            }
+            Some(None) => {
+                // must exceed the tip
+                assert!(sn.block_height >= tip.block_height);
+            }
+            None => {
+                panic!("Missing header for {}", sn.block_height);
+            }
+        }
+    }
+
+    test_observer::clear();
+    coord_channel.stop_chains_coordinator();
 }
