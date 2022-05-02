b60cf7763269b577b8af9a4ad7aaf1b825c84844
Pavitthra Pandurangan <paviusa23@gmail.com>
2022-05-02 10:41:19 -0400

    test: small fix

--- src/burnchains/test.rs
+++ src/burnchains/test.rs
@@ -1297,7 +1297,7 @@ fn create_stacks_event_block_for_block_commit() {
         1,
         "Only one event from the watched contract committed"
     );
-    assert_eq!(stacks_event_block.ops[0].event_index, 2);
+    assert_eq!(stacks_event_block.ops[0].event_index, 0);
 }
 
 #[test]
