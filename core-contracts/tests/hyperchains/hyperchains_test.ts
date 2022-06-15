3c7e348d9738ea7c214bc00449508380ce951165
Greg Coppola <greg@hiro.so>
2022-06-15 12:30:19 -0500

    fix clarinet test

--- core-contracts/tests/hyperchains/hyperchains_test.ts
+++ core-contracts/tests/hyperchains/hyperchains_test.ts
@@ -43,7 +43,7 @@ Clarinet.test({
 
 
         // Try and fail to commit a block at height 1 with an invalid miner.
-        const id_header_hash2 = chain.callReadOnlyFn('hyperchains', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
+        const id_header_hash2 = chain.callReadOnlyFn('test-helpers', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
         block = chain.mineBlock([
             Tx.contractCall("hyperchains", "commit-block",
                 [
@@ -60,7 +60,7 @@ Clarinet.test({
             .expectInt(2);
 
         // Successfully commit block at height 1 with valid miner.
-        const id_header_hash3 = chain.callReadOnlyFn('hyperchains', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
+        const id_header_hash3 = chain.callReadOnlyFn('test-helpers', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
         block = chain.mineBlock([
             Tx.contractCall("hyperchains", "commit-block",
                 [
@@ -179,7 +179,7 @@ Clarinet.test({
 
         let root_hash = new Uint8Array([203, 225, 170, 121, 99, 143, 221, 118, 153, 59, 252, 68, 117, 30, 27, 33, 49, 100, 166, 167, 250, 154, 172, 149, 149, 79, 236, 105, 254, 184, 172, 103]);
         // Miner should commit a block with the appropriate root hash
-        const id_header_hash1 = chain.callReadOnlyFn('hyperchains', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
+        const id_header_hash1 = chain.callReadOnlyFn('test-helpers', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
         block = chain.mineBlock([
             // Successfully commit block at height 0 with alice.
             Tx.contractCall("hyperchains", "commit-block",
@@ -365,7 +365,7 @@ Clarinet.test({
 
         let root_hash = new Uint8Array([203, 225, 170, 121, 99, 143, 221, 118, 153, 59, 252, 68, 117, 30, 27, 33, 49, 100, 166, 167, 250, 154, 172, 149, 149, 79, 236, 105, 254, 184, 172, 103]);
         // Miner should commit a block with the appropriate root hash
-        const id_header_hash1 = chain.callReadOnlyFn('hyperchains', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
+        const id_header_hash1 = chain.callReadOnlyFn('test-helpers', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
         block = chain.mineBlock([
             // Successfully commit block at height 0 with alice.
             Tx.contractCall("hyperchains", "commit-block",
@@ -546,7 +546,7 @@ Clarinet.test({
         // Here we are using the root hash that would be constructed for 3 withdrawal requests.
         // The data used for this can be seen in the test `test_verify_withdrawal_merkle_tree` in `withdrawal.rs`
         let root_hash = new Uint8Array([186, 138, 157, 125, 128, 50, 197, 200, 75, 139, 27, 104, 110, 157, 182, 49, 140, 62, 51, 70, 251, 139, 131, 82, 67, 53, 118, 168, 54, 239, 111, 30]);
-        const id_header_hash1 = chain.callReadOnlyFn('hyperchains', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
+        const id_header_hash1 = chain.callReadOnlyFn('test-helpers', 'get-id-header-hash', [], alice.address).result.expectOk().toString();
         block = chain.mineBlock([
             // Successfully commit block at height 0 with alice.
             Tx.contractCall("hyperchains", "commit-block",
