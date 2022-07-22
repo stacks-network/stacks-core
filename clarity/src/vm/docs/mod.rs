6d87ac108a49ae06e5b8071638dace708bd26b42
Jude Nelson <judecn@gmail.com>
2022-07-21 22:03:45 -0400

    chore: API sync documentation

--- clarity/src/vm/docs/mod.rs
+++ clarity/src/vm/docs/mod.rs
@@ -2560,6 +2560,7 @@ mod test {
                 env.execute_in_env::<_, _, ()>(
                     QualifiedContractIdentifier::local("tokens").unwrap().into(),
                     None,
+                    None,
                     |e| {
                         let mut snapshot = e
                             .global_context
