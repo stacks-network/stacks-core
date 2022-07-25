4b93d1b7cb6c00eea44a7934bf297f21facfa22a
Greg Coppola <greg@hiro.so>
2022-07-25 16:34:45 -0500

    tried to log another choke point but still at the same place
    greg@home-base:~/main$ grep =miss /log/try-mine.4 | wc
      39042  312324 5636171

--- src/chainstate/stacks/index/storage.rs
+++ src/chainstate/stacks/index/storage.rs
@@ -2549,7 +2549,17 @@ impl<'a, T: MarfTrieId> TrieStorageConnection<'a, T> {
                     self.bench.read_node_hash_finish(true);
                     Ok(res)
                 } else {
+                    // Note: Need to log here.
+                    let start_time = Instant::now();
+
                     let node_hash = self.inner_read_persisted_node_hash(block_id, ptr)?;
+
+                    let end_time = Instant::now();
+                    let delta = end_time - start_time;
+                    info!(
+                            "read_node_hash_bytes: result=miss, block_id={}, time_cost={:?}",
+                            block_id, delta
+                        );
                     self.cache
                         .store_node_hash(block_id, ptr.clone(), node_hash.clone());
                     self.bench.read_node_hash_finish(false);
