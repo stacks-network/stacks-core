cd1372b4b541e61cd449ab0028b60e74f19d9704
Jude Nelson <judecn@gmail.com>
2022-09-08 14:35:02 -0400

    feat: get-burn-block-info pox-addrs returns a tuple with a buff 32

--- clarity/src/vm/types/mod.rs
+++ clarity/src/vm/types/mod.rs
@@ -700,7 +700,7 @@ impl BurnBlockInfoProperty {
                         TypeSignature::TupleType(
                             TupleTypeSignature::try_from(vec![
                                 ("version".into(), BUFF_1.clone()),
-                                ("hashbytes".into(), BUFF_20.clone()),
+                                ("hashbytes".into(), BUFF_32.clone()),
                             ])
                             .expect("FATAL: bad type signature for pox addr"),
                         ),
