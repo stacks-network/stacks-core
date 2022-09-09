217aaece48cc70ad69b0c7de71ac927427b082bf
Jude Nelson <judecn@gmail.com>
2022-09-08 21:16:24 -0400

    fix: in the clarity sim environment, we have to instantiate pox-2 explicitly as clarity2

--- src/chainstate/stacks/boot/contract_tests.rs
+++ src/chainstate/stacks/boot/contract_tests.rs
@@ -563,8 +563,13 @@ fn pox_2_contract_caller_units() {
     sim.execute_next_block(|_env| {});
 
     sim.execute_next_block(|env| {
-        env.initialize_contract(POX_2_CONTRACT_TESTNET.clone(), &POX_2_TESTNET_CODE, None)
-            .unwrap()
+        env.initialize_versioned_contract(
+            POX_2_CONTRACT_TESTNET.clone(),
+            ClarityVersion::Clarity2,
+            &POX_2_TESTNET_CODE,
+            None,
+        )
+        .unwrap()
     });
 
     let cc = boot_code_id("stack-through", false);
@@ -572,7 +577,7 @@ fn pox_2_contract_caller_units() {
     sim.execute_next_block(|env| {
         env.initialize_contract(cc.clone(),
                                 "(define-public (cc-stack-stx (amount-ustx uint)
-                                                           (pox-addr (tuple (version (buff 1)) (hashbytes (buff 20))))
+                                                           (pox-addr (tuple (version (buff 1)) (hashbytes (buff 32))))
                                                            (start-burn-ht uint)
                                                            (lock-period uint))
                                    (contract-call? .pox-2 stack-stx amount-ustx pox-addr start-burn-ht lock-period))",
