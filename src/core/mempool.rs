53221cfb167348afae22b9b531a139fbc4b8e6ce
Greg Coppola <greg@hiro.so>
2022-10-04 16:14:36 -0500

    reduce logging

--- src/core/mempool.rs
+++ src/core/mempool.rs
@@ -1225,19 +1225,12 @@ impl MemPoolDB {
         fee_rate_transactions.reverse();
         null_rate_transactions.reverse();
 
-        info!("fee_rate_transactions {:?}", &fee_rate_transactions);
-        info!("null_rate_transactions {:?}", &null_rate_transactions);
-
         let mut buffer = vec![];
         let mut rng = rand::thread_rng();
         let mut fee_cursor = fee_rate_transactions.pop();
         let mut null_cursor = null_rate_transactions.pop();
         while fee_cursor.is_some() || null_cursor.is_some() {
             let f: f64 = rng.gen();
-            info!(
-                "f {} null_cursor {:?} fee_cursor {:?}",
-                &f, &fee_cursor, &null_cursor
-            );
             if (f < null_estimate_fraction && null_cursor.is_some()) || fee_cursor.is_none() {
                 // Assume: null_cursor.is_some()
                 buffer.push(
@@ -1333,7 +1326,7 @@ impl MemPoolDB {
             let lookup_nonce_start = Instant::now();
             let nonces_match = Self::nonces_match_expected(clarity_tx, tx_reduced_info);
             total_lookup_nonce_time += lookup_nonce_start.elapsed();
-            debug!("Nonce check: for tx_reduced_info {:?}, nonces_match={}", tx_reduced_info, nonces_match);
+            test_debug!("Nonce check: for tx_reduced_info {:?}, nonces_match={}", tx_reduced_info, nonces_match);
             if !nonces_match {
                 continue;
             }
