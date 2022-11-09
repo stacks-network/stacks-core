14ef13a04d66136ef37a5f1dd23a756966a4e459
Pavitthra Pandurangan <pavitthp@andrew.cmu.edu>
2022-11-09 13:14:52 -0500

    dupe import

--- testnet/stacks-node/src/run_loop/neon.rs
+++ testnet/stacks-node/src/run_loop/neon.rs
@@ -36,7 +36,6 @@ use crate::monitoring::start_serving_monitoring_metrics;
 use crate::neon_node::Globals;
 use crate::neon_node::StacksNode;
 use crate::neon_node::BLOCK_PROCESSOR_STACK_SIZE;
-use crate::neon_node::BLOCK_PROCESSOR_STACK_SIZE;
 use crate::neon_node::RELAYER_MAX_BUFFER;
 use crate::node::use_test_genesis_chainstate;
 use crate::syncctl::{PoxSyncWatchdog, PoxSyncWatchdogComms};
