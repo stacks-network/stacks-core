69294d5671839d0e14ee9e6cc552ff2777565263
Jude Nelson <judecn@gmail.com>
2022-11-10 22:00:15 -0500

    fix: burnchain regtest must now start with regtest header

--- src/chainstate/stacks/boot/pox_2_tests.rs
+++ src/chainstate/stacks/boot/pox_2_tests.rs
@@ -3899,7 +3899,10 @@ fn stack_aggregation_increase() {
     //  tenures start being tracked.
     let EMPTY_SORTITIONS = 25;
 
-    let mut burnchain = Burnchain::default_unittest(0, &BurnchainHeaderHash::zero());
+    let mut burnchain = Burnchain::default_unittest(
+        0,
+        &BurnchainHeaderHash::from_hex(BITCOIN_REGTEST_FIRST_BLOCK_HASH).unwrap(),
+    );
     burnchain.pox_constants.reward_cycle_length = 5;
     burnchain.pox_constants.prepare_length = 2;
     burnchain.pox_constants.anchor_threshold = 1;
