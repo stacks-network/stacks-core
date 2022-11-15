cbebef50e6380ccf338fd5cda4b8d788dce3f6d5
Jude Nelson <judecn@gmail.com>
2022-11-14 23:43:17 -0500

    doc: comment on the necessity of having a free cost tracker for pox-2's instantiation

--- src/clarity_vm/clarity.rs
+++ src/clarity_vm/clarity.rs
@@ -798,7 +798,9 @@ impl<'a, 'b> ClarityBlockConnection<'a, 'b> {
         // use the `using!` statement to ensure that the old cost_tracker is placed
         //  back in all branches after initialization
         using!(self.cost_track, "cost tracker", |old_cost_tracker| {
-            // epoch initialization is *free*
+            // epoch initialization is *free*.
+            // NOTE: this also means that cost functions won't be evaluated. 
+            // This is important because pox-2 is instantiated before costs-3.
             self.cost_track.replace(LimitedCostTracker::new_free());
 
             let mainnet = self.mainnet;
