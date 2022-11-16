506d2e82be8bf7c95b3ed5ce5c31df454bc8cd5f
Jude Nelson <judecn@gmail.com>
2022-11-15 21:11:49 -0500

    fix: doc test

--- clarity/src/vm/docs/mod.rs
+++ clarity/src/vm/docs/mod.rs
@@ -644,8 +644,8 @@ should use `*`, `/`, and `pow` instead of the shift operators.
 (bit-shift-left 16 u2) ;; Returns 64
 (bit-shift-left -64 u1) ;; Returns -128
 (bit-shift-left u4 u2) ;; Returns u16
-(bit-shift-left 123 u9999999999) ;; Returns -170141183460469231731687303715884105728 (== 123 bit-shift-left 127)
-(bit-shift-left u123 u9999999999) ;; Returns u170141183460469231731687303715884105728 (== u123 bit-shift-left 127)
+(bit-shift-left 123 u9999999999) ;; Returns -170141183460469231731687303715884105728
+(bit-shift-left u123 u9999999999) ;; Returns u170141183460469231731687303715884105728
 (bit-shift-left -1 u7) ;; Returns -128
 (bit-shift-left -1 u128) ;; Returns -1
 "
