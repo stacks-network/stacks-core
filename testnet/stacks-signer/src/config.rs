363f87cf8808c44b29e91d112c67c2b064f162fb
Don Park <donp@donp.org>
2022-12-06 06:48:26 -0800

    load stacker.toml

---
+++ testnet/stacks-signer/src/config.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use toml;
+use serde::Deserialize;
+
+#[derive(Clone, Deserialize, Default, Debug)]
+pub struct Config {
+
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> Result<Config, String> {
+        let content = fs::read_to_string(path).map_err(|e| format!("Invalid path: {}", &e))?;
+        Self::from_str(&content)
+    }
+    pub fn from_str(content: &str) -> Result<Config, String> {
+        let config: Config =
+            toml::from_str(content).map_err(|e| format!("Invalid toml: {}", e))?;
+        Ok(config)
+    }
+}
--- testnet/stacks-signer/src/main.rs
+++ testnet/stacks-signer/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use stacks_signer::config::{Config};
+use slog::{Drain, o, info, Logger};
+
+fn main() {
+    let log = make_logger();
+    let config = Config::from_file("conf/stacker.toml").unwrap();
+    info!(log, "{:?}", config);
+}
+
+fn make_logger() -> Logger {
+    let decorator = slog_term::PlainDecorator::new(std::io::stdout());
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let log = slog::Logger::root(drain, o!());
+    log
+}
