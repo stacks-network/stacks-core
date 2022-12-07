803c98f6c51b86b5f0956267e28e4f4ff4327e1e
Don Park <donp@donp.org>
2022-12-07 03:22:20 -0800

    add logger.rs

--- testnet/stacks-signer/src/main.rs
+++ testnet/stacks-signer/src/main.rs
@@ -1,17 +1,9 @@
+use slog::info;
 use stacks_signer::config::{Config};
-use slog::{Drain, o, info, Logger};
+use stacks_signer::logger;
 
 fn main() {
-    let log = make_logger();
+    let log = logger::setup();
     let config = Config::from_file("conf/stacker.toml").unwrap();
     info!(log, "{:?}", config);
-}
-
-fn make_logger() -> Logger {
-    let decorator = slog_term::PlainDecorator::new(std::io::stdout());
-    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-
-    let log = slog::Logger::root(drain, o!());
-    log
 }
