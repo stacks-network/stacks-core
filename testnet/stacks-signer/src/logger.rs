48236c0c11939b809943828c10bd38107ece9ea4
Don Park <donp@donp.org>
2022-12-07 03:22:20 -0800

    add logger.rs

---
+++ testnet/stacks-signer/src/logger.rs
@@ -0,0 +1,10 @@
+use slog::{Drain, o, Logger};
+
+pub fn setup() -> Logger {
+    let decorator = slog_term::PlainDecorator::new(std::io::stdout());
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let log = slog::Logger::root(drain, o!());
+    log
+}
