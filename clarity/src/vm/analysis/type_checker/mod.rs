3d6ecf26870fa24a00f2e3517bf2ceebfaab9111
Brice Dobry <brice@hiro.so>
2022-12-16 19:08:36 -0500

    chore: add copyright to new type-checker files

--- clarity/src/vm/analysis/type_checker/mod.rs
+++ clarity/src/vm/analysis/type_checker/mod.rs
@@ -1,5 +1,5 @@
 // Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
-// Copyright (C) 2020 - 2022 Stacks Open Internet Foundation
+// Copyright (C) 2020-2022 Stacks Open Internet Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
