a60d6ce40c5a2cf3a88b66fa31217d3a8b7ae5c8
Don Park <donp@donp.org>
2022-12-22 06:08:51 -0800

    async-std and libp2p first steps

--- testnet/stacks-signer/tests/messages.rs
+++ testnet/stacks-signer/tests/messages.rs
@@ -3,23 +3,23 @@ use stacks_signer::net::Net;
 use stacks_signer::signer;
 use stacks_signer::signer::{MessageTypes, Signer};
 
-fn setup() -> (Signer, Net) {
+async fn setup() -> (Signer, Net) {
     let mut signer = Signer::new();
     signer.reset(1, 2);
-    (signer, Net::new())
+    (signer, Net::new().await.unwrap())
 }
 
-#[test]
-fn receive_message() {
-    let (mut signer, _net) = setup();
+#[async_std::test]
+async fn receive_message() {
+    let (mut signer, _net) = setup().await;
 
     let join = MessageTypes::Join;
     assert!(signer.process(join));
 }
 
-#[test]
-fn broadcast_share() {
-    let (_signer, net) = setup();
+#[async_std::test]
+async fn broadcast_share() {
+    let (_signer, net) = setup().await;
 
     let share = signer::frost::SignatureShare {
         id: 0,
