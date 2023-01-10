26d4fc34e0067357d7ae4263fd05dce663207e64
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-01-10 10:58:40 -0800

    few more comments

--- contrib/tools/stacks-relay/src/state.rs
+++ contrib/tools/stacks-relay/src/state.rs
@@ -2,13 +2,17 @@ use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct State {
+    // the value for this map is an index for the last read message.
     highwaters: HashMap<u64, usize>,
     queue: Vec<String>,
 }
 
 impl State {
     pub fn get(&mut self, node_id: u64) -> Option<&String> {
-        let first_unread= self.highwaters.get(&node_id).map_or(0, |v| *v + 1);
+        let first_unread = self
+            .highwaters
+            .get(&node_id)
+            .map_or(0, |last_read| *last_read + 1);
         let result = self.queue.get(first_unread);
         if result != None {
             self.highwaters.insert(node_id, first_unread);
