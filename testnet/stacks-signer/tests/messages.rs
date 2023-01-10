89061acada4d23ef587ea9b6183883fbb6e69987
Don Park <donp@donp.org>
2023-01-09 16:39:12 -0800

    fixup test use of frost api

--- testnet/stacks-signer/tests/messages.rs
+++ testnet/stacks-signer/tests/messages.rs
@@ -1,14 +1,17 @@
 use secp256k1_math::scalar::Scalar;
 use stacks_signer::net::Net;
-use stacks_signer::signer;
 use stacks_signer::signer::{MessageTypes, Signer};
+use frost::common::SignatureShare;
+use stacks_signer::config::Config;
 
-async fn setup() -> (Signer, Net) {
+fn setup() -> (Signer, Net) {
     let mut signer = Signer::new();
     signer.reset(1, 2);
-    (signer, Net::new().await.unwrap())
+    let config = Config::default();
+    (signer, Net::new(&config))
 }
 
+#[test]
 fn receive_message() {
     let (mut signer, _net) = setup();
 
@@ -16,10 +19,11 @@ fn receive_message() {
     assert!(signer.process(join));
 }
 
+#[test]
 fn broadcast_share() {
     let (_signer, net) = setup();
 
-    let share = signer::frost::SignatureShare {
+    let share = SignatureShare {
         id: 0,
         z_i: Scalar::new(),
         public_key: Default::default(),
