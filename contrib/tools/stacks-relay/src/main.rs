ac50e40e893354bb0e67e7b9b86e5fa1cd9fe25b
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-01-11 10:46:19 -0800

    stacks-relay, Rust version, busines-logic only. No IO

---
+++ contrib/tools/stacks-relay/src/main.rs
@@ -0,0 +1,5 @@
+mod state;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ contrib/tools/stacks-relay/src/state.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+#[derive(Default)]
+pub struct State {
+    // the value for this map is an index for the last read message for this node.
+    highwaters: HashMap<u64, usize>,
+    queue: Vec<String>,
+}
+
+impl State {
+    pub fn get(&mut self, node_id: u64) -> Option<&String> {
+        let first_unread = self
+            .highwaters
+            .get(&node_id)
+            .map_or(0, |last_read| *last_read + 1);
+        let result = self.queue.get(first_unread);
+        if result != None {
+            self.highwaters.insert(node_id, first_unread);
+        };
+        result
+    }
+    pub fn post(&mut self, msg: String) {
+        self.queue.push(msg);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::State;
+    #[test]
+    fn state_test() {
+        let mut state = State::default();
+        assert_eq!(None, state.get(1));
+        assert_eq!(None, state.get(3));
+        assert_eq!(0, state.highwaters.len());
+        state.post("Msg # 0".to_string());
+        assert_eq!(Some(&"Msg # 0".to_string()), state.get(1));
+        assert_eq!(Some(&"Msg # 0".to_string()), state.get(5));
+        assert_eq!(Some(&"Msg # 0".to_string()), state.get(4));
+        assert_eq!(None, state.get(1));
+        state.post("Msg # 1".to_string());
+        assert_eq!(Some(&"Msg # 1".to_string()), state.get(1));
+        assert_eq!(Some(&"Msg # 0".to_string()), state.get(3));
+        assert_eq!(Some(&"Msg # 1".to_string()), state.get(5));
+        state.post("Msg # 2".to_string());
+        assert_eq!(Some(&"Msg # 2".to_string()), state.get(1));
+        assert_eq!(Some(&"Msg # 1".to_string()), state.get(4));
+        assert_eq!(Some(&"Msg # 2".to_string()), state.get(4));
+    }
+}
