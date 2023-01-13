dcd79b806d42d94e64da687e7cada3ffc5e2077e
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2023-01-13 11:51:38 -0800

    clean-up

--- contrib/tools/relay-server/src/http.rs
+++ contrib/tools/relay-server/src/http.rs
@@ -11,11 +11,13 @@ pub struct RequestMessage {
 
 pub trait RequestMessageEx: Read {
     fn read_http_request_message(&mut self) -> RequestMessage {
+
         let mut read_byte = || {
             let mut buf = [0; 1];
             self.read_exact(&mut buf).unwrap();
             buf[0]
         };
+        
         let mut read_line = || {
             let mut result = String::new();
             loop {
@@ -28,12 +30,16 @@ pub trait RequestMessageEx: Read {
             assert_eq!(read_byte(), 10);
             result
         };
+
+        // read and parse the request line
         let request_line = read_line();
         let mut split = request_line.split(' ');
         let mut next = || split.next().unwrap().to_string();
-        let method = next();
+        let method = next();        
         let url = next();
         let protocol = next();
+
+        // read and parse headers
         let mut headers = HashMap::new();
         loop {
             let line = read_line();
@@ -43,12 +49,16 @@ pub trait RequestMessageEx: Read {
             let (name, value) = line.split_once(':').unwrap();
             headers.insert(name.to_lowercase(), value.trim().to_string());
         }
+
+        // read content
         let content_length = headers
             .get("content-length")
             .map_or(0, |v| v.parse::<usize>().unwrap());
         let mut content = Vec::new();
         content.resize(content_length, 0);
         self.read_exact(content.as_mut_slice()).unwrap();
+
+        // return the message
         RequestMessage {
             method,
             url,
