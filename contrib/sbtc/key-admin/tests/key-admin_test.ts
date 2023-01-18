3525d42d5e48ce7e7711cbc3d845824935504671
Joey Yandle <xoloki@gmail.com>
2023-01-17 19:27:46 -0500

    add unit tests

--- contrib/sbtc/key-admin/tests/key-admin_test.ts
+++ contrib/sbtc/key-admin/tests/key-admin_test.ts
@@ -3,24 +3,58 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Ensure that coordinator starts empty",
     async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+        let deployer = accounts.get("deployer")!;
 
-        block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
+        let coordinator = chain.callReadOnlyFn("key-admin", "get-coordinator-key", [], deployer.address);
+
+        coordinator.result.expectNone();
+
+        //assertEquals(block.height, 3);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that coordinator can be written then read",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+
+        let block = chain.mineBlock([
+            // Generate a contract call to count-up from the deployer address.
+            Tx.contractCall("key-admin", "set-coordinator-key", [types.principal(deployer.address)], deployer.address),
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+
+        // Get the first (and only) transaction receipt.
+        let [receipt] = block.receipts;
+
+        // Assert that the returned result is a boolean true.
+        receipt.result.expectOk().expectBool(true);
+
+        let coordinator = chain.callReadOnlyFn("key-admin", "get-coordinator-key", [], deployer.address);
+
+        coordinator.result.expectSome(deployer.address);
+    },
+});
+
+Clarinet.test({
+    name: "Ensure that signer can be written then read",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+
+        let block = chain.mineBlock([
+            // Generate a contract call to count-up from the deployer address.
+            Tx.contractCall("key-admin", "set-signer-key", [types.uint(1), types.principal(deployer.address)], deployer.address),
+        ]);
+
+        // Get the first (and only) transaction receipt.
+        let [receipt] = block.receipts;
+
+        // Assert that the returned result is a boolean true.
+        receipt.result.expectOk().expectBool(true);
+
+        let coordinator = chain.callReadOnlyFn("key-admin", "get-signer-key", [types.uint(1)], deployer.address);
+
+        coordinator.result.expectSome(deployer.address);
     },
 });
