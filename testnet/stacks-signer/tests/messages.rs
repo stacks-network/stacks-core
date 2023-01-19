291c56bb9d3e3a8cfbb077ecf3e4f95a96987657
Don Park <donp@donp.org>
2023-01-18 16:11:45 -0800

    assert dkg_begin populates commitments array

--- testnet/stacks-signer/tests/messages.rs
+++ testnet/stacks-signer/tests/messages.rs
@@ -1,23 +1,25 @@
 use secp256k1_math::scalar::Scalar;
 use stacks_signer::signer::{MessageTypes, SignatureShare, Signer};
 
-fn setup_signer() -> Signer {
+fn setup_signer(total: usize, threshold: usize) -> Signer {
     let my_id = 1;
-    let mut signer = Signer::new(my_id);
-    signer.reset(1, 2);
+    let mut signer = Signer::new(my_id, threshold, total);
+    signer.reset();
     signer
 }
 
 #[test]
 fn dkg_begin() {
-    let mut signer = setup_signer();
+    let mut signer = setup_signer(2, 1);
+    assert_eq!(signer.commitments.len(), 0);
 
     let dkg_begin_msg = MessageTypes::DkgBegin;
     assert!(signer.process(dkg_begin_msg));
+    assert_eq!(signer.commitments.len(), signer.total);
 }
 
 #[test]
-fn receive_share() {
+fn signature_share() {
     let share = frost::common::SignatureShare {
         id: 0,
         z_i: Scalar::new(),
@@ -28,6 +30,6 @@ fn receive_share() {
         signature_share: share,
     });
 
-    let mut signer = setup_signer();
+    let mut signer = setup_signer(2, 1);
     signer.process(msg_share);
 }
