0a48b82cd30ced51218da55d4d648d1f6d04d618
Mårten Blankfors <marten@blankfors.se>
2023-01-23 10:24:32 +0100

    tests: Check all fields in sortdb insertion tests

--- src/chainstate/burn/db/sortdb.rs
+++ src/chainstate/burn/db/sortdb.rs
@@ -6598,7 +6598,7 @@ pub mod tests {
                 .expect("Failed to get peg-in ops from sortition DB");
 
         assert_eq!(res_peg_out_requests.len(), 1);
-        assert_eq!(res_peg_out_requests[0].amount, 1337);
+        assert_eq!(res_peg_out_requests[0], peg_out_request);
     }
 
     #[test]
@@ -6644,7 +6644,7 @@ pub mod tests {
                 .expect("Failed to get peg-in ops from sortition DB");
 
         assert_eq!(res_peg_out_fulfillments.len(), 1);
-        assert_eq!(res_peg_out_fulfillments[0].amount, 1337);
+        assert_eq!(res_peg_out_fulfillments[0], peg_out_fulfill);
     }
 
     #[test]
