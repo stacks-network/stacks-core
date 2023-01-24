407cbc88469a089a89e385bf63e2c5d3cd4167b4
Mårten Blankfors <marten@blankfors.se>
2023-01-24 11:20:50 +0100

    wip: Basic boilerplate

--- testnet/stacks-signer/src/bin/coordinator.rs
+++ testnet/stacks-signer/src/bin/coordinator.rs
@@ -1,3 +1,54 @@
+use clap::Parser;
+
+use stacks_signer::config::Config;
+use stacks_signer::net::{HttpNet, Message, Net};
+
 fn main() {
-    println!("Hello world!");
+    let cli = Cli::parse();
+    let config = Config::from_file("conf/stacker.toml").unwrap();
+
+    let mut net: HttpNet = HttpNet::new(config.common.stacks_node_url.clone(), vec![]);
+
+    cli.command
+        .run(&mut net)
+        .expect("Failed to execute command");
 }
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Command {
+    Dkg,
+    Sign { msg: String },
+    GetAggregatePublicKey,
+}
+
+impl Command {
+    fn run<N: Net>(&self, network: &mut N) -> Result<(), Error> {
+        match self {
+            Self::Dkg => self.run_distributed_key_generation(network),
+            Self::Sign { msg } => self.sign_message(network, msg),
+            Self::GetAggregatePublicKey => self.get_aggregate_public_key(network),
+        }
+    }
+
+    fn run_distributed_key_generation<N: Net>(&self, network: &mut N) -> Result<(), Error> {
+        todo!();
+    }
+
+    fn sign_message<N: Net>(&self, network: &mut N, msg: &str) -> Result<(), Error> {
+        todo!();
+    }
+
+    fn get_aggregate_public_key<N: Net>(&self, network: &mut N) -> Result<(), Error> {
+        todo!();
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {}
