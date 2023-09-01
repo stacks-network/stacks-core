f945ea56d77781288e6c5e20a6454afe0fd92433
Brice Dobry <brice@hiro.so>
2023-09-01 10:35:10 -0400

    chore: add comment to `initialize_contract` method
    This comment clarifies that this function should not be used during
    normal execution.

--- clarity/src/vm/contexts.rs
+++ clarity/src/vm/contexts.rs
@@ -1261,6 +1261,10 @@ impl<'a, 'b> Environment<'a, 'b> {
         result
     }
 
+    /// Initialize a contract from a string of Clarity code.
+    /// This function should only be used for testing and the CLI interface.
+    /// Normal execution reaches the `initialize_contract_from_ast` method
+    /// below.
     pub fn initialize_contract(
         &mut self,
         contract_identifier: QualifiedContractIdentifier,
