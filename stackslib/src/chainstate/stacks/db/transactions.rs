fad9c0b79cfd71dd1ae3927269f89e36c33b1d93
Brice Dobry <brice@hiro.so>
2023-09-01 15:05:37 -0400

    fix: handle wasm generator errors cleanly

--- stackslib/src/chainstate/stacks/db/transactions.rs
+++ stackslib/src/chainstate/stacks/db/transactions.rs
@@ -31,7 +31,7 @@ use clarity::vm::contracts::Contract;
 use clarity::vm::costs::cost_functions::ClarityCostFunction;
 use clarity::vm::costs::{cost_functions, runtime_cost, CostTracker, ExecutionCost};
 use clarity::vm::database::{ClarityBackingStore, ClarityDatabase};
-use clarity::vm::errors::Error as InterpreterError;
+use clarity::vm::errors::{Error as InterpreterError, WasmError};
 use clarity::vm::representations::{ClarityName, ContractName};
 use clarity::vm::types::serialization::SerializationError as ClaritySerializationError;
 use clarity::vm::types::{
@@ -1209,8 +1209,8 @@ impl StacksChainState {
                 let sponsor = tx.sponsor_address().map(|a| a.to_account_principal());
 
                 // Compile the contract to Wasm
-                let mut module =
-                    compile_contract(&mut contract_analysis).expect("Failed to compile contract");
+                let mut module = compile_contract(&mut contract_analysis)
+                    .map_err(|e| Error::Wasm(WasmError::WasmGeneratorError(e)))?;
                 contract_ast.wasm_module = Some(module.emit_wasm());
 
                 // execution -- if this fails due to a runtime error, then the transaction is still
