3a7f82a30aee60461723a5354e29da6d38f1768c
Jude Nelson <judecn@gmail.com>
2023-10-02 16:15:41 -0400

    refactor: put post-blcok RPC handler into its own file

---
+++ stackslib/src/net/api/postblock.rs
@@ -0,0 +1,306 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use regex::{Captures, Regex};
+use std::io::{Read, Write};
+
+use crate::net::{
+    httpcore::{
+        request, HttpPreambleExtensions, HttpRequestContentsExtensions, RPCRequestHandler,
+        StacksHttpRequest, StacksHttpResponse,
+    },
+    p2p::PeerNetwork,
+    Error as NetError, StacksNodeState,
+};
+
+use crate::net::http::{
+    parse_json, Error, HttpBadRequest, HttpContentType, HttpNotFound, HttpRequest,
+    HttpRequestContents, HttpRequestPreamble, HttpResponse, HttpResponseContents,
+    HttpResponsePayload, HttpResponsePreamble, HttpServerError,
+};
+
+use crate::burnchains::affirmation::AffirmationMap;
+use crate::burnchains::Txid;
+use crate::chainstate::burn::db::sortdb::SortitionDB;
+use crate::chainstate::stacks::db::blocks::MINIMUM_TX_FEE_RATE_PER_BYTE;
+use crate::chainstate::stacks::db::StacksChainState;
+use crate::chainstate::stacks::StacksBlock;
+use crate::chainstate::stacks::StacksBlockHeader;
+use crate::chainstate::stacks::StacksTransaction;
+use crate::chainstate::stacks::TransactionPayload;
+use crate::core::mempool::MemPoolDB;
+use crate::net::relay::Relayer;
+use crate::net::Attachment;
+use crate::net::BlocksData;
+use crate::net::BlocksDatum;
+use crate::net::StacksMessageType;
+
+use stacks_common::codec::Error as CodecError;
+use stacks_common::codec::StacksMessageCodec;
+use stacks_common::codec::MAX_PAYLOAD_LEN;
+use stacks_common::types::chainstate::BlockHeaderHash;
+use stacks_common::types::chainstate::ConsensusHash;
+use stacks_common::types::chainstate::StacksBlockId;
+use stacks_common::types::chainstate::StacksPublicKey;
+use stacks_common::types::net::PeerHost;
+use stacks_common::types::StacksPublicKeyBuffer;
+use stacks_common::util::hash::hex_bytes;
+use stacks_common::util::hash::Hash160;
+use stacks_common::util::hash::Sha256Sum;
+use stacks_common::util::retry::BoundReader;
+
+use crate::cost_estimates::FeeRateEstimate;
+
+use clarity::vm::costs::ExecutionCost;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct StacksBlockAcceptedData {
+    pub stacks_block_id: StacksBlockId,
+    pub accepted: bool,
+}
+
+#[derive(Clone)]
+pub struct RPCPostBlockRequestHandler {
+    pub block: Option<StacksBlock>,
+    pub consensus_hash: Option<ConsensusHash>,
+}
+
+impl RPCPostBlockRequestHandler {
+    pub fn new() -> Self {
+        Self {
+            block: None,
+            consensus_hash: None,
+        }
+    }
+
+    /// Decode a bare block from the body
+    fn parse_postblock_octets(mut body: &[u8]) -> Result<StacksBlock, Error> {
+        let block = StacksBlock::consensus_deserialize(&mut body).map_err(|e| {
+            if let CodecError::DeserializeError(msg) = e {
+                Error::DecodeError(format!("Failed to deserialize posted transaction: {}", msg))
+            } else {
+                e.into()
+            }
+        })?;
+        Ok(block)
+    }
+}
+
+/// Decode the HTTP request
+impl HttpRequest for RPCPostBlockRequestHandler {
+    fn verb(&self) -> &'static str {
+        "POST"
+    }
+
+    fn path_regex(&self) -> Regex {
+        Regex::new(r#"^/v2/blocks/upload/(?P<consensus_hash>[0-9a-f]{40})$"#).unwrap()
+    }
+
+    /// Try to decode this request.
+    /// There's nothing to load here, so just make sure the request is well-formed.
+    fn try_parse_request(
+        &mut self,
+        preamble: &HttpRequestPreamble,
+        captures: &Captures,
+        query: Option<&str>,
+        body: &[u8],
+    ) -> Result<HttpRequestContents, Error> {
+        if preamble.get_content_length() == 0 {
+            return Err(Error::DecodeError(
+                "Invalid Http request: expected non-zero-length body for PostBlock".to_string(),
+            ));
+        }
+
+        if preamble.get_content_length() > MAX_PAYLOAD_LEN {
+            return Err(Error::DecodeError(
+                "Invalid Http request: PostBlock body is too big".to_string(),
+            ));
+        }
+
+        if Some(HttpContentType::Bytes) != preamble.content_type || preamble.content_type.is_none()
+        {
+            return Err(Error::DecodeError(
+                "Invalid Http request: PostBlock takes application/octet-stream".to_string(),
+            ));
+        }
+
+        let consensus_hash = request::get_consensus_hash(captures, "consensus_hash")?;
+        let block = Self::parse_postblock_octets(body)?;
+
+        self.consensus_hash = Some(consensus_hash);
+        self.block = Some(block);
+        Ok(HttpRequestContents::new().query_string(query))
+    }
+}
+
+impl RPCRequestHandler for RPCPostBlockRequestHandler {
+    /// Reset internal state
+    fn restart(&mut self) {
+        self.consensus_hash = None;
+        self.block = None;
+    }
+
+    /// Make the response
+    fn try_handle_request(
+        &mut self,
+        preamble: HttpRequestPreamble,
+        _contents: HttpRequestContents,
+        node: &mut StacksNodeState,
+    ) -> Result<(HttpResponsePreamble, HttpResponseContents), NetError> {
+        // get out the request body
+        let block = self
+            .block
+            .take()
+            .ok_or(NetError::SendError("`block` not set".into()))?;
+        let consensus_hash = self
+            .consensus_hash
+            .take()
+            .ok_or(NetError::SendError("`consensus_hash` not set".into()))?;
+
+        let block_hash = block.block_hash();
+
+        let data_resp =
+            node.with_node_state(|_network, sortdb, chainstate, _mempool, _rpc_args| {
+                match SortitionDB::get_sortition_id_by_consensus(&sortdb.conn(), &consensus_hash) {
+                    Ok(Some(_)) => {
+                        // we recognize this consensus hash
+                        let ic = sortdb.index_conn();
+                        match Relayer::process_new_anchored_block(
+                            &ic,
+                            chainstate,
+                            &consensus_hash,
+                            &block,
+                            0,
+                        ) {
+                            Ok(accepted) => {
+                                debug!(
+                                    "{} Stacks block {}/{}",
+                                    if accepted {
+                                        "Accepted"
+                                    } else {
+                                        "Did not accept"
+                                    },
+                                    &consensus_hash,
+                                    &block_hash,
+                                );
+                                return Ok(accepted);
+                            }
+                            Err(e) => {
+                                let msg = format!(
+                                    "Failed to process anchored block {}/{}: {:?}",
+                                    consensus_hash,
+                                    &block.block_hash(),
+                                    &e
+                                );
+                                error!("{}", &msg);
+                                return Err(StacksHttpResponse::new_error(
+                                    &preamble,
+                                    &HttpServerError::new(msg),
+                                ));
+                            }
+                        }
+                    }
+                    Ok(None) => {
+                        let msg = format!(
+                            "Unrecognized consensus hash {} for block {}",
+                            consensus_hash,
+                            &block.block_hash()
+                        );
+                        debug!("{}", &msg);
+                        return Err(StacksHttpResponse::new_error(
+                            &preamble,
+                            &HttpNotFound::new(msg),
+                        ));
+                    }
+                    Err(e) => {
+                        let msg = format!(
+                            "Failed to query sortition ID by consensus '{}': {:?}",
+                            consensus_hash, &e
+                        );
+                        error!("{}", &msg);
+                        return Err(StacksHttpResponse::new_error(
+                            &preamble,
+                            &HttpServerError::new(msg),
+                        ));
+                    }
+                }
+            });
+
+        let data_resp = match data_resp {
+            Ok(accepted) => StacksBlockAcceptedData {
+                accepted,
+                stacks_block_id: StacksBlockHeader::make_index_block_hash(
+                    &consensus_hash,
+                    &block_hash,
+                ),
+            },
+            Err(response) => {
+                return response.try_into_contents().map_err(NetError::from);
+            }
+        };
+
+        // don't forget to forward this to the p2p network!
+        if data_resp.accepted {
+            node.set_relay_message(StacksMessageType::Blocks(BlocksData {
+                blocks: vec![BlocksDatum(consensus_hash, block)],
+            }));
+        }
+
+        let mut preamble = HttpResponsePreamble::ok_json(&preamble);
+        preamble.set_canonical_stacks_tip_height(Some(node.canonical_stacks_tip_height()));
+        let body = HttpResponseContents::try_from_json(&data_resp)?;
+        Ok((preamble, body))
+    }
+}
+
+/// Decode the HTTP response
+impl HttpResponse for RPCPostBlockRequestHandler {
+    fn try_parse_response(
+        &self,
+        preamble: &HttpResponsePreamble,
+        body: &[u8],
+    ) -> Result<HttpResponsePayload, Error> {
+        let accepted: StacksBlockAcceptedData = parse_json(preamble, body)?;
+        Ok(HttpResponsePayload::try_from_json(accepted)?)
+    }
+}
+
+impl StacksHttpRequest {
+    /// Make a new post-block request
+    pub fn new_post_block(
+        host: PeerHost,
+        ch: ConsensusHash,
+        block: StacksBlock,
+    ) -> StacksHttpRequest {
+        StacksHttpRequest::new_for_peer(
+            host,
+            "POST".into(),
+            format!("/v2/blocks/upload/{}", &ch),
+            HttpRequestContents::new().payload_stacks(&block),
+        )
+        .expect("FATAL: failed to construct request from infallible data")
+    }
+}
+
+impl StacksHttpResponse {
+    pub fn decode_stacks_block_accepted(self) -> Result<StacksBlockAcceptedData, NetError> {
+        let contents = self.get_http_payload_ok()?;
+        let response_json: serde_json::Value = contents.try_into()?;
+        let result: StacksBlockAcceptedData = serde_json::from_value(response_json)
+            .map_err(|_e| NetError::DeserializeError("Failed to load from JSON".to_string()))?;
+        Ok(result)
+    }
+}
---
+++ stackslib/src/net/api/tests/postblock.rs
@@ -0,0 +1,173 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
+
+use crate::net::httpcore::{HttpRequestContentsExtensions, StacksHttp, StacksHttpRequest};
+
+use stacks_common::types::net::PeerHost;
+
+use stacks_common::types::chainstate::ConsensusHash;
+use stacks_common::types::chainstate::StacksAddress;
+use stacks_common::types::Address;
+
+use clarity::vm::types::PrincipalData;
+use clarity::vm::types::QualifiedContractIdentifier;
+use clarity::vm::types::StacksAddressExtensions;
+use clarity::vm::ClarityName;
+use clarity::vm::ContractName;
+use clarity::vm::Value;
+
+use crate::net::api::*;
+use crate::net::ProtocolFamily;
+use crate::net::TipRequest;
+
+use crate::core::BLOCK_LIMIT_MAINNET_21;
+
+use crate::chainstate::stacks::test::make_codec_test_block;
+use crate::chainstate::stacks::StacksBlockHeader;
+use crate::net::httpcore::RPCRequestHandler;
+
+use crate::net::connection::ConnectionOptions;
+
+use super::TestRPC;
+
+#[test]
+fn test_try_parse_request() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+
+    let block = make_codec_test_block(3);
+    let request =
+        StacksHttpRequest::new_post_block(addr.into(), ConsensusHash([0x11; 20]), block.clone());
+    let bytes = request.try_serialize().unwrap();
+
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+    let mut handler = postblock::RPCPostBlockRequestHandler::new();
+    let mut parsed_request = http
+        .handle_try_parse_request(
+            &mut handler,
+            &parsed_preamble.expect_request(),
+            &bytes[offset..],
+        )
+        .unwrap();
+
+    assert_eq!(handler.consensus_hash, Some(ConsensusHash([0x11; 20])));
+    assert_eq!(handler.block, Some(block.clone()));
+
+    // parsed request consumes headers that would not be in a constructed reqeuest
+    parsed_request.clear_headers();
+    let (preamble, contents) = parsed_request.destruct();
+
+    assert_eq!(&preamble, request.preamble());
+
+    handler.restart();
+    assert!(handler.consensus_hash.is_none());
+    assert!(handler.block.is_none());
+
+    // try to deal with an invalid block
+    let mut bad_block = block.clone();
+    bad_block.txs.clear();
+
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+    let request = StacksHttpRequest::new_post_block(
+        addr.into(),
+        ConsensusHash([0x11; 20]),
+        bad_block.clone(),
+    );
+    let bytes = request.try_serialize().unwrap();
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+    let mut handler = postblock::RPCPostBlockRequestHandler::new();
+    match http.handle_try_parse_request(
+        &mut handler,
+        &parsed_preamble.expect_request(),
+        &bytes[offset..],
+    ) {
+        Err(NetError::Http(Error::DecodeError(..))) => {}
+        _ => {
+            panic!("worked with bad block");
+        }
+    }
+}
+
+#[test]
+fn test_try_make_response() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+
+    let rpc_test = TestRPC::setup(function_name!());
+    let stacks_block_id = StacksBlockHeader::make_index_block_hash(
+        &rpc_test.next_block.0,
+        &rpc_test.next_block.1.block_hash(),
+    );
+    let mut requests = vec![];
+
+    // post the block
+    let request = StacksHttpRequest::new_post_block(
+        addr.into(),
+        rpc_test.next_block.0.clone(),
+        rpc_test.next_block.1.clone(),
+    );
+    requests.push(request);
+
+    // idempotent
+    let request = StacksHttpRequest::new_post_block(
+        addr.into(),
+        rpc_test.next_block.0.clone(),
+        rpc_test.next_block.1.clone(),
+    );
+    requests.push(request);
+
+    // fails if the consensus hash is not recognized
+    let request = StacksHttpRequest::new_post_block(
+        addr.into(),
+        ConsensusHash([0x11; 20]),
+        rpc_test.next_block.1.clone(),
+    );
+    requests.push(request);
+
+    let mut responses = rpc_test.run(requests);
+
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let resp = response.decode_stacks_block_accepted().unwrap();
+    assert_eq!(resp.accepted, true);
+    assert_eq!(resp.stacks_block_id, stacks_block_id);
+
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let resp = response.decode_stacks_block_accepted().unwrap();
+    assert_eq!(resp.accepted, false);
+    assert_eq!(resp.stacks_block_id, stacks_block_id);
+
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let (preamble, body) = response.destruct();
+    assert_eq!(preamble.status_code, 404);
+}
