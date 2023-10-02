2da1bc9f7b046e8840ab70d678a85bc9cba3ee87
Jude Nelson <judecn@gmail.com>
2023-10-02 16:08:30 -0400

    refactor: put get-attachment RPC handler into its own file

---
+++ stackslib/src/net/api/getattachment.rs
@@ -0,0 +1,175 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use regex::{Captures, Regex};
+use std::collections::HashSet;
+use std::io::{Read, Write};
+
+use crate::net::{
+    httpcore::{HttpPreambleExtensions, RPCRequestHandler, StacksHttpRequest, StacksHttpResponse},
+    p2p::PeerNetwork,
+    Error as NetError, StacksNodeState,
+};
+
+use crate::net::http::{
+    parse_json, Error, HttpBadRequest, HttpNotFound, HttpRequest, HttpRequestContents,
+    HttpRequestPreamble, HttpResponse, HttpResponseContents, HttpResponsePayload,
+    HttpResponsePreamble, HttpServerError,
+};
+
+use crate::net::atlas::{AttachmentPage, MAX_ATTACHMENT_INV_PAGES_PER_REQUEST};
+
+use crate::net::atlas::GetAttachmentResponse;
+use stacks_common::types::net::PeerHost;
+use stacks_common::util::hash::Hash160;
+
+use url::form_urlencoded;
+
+#[derive(Clone)]
+pub struct RPCGetAttachmentRequestHandler {
+    pub attachment_hash: Option<Hash160>,
+}
+
+impl RPCGetAttachmentRequestHandler {
+    pub fn new() -> Self {
+        Self {
+            attachment_hash: None,
+        }
+    }
+}
+
+/// Decode the HTTP request
+impl HttpRequest for RPCGetAttachmentRequestHandler {
+    fn verb(&self) -> &'static str {
+        "GET"
+    }
+
+    fn path_regex(&self) -> Regex {
+        Regex::new(r#"^/v2/attachments/(?P<attachment_hash>[0-9a-f]{40})$"#).unwrap()
+    }
+
+    /// Try to decode this request.
+    /// There's nothing to load here, so just make sure the request is well-formed.
+    fn try_parse_request(
+        &mut self,
+        preamble: &HttpRequestPreamble,
+        captures: &Captures,
+        query: Option<&str>,
+        _body: &[u8],
+    ) -> Result<HttpRequestContents, Error> {
+        if preamble.get_content_length() != 0 {
+            return Err(Error::DecodeError(
+                "Invalid Http request: expected 0-length body".to_string(),
+            ));
+        }
+
+        let attachment_hash_str = captures
+            .name("attachment_hash")
+            .ok_or(Error::DecodeError(
+                "Failed to match path to attachment_hash group".to_string(),
+            ))?
+            .as_str();
+
+        self.attachment_hash = Some(
+            Hash160::from_hex(attachment_hash_str)
+                .map_err(|_| Error::DecodeError("Failed to decode `attachment_hash`".into()))?,
+        );
+
+        Ok(HttpRequestContents::new().query_string(query))
+    }
+}
+
+impl RPCRequestHandler for RPCGetAttachmentRequestHandler {
+    /// Reset internal state
+    fn restart(&mut self) {
+        self.attachment_hash = None;
+    }
+
+    fn try_handle_request(
+        &mut self,
+        preamble: HttpRequestPreamble,
+        _contents: HttpRequestContents,
+        node: &mut StacksNodeState,
+    ) -> Result<(HttpResponsePreamble, HttpResponseContents), NetError> {
+        let attachment_hash = self
+            .attachment_hash
+            .take()
+            .ok_or(NetError::SendError("Missing `attachment_hash`".into()))?;
+
+        let attachment_res = node.with_node_state(
+            |network, _sortdb, _chainstate, _mempool, _rpc_args| match network
+                .get_atlasdb()
+                .find_attachment(&attachment_hash)
+            {
+                Ok(Some(attachment)) => Ok(GetAttachmentResponse { attachment }),
+                _ => {
+                    let msg = format!("Unable to find attachment");
+                    warn!("{}", msg);
+                    Err(StacksHttpResponse::new_error(
+                        &preamble,
+                        &HttpNotFound::new(msg),
+                    ))
+                }
+            },
+        );
+        let attachment = match attachment_res {
+            Ok(attachment) => attachment,
+            Err(response) => {
+                return response.try_into_contents().map_err(NetError::from);
+            }
+        };
+
+        let mut preamble = HttpResponsePreamble::ok_json(&preamble);
+        preamble.set_canonical_stacks_tip_height(Some(node.canonical_stacks_tip_height()));
+        let body = HttpResponseContents::try_from_json(&attachment)?;
+        Ok((preamble, body))
+    }
+}
+
+/// Decode the HTTP response
+impl HttpResponse for RPCGetAttachmentRequestHandler {
+    fn try_parse_response(
+        &self,
+        preamble: &HttpResponsePreamble,
+        body: &[u8],
+    ) -> Result<HttpResponsePayload, Error> {
+        let pages: GetAttachmentResponse = parse_json(preamble, body)?;
+        Ok(HttpResponsePayload::try_from_json(pages)?)
+    }
+}
+
+impl StacksHttpRequest {
+    /// Make a new request for an attachment
+    pub fn new_getattachment(host: PeerHost, attachment_id: Hash160) -> StacksHttpRequest {
+        StacksHttpRequest::new_for_peer(
+            host,
+            "GET".into(),
+            format!("/v2/attachments/{}", &attachment_id),
+            HttpRequestContents::new(),
+        )
+        .expect("FATAL: failed to construct request from infallible data")
+    }
+}
+
+impl StacksHttpResponse {
+    pub fn decode_atlas_get_attachment(self) -> Result<GetAttachmentResponse, NetError> {
+        let contents = self.get_http_payload_ok()?;
+        let contents_json: serde_json::Value = contents.try_into()?;
+        let resp: GetAttachmentResponse = serde_json::from_value(contents_json)
+            .map_err(|_e| NetError::DeserializeError("Failed to load from JSON".to_string()))?;
+        Ok(resp)
+    }
+}
