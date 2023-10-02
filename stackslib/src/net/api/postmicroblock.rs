2a8ee3d1a99d4f0af627d717e922aa58c7a5e6b8
Jude Nelson <judecn@gmail.com>
2023-10-02 16:16:41 -0400

    refactor: put post-microblock RPC handler into its own file

---
+++ stackslib/src/net/api/postmicroblock.rs
@@ -0,0 +1,287 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use regex::{Captures, Regex};
+use std::io::{Read, Write};
+
+use crate::net::{
+    httpcore::{
+        HttpPreambleExtensions, HttpRequestContentsExtensions, RPCRequestHandler,
+        StacksHttpRequest, StacksHttpResponse,
+    },
+    p2p::PeerNetwork,
+    Error as NetError, StacksNodeState,
+};
+
+use crate::net::http::{
+    parse_json, Error, HttpBadRequest, HttpContentType, HttpNotFound, HttpRequest,
+    HttpRequestContents, HttpRequestPreamble, HttpResponse, HttpResponseContents,
+    HttpResponsePayload, HttpResponsePreamble, HttpServerError,
+};
+
+use crate::burnchains::affirmation::AffirmationMap;
+use crate::burnchains::Txid;
+use crate::chainstate::burn::db::sortdb::SortitionDB;
+use crate::chainstate::stacks::db::blocks::MINIMUM_TX_FEE_RATE_PER_BYTE;
+use crate::chainstate::stacks::db::StacksChainState;
+use crate::chainstate::stacks::Error as ChainError;
+use crate::chainstate::stacks::StacksBlockHeader;
+use crate::chainstate::stacks::StacksMicroblock;
+use crate::chainstate::stacks::StacksTransaction;
+use crate::chainstate::stacks::TransactionPayload;
+use crate::core::mempool::MemPoolDB;
+use crate::net::relay::Relayer;
+use crate::net::Attachment;
+use crate::net::MicroblocksData;
+use crate::net::StacksMessageType;
+use crate::net::TipRequest;
+
+use stacks_common::codec::Error as CodecError;
+use stacks_common::codec::StacksMessageCodec;
+use stacks_common::codec::MAX_PAYLOAD_LEN;
+use stacks_common::types::chainstate::BlockHeaderHash;
+use stacks_common::types::chainstate::ConsensusHash;
+use stacks_common::types::chainstate::StacksBlockId;
+use stacks_common::types::chainstate::StacksPublicKey;
+use stacks_common::types::net::PeerHost;
+use stacks_common::types::StacksPublicKeyBuffer;
+use stacks_common::util::hash::hex_bytes;
+use stacks_common::util::hash::Hash160;
+use stacks_common::util::hash::Sha256Sum;
+use stacks_common::util::retry::BoundReader;
+
+use crate::cost_estimates::FeeRateEstimate;
+
+use clarity::vm::costs::ExecutionCost;
+
+#[derive(Clone)]
+pub struct RPCPostMicroblockRequestHandler {
+    pub microblock: Option<StacksMicroblock>,
+}
+
+impl RPCPostMicroblockRequestHandler {
+    pub fn new() -> Self {
+        Self { microblock: None }
+    }
+
+    /// Decode a bare block from the body
+    fn parse_postmicroblock_octets(mut body: &[u8]) -> Result<StacksMicroblock, Error> {
+        let mblock = StacksMicroblock::consensus_deserialize(&mut body).map_err(|e| {
+            if let CodecError::DeserializeError(msg) = e {
+                Error::DecodeError(format!("Failed to deserialize posted microblock: {}", msg))
+            } else {
+                e.into()
+            }
+        })?;
+        Ok(mblock)
+    }
+}
+
+/// Decode the HTTP request
+impl HttpRequest for RPCPostMicroblockRequestHandler {
+    fn verb(&self) -> &'static str {
+        "POST"
+    }
+
+    fn path_regex(&self) -> Regex {
+        Regex::new(r#"^/v2/microblocks$"#).unwrap()
+    }
+
+    /// Try to decode this request.
+    /// There's nothing to load here, so just make sure the request is well-formed.
+    fn try_parse_request(
+        &mut self,
+        preamble: &HttpRequestPreamble,
+        _captures: &Captures,
+        query: Option<&str>,
+        body: &[u8],
+    ) -> Result<HttpRequestContents, Error> {
+        if preamble.get_content_length() == 0 {
+            return Err(Error::DecodeError(
+                "Invalid Http request: expected non-zero-length body for PostMicroblock"
+                    .to_string(),
+            ));
+        }
+
+        if preamble.get_content_length() > MAX_PAYLOAD_LEN {
+            return Err(Error::DecodeError(
+                "Invalid Http request: PostMicroblock body is too big".to_string(),
+            ));
+        }
+
+        if Some(HttpContentType::Bytes) != preamble.content_type || preamble.content_type.is_none()
+        {
+            return Err(Error::DecodeError(
+                "Invalid Http request: PostMicroblock takes application/octet-stream".to_string(),
+            ));
+        }
+
+        let microblock = Self::parse_postmicroblock_octets(&body)?;
+        self.microblock = Some(microblock);
+
+        Ok(HttpRequestContents::new().query_string(query))
+    }
+}
+
+impl RPCRequestHandler for RPCPostMicroblockRequestHandler {
+    /// Reset internal state
+    fn restart(&mut self) {
+        self.microblock = None;
+    }
+
+    /// Make the response
+    fn try_handle_request(
+        &mut self,
+        preamble: HttpRequestPreamble,
+        contents: HttpRequestContents,
+        node: &mut StacksNodeState,
+    ) -> Result<(HttpResponsePreamble, HttpResponseContents), NetError> {
+        let microblock = self
+            .microblock
+            .take()
+            .ok_or(NetError::SendError("`microblock` not set".into()))?;
+        let tip = match node.load_stacks_chain_tip(&preamble, &contents) {
+            Ok(tip) => tip,
+            Err(error_resp) => {
+                return error_resp.try_into_contents().map_err(NetError::from);
+            }
+        };
+        let data_resp = node.with_node_state(|_network, sortdb, chainstate, _mempool, _rpc_args| {
+            let stacks_tip = match StacksChainState::load_staging_block_info(chainstate.db(), &tip) {
+                Ok(Some(tip_info)) => tip_info,
+                Ok(None) => {
+                    return Err(StacksHttpResponse::new_error(&preamble, &HttpNotFound::new("No such stacks tip".into())));
+                },
+                Err(e) => {
+                    return Err(StacksHttpResponse::new_error(&preamble, &HttpServerError::new(format!("Failed to load chain tip: {:?}", &e))));
+                }
+            };
+
+            let consensus_hash = &stacks_tip.consensus_hash;
+            let block_hash = &stacks_tip.anchored_block_hash;
+
+            // make sure we can accept this
+            let ch_sn = match SortitionDB::get_block_snapshot_consensus(sortdb.conn(), consensus_hash) {
+                Ok(Some(sn)) => sn,
+                Ok(None) => {
+                    return Err(StacksHttpResponse::new_error(&preamble, &HttpNotFound::new("No such snapshot for Stacks tip consensus hash".to_string())));
+                }
+                Err(e) => {
+                    debug!("No block snapshot for consensus hash {}", &consensus_hash);
+                    return Err(StacksHttpResponse::new_error(&preamble, &HttpBadRequest::new_json(ChainError::DBError(e).into_json())));
+                }
+            };
+
+            let sort_handle = sortdb.index_handle(&ch_sn.sortition_id);
+            let parent_block_snapshot = Relayer::get_parent_stacks_block_snapshot(&sort_handle, consensus_hash, block_hash)
+                .map_err(|e| StacksHttpResponse::new_error(&preamble, &HttpServerError::new(format!("Failed to load parent block for Stacks tip: {:?}", &e))))?;
+
+            let ast_rules = SortitionDB::get_ast_rules(&sort_handle, parent_block_snapshot.block_height)
+                .map_err(|e| StacksHttpResponse::new_error(&preamble, &HttpServerError::new(format!("Failed to load AST rules for Bitcoin block height {}: {:?}", parent_block_snapshot.block_height, &e))))?;
+
+            let epoch_id = self.get_stacks_epoch(&preamble, sortdb, parent_block_snapshot.block_height)?.epoch_id;
+
+            if !Relayer::static_check_problematic_relayed_microblock(
+                chainstate.mainnet,
+                epoch_id,
+                &microblock,
+                ast_rules,
+            ) {
+                info!("Microblock {} from {}/{} is problematic; will not store or relay it, nor its descendants", &microblock.block_hash(), consensus_hash, &block_hash);
+
+                // NOTE: txid is ignored in chainstate error .into_json()
+                return Err(StacksHttpResponse::new_error(&preamble, &HttpBadRequest::new_json(ChainError::ProblematicTransaction(Txid([0x00; 32])).into_json())));
+            }
+
+            match chainstate.preprocess_streamed_microblock(consensus_hash, block_hash, &microblock) {
+                Ok(accepted) => {
+                    debug!("{} uploaded microblock {}/{}-{}",
+                           if accepted { "Accepted" } else { "Did not accept" },
+                           consensus_hash,
+                           block_hash,
+                           &microblock.block_hash()
+                    );
+                    return Ok((accepted, StacksBlockHeader::make_index_block_hash(consensus_hash, block_hash)));
+                },
+                Err(e) => {
+                    debug!("Failed to process microblock {}/{}-{}: {:?}", &consensus_hash, &block_hash, &microblock.block_hash(), &e);
+                    return Err(StacksHttpResponse::new_error(&preamble, &HttpBadRequest::new_json(e.into_json())));
+                }
+            }
+        });
+
+        let (accepted, parent_block_id, data_resp) = match data_resp {
+            Ok((accepted, parent_block_id)) => (accepted, parent_block_id, microblock.block_hash()),
+            Err(response) => {
+                return response.try_into_contents().map_err(NetError::from);
+            }
+        };
+
+        // don't forget to forward this to the p2p network!
+        if accepted {
+            node.set_relay_message(StacksMessageType::Microblocks(MicroblocksData {
+                index_anchor_block: parent_block_id,
+                microblocks: vec![microblock],
+            }));
+        }
+
+        let mut preamble = HttpResponsePreamble::ok_json(&preamble);
+        preamble.set_canonical_stacks_tip_height(Some(node.canonical_stacks_tip_height()));
+        let body = HttpResponseContents::try_from_json(&data_resp)?;
+        Ok((preamble, body))
+    }
+}
+
+/// Decode the HTTP response
+impl HttpResponse for RPCPostMicroblockRequestHandler {
+    fn try_parse_response(
+        &self,
+        preamble: &HttpResponsePreamble,
+        body: &[u8],
+    ) -> Result<HttpResponsePayload, Error> {
+        let mblock_hash: BlockHeaderHash = parse_json(preamble, body)?;
+        Ok(HttpResponsePayload::try_from_json(mblock_hash)?)
+    }
+}
+
+impl StacksHttpRequest {
+    /// Make a new post-microblock request
+    pub fn new_post_microblock(
+        host: PeerHost,
+        mblock: StacksMicroblock,
+        tip_req: TipRequest,
+    ) -> StacksHttpRequest {
+        StacksHttpRequest::new_for_peer(
+            host,
+            "POST".into(),
+            "/v2/microblocks".into(),
+            HttpRequestContents::new()
+                .payload_stacks(&mblock)
+                .for_tip(tip_req),
+        )
+        .expect("FATAL: failed to construct request from infallible data")
+    }
+}
+
+impl StacksHttpResponse {
+    pub fn decode_stacks_microblock_response(self) -> Result<BlockHeaderHash, NetError> {
+        let contents = self.get_http_payload_ok()?;
+        let response_json: serde_json::Value = contents.try_into()?;
+        let result: BlockHeaderHash = serde_json::from_value(response_json)
+            .map_err(|_e| NetError::DeserializeError("Failed to load from JSON".to_string()))?;
+        Ok(result)
+    }
+}
---
+++ stackslib/src/net/api/tests/postmicroblock.rs
@@ -0,0 +1,160 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
+
+use crate::net::httpcore::{HttpRequestContentsExtensions, StacksHttp, StacksHttpRequest};
+
+use stacks_common::types::net::PeerHost;
+
+use stacks_common::types::chainstate::StacksAddress;
+use stacks_common::types::Address;
+
+use clarity::vm::types::PrincipalData;
+use clarity::vm::types::QualifiedContractIdentifier;
+use clarity::vm::types::StacksAddressExtensions;
+use clarity::vm::ClarityName;
+use clarity::vm::ContractName;
+use clarity::vm::Value;
+
+use crate::net::api::*;
+use crate::net::ProtocolFamily;
+use crate::net::TipRequest;
+
+use crate::core::BLOCK_LIMIT_MAINNET_21;
+
+use crate::chainstate::stacks::test::make_codec_test_microblock;
+use crate::chainstate::stacks::StacksMicroblock;
+use crate::net::httpcore::RPCRequestHandler;
+
+use crate::net::connection::ConnectionOptions;
+
+use super::TestRPC;
+
+#[test]
+fn test_try_parse_request() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+
+    let mblock = make_codec_test_microblock(3);
+    let request = StacksHttpRequest::new_post_microblock(
+        addr.into(),
+        mblock.clone(),
+        TipRequest::SpecificTip(StacksBlockId([0x22; 32])),
+    );
+    assert_eq!(
+        request.contents().tip_request(),
+        TipRequest::SpecificTip(StacksBlockId([0x22; 32]))
+    );
+
+    let bytes = request.try_serialize().unwrap();
+
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+    let mut handler = postmicroblock::RPCPostMicroblockRequestHandler::new();
+    let mut parsed_request = http
+        .handle_try_parse_request(
+            &mut handler,
+            &parsed_preamble.expect_request(),
+            &bytes[offset..],
+        )
+        .unwrap();
+
+    assert_eq!(handler.microblock, Some(mblock.clone()));
+
+    // parsed request consumes headers that would not be in a constructed reqeuest
+    parsed_request.clear_headers();
+    let (preamble, contents) = parsed_request.destruct();
+
+    assert_eq!(&preamble, request.preamble());
+
+    handler.restart();
+    assert!(handler.microblock.is_none());
+
+    // try to decode a bad microblock
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+    let mut bad_mblock = mblock.clone();
+    bad_mblock.txs.clear();
+    let request = StacksHttpRequest::new_post_microblock(
+        addr.into(),
+        bad_mblock.clone(),
+        TipRequest::SpecificTip(StacksBlockId([0x22; 32])),
+    );
+
+    let bytes = request.try_serialize().unwrap();
+
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+    let mut handler = postmicroblock::RPCPostMicroblockRequestHandler::new();
+    match http.handle_try_parse_request(
+        &mut handler,
+        &parsed_preamble.expect_request(),
+        &bytes[offset..],
+    ) {
+        Err(NetError::Http(Error::DecodeError(..))) => {}
+        _ => {
+            panic!("worked with bad microblock");
+        }
+    }
+}
+
+#[test]
+fn test_try_make_response() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+
+    let test_rpc = TestRPC::setup_ex(function_name!(), false);
+    let mblock = test_rpc.next_microblock.clone();
+
+    let mut requests = vec![];
+
+    // fails due to bad tip
+    let request = StacksHttpRequest::new_post_microblock(
+        addr.into(),
+        mblock.clone(),
+        TipRequest::SpecificTip(StacksBlockId([0x22; 32])),
+    );
+    requests.push(request);
+
+    // succeeds
+    let request = StacksHttpRequest::new_post_microblock(
+        addr.into(),
+        mblock.clone(),
+        TipRequest::UseLatestAnchoredTip,
+    );
+    requests.push(request);
+
+    let mut responses = test_rpc.run(requests);
+
+    // fails due to bad tip
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let (preamble, body) = response.destruct();
+    assert_eq!(preamble.status_code, 404);
+
+    // succeeds
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let mblock_hash = response.decode_stacks_microblock_response().unwrap();
+    assert_eq!(mblock_hash, mblock.block_hash());
+}
