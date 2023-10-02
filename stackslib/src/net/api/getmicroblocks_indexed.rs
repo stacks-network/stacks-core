7ad3b28276e70ab2d4d65f1344a22c5987fe70c4
Jude Nelson <judecn@gmail.com>
2023-10-02 16:12:54 -0400

    refactor: put get-indexed-microblocks handler into its own file

---
+++ stackslib/src/net/api/getmicroblocks_indexed.rs
@@ -0,0 +1,329 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use regex::{Captures, Regex};
+use std::fs;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::{Read, Seek, SeekFrom, Write};
+
+use crate::net::http::{
+    parse_bytes, Error, HttpBadRequest, HttpChunkGenerator, HttpContentType, HttpNotFound,
+    HttpRequest, HttpRequestContents, HttpRequestPreamble, HttpResponse, HttpResponseContents,
+    HttpResponsePayload, HttpResponsePreamble, HttpServerError, HttpVersion,
+};
+use crate::net::httpcore::{
+    HttpRequestContentsExtensions, RPCRequestHandler, StacksHttpRequest, StacksHttpResponse,
+};
+use crate::net::StacksNodeState;
+use crate::net::MAX_HEADERS;
+use crate::net::{
+    httpcore::{request, StacksHttp},
+    Error as NetError, TipRequest,
+};
+
+use crate::chainstate::stacks::Error as ChainError;
+
+use crate::chainstate::stacks::db::StacksChainState;
+use crate::chainstate::stacks::StacksBlockHeader;
+use crate::chainstate::stacks::StacksMicroblock;
+
+use stacks_common::codec::read_next;
+use stacks_common::codec::StacksMessageCodec;
+use stacks_common::codec::MAX_MESSAGE_LEN;
+use stacks_common::types::chainstate::BlockHeaderHash;
+use stacks_common::types::chainstate::StacksBlockId;
+use stacks_common::types::net::PeerHost;
+use stacks_common::util::hash::to_hex;
+
+use crate::util_lib::db::DBConn;
+use crate::util_lib::db::Error as DBError;
+
+use serde;
+use serde::de::Error as de_Error;
+use serde_json;
+
+#[derive(Clone)]
+pub struct RPCMicroblocksIndexedRequestHandler {
+    pub tail_microblock_id: Option<StacksBlockId>,
+}
+impl RPCMicroblocksIndexedRequestHandler {
+    pub fn new() -> Self {
+        Self {
+            tail_microblock_id: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct StacksIndexedMicroblockStream {
+    /// length prefix
+    pub num_items_buf: [u8; 4],
+    pub num_items_ptr: usize,
+
+    /// microblock pointer
+    pub microblock_hash: BlockHeaderHash,
+    pub parent_index_block_hash: StacksBlockId,
+
+    /// connection to the chain state
+    chainstate_db: DBConn,
+}
+
+impl StacksIndexedMicroblockStream {
+    pub fn new(
+        chainstate: &StacksChainState,
+        tail_index_microblock_hash: &StacksBlockId,
+    ) -> Result<Self, ChainError> {
+        // look up parent
+        let mblock_info = StacksChainState::load_staging_microblock_info_indexed(
+            &chainstate.db(),
+            tail_index_microblock_hash,
+        )?
+        .ok_or(ChainError::NoSuchBlockError)?;
+
+        let parent_index_block_hash = StacksBlockHeader::make_index_block_hash(
+            &mblock_info.consensus_hash,
+            &mblock_info.anchored_block_hash,
+        );
+
+        // need to send out the consensus_serialize()'ed array length before sending microblocks.
+        // this is exactly what seq tells us, though.
+        test_debug!(
+            "Will stream {} microblocks back from {}",
+            mblock_info.sequence,
+            &tail_index_microblock_hash
+        );
+        let num_items_buf = ((mblock_info.sequence as u32) + 1).to_be_bytes();
+
+        Ok(StacksIndexedMicroblockStream {
+            microblock_hash: mblock_info.microblock_hash,
+            parent_index_block_hash: parent_index_block_hash,
+            num_items_buf: num_items_buf,
+            num_items_ptr: 0,
+            chainstate_db: chainstate.reopen_db()?,
+        })
+    }
+}
+
+/// Decode the HTTP request
+impl HttpRequest for RPCMicroblocksIndexedRequestHandler {
+    fn verb(&self) -> &'static str {
+        "GET"
+    }
+
+    fn path_regex(&self) -> Regex {
+        Regex::new(r#"^/v2/microblocks/(?P<tail_microblock_id>[0-9a-f]{64})$"#).unwrap()
+    }
+
+    /// Try to decode this request.
+    /// There's nothing to load here, so just make sure the request is well-formed.
+    fn try_parse_request(
+        &mut self,
+        preamble: &HttpRequestPreamble,
+        captures: &Captures,
+        query: Option<&str>,
+        _body: &[u8],
+    ) -> Result<HttpRequestContents, Error> {
+        if preamble.get_content_length() != 0 {
+            return Err(Error::DecodeError(
+                "Invalid Http request: expected 0-length body".to_string(),
+            ));
+        }
+
+        let tail_microblock_id = request::get_block_hash(captures, "tail_microblock_id")?;
+
+        self.tail_microblock_id = Some(tail_microblock_id);
+
+        Ok(HttpRequestContents::new().query_string(query))
+    }
+}
+
+impl RPCRequestHandler for RPCMicroblocksIndexedRequestHandler {
+    /// Reset internal state
+    fn restart(&mut self) {
+        self.tail_microblock_id = None;
+    }
+
+    /// Make the response
+    fn try_handle_request(
+        &mut self,
+        preamble: HttpRequestPreamble,
+        _contents: HttpRequestContents,
+        node: &mut StacksNodeState,
+    ) -> Result<(HttpResponsePreamble, HttpResponseContents), NetError> {
+        let tail_microblock_id = self
+            .tail_microblock_id
+            .take()
+            .ok_or(NetError::SendError("`tail_microblock_id` not set".into()))?;
+        let stream_res =
+            node.with_node_state(|_network, _sortdb, chainstate, _mempool, _rpc_args| {
+                StacksIndexedMicroblockStream::new(chainstate, &tail_microblock_id)
+            });
+
+        // start loading up the microblocks
+        let stream = match stream_res {
+            Ok(stream) => stream,
+            Err(ChainError::NoSuchBlockError) => {
+                return StacksHttpResponse::new_error(
+                    &preamble,
+                    &HttpNotFound::new(format!("No such microblock {:?}\n", &tail_microblock_id)),
+                )
+                .try_into_contents()
+                .map_err(NetError::from)
+            }
+            Err(e) => {
+                // nope -- error trying to check
+                let msg = format!("Failed to load microblock: {:?}\n", &e);
+                warn!("{}", &msg);
+                return StacksHttpResponse::new_error(&preamble, &HttpServerError::new(msg))
+                    .try_into_contents()
+                    .map_err(NetError::from);
+            }
+        };
+
+        let resp_preamble = HttpResponsePreamble::from_http_request_preamble(
+            &preamble,
+            200,
+            "OK",
+            None,
+            HttpContentType::Bytes,
+        );
+
+        Ok((
+            resp_preamble,
+            HttpResponseContents::from_stream(Box::new(stream)),
+        ))
+    }
+}
+
+/// Decode the HTTP response
+impl HttpResponse for RPCMicroblocksIndexedRequestHandler {
+    /// Decode this response from a byte stream.  This is called by the client to decode this
+    /// message
+    fn try_parse_response(
+        &self,
+        preamble: &HttpResponsePreamble,
+        body: &[u8],
+    ) -> Result<HttpResponsePayload, Error> {
+        let bytes = parse_bytes(preamble, body, MAX_MESSAGE_LEN.into())?;
+        Ok(HttpResponsePayload::Bytes(bytes))
+    }
+}
+
+/// Stream implementation for HeaderStreamData
+impl HttpChunkGenerator for StacksIndexedMicroblockStream {
+    #[cfg(not(test))]
+    fn hint_chunk_size(&self) -> usize {
+        4096
+    }
+
+    #[cfg(test)]
+    fn hint_chunk_size(&self) -> usize {
+        // make this hurt
+        32
+    }
+
+    /// Stream back microblock chunks.
+    /// The first chunk is a 4-byte length prefix
+    /// Subsequent chunks are microblocks
+    fn generate_next_chunk(&mut self) -> Result<Vec<u8>, String> {
+        if self.num_items_ptr == 0 {
+            // send length prefix
+            self.num_items_ptr += self.num_items_buf.len();
+            return Ok(self.num_items_buf.to_vec());
+        }
+
+        // load next microblock
+        let mblock_info_opt = StacksChainState::load_staging_microblock_indexed(
+            &self.chainstate_db,
+            &self.parent_index_block_hash,
+            &self.microblock_hash,
+        ).map_err(|e| {
+            warn!("Failed to load microblock"; "microblock" => %self.microblock_hash, "parent anchored block" => %self.parent_index_block_hash, "error" => %e);
+            let msg = format!("Failed to load microblock {}-{}: {:?}", &self.parent_index_block_hash, &self.microblock_hash, &e);
+            msg
+        })?;
+
+        let mblock_info = if let Some(x) = mblock_info_opt {
+            x
+        } else {
+            // out of microblocks
+            debug!(
+                "Out of microblocks to stream";
+                "last microblock" => %self.microblock_hash,
+                "parent anchored block" => %self.parent_index_block_hash
+            );
+            return Ok(vec![]);
+        };
+
+        let buf = mblock_info.block_data;
+
+        self.microblock_hash = mblock_info.parent_hash;
+        return Ok(buf);
+    }
+}
+
+impl StacksHttpRequest {
+    pub fn new_getmicroblocks_indexed(
+        host: PeerHost,
+        index_microblock_hash: StacksBlockId,
+    ) -> StacksHttpRequest {
+        StacksHttpRequest::new_for_peer(
+            host,
+            "GET".into(),
+            format!("/v2/microblocks/{}", &index_microblock_hash),
+            HttpRequestContents::new(),
+        )
+        .expect("FATAL: failed to construct request from infallible data")
+    }
+}
+
+impl StacksHttpResponse {
+    #[cfg(test)]
+    pub fn new_getmicroblocks_indexed(
+        mblocks: Vec<StacksMicroblock>,
+        with_content_length: bool,
+    ) -> StacksHttpResponse {
+        let value = mblocks.serialize_to_vec();
+        let length = value.len();
+        let preamble = HttpResponsePreamble::new(
+            HttpVersion::Http11,
+            200,
+            "OK".to_string(),
+            if with_content_length {
+                Some(length as u32)
+            } else {
+                None
+            },
+            HttpContentType::Bytes,
+            true,
+        );
+        let body = HttpResponsePayload::Bytes(value);
+        StacksHttpResponse::new(preamble, body)
+    }
+
+    /// Decode an HTTP response into a microblock stream
+    /// If it fails, return Self::Error(..)
+    pub fn decode_microblocks(self) -> Result<Vec<StacksMicroblock>, NetError> {
+        let contents = self.get_http_payload_ok()?;
+
+        // contents will be a SIP-003 bytestream
+        let mblock_bytes: Vec<u8> = contents.try_into()?;
+        let microblocks: Vec<StacksMicroblock> = read_next(&mut &mblock_bytes[..])?;
+
+        Ok(microblocks)
+    }
+}
---
+++ stackslib/src/net/api/tests/getmicroblocks_indexed.rs
@@ -0,0 +1,314 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use stacks_common::codec::StacksMessageCodec;
+use stacks_common::types::chainstate::ConsensusHash;
+use stacks_common::types::chainstate::StacksBlockId;
+use stacks_common::types::chainstate::StacksPrivateKey;
+
+use crate::util_lib::db::DBConn;
+
+use crate::chainstate::stacks::db::{ExtendedStacksHeader, StacksChainState};
+use crate::chainstate::stacks::Error as chainstate_error;
+use crate::chainstate::stacks::StacksBlock;
+use crate::chainstate::stacks::StacksBlockHeader;
+use crate::chainstate::stacks::StacksMicroblock;
+
+use crate::chainstate::stacks::db::blocks::test::*;
+use crate::chainstate::stacks::db::test::instantiate_chainstate;
+use crate::chainstate::stacks::test::make_codec_test_block;
+
+use crate::net::api::getmicroblocks_indexed::StacksIndexedMicroblockStream;
+
+use crate::net::http::HttpChunkGenerator;
+
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
+
+use crate::net::httpcore::{HttpRequestContentsExtensions, StacksHttp, StacksHttpRequest};
+
+use stacks_common::types::net::PeerHost;
+
+use stacks_common::types::chainstate::StacksAddress;
+use stacks_common::types::Address;
+
+use clarity::vm::types::PrincipalData;
+use clarity::vm::types::QualifiedContractIdentifier;
+use clarity::vm::types::StacksAddressExtensions;
+use clarity::vm::ClarityName;
+use clarity::vm::ContractName;
+use clarity::vm::Value;
+
+use crate::net::api::*;
+use crate::net::ProtocolFamily;
+use crate::net::TipRequest;
+
+use crate::core::BLOCK_LIMIT_MAINNET_21;
+use crate::net::httpcore::RPCRequestHandler;
+
+use crate::net::connection::ConnectionOptions;
+
+use super::TestRPC;
+
+#[test]
+fn test_try_parse_request() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+
+    let request =
+        StacksHttpRequest::new_getmicroblocks_indexed(addr.into(), StacksBlockId([0x22; 32]));
+    let bytes = request.try_serialize().unwrap();
+
+    debug!("Request:\n{}\n", std::str::from_utf8(&bytes).unwrap());
+
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+    let mut handler = getmicroblocks_indexed::RPCMicroblocksIndexedRequestHandler::new();
+    let mut parsed_request = http
+        .handle_try_parse_request(
+            &mut handler,
+            &parsed_preamble.expect_request(),
+            &bytes[offset..],
+        )
+        .unwrap();
+
+    // consumed path args and body
+    assert_eq!(handler.tail_microblock_id, Some(StacksBlockId([0x22; 32])));
+
+    // parsed request consumes headers that would not be in a constructed reqeuest
+    parsed_request.clear_headers();
+    let (preamble, contents) = parsed_request.destruct();
+
+    assert_eq!(&preamble, request.preamble());
+
+    handler.restart();
+    assert!(handler.tail_microblock_id.is_none());
+}
+
+#[test]
+fn test_try_make_response() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+
+    let mut rpc_test = TestRPC::setup(function_name!());
+
+    // store an additional block and microblock stream, so we can fetch it.
+    let privk = StacksPrivateKey::from_hex(
+        "eb05c83546fdd2c79f10f5ad5434a90dd28f7e3acb7c092157aa1bc3656b012c01",
+    )
+    .unwrap();
+    let parent_block = make_codec_test_block(25);
+    let parent_consensus_hash = ConsensusHash([0x02; 20]);
+    let parent_index_block_hash = StacksBlockHeader::make_index_block_hash(
+        &parent_consensus_hash,
+        &parent_block.block_hash(),
+    );
+
+    let mut mblocks = make_sample_microblock_stream(&privk, &parent_block.block_hash());
+    mblocks.truncate(15);
+
+    let mut child_block = make_codec_test_block(25);
+    let child_consensus_hash = ConsensusHash([0x03; 20]);
+
+    child_block.header.parent_block = parent_block.block_hash();
+    child_block.header.parent_microblock = mblocks.last().as_ref().unwrap().block_hash();
+    child_block.header.parent_microblock_sequence =
+        mblocks.last().as_ref().unwrap().header.sequence;
+
+    store_staging_block(
+        rpc_test.peer_2.chainstate(),
+        &parent_consensus_hash,
+        &parent_block,
+        &ConsensusHash([0x01; 20]),
+        456,
+        123,
+    );
+    set_block_processed(
+        rpc_test.peer_2.chainstate(),
+        &parent_consensus_hash,
+        &parent_block.block_hash(),
+        true,
+    );
+
+    store_staging_block(
+        rpc_test.peer_2.chainstate(),
+        &child_consensus_hash,
+        &child_block,
+        &parent_consensus_hash,
+        456,
+        123,
+    );
+    set_block_processed(
+        rpc_test.peer_2.chainstate(),
+        &child_consensus_hash,
+        &child_block.block_hash(),
+        true,
+    );
+
+    let index_microblock_hash = StacksBlockHeader::make_index_block_hash(
+        &parent_consensus_hash,
+        &mblocks.last().as_ref().unwrap().block_hash(),
+    );
+
+    for mblock in mblocks.iter() {
+        store_staging_microblock(
+            rpc_test.peer_2.chainstate(),
+            &parent_consensus_hash,
+            &parent_block.block_hash(),
+            &mblock,
+        );
+    }
+
+    set_microblocks_processed(
+        rpc_test.peer_2.chainstate(),
+        &child_consensus_hash,
+        &child_block.block_hash(),
+        &mblocks.last().as_ref().unwrap().block_hash(),
+    );
+
+    let mut requests = vec![];
+
+    // query existing microblock stream
+    let request =
+        StacksHttpRequest::new_getmicroblocks_indexed(addr.into(), index_microblock_hash.clone());
+    requests.push(request);
+
+    // query non-existant microblock stream
+    let request =
+        StacksHttpRequest::new_getmicroblocks_indexed(addr.into(), StacksBlockId([0x11; 32]));
+    requests.push(request);
+
+    let mut responses = rpc_test.run(requests);
+
+    // got the microblock stream
+    let response = responses.remove(0);
+    let mut resp = response.decode_microblocks().unwrap();
+
+    resp.reverse();
+    debug!("microblocks: {:?}", &resp);
+    assert_eq!(resp, mblocks);
+
+    // no microblock stream
+    let response = responses.remove(0);
+    let (preamble, body) = response.destruct();
+
+    assert_eq!(preamble.status_code, 404);
+}
+
+#[test]
+fn test_stream_indexed_microblocks() {
+    let mut chainstate = instantiate_chainstate(false, 0x80000000, function_name!());
+    let privk = StacksPrivateKey::from_hex(
+        "eb05c83546fdd2c79f10f5ad5434a90dd28f7e3acb7c092157aa1bc3656b012c01",
+    )
+    .unwrap();
+
+    let block = make_empty_coinbase_block(&privk);
+    let mut mblocks = make_sample_microblock_stream(&privk, &block.block_hash());
+    mblocks.truncate(5);
+
+    let mut child_block = make_empty_coinbase_block(&privk);
+    child_block.header.parent_block = block.block_hash();
+    child_block.header.parent_microblock = mblocks.last().as_ref().unwrap().block_hash();
+    child_block.header.parent_microblock_sequence =
+        mblocks.last().as_ref().unwrap().header.sequence;
+
+    let consensus_hash = ConsensusHash([2u8; 20]);
+    let parent_consensus_hash = ConsensusHash([1u8; 20]);
+    let child_consensus_hash = ConsensusHash([3u8; 20]);
+
+    // store microblocks to staging
+    for (i, mblock) in mblocks.iter().enumerate() {
+        store_staging_microblock(
+            &mut chainstate,
+            &consensus_hash,
+            &block.block_hash(),
+            mblock,
+        );
+    }
+
+    // store block to staging
+    store_staging_block(
+        &mut chainstate,
+        &consensus_hash,
+        &block,
+        &parent_consensus_hash,
+        1,
+        2,
+    );
+
+    // store child block to staging
+    store_staging_block(
+        &mut chainstate,
+        &child_consensus_hash,
+        &child_block,
+        &consensus_hash,
+        1,
+        2,
+    );
+
+    // accept it
+    set_block_processed(&mut chainstate, &consensus_hash, &block.block_hash(), true);
+    set_block_processed(
+        &mut chainstate,
+        &child_consensus_hash,
+        &child_block.block_hash(),
+        true,
+    );
+
+    for i in 0..mblocks.len() {
+        // set different parts of this stream as confirmed
+        set_microblocks_processed(
+            &mut chainstate,
+            &child_consensus_hash,
+            &child_block.block_hash(),
+            &mblocks[i].block_hash(),
+        );
+
+        // verify that we can stream everything
+        let microblock_index_header =
+            StacksBlockHeader::make_index_block_hash(&consensus_hash, &mblocks[i].block_hash());
+
+        let mut stream =
+            StacksIndexedMicroblockStream::new(&chainstate, &microblock_index_header).unwrap();
+
+        let mut confirmed_mblock_bytes = vec![];
+        loop {
+            let mut next_bytes = stream.generate_next_chunk().unwrap();
+            if next_bytes.len() == 0 {
+                break;
+            }
+            test_debug!(
+                "Got {} more bytes from staging; add to {} total",
+                next_bytes.len(),
+                confirmed_mblock_bytes.len()
+            );
+            confirmed_mblock_bytes.append(&mut next_bytes);
+        }
+
+        // decode stream (should be length-prefixed)
+        let mut confirmed_mblocks =
+            Vec::<StacksMicroblock>::consensus_deserialize(&mut &confirmed_mblock_bytes[..])
+                .unwrap();
+
+        confirmed_mblocks.reverse();
+
+        assert_eq!(confirmed_mblocks.len(), mblocks[0..(i + 1)].len());
+        for j in 0..(i + 1) {
+            test_debug!("check {}", j);
+            assert_eq!(confirmed_mblocks[j], mblocks[j])
+        }
+    }
+}
