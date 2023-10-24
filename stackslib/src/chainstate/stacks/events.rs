489f385ccde1bf9fc23d75fc651a1f39056f69b3
Jude Nelson <judecn@gmail.com>
2023-10-23 23:42:00 -0400

    feat: capture event observer data in StacksBlockEventData, which is compatible with both StacksBlock and NakamotoBlock

--- stackslib/src/chainstate/stacks/events.rs
+++ stackslib/src/chainstate/stacks/events.rs
@@ -1,5 +1,7 @@
 use crate::burnchains::Txid;
 use crate::chainstate::burn::operations::BlockstackOperationType;
+use crate::chainstate::nakamoto::NakamotoBlock;
+use crate::chainstate::stacks::StacksBlock;
 use crate::chainstate::stacks::StacksMicroblockHeader;
 use crate::chainstate::stacks::StacksTransaction;
 use clarity::vm::analysis::ContractAnalysis;
@@ -9,6 +11,7 @@ use clarity::vm::types::{
     AssetIdentifier, PrincipalData, QualifiedContractIdentifier, StandardPrincipalData, Value,
 };
 use stacks_common::codec::StacksMessageCodec;
+use stacks_common::types::chainstate::BlockHeaderHash;
 use stacks_common::types::chainstate::StacksAddress;
 use stacks_common::util::hash::to_hex;
 
@@ -54,4 +57,34 @@ pub struct StacksTransactionReceipt {
     pub tx_index: u32,
     /// This is really a string-formatted CheckError (which can't be clone()'ed)
     pub vm_error: Option<String>,
+}
+
+#[derive(Clone)]
+pub struct StacksBlockEventData {
+    pub block_hash: BlockHeaderHash,
+    pub parent_block_hash: BlockHeaderHash,
+    pub parent_microblock_hash: BlockHeaderHash,
+    pub parent_microblock_sequence: u16,
+}
+
+impl From<StacksBlock> for StacksBlockEventData {
+    fn from(block: StacksBlock) -> StacksBlockEventData {
+        StacksBlockEventData {
+            block_hash: block.block_hash(),
+            parent_block_hash: block.header.parent_block,
+            parent_microblock_hash: block.header.parent_microblock,
+            parent_microblock_sequence: block.header.parent_microblock_sequence,
+        }
+    }
+}
+
+impl From<(NakamotoBlock, BlockHeaderHash)> for StacksBlockEventData {
+    fn from(block: (NakamotoBlock, BlockHeaderHash)) -> StacksBlockEventData {
+        StacksBlockEventData {
+            block_hash: block.0.header.block_hash(),
+            parent_block_hash: block.1,
+            parent_microblock_hash: BlockHeaderHash([0u8; 32]),
+            parent_microblock_sequence: 0,
+        }
+    }
 }
