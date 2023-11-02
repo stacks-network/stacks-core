f10b817b2c81c0a50d1565fd6632459d098dd5fd
Jude Nelson <judecn@gmail.com>
2023-11-02 13:55:20 -0400

    chore: add v3 unlock and pox-4 activation

--- clarity/src/vm/docs/mod.rs
+++ clarity/src/vm/docs/mod.rs
@@ -2772,8 +2772,16 @@ mod test {
         fn get_v2_unlock_height(&self) -> u32 {
             u32::MAX
         }
+        
+        fn get_v3_unlock_height(&self) -> u32 {
+            u32::MAX
+        }
 
         fn get_pox_3_activation_height(&self) -> u32 {
+            u32::MAX
+        }
+        
+        fn get_pox_4_activation_height(&self) -> u32 {
             u32::MAX
         }
 
