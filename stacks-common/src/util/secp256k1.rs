2e45ed873f595fb12a40ac42a4f6ba5677120fd1
Jacinta Ferrant <jacinta@trustmachines.co>
2023-11-08 13:54:53 -0500

    Add wsts::common::Signature to and from SchnorrSignature conversion
    Signed-off-by: Jacinta Ferrant <jacinta@trustmachines.co>

--- stacks-common/src/util/secp256k1.rs
+++ stacks-common/src/util/secp256k1.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+use p256k1::point::Compressed;
 use rand::{thread_rng, RngCore};
 use secp256k1;
 use secp256k1::ecdsa::{
@@ -27,8 +28,11 @@ use secp256k1::{
 use serde::de::{Deserialize, Error as de_Error};
 use serde::ser::Error as ser_Error;
 use serde::Serialize;
+use wsts::common::Signature as WSTSSignature;
+use wsts::{Point, Scalar};
 
 use super::hash::Sha256Sum;
+use crate::impl_byte_array_message_codec;
 use crate::types::{PrivateKey, PublicKey};
 use crate::util::hash::{hex_bytes, to_hex};
 
@@ -112,6 +116,45 @@ impl MessageSignature {
 impl Default for Secp256k1PublicKey {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub struct SchnorrSignature(pub [u8; 65]);
+impl_array_newtype!(SchnorrSignature, u8, 65);
+impl_array_hexstring_fmt!(SchnorrSignature);
+impl_byte_array_newtype!(SchnorrSignature, u8, 65);
+impl_byte_array_serde!(SchnorrSignature);
+pub const SCHNORR_SIGNATURE_ENCODED_SIZE: u32 = 65;
+
+impl Default for SchnorrSignature {
+    /// Creates a default Schnorr Signature. Note this is not a valid signature.
+    fn default() -> Self {
+        Self([0u8; 65])
+    }
+}
+
+impl SchnorrSignature {
+    /// Attempt to convert a Schnorr signature to a WSTS Signature
+    pub fn to_wsts_signature(&self) -> Option<WSTSSignature> {
+        // TODO: update wsts to add a TryFrom for a [u8; 65] and a slice to a Signature
+        let point_bytes: [u8; 33] = self.0[..33].try_into().ok()?;
+        let scalar_bytes: [u8; 32] = self.0[33..].try_into().ok()?;
+        let point = Point::try_from(&Compressed::from(point_bytes)).ok()?;
+        let scalar = Scalar::from(scalar_bytes);
+        Some(WSTSSignature {
+            R: point,
+            z: scalar,
+        })
+    }
+}
+
+/// Convert a WSTS Signature to a SchnorrSignature
+impl From<&WSTSSignature> for SchnorrSignature {
+    fn from(signature: &WSTSSignature) -> Self {
+        let mut buf = [0u8; 65];
+        buf[..33].copy_from_slice(&signature.R.compress().data);
+        buf[33..].copy_from_slice(&signature.z.to_bytes());
+        SchnorrSignature(buf)
     }
 }
 
