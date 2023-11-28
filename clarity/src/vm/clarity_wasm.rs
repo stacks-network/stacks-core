a994a4c6f652d601307a711d9d9d671ed829fbea
Hugo Caillard <911307+hugocaillard@users.noreply.github.com>
2023-11-28 18:55:45 +0100

    fix: fix build

--- clarity/src/vm/clarity_wasm.rs
+++ clarity/src/vm/clarity_wasm.rs
@@ -375,7 +375,7 @@ fn placeholder_for_type(ty: ValType) -> Val {
         ValType::I64 => Val::I64(0),
         ValType::F32 => Val::F32(0),
         ValType::F64 => Val::F64(0),
-        ValType::V128 => Val::V128(0),
+        ValType::V128 => Val::V128(0.into()),
         ValType::ExternRef => unimplemented!("ExternRef"),
         ValType::FuncRef => unimplemented!("FuncRef"),
     }
