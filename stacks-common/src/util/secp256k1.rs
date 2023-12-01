d650b72aca852e7853af7fe1eefb836592559400
Jude Nelson <judecn@gmail.com>
2023-12-01 15:59:26 -0500

    chore: remove commented-out code

--- stacks-common/src/util/secp256k1.rs
+++ stacks-common/src/util/secp256k1.rs
@@ -118,47 +118,6 @@ impl Default for Secp256k1PublicKey {
     }
 }
 
-/*
-pub struct SchnorrSignature(pub [u8; 65]);
-impl_array_newtype!(SchnorrSignature, u8, 65);
-impl_array_hexstring_fmt!(SchnorrSignature);
-impl_byte_array_newtype!(SchnorrSignature, u8, 65);
-impl_byte_array_serde!(SchnorrSignature);
-pub const SCHNORR_SIGNATURE_ENCODED_SIZE: u32 = 65;
-
-impl Default for SchnorrSignature {
-    /// Creates a default Schnorr Signature. Note this is not a valid signature.
-    fn default() -> Self {
-        Self([0u8; 65])
-    }
-}
-
-impl SchnorrSignature {
-    /// Attempt to convert a Schnorr signature to a WSTS Signature
-    pub fn to_wsts_signature(&self) -> Option<WSTSSignature> {
-        // TODO: update wsts to add a TryFrom for a [u8; 65] and a slice to a Signature
-        let point_bytes: [u8; 33] = self.0[..33].try_into().ok()?;
-        let scalar_bytes: [u8; 32] = self.0[33..].try_into().ok()?;
-        let point = Point::try_from(&Compressed::from(point_bytes)).ok()?;
-        let scalar = Scalar::from(scalar_bytes);
-        Some(WSTSSignature {
-            R: point,
-            z: scalar,
-        })
-    }
-}
-
-/// Convert a WSTS Signature to a SchnorrSignature
-impl From<&WSTSSignature> for SchnorrSignature {
-    fn from(signature: &WSTSSignature) -> Self {
-        let mut buf = [0u8; 65];
-        buf[..33].copy_from_slice(&signature.R.compress().data);
-        buf[33..].copy_from_slice(&signature.z.to_bytes());
-        SchnorrSignature(buf)
-    }
-}
-*/
-
 impl Secp256k1PublicKey {
     #[cfg(any(test, feature = "testing"))]
     pub fn new() -> Secp256k1PublicKey {
