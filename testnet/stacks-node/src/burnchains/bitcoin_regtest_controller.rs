87940b9a9b9a0fd2d6f8990db0a7aecf0d524857
Brice Dobry <brice@hiro.so>
2023-12-06 17:04:21 -0500

    fix: use compile-time macro for `wasm` feature isolation

--- testnet/stacks-node/src/burnchains/bitcoin_regtest_controller.rs
+++ testnet/stacks-node/src/burnchains/bitcoin_regtest_controller.rs
@@ -1866,13 +1866,17 @@ impl BitcoinRegtestController {
                 let message = signer
                     .sign_message(sig_hash.as_bytes())
                     .expect("Unable to sign message");
-                if cfg!(feature = "wasm") {
+                #[cfg(feature = "wasm")]
+                {
                     message
                         .to_secp256k1_recoverable()
                         .expect("Unable to get recoverable signature")
                         .0
                         .serialize_der()
-                } else {
+                }
+
+                #[cfg(not(feature = "wasm"))]
+                {
                     message
                         .to_secp256k1_recoverable()
                         .expect("Unable to get recoverable signature")
