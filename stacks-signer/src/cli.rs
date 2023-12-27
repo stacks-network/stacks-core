ca57f47f32055aa1e689996aef30913f1f03cf50
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-12-27 16:20:33 -0600

    copyrights

--- stacks-signer/src/cli.rs
+++ stacks-signer/src/cli.rs
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use std::io::{self, Read};
 use std::net::SocketAddr;
 use std::path::PathBuf;
