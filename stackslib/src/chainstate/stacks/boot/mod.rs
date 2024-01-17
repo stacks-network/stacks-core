d7fc6546ea6aab75f7e542e00455fb61d18ae481
jesus <jnajera1917@gmail.com>
2024-01-17 12:56:28 -0500

    added delegate_stack_increase

--- stackslib/src/chainstate/stacks/boot/mod.rs
+++ stackslib/src/chainstate/stacks/boot/mod.rs
@@ -1877,7 +1877,7 @@ pub mod test {
         key: &StacksPrivateKey,
         nonce: u64,
         stacker: &PrincipalData,
-        pox_addr: Value,
+        pox_addr: PoxAddress,
         amount: u128,
     ) -> StacksTransaction {
         //let addr_tuple = Value::Tuple(pox_addr.as_clarity_tuple().unwrap());
@@ -1887,7 +1887,7 @@ pub mod test {
             "delegate-stack-increase",
             vec![
                 Value::Principal(stacker.clone()),
-                pox_addr,
+                Value::Tuple(pox_addr.as_clarity_tuple().unwrap()),
                 Value::UInt(amount),
             ],
         )
