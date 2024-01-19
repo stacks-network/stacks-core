26b23a19aea490040fe4068f6a1f00c323ede4ac
jesus <jnajera1917@gmail.com>
2024-01-19 06:51:01 -0500

    removed unused import

--- stackslib/src/chainstate/stacks/boot/pox_4_tests.rs
+++ stackslib/src/chainstate/stacks/boot/pox_4_tests.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::any::Any;
 use std::collections::{BTreeMap, HashMap, HashSet, VecDeque};
 use std::convert::{TryFrom, TryInto};
 
