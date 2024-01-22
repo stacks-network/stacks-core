460b758fd84ac90377456a7028a0273d824fe4d9
jesus <jnajera1917@gmail.com>
2024-01-22 13:27:34 -0500

    updated asserts to check values instead of strings

--- stackslib/src/chainstate/stacks/boot/pox_4_tests.rs
+++ stackslib/src/chainstate/stacks/boot/pox_4_tests.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::collections::{ HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::convert::{TryFrom, TryInto};
 
 use clarity::vm::clarity::ClarityConnection;
@@ -1679,8 +1679,8 @@ fn delegate_stack_stx_signer_key() {
     let state_signer_key = stacking_state.get("signer-key").unwrap();
 
     assert_eq!(
-        state_signer_key.to_string(),
-        format!("0x{}", signer_public_key.to_hex())
+        state_signer_key,
+        &Value::buff_from(signer_public_key.to_bytes_compressed()).unwrap()
     );
 }
 
@@ -1749,8 +1749,8 @@ fn delegate_stack_stx_extend_signer_key() {
     // Testing initial signer-key correctly set
     let state_signer_key = stacking_state.get("signer-key").unwrap();
     assert_eq!(
-        state_signer_key.to_string(),
-        format!("0x{}", signer_public_key.to_hex())
+        state_signer_key,
+        &Value::buff_from(signer_public_key.to_bytes_compressed()).unwrap()
     );
 
     stacker_nonce += 1;
@@ -1779,8 +1779,8 @@ fn delegate_stack_stx_extend_signer_key() {
     // Testing new signer-key correctly set
     let state_signer_key_new = new_stacking_state.get("signer-key").unwrap();
     assert_eq!(
-        state_signer_key_new.to_string(),
-        format!("0x{}", new_signer_public_key.to_hex())
+        state_signer_key_new,
+        &Value::buff_from(new_signer_public_key.to_bytes_compressed()).unwrap()
     );
 }
 
@@ -1827,8 +1827,8 @@ fn stack_increase() {
     // Testing initial signer-key correctly set
     let state_signer_key = stacking_state.get("signer-key").unwrap();
     assert_eq!(
-        state_signer_key.to_string(),
-        format!("0x{}", signer_public_key.to_hex())
+        state_signer_key,
+        &Value::buff_from(signer_public_key.to_bytes_compressed()).unwrap()
     );
 
     stacker_nonce += 1;
