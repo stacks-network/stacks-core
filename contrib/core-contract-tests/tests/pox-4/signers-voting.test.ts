c883ff089cb5d86a3339201627c1480b792b2b34
friedger <mail@friedger.de>
2024-01-30 11:22:07 +0100

    fix: remove untestable calls, add more tests

--- contrib/core-contract-tests/tests/pox-4/signers-voting.test.ts
+++ contrib/core-contract-tests/tests/pox-4/signers-voting.test.ts
@@ -17,216 +17,37 @@ const ERR_INVALID_BURN_BLOCK_HEIGHT = 10007
 
 const KEY_1 = "123456789a123456789a123456789a123456789a123456789a123456789a010203";
 const KEY_2 = "123456789a123456789a123456789a123456789a123456789a123456789ab0b1b2";
+const SIGNERS_VOTING = "signers-voting";
 
 describe("test signers-voting contract voting rounds", () => {
-    describe("test get-last-round", () => {
-        it("should return none before any vote", () => {
-
-            const { result: resultRound } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
-                [Cl.uint(0)],
-                alice,
-            );
-            expect(resultRound).toEqual(Cl.none());
-        })
-
-        it("should return none after invalid vote", () => {
-            const { result: resultVote } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(0), Cl.bufferFromHex("12"), Cl.uint(0),], alice);
-            expect(resultVote).toEqual(Cl.error(Cl.uint(ERR_INVALID_AGGREGATE_PUBLIC_KEY)));
-
-            const { result: resultRound } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
-                [Cl.uint(0)],
-                alice,
-            );
-            expect(resultRound).toEqual(Cl.none());
-
-        })
-
-        it("should return round after valid vote", () => {
-            const { result: resultVote } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-            expect(resultVote).toEqual(Cl.ok(Cl.bool(true)));
-
-            const { result: resultRound } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
-                [Cl.uint(0)],
-                alice,
-            );
-            expect(resultRound).toEqual(Cl.some(Cl.uint(0)));
-
-        })
-
-        it("should return last round after valid votes for two rounds", () => {
-            // Alice votes for cycle 0, round 0
-            const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-            expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-            // Bob votes for cycle 0, round 1
-            const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(1), Cl.bufferFromHex(KEY_2), Cl.uint(1),], bob);
-            expect(resultVoteBob).toEqual(Cl.ok(Cl.bool(true)));
-
-            const { result: resultLastRound0 } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
-                [Cl.uint(0)],
-                alice,
-            );
-            expect(resultLastRound0).toEqual(Cl.some(Cl.uint(1)));
-        })
-
-        it("should return last round after valid votes for different cycles", () => {
-            // Alice votes for cycle 0, round 1
-            const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(1),], alice);
-            expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-            // advance to next cycle
-            simnet.mineEmptyBlocks(1050);
-
-            // Bob votes for cycle 1, round 0
-            const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(1), Cl.bufferFromHex(KEY_2), Cl.uint(0),], bob);
-            expect(resultVoteBob).toEqual(Cl.ok(Cl.bool(true)));
-
-            const { result: resultLastRound0 } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
-                [Cl.uint(0)],
-                alice,
-            );
-            expect(resultLastRound0).toEqual(Cl.some(Cl.uint(1)));
-
-
-            const { result: resultLastRound1 } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-last-round",
+    describe("test pox-info", () => {
+        it("should return correct burn-height", () => {
+            const { result:result1 } = simnet.callReadOnlyFn(SIGNERS_VOTING,
+                "reward-cycle-to-burn-height",
                 [Cl.uint(1)],
-                alice,
-            );
-            expect(resultLastRound1).toEqual(Cl.some(Cl.uint(0)));
+                alice)
+            expect(result1).toEqual(Cl.uint(1050))
 
+            const { result:result2 } = simnet.callReadOnlyFn(SIGNERS_VOTING,
+                "reward-cycle-to-burn-height",
+                [Cl.uint(2)],
+                alice)
+            expect(result2).toEqual(Cl.uint(2100))
         })
 
-    }),
+        it("should return correct reward-cycle", () => {
+            const { result: result1 } = simnet.callReadOnlyFn(SIGNERS_VOTING,
+                "burn-height-to-reward-cycle",
+                [Cl.uint(1)],
+                alice)
+            expect(result1).toEqual(Cl.uint(0))
 
-        describe("test voting", () => {
-
-            it("should fail on same key for different round", () => {
-                // Alice votes for cycle 0, round 0
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-                // Bob votes for cycle 0, round 1
-                const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(1), Cl.bufferFromHex(KEY_1), Cl.uint(1),], bob);
-                expect(resultVoteBob).toEqual(Cl.error(Cl.uint(ERR_DUPLICATE_AGGREGATE_PUBLIC_KEY)));
-
-            })
-
-            it("should fail on same key for different cycles", () => {
-                // Alice votes for cycle 0, round 0
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-                // advance to next cycle
-                simnet.mineEmptyBlocks(1050);
-
-                // Bob votes for cycle 1, round 0
-                const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(1), Cl.bufferFromHex(KEY_1), Cl.uint(0),], bob);
-                expect(resultVoteBob).toEqual(Cl.error(Cl.uint(ERR_DUPLICATE_AGGREGATE_PUBLIC_KEY)));
-
-            })
-
-            it("should fail on same key for different cycles", () => {
-                // Alice votes for cycle 0, round 0
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-                // advance to next cycle
-                simnet.mineEmptyBlocks(1050);
-
-                // Bob votes for cycle 1, round 0
-                const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(1), Cl.bufferFromHex(KEY_1), Cl.uint(0),], bob);
-                expect(resultVoteBob).toEqual(Cl.error(Cl.uint(ERR_DUPLICATE_AGGREGATE_PUBLIC_KEY)));
-
-            })
-
-            it("should fail on second vote for same cycle and round", () => {
-                // Alice votes for cycle 0, round 0
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-                // Alice votes for cycle 0, round 0 again
-                const { result: resultVoteAlice2 } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice2).toEqual(Cl.error(Cl.uint(ERR_DUPLICATE_VOTE)));
-
-            })
-
-            it("should fail on early vote", () => {
-                // Alice votes for cycle 1, round 0 during cycle 0
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-                expect(resultVoteAlice).toEqual(Cl.error(Cl.uint(ERR_INVALID_SIGNER_INDEX)));
-
-            })
-
-            it("should fail on late round", () => {
-                // Alice votes for cycle 0, round 1
-                const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(1),], alice);
-                expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-                // Bob votes for cycle 0, round 0
-                const { result: resultVoteBob } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                    [Cl.uint(1), Cl.bufferFromHex(KEY_1), Cl.uint(0),], bob);
-                expect(resultVoteBob).toEqual(Cl.error(Cl.uint(ERR_OLD_ROUND)));
-            })
+            const { result: result2000 } = simnet.callReadOnlyFn(SIGNERS_VOTING,
+                "burn-height-to-reward-cycle",
+                [Cl.uint(2000)],
+                alice)
+            expect(result2000).toEqual(Cl.uint(1))
         })
-
-    describe("test get-vote", () => {
-        it("should return correct aggregate-public-key and shared", () => {
-            // Alice votes for cycle 0, round 0
-            const { result: resultVoteAlice } = simnet.callPublicFn("signers-voting", "vote-for-aggregate-public-key",
-                [Cl.uint(0), Cl.bufferFromHex(KEY_1), Cl.uint(0),], alice);
-            expect(resultVoteAlice).toEqual(Cl.ok(Cl.bool(true)));
-
-            const { result: vote } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-vote",
-                [Cl.uint(0), Cl.uint(0), Cl.standardPrincipal(alice)],
-                alice,
-            );
-            expect(vote).toEqual(Cl.some(Cl.tuple({
-                "aggregate-public-key": Cl.bufferFromHex(KEY_1),
-                "reward-slots":
-                    Cl.uint(1)
-            })));
-
-        });
-
-        it("should return none when not yet voted", () => {
-            const { result: vote } = simnet.callReadOnlyFn(
-                "signers-voting",
-                "get-vote",
-                [Cl.uint(0), Cl.uint(0), Cl.standardPrincipal(alice)],
-                alice,
-            );
-            expect(vote).toEqual(Cl.none());
-
-        });
     })
+
 });
