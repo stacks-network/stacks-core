21feeb495fadc964e8f96d11ed4cf5cb4e9df7e4
Jude Nelson <judecn@gmail.com>
2024-02-06 17:19:15 -0500

    fix: get stackerdb client to use all stackerdb instances

--- stacks-signer/src/client/stackerdb.rs
+++ stacks-signer/src/client/stackerdb.rs
@@ -25,7 +25,7 @@ use libstackerdb::{StackerDBChunkAckData, StackerDBChunkData};
 use slog::{slog_debug, slog_warn};
 use stacks_common::codec::{read_next, StacksMessageCodec};
 use stacks_common::consts::SIGNER_SLOTS_PER_USER;
-use stacks_common::types::chainstate::StacksPrivateKey;
+use stacks_common::types::chainstate::{StacksAddress, StacksPrivateKey, StacksPublicKey};
 use stacks_common::{debug, warn};
 
 use super::ClientError;
@@ -44,8 +44,8 @@ pub struct StackerDB {
     stacks_private_key: StacksPrivateKey,
     /// A map of a (signer-set, message ID) to last chunk version for each session
     slot_versions: HashMap<(u32, u32), HashMap<u32, u32>>,
-    /// The signer ID
-    signer_id: u32,
+    /// The signer ID -- the index into the signer list for this signer daemon's signing key.
+    signer_slot_id: u32,
     /// Which signer set to use (0 or 1).
     /// Depends on whether or not we're signing in an even or odd reward cycle
     signer_set: u32,
@@ -76,7 +76,7 @@ impl From<&Config> for StackerDB {
             signers_message_stackerdb_sessions,
             stacks_private_key: config.stacks_private_key,
             slot_versions: HashMap::new(),
-            signer_id: config.signer_id,
+            signer_slot_id: config.signer_id,
             signer_set: 0,
         }
     }
@@ -110,7 +110,7 @@ impl StackerDB {
             signers_message_stackerdb_sessions,
             stacks_private_key,
             slot_versions: HashMap::new(),
-            signer_id,
+            signer_slot_id: signer_id,
             signer_set: 0,
         }
     }
@@ -123,7 +123,7 @@ impl StackerDB {
         let message_bytes = message.serialize_to_vec();
         let msg_id = message.msg_id();
         let signer_set = self.signer_set;
-        let slot_id = self.signer_id;
+        let slot_id = self.signer_slot_id;
         loop {
             let slot_version =
                 if let Some(versions) = self.slot_versions.get_mut(&(signer_set, msg_id)) {
@@ -161,7 +161,9 @@ impl StackerDB {
 
             if let Some(versions) = self.slot_versions.get_mut(&(signer_set, msg_id)) {
                 // NOTE: per the above, this is always executed
-                versions.insert(slot_id, slot_version);
+                versions.insert(slot_id, slot_version.saturating_add(1));
+            } else {
+                return Err(ClientError::NotConnected);
             }
 
             if chunk_ack.accepted {
@@ -175,6 +177,12 @@ impl StackerDB {
                 // See: https://github.com/stacks-network/stacks-blockchain/issues/3917
                 if reason.contains("Data for this slot and version already exist") {
                     warn!("Failed to send message to stackerdb due to wrong version number {}. Incrementing and retrying...", slot_version);
+                    if let Some(versions) = self.slot_versions.get_mut(&(signer_set, msg_id)) {
+                        // NOTE: per the above, this is always executed
+                        versions.insert(slot_id, slot_version.saturating_add(1));
+                    } else {
+                        return Err(ClientError::NotConnected);
+                    }
                 } else {
                     warn!("Failed to send message to stackerdb: {}", reason);
                     return Err(ClientError::PutChunkRejected(reason));
@@ -188,17 +196,20 @@ impl StackerDB {
         &mut self,
         signer_ids: &[u32],
     ) -> Result<Vec<StacksTransaction>, ClientError> {
-        let slot_ids: Vec<_> = signer_ids
-            .iter()
-            .map(|id| id * SIGNER_SLOTS_PER_USER + TRANSACTIONS_SLOT_ID)
-            .collect();
         debug!(
             "Getting latest chunks from stackerdb for the following signers: {:?}",
             signer_ids
         );
+        let Some(transactions_session) = self
+            .signers_message_stackerdb_sessions
+            .get_mut(&(self.signer_set, TRANSACTIONS_SLOT_ID))
+        else {
+            return Err(ClientError::NotConnected);
+        };
+
         let send_request = || {
-            self.signers_stackerdb_session
-                .get_latest_chunks(&slot_ids)
+            transactions_session
+                .get_latest_chunks(signer_ids)
                 .map_err(backoff::Error::transient)
         };
         let chunk_ack = retry_with_exponential_backoff(send_request)?;
@@ -242,6 +253,19 @@ impl StackerDB {
     /// Set the signer set from a reward cycle
     pub fn set_signer_set(&mut self, set: u32) {
         self.signer_set = set
+    }
+
+    /// Set the signer slot ID
+    pub fn set_signer_slot_id(&mut self, slot_id: u32) {
+        self.signer_slot_id = slot_id;
+    }
+
+    /// Get our signer address
+    pub fn get_signer_address(&self, mainnet: bool) -> StacksAddress {
+        StacksAddress::p2pkh(
+            mainnet,
+            &StacksPublicKey::from_private(&self.stacks_private_key),
+        )
     }
 }
 
