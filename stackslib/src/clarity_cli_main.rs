168d566fac4273f589f21bbf41a3f8ea0e806ad9
BowTiedDevOps <157840260+BowTiedDevOps@users.noreply.github.com>
2024-02-29 19:08:47 +0200

    feat: use composite rustfmt action

--- stackslib/src/clarity_cli_main.rs
+++ stackslib/src/clarity_cli_main.rs
@@ -31,7 +31,14 @@ use stacks_common::util::log;
 fn main() {
     let argv: Vec<String> = env::args().collect();
 
-    let result = clarity::invoke_command(&argv[0], &argv[1..]);
+    let result = 
+    clarity::
+    invoke_command
+    (
+        &argv[0], 
+        &argv[1..]
+    )
+    ;
     match result {
         (exit_code, Some(output)) => {
             println!("{}", &serde_json::to_string(&output).unwrap());
