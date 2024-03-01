53d80d69afa6aee0b2c5b9d5ca6d77dd78dc38bb
BowTiedDevOps <157840260+BowTiedDevOps@users.noreply.github.com>
2024-03-01 19:18:54 +0200

    default alias, no issues

--- stackslib/src/clarity_cli_main.rs
+++ stackslib/src/clarity_cli_main.rs
@@ -30,13 +30,9 @@ use stacks_common::util::log;
 
 fn main() {
     let argv: Vec<String> = env::args().collect();
-    // run ci
     let a = 0;
     println!("{a}");
-    let result = clarity::
-    invoke_command(
-        &argv[0], &argv[1..]
-    );
+    let result = clarity::invoke_command(&argv[0], &argv[1..]);
     match result {
         (exit_code, Some(output)) => {
             println!("{}", &serde_json::to_string(&output).unwrap());
