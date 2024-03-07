cd82f1c7cfcad42f5bf2506a2846a88d0e6339d8
BowTiedRadone <bowtiedradone@gmail.com>
2024-03-07 14:09:12 +0200

    added test for get-signer-key-message-hash and utils file

---
+++ contrib/core-contract-tests/tests/pox-4/pox-4-utils/utils.ts
@@ -0,0 +1,58 @@
+import { Cl, ClarityValue, serializeCV } from "@stacks/transactions";
+import { createHash } from "crypto";
+
+function sha256(data: Buffer): Buffer {
+  return createHash("sha256").update(data).digest();
+}
+
+function structuredDataHash(structuredData: ClarityValue): Buffer {
+  return sha256(Buffer.from(serializeCV(structuredData)));
+}
+
+const generateDomainHash = () =>
+  Cl.tuple({
+    name: Cl.stringAscii("pox-4-signer"),
+    version: Cl.stringAscii("1.0.0"),
+    "chain-id": Cl.uint(2147483648),
+  });
+
+const generateMessageHash = (
+  version: number,
+  hashbytes: number[],
+  reward_cycle: number,
+  topic: string,
+  period: number
+) =>
+  Cl.tuple({
+    "pox-addr": Cl.tuple({
+      version: Cl.buffer(Uint8Array.from([version])),
+      hashbytes: Cl.buffer(Uint8Array.from(hashbytes)),
+    }),
+    "reward-cycle": Cl.uint(reward_cycle),
+    topic: Cl.stringAscii(topic),
+    period: Cl.uint(period),
+  });
+
+const generateMessagePrefixBuffer = (prefix: string) =>
+  Buffer.from(prefix, "hex");
+
+export const buildSignerKeyMessageHash = (
+  version: number,
+  hashbytes: number[],
+  reward_cycle: number,
+  topic: string,
+  period: number
+) => {
+  const sip018_msg_prefix = "534950303138";
+  const domain_hash = structuredDataHash(generateDomainHash());
+  const message_hash = structuredDataHash(
+    generateMessageHash(version, hashbytes, reward_cycle, topic, period)
+  );
+  const structuredDataPrefix = generateMessagePrefixBuffer(sip018_msg_prefix);
+
+  const signer_key_message_hash = sha256(
+    Buffer.concat([structuredDataPrefix, domain_hash, message_hash])
+  );
+
+  return signer_key_message_hash;
+};
