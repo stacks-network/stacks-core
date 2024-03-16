0f1dddf175f0da467d0787497ca38b7bba3e501f
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2024-03-16 10:01:26 +0100

    wip(pox-4): StackStxCommand & stateful property tests planning
    This commit lays the groundwork for the StackStxCommand and
    GetStackingMinimumCommand classes for PoX-4. It also proposes the
    introduction of fast-check based stateful tests, similar to the efforts
    for sBTC (https://github.com/stacks-network/sbtc/pull/152).
    
    As highlighted in https://github.com/stacks-network/stacks-core/issues/4548,
    this initiative is part of an ongoing effort to embrace a more rigorous,
    property-based testing strategy for PoX-4 interactions.
    
    The planned stateful tests aim to simulate various stacking scenarios,
    ensuring compliance with PoX-4 protocols and robust error handling. This
    strategy is expected to greatly enhance test coverage and the reliability
    of PoX-4 stacking operations, bolstering confidence in the protocol’s
    robustness and correctness.
    
    Note: This is an early-stage WIP commit. Implementation details and testing
    strategies are subject to substantial development and refinement.

---
+++ contrib/core-contract-tests/tests/pox-4/pox_Commands.ts
@@ -0,0 +1,48 @@
+import fc from "fast-check";
+import { Real, Stub, StxAddress, Wallet } from "./pox_CommandModel";
+import { GetStackingMinimumCommand } from "./pox_GetStackingMinimumCommand";
+import { StackStxCommand } from "./pox_StackStxCommand";
+
+export function PoxCommands(
+  wallets: Map<StxAddress, Wallet>,
+): fc.Arbitrary<Iterable<fc.Command<Stub, Real>>> {
+  const cmds = [
+    // GetStackingMinimumCommand
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+    }).map((
+      r: {
+        wallet: Wallet;
+      },
+    ) =>
+      new GetStackingMinimumCommand(
+        r.wallet,
+      )
+    ),
+    // StackStxCommand
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+      authId: fc.nat(),
+      period: fc.integer({ min: 1, max: 12 }),
+      margin: fc.integer({ min: 1, max: 9 }),
+    }).map((
+      r: {
+        wallet: Wallet;
+        authId: number;
+        period: number;
+        margin: number;
+      },
+    ) =>
+      new StackStxCommand(
+        r.wallet,
+        r.authId,
+        r.period,
+        r.margin,
+      )
+    ),
+  ];
+
+  // More on size: https://github.com/dubzzz/fast-check/discussions/2978
+  // More on cmds: https://github.com/dubzzz/fast-check/discussions/3026
+  return fc.commands(cmds, { size: "large" });
+}
---
+++ contrib/core-contract-tests/tests/pox-4/pox_GetStackingMinimumCommand.ts
@@ -0,0 +1,60 @@
+import { PoxCommand, Real, Stub, Wallet } from "./pox_CommandModel.ts";
+import { assert } from "vitest";
+import { ClarityType, isClarityType } from "@stacks/transactions";
+
+/**
+ * Implements the `PoxCommand` interface to get the minimum stacking amount
+ * required for a given reward cycle.
+ */
+export class GetStackingMinimumCommand implements PoxCommand {
+  readonly wallet: Wallet;
+
+  /**
+   * Constructs a new `GetStackingMinimumCommand`.
+   *
+   * @param wallet The wallet information, including the STX address used to
+   *               query the stacking minimum requirement.
+   */
+  constructor(wallet: Wallet) {
+    this.wallet = wallet;
+  }
+
+  check(_model: Readonly<Stub>): boolean {
+    // Can always check the minimum number of uSTX to be stacked in the given
+    // reward cycle.
+    return true;
+  }
+
+  run(model: Stub, real: Real): void {
+    // Act
+    const { result: stackingMinimum } = real.network.callReadOnlyFn(
+      "ST000000000000000000002AMW42H.pox-4",
+      "get-stacking-minimum",
+      [],
+      this.wallet.stxAddress,
+    );
+    assert(isClarityType(stackingMinimum, ClarityType.UInt));
+
+    // Update the model with the new stacking minimum. This is important for
+    // the `check` method of the `StackStxCommand` class to work correctly, as
+    // we as other tests that may depend on the stacking minimum.
+    model.stackingMinimum = Number(stackingMinimum.value);
+
+    // Log to console for debugging purposes. This is not necessary for the
+    // test to pass but it is useful for debugging and eyeballing the test.
+    console.info(
+      `✓ ${this.wallet.stxAddress.padStart(8, " ")} ${
+        "get-stacking-minimum".padStart(34, " ")
+      } ${"pox-4".padStart(12, " ")} ${
+        stackingMinimum.value.toString().padStart(13, " ")
+      }`,
+    );
+  }
+
+  toString() {
+    // fast-check will call toString() in case of errors, e.g. property failed.
+    // It will then make a minimal counterexample, a process called 'shrinking'
+    // https://github.com/dubzzz/fast-check/issues/2864#issuecomment-1098002642
+    return `${this.wallet.stxAddress} get-stacking-minimum`;
+  }
+}
