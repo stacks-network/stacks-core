e33895fa78fd957d70654de49a9549872b783706
BowTiedRadone <bowtiedradone@gmail.com>
2024-04-22 16:19:13 +0300

    fix(pox-4-tests): Update AllowContractCallerCommand to remove the wallet from ex-allowed's list

--- contrib/core-contract-tests/tests/pox-4/pox_AllowContractCallerCommand.ts
+++ contrib/core-contract-tests/tests/pox-4/pox_AllowContractCallerCommand.ts
@@ -74,6 +74,27 @@ export class AllowContractCallerCommand implements PoxCommand {
 
     // Get the wallets involved from the model and update it with the new state.
     const wallet = model.stackers.get(this.wallet.stxAddress)!;
+    const callerAllowedBefore = wallet.allowedContractCaller;
+
+    const callerAllowedBeforeState = model.stackers.get(callerAllowedBefore) ||
+      null;
+
+    if (callerAllowedBeforeState) {
+      // Remove the allower from the ex-allowed caller's allowance list.
+
+      const walletIndexInsideAllowedByList = callerAllowedBeforeState
+        .callerAllowedBy.indexOf(
+          this.wallet.stxAddress,
+        );
+
+      expect(walletIndexInsideAllowedByList).toBeGreaterThan(-1);
+
+      callerAllowedBeforeState.callerAllowedBy.splice(
+        walletIndexInsideAllowedByList,
+        1,
+      );
+    }
+
     const callerToAllow = model.stackers.get(this.allowanceTo.stxAddress)!;
     // Update model so that we know this wallet has authorized a contract-caller.
 
