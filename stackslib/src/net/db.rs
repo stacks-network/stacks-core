56f754ed4bed278f3d0abc5230c0ae3046653a3b
Jude Nelson <judecn@gmail.com>
2024-05-03 16:58:58 -0400

    chore: expose set_initial_peer

--- stackslib/src/net/db.rs
+++ stackslib/src/net/db.rs
@@ -1142,7 +1142,7 @@ impl PeerDB {
     }
 
     /// Set a peer as an initial peer
-    fn set_initial_peer(
+    pub fn set_initial_peer(
         tx: &Transaction,
         network_id: u32,
         peer_addr: &PeerAddress,
