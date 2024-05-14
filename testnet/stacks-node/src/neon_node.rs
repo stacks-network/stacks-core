e6021f5bee2c620d351014519e92efaaf3b64d2f
Jude Nelson <judecn@gmail.com>
2024-05-14 14:19:19 -0400

    chore: fix compile issues in stacks-node

--- testnet/stacks-node/src/neon_node.rs
+++ testnet/stacks-node/src/neon_node.rs
@@ -4171,7 +4171,7 @@ impl PeerThread {
         net.bind(&p2p_sock, &rpc_sock)
             .expect("BUG: PeerNetwork could not bind or is already bound");
 
-        let poll_timeout = config.get_poll_timeout();
+        let poll_timeout = config.get_poll_time();
 
         PeerThread {
             config,
