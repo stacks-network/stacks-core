7a0c1fd96e3f8ba4392720501b252f343ca9c582
BowTiedRadone <bowtiedradone@gmail.com>
2024-05-22 19:01:19 +0300

    Add the unhappy path cases for `StackStxXCommand_Err`
    This commit:
    - adds 6 unhappy path cases for the `stack-stx` PoX-4 method, 3 for each signing method (authorization or signature)
    - adds a dictionary that contains the PoX-4 error names and the error codes
    - adds the command run tracking inside the `check` method, resulting in displaying all the paths hit and the number of times.

--- contrib/boot-contracts-stateful-prop-tests/tests/pox-4/err_Commands.ts
+++ contrib/boot-contracts-stateful-prop-tests/tests/pox-4/err_Commands.ts
@@ -1,8 +1,19 @@
 import fc from "fast-check";
-import { PoxCommand, Stacker, Stub, StxAddress, Wallet } from "./pox_CommandModel";
+import {
+  PoxCommand,
+  Stacker,
+  Stub,
+  StxAddress,
+  Wallet,
+} from "./pox_CommandModel";
 import { StackStxSigCommand_Err } from "./pox_StackStxSigCommand_Err";
 import { StackStxAuthCommand_Err } from "./pox_StackStxAuthCommand_Err";
 import { Simnet } from "@hirosystems/clarinet-sdk";
+
+const POX_4_ERRORS = {
+  ERR_STACKING_ALREADY_STACKED: 3,
+  ERR_STACKING_ALREADY_DELEGATED: 20,
+};
 
 export function ErrCommands(
   wallets: Map<StxAddress, Wallet>,
@@ -10,7 +21,7 @@ export function ErrCommands(
   network: Simnet,
 ): fc.Arbitrary<PoxCommand>[] {
   const cmds = [
-    // StackStxAuthCommand_Err
+    // StackStxAuthCommand_Err_Stacking_Already_Stacked_1
     fc.record({
       wallet: fc.constantFrom(...wallets.values()),
       authId: fc.nat(),
@@ -29,17 +40,102 @@ export function ErrCommands(
         r.authId,
         r.period,
         r.margin,
-        function (this: StackStxAuthCommand_Err, model: Readonly<Stub>): boolean {
+        function (
+          this: StackStxAuthCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
           const stacker = model.stackers.get(this.wallet.stxAddress)!;
-          return (
-            model.stackingMinimum > 0 && !stacker.isStacking &&
+          if (
+            model.stackingMinimum > 0 &&
+            stacker.isStacking &&
             !stacker.hasDelegated
-          );
+          ) {
+            model.trackCommandRun(
+              "StackStxAuthCommand_Err_Stacking_Already_Stacked_1",
+            );
+            return true;
+          } else return false;
         },
-        123,
+        POX_4_ERRORS.ERR_STACKING_ALREADY_STACKED,
       )
     ),
-    // StackStxSigCommand_Err
+    // StackStxAuthCommand_Err_Stacking_Already_Stacked_2
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+      authId: fc.nat(),
+      period: fc.integer({ min: 1, max: 12 }),
+      margin: fc.integer({ min: 1, max: 9 }),
+    }).map((
+      r: {
+        wallet: Wallet;
+        authId: number;
+        period: number;
+        margin: number;
+      },
+    ) =>
+      new StackStxAuthCommand_Err(
+        r.wallet,
+        r.authId,
+        r.period,
+        r.margin,
+        function (
+          this: StackStxAuthCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
+          const stacker = model.stackers.get(this.wallet.stxAddress)!;
+          if (
+            model.stackingMinimum > 0 &&
+            stacker.isStacking &&
+            stacker.hasDelegated
+          ) {
+            model.trackCommandRun(
+              "StackStxAuthCommand_Err_Stacking_Already_Stacked_2",
+            );
+            return true;
+          } else return false;
+        },
+        POX_4_ERRORS.ERR_STACKING_ALREADY_STACKED,
+      )
+    ),
+    // StackStxAuthCommand_Err_Stacking_Already_Delegated
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+      authId: fc.nat(),
+      period: fc.integer({ min: 1, max: 12 }),
+      margin: fc.integer({ min: 1, max: 9 }),
+    }).map((
+      r: {
+        wallet: Wallet;
+        authId: number;
+        period: number;
+        margin: number;
+      },
+    ) =>
+      new StackStxAuthCommand_Err(
+        r.wallet,
+        r.authId,
+        r.period,
+        r.margin,
+        function (
+          this: StackStxAuthCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
+          const stacker = model.stackers.get(this.wallet.stxAddress)!;
+          if (
+            model.stackingMinimum > 0 &&
+            !stacker.isStacking &&
+            stacker.hasDelegated
+          ) {
+            model.trackCommandRun(
+              "StackStxAuthCommand_Err_Stacking_Already_Delegated",
+            );
+            return true;
+          } else return false;
+        },
+        POX_4_ERRORS.ERR_STACKING_ALREADY_DELEGATED,
+      )
+    ),
+    // StackStxSigCommand_Err_Stacking_Already_Stacked_1
     fc.record({
       wallet: fc.constantFrom(...wallets.values()),
       authId: fc.nat(),
@@ -58,14 +154,99 @@ export function ErrCommands(
         r.authId,
         r.period,
         r.margin,
-        function (this: StackStxSigCommand_Err, model: Readonly<Stub>): boolean {
+        function (
+          this: StackStxSigCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
           const stacker = model.stackers.get(this.wallet.stxAddress)!;
-          return (
-            model.stackingMinimum > 0 && !stacker.isStacking &&
+          if (
+            model.stackingMinimum > 0 &&
+            stacker.isStacking &&
             !stacker.hasDelegated
-          );
+          ) {
+            model.trackCommandRun(
+              "StackStxSigCommand_Err_Stacking_Already_Stacked_1",
+            );
+            return true;
+          } else return false;
         },
-        123,
+        POX_4_ERRORS.ERR_STACKING_ALREADY_STACKED,
+      )
+    ),
+    // StackStxSigCommand_Err_Stacking_Already_Stacked_2
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+      authId: fc.nat(),
+      period: fc.integer({ min: 1, max: 12 }),
+      margin: fc.integer({ min: 1, max: 9 }),
+    }).map((
+      r: {
+        wallet: Wallet;
+        authId: number;
+        period: number;
+        margin: number;
+      },
+    ) =>
+      new StackStxSigCommand_Err(
+        r.wallet,
+        r.authId,
+        r.period,
+        r.margin,
+        function (
+          this: StackStxSigCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
+          const stacker = model.stackers.get(this.wallet.stxAddress)!;
+          if (
+            model.stackingMinimum > 0 &&
+            stacker.isStacking &&
+            stacker.hasDelegated
+          ) {
+            model.trackCommandRun(
+              "StackStxSigCommand_Err_Stacking_Already_Stacked_2",
+            );
+            return true;
+          } else return false;
+        },
+        POX_4_ERRORS.ERR_STACKING_ALREADY_STACKED,
+      )
+    ),
+    // StackStxSigCommand_Err_Stacking_Already_Delegated
+    fc.record({
+      wallet: fc.constantFrom(...wallets.values()),
+      authId: fc.nat(),
+      period: fc.integer({ min: 1, max: 12 }),
+      margin: fc.integer({ min: 1, max: 9 }),
+    }).map((
+      r: {
+        wallet: Wallet;
+        authId: number;
+        period: number;
+        margin: number;
+      },
+    ) =>
+      new StackStxSigCommand_Err(
+        r.wallet,
+        r.authId,
+        r.period,
+        r.margin,
+        function (
+          this: StackStxSigCommand_Err,
+          model: Readonly<Stub>,
+        ): boolean {
+          const stacker = model.stackers.get(this.wallet.stxAddress)!;
+          if (
+            model.stackingMinimum > 0 &&
+            !stacker.isStacking &&
+            stacker.hasDelegated
+          ) {
+            model.trackCommandRun(
+              "StackStxSigCommand_Err_Stacking_Already_Delegated",
+            );
+            return true;
+          } else return false;
+        },
+        POX_4_ERRORS.ERR_STACKING_ALREADY_DELEGATED,
       )
     ),
   ];
