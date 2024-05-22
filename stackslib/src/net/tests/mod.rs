d2c2847edb7f1efe2039dd7d610c28b11dbda1f5
Jude Nelson <judecn@gmail.com>
2024-05-22 17:40:08 -0400

    chore: fmt

--- stackslib/src/net/tests/mod.rs
+++ stackslib/src/net/tests/mod.rs
@@ -735,10 +735,12 @@ impl NakamotoBootPlan {
             let chainstate = &mut peer.stacks_node.as_mut().unwrap().chainstate;
             let sort_db = peer.sortdb.as_mut().unwrap();
             let tip = SortitionDB::get_canonical_burn_chain_tip(sort_db.conn()).unwrap();
-            let tenure =
-                NakamotoChainState::get_highest_nakamoto_tenure(chainstate.db(), &sort_db.index_handle_at_tip())
-                    .unwrap()
-                    .unwrap();
+            let tenure = NakamotoChainState::get_highest_nakamoto_tenure(
+                chainstate.db(),
+                &sort_db.index_handle_at_tip(),
+            )
+            .unwrap()
+            .unwrap();
             (tenure, tip)
         };
 
