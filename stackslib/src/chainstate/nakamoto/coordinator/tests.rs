868be4c544337b15879e70089525e7a812d52273
Jude Nelson <judecn@gmail.com>
2024-06-11 17:22:39 -0400

    chore: take &NakamotoBlock instead of NakamotoBlock

--- stackslib/src/chainstate/nakamoto/coordinator/tests.rs
+++ stackslib/src/chainstate/nakamoto/coordinator/tests.rs
@@ -404,7 +404,7 @@ fn replay_reward_cycle(
             &sortdb,
             &mut sort_handle,
             &mut node.chainstate,
-            block.clone(),
+            &block,
             None,
         )
         .unwrap_or(false);
