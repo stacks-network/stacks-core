1e5f342baae3c10b8ed6684b777187aa721d9bd2
Jude Nelson <judecn@gmail.com>
2024-06-28 22:54:53 -0400

    chore: API sync

--- stackslib/src/net/unsolicited.rs
+++ stackslib/src/net/unsolicited.rs
@@ -803,7 +803,7 @@ impl PeerNetwork {
     ) -> bool {
         if chainstate
             .nakamoto_blocks_db()
-            .has_nakamoto_block(&nakamoto_block.block_id())
+            .has_nakamoto_block_with_index_hash(&nakamoto_block.block_id())
             .unwrap_or(false)
         {
             debug!(
