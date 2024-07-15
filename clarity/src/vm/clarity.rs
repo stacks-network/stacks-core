6c382f22720179e289949a156368c468cbebbc2d
Brice Dobry <brice@hiro.so>
2024-07-15 10:30:09 -0400

    fix: set the flag to make the type-checker store a map of types
    Without this change, the types we need are not saved by the
    type-checker, which instead just uses a set to perform the checks it
    needs.

--- clarity/src/vm/clarity.rs
+++ clarity/src/vm/clarity.rs
@@ -213,7 +213,7 @@ pub trait TransactionConnection: ClarityConnection {
                 cost_track,
                 epoch_id,
                 clarity_version,
-                false,
+                true,
             );
 
             match result {
