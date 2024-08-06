38ea7d60265f5ae0d264b1c9c022be737942c2fd
Jeff Bencin <jeff.bencin@gmail.com>
2024-08-06 10:43:15 -0400

    chore: Remove `clippy` feature flag

--- stacks-common/src/deps_common/bitcoin/mod.rs
+++ stacks-common/src/deps_common/bitcoin/mod.rs
@@ -26,8 +26,8 @@
 //!
 
 // Clippy flags
-#![cfg_attr(feature = "clippy", allow(needless_range_loop))] // suggests making a big mess of array newtypes
-#![cfg_attr(feature = "clippy", allow(extend_from_slice))] // `extend_from_slice` only available since 1.6
+#![allow(clippy::needless_range_loop)] // suggests making a big mess of array newtypes
+#![allow(clippy::extend_from_slice)] // `extend_from_slice` only available since 1.6
 
 // Coding conventions
 #![deny(non_upper_case_globals)]
