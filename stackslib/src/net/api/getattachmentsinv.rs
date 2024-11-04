bedef8f45420a8d497b8ef77b73b3ed0e4ad22e2
Brice Dobry <brice@hiro.so>
2024-11-04 15:43:37 -0500

    chore: remove irrefutable if let
    This causes a warning in the latest versions of Rust.

--- stackslib/src/net/api/getattachmentsinv.rs
+++ stackslib/src/net/api/getattachmentsinv.rs
@@ -96,11 +96,13 @@ impl HttpRequest for RPCGetAttachmentsInvRequestHandler {
             if key == "index_block_hash" {
                 index_block_hash = StacksBlockId::from_hex(&value).ok();
             } else if key == "pages_indexes" {
-                if let Ok(pages_indexes_value) = value.parse::<String>() {
-                    for entry in pages_indexes_value.split(',') {
-                        if let Ok(page_index) = entry.parse::<u32>() {
-                            page_indexes.insert(page_index);
-                        }
+                #[allow(clippy::expect_used)]
+                let pages_indexes_value = value
+                    .parse::<String>()
+                    .expect("parse from Cow<str> is always safe");
+                for entry in pages_indexes_value.split(',') {
+                    if let Ok(page_index) = entry.parse::<u32>() {
+                        page_indexes.insert(page_index);
                     }
                 }
             }
