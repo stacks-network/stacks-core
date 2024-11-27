5b3ac9d05e49acfcb79ee49593e9237a6670e777
Chris Guimaraes <cguimaraes@hiro.so>
2024-11-27 14:05:36 +0000

    chore: add missing trait functions implementations

--- clarity/src/vm/database/sqlite.rs
+++ clarity/src/vm/database/sqlite.rs
@@ -324,8 +324,19 @@ impl ClarityBackingStore for MemoryBackingStore {
         SqliteConnection::get(self.get_side_store(), key)
     }
 
+    fn get_data_from_path(&mut self, hash: &TrieHash) -> Result<Option<String>> {
+        SqliteConnection::get(self.get_side_store(), hash.to_string().as_str())
+    }
+
     fn get_data_with_proof(&mut self, key: &str) -> Result<Option<(String, Vec<u8>)>> {
         Ok(SqliteConnection::get(self.get_side_store(), key)?.map(|x| (x, vec![])))
+    }
+
+    fn get_data_with_proof_from_path(
+        &mut self,
+        hash: &TrieHash,
+    ) -> Result<Option<(String, Vec<u8>)>> {
+        self.get_data_with_proof(&hash.to_string())
     }
 
     fn get_side_store(&mut self) -> &Connection {
