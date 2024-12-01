e5d15ca8c2d36120af9fceb9b74d6b304cb8be4a
Roberto De Ioris <roberto.deioris@gmail.com>
2024-12-01 11:57:05 +0100

    removed copy/paste useless comment

--- stackslib/src/net/api/tests/gettransaction.rs
+++ stackslib/src/net/api/tests/gettransaction.rs
@@ -56,7 +56,6 @@ fn test_try_parse_request() {
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
     let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
 
-    // NOTE: MARF enforces the height to be a u32 value
     let request = StacksHttpRequest::new_gettransaction(
         addr.into(),
         Txid::from_hex("00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF").unwrap(),
