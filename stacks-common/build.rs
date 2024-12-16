d39e54593601fa8955f2c677c0c9bce8917e9407
Hank Stoever <hstove@gmail.com>
2024-12-16 15:26:32 -0800

    feat: add build-time git info

--- stacks-common/build.rs
+++ stacks-common/build.rs
@@ -1,7 +1,63 @@
 use std::path::Path;
+use std::process::Command;
 use std::{env, fs};
 
 use toml::Value;
+
+fn current_git_hash() -> Option<String> {
+    if option_env!("GIT_COMMIT") == None {
+        let commit = Command::new("git")
+            .arg("log")
+            .arg("-1")
+            .arg("--pretty=format:%h") // Abbreviated commit hash
+            .current_dir(env!("CARGO_MANIFEST_DIR"))
+            .output();
+
+        if let Ok(commit) = commit {
+            if let Ok(commit) = String::from_utf8(commit.stdout) {
+                return Some(commit.trim().to_string());
+            }
+        }
+    } else {
+        return option_env!("GIT_COMMIT").map(String::from);
+    }
+
+    None
+}
+
+fn current_git_branch() -> Option<String> {
+    if option_env!("GIT_BRANCH") == None {
+        let commit = Command::new("git")
+            .arg("rev-parse")
+            .arg("--abbrev-ref")
+            .arg("HEAD")
+            .output();
+        if let Ok(commit) = commit {
+            if let Ok(commit) = String::from_utf8(commit.stdout) {
+                return Some(commit.trim().to_string());
+            }
+        }
+    } else {
+        return option_env!("GIT_BRANCH").map(String::from);
+    }
+
+    None
+}
+
+fn is_working_tree_clean() -> bool {
+    let status = Command::new("git")
+        .arg("diff")
+        .arg("--quiet")
+        .arg("--exit-code")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .status();
+
+    if let Ok(status) = status {
+        status.code() == Some(0)
+    } else {
+        true
+    }
+}
 
 fn main() {
     let toml_content =
@@ -32,6 +88,40 @@ fn main() {
         _ => {
             panic!("Invalid value type in versions.toml: {:?}", config);
         }
+    }
+
+    if let Some(git) = current_git_hash() {
+        // println!("git commit: {}", git);
+        rust_code.push_str(&format!(
+            "pub const GIT_COMMIT: Option<&'static str> = Some(\"{}\");\n",
+            git
+        ));
+        println!("cargo:rustc-env=GIT_COMMIT={}", git);
+    } else {
+        rust_code.push_str(&format!(
+            "pub const GIT_COMMIT: Option<&'static str> = None;\n"
+        ));
+    }
+    if let Some(git) = current_git_branch() {
+        rust_code.push_str(&format!(
+            "pub const GIT_BRANCH: Option<&'static str> = Some(\"{}\");\n",
+            git
+        ));
+        println!("cargo:rustc-env=GIT_BRANCH={}", git);
+    } else {
+        rust_code.push_str(&format!(
+            "pub const GIT_BRANCH: Option<&'static str> = None;\n"
+        ));
+    }
+    if !is_working_tree_clean() {
+        rust_code.push_str(&format!(
+            "pub const GIT_TREE_CLEAN: Option<&'static str> = Some(\"\");\n"
+        ));
+        println!("cargo:rustc-env=GIT_TREE_CLEAN=+");
+    } else {
+        rust_code.push_str(&format!(
+            "pub const GIT_TREE_CLEAN: Option<&'static str> = Some(\"+\");\n"
+        ));
     }
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
