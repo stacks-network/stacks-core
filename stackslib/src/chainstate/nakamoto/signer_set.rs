07b019871d49c727d16977e5b1b317818b31b2cf
Jacinta Ferrant <jacinta@trustmachines.co>
2024-12-30 13:39:35 -0500

    TEST: DO NOT MERGE
    Signed-off-by: Jacinta Ferrant <jacinta@trustmachines.co>

--- stackslib/src/chainstate/nakamoto/signer_set.rs
+++ stackslib/src/chainstate/nakamoto/signer_set.rs
@@ -12,6 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Just testing something
 
 use std::collections::{BTreeMap, HashMap, HashSet};
 use std::ops::DerefMut;
