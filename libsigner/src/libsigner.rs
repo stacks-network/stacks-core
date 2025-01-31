dcb36a257bc9f2c9bc788f2adca75733f667d86d
Hank Stoever <hstove@gmail.com>
2025-01-31 07:20:32 -0800

    fix: remove unnecessary var

--- libsigner/src/libsigner.rs
+++ libsigner/src/libsigner.rs
@@ -88,6 +88,5 @@ lazy_static! {
 
 #[test]
 fn test_version_string() {
-    let version = VERSION_STRING.to_string();
-    assert!(version.contains(format!("stacks-signer {}", STACKS_SIGNER_VERSION).as_str()));
+    assert!(VERSION_STRING.contains(format!("stacks-signer {}", STACKS_SIGNER_VERSION).as_str()));
 }
