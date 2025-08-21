22c019631866fba262011b61533dde219523b3f5
Jude Nelson <jude@blockstack.com>
2025-08-20 21:39:09 -0400

    feat: add test with Nakamoto block replay

--- stackslib/src/clarity_vm/tests/ephemeral.rs
+++ stackslib/src/clarity_vm/tests/ephemeral.rs
@@ -16,25 +16,37 @@
 use std::fs;
 
 use clarity::vm::ast::ASTRules;
+use clarity::vm::types::StacksAddressExtensions;
+use clarity::vm::{ClarityName, ContractName};
 use proptest::prelude::*;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use stacks_common::types::chainstate::StacksBlockId;
+use stacks_common::address::{AddressHashMode, C32_ADDRESS_VERSION_TESTNET_SINGLESIG};
+use stacks_common::types::chainstate::{
+    StacksAddress, StacksBlockId, StacksPrivateKey, StacksPublicKey,
+};
+use stacks_common::types::Address;
 
 use crate::chainstate::burn::db::sortdb::{SortitionDB, SortitionHandleConn};
 use crate::chainstate::nakamoto::miner::NakamotoBlockBuilder;
+use crate::chainstate::nakamoto::tests::node::TestStacker;
 use crate::chainstate::nakamoto::{NakamotoBlock, NakamotoChainState};
 use crate::chainstate::stacks::db::StacksChainState;
 use crate::chainstate::stacks::index::marf::{MARFOpenOpts, MARF};
 use crate::chainstate::stacks::index::storage::{TrieFileStorage, TrieHashCalculationMode};
 use crate::chainstate::stacks::index::ClarityMarfTrieId;
 use crate::chainstate::stacks::miner::{BlockBuilder, BlockLimitFunction, TransactionResult};
-use crate::chainstate::stacks::TransactionPayload;
+use crate::chainstate::stacks::{
+    StacksTransaction, StacksTransactionSigner, TransactionAnchorMode, TransactionAuth,
+    TransactionContractCall, TransactionPayload, TransactionPostConditionMode,
+    TransactionSmartContract, TransactionVersion,
+};
 use crate::clarity::vm::database::ClarityBackingStore;
 use crate::clarity_vm::database::marf::{MarfedKV, WritableMarfStore};
 use crate::net::test::{TestEventObserver, TestPeer};
 use crate::net::tests::inv::nakamoto::make_nakamoto_peer_from_invs;
-use crate::net::tests::NakamotoBootPlan;
+use crate::net::tests::{NakamotoBootPlan, NakamotoBootStep, NakamotoBootTenure};
+use crate::util_lib::strings::StacksString;
 
 /// Verify that an ephemeral MARF can be created off of an already-written block of an on-disk
 /// MARF.
@@ -111,6 +123,10 @@ fn test_ephemeral_marf_store() {
     // verify that we can read all keys with an ephemeral MARF opened to each block_id as its
     // base_tip
     for (i, block_id) in blocks.iter().enumerate() {
+        // skip opening ::sentinel() since it's not mapped
+        if i == 0 {
+            continue;
+        }
         debug!("ephemeral: open block #{}: {}", i, block_id);
         let ephemeral_tip = StacksBlockId([0xf0; 32]);
         let mut marf_ephemeral = marfed_kv
@@ -146,6 +162,10 @@ fn test_ephemeral_marf_store() {
     // verify that we can read all other keys.
     // verify that the ephemeral data is dropped along with the ephemeral tx
     for (i, block_id) in blocks.iter().enumerate() {
+        // skip opening ::sentinel() since it's not mapped
+        if i == 0 {
+            continue;
+        }
         let ephemeral_tip = StacksBlockId([0xf0; 32]);
         let final_block_id = StacksBlockId([i as u8; 32]);
 
@@ -432,6 +452,312 @@ fn test_ephemeral_nakamoto_block_replay_simple() {
     }
 }
 
+/// Test block replay with contract-calls which exercise the clarity DB
+#[test]
+fn test_ephemeral_nakamoto_block_replay_smart_contract() {
+    let test_name = function_name!();
+    let observer = TestEventObserver::new();
+
+    let private_key = StacksPrivateKey::from_seed(&[2]);
+    let addr = StacksAddress::from_public_keys(
+        C32_ADDRESS_VERSION_TESTNET_SINGLESIG,
+        &AddressHashMode::SerializeP2PKH,
+        1,
+        &vec![StacksPublicKey::from_private(&private_key)],
+    )
+    .unwrap();
+    let recipient_addr =
+        StacksAddress::from_string("ST2YM3J4KQK09V670TD6ZZ1XYNYCNGCWCVTASN5VM").unwrap();
+
+    let initial_balances = vec![(addr.to_account_principal(), 1_000_000)];
+
+    let code_body = r#"
+(define-constant RECIPIENT 'ST2YM3J4KQK09V670TD6ZZ1XYNYCNGCWCVTASN5VM)
+(define-map test-data-map uint uint)
+(define-data-var test-var uint u0)
+(define-fungible-token stackaroos)
+(define-non-fungible-token stacka-nfts uint)
+(define-data-var start-height uint stacks-block-height)
+
+(define-private (test-get-burn-block-info? (height uint))
+    (begin
+        (print (get-burn-block-info? header-hash height))
+        (print (get-burn-block-info? pox-addrs height))
+        true))
+
+(define-private (test-get-stacks-block-info? (height uint))
+    (begin
+        (print (get-stacks-block-info? id-header-hash height))
+        (print (get-stacks-block-info? header-hash height))
+        (print (get-stacks-block-info? time height))
+        true))
+
+(define-private (test-get-tenure-info? (height uint))
+    (begin
+        (print (get-tenure-info? burnchain-header-hash height))
+        (print (get-tenure-info? miner-address height))
+        (print (get-tenure-info? time height))
+        (print (get-tenure-info? vrf-seed height))
+        (print (get-tenure-info? block-reward height))
+        (print (get-tenure-info? miner-spend-total height))
+        (print (get-tenure-info? miner-spend-winner height))
+        true))
+
+(define-read-only (test-readonly-map (height uint))
+    (begin
+        (print (map-get? test-data-map height))
+        true))
+
+(define-private (test-map (height uint))
+    (begin
+        (test-readonly-map height)
+        (print (map-set test-data-map (+ u1 height) (+ u1 height)))
+        true))
+
+(define-read-only (test-readonly-data-var (height uint))
+    (begin
+        (print (var-get test-var))
+        true))
+
+(define-private (test-data-var (height uint))
+    (begin
+        (test-readonly-data-var height)
+        (print (var-set test-var height))
+        true))
+
+(define-read-only (test-readonly-ft (height uint) (user principal))
+    (begin
+        (print (ft-get-balance stackaroos user))
+        (print (ft-get-supply stackaroos))
+        true))
+
+(define-private (test-ft (height uint))
+    (begin
+        (test-readonly-ft height tx-sender)
+        (test-readonly-ft height RECIPIENT)
+        (print (match (ft-mint? stackaroos height tx-sender) success success failure false))
+        (test-readonly-ft height tx-sender)
+        (test-readonly-ft height RECIPIENT)
+        (print (match (ft-transfer? stackaroos height tx-sender RECIPIENT) success success failure false))
+        (test-readonly-ft height tx-sender)
+        (test-readonly-ft height RECIPIENT)
+        true))
+
+(define-read-only (test-readonly-nft (height uint))
+    (begin
+        (print (nft-get-owner? stacka-nfts height))
+        true))
+
+(define-private (test-nft (height uint))
+    (begin
+        (print (match (nft-mint? stacka-nfts height tx-sender) success success failure false))
+        (test-readonly-nft height)
+        (print (match (nft-transfer? stacka-nfts height tx-sender RECIPIENT) success success failure false))
+        (test-readonly-nft height)
+        true))
+
+(define-read-only (test-readonly-stx-account (user principal))
+    (begin
+        (print (stx-get-balance user))
+        (print (stx-account user))
+        true))
+
+(define-private (test-stx-account (height uint))
+    (begin
+        (test-readonly-stx-account tx-sender)
+        (test-readonly-stx-account RECIPIENT)
+        (print (match (stx-transfer? height tx-sender RECIPIENT) success success failure false))
+        (print (match (stx-transfer-memo? height tx-sender RECIPIENT 0x01) success success failure false))
+        (test-readonly-stx-account tx-sender)
+        (test-readonly-stx-account RECIPIENT)
+        true))
+
+(define-read-only (test-readonly-clarity-db-funcs)
+    (let (
+        (prev-stacks-block-height (if (> stacks-block-height (var-get start-height)) (- stacks-block-height u1) stacks-block-height))
+        (prev-burn-block-height (if (> burn-block-height u0) (- burn-block-height u1) burn-block-height))
+    )
+    (test-get-burn-block-info? burn-block-height)
+    (test-get-burn-block-info? prev-burn-block-height)
+
+    (test-get-stacks-block-info? stacks-block-height)
+    (test-get-stacks-block-info? prev-stacks-block-height)
+
+    (test-get-tenure-info? stacks-block-height)
+    (test-get-tenure-info? prev-stacks-block-height)
+    
+    (test-readonly-map stacks-block-height)
+    (test-readonly-map prev-stacks-block-height)
+
+    (test-readonly-data-var stacks-block-height)
+    (test-readonly-data-var prev-stacks-block-height)
+
+    (test-readonly-ft stacks-block-height tx-sender)
+    (test-readonly-ft stacks-block-height RECIPIENT)
+    (test-readonly-ft prev-stacks-block-height tx-sender)
+    (test-readonly-ft prev-stacks-block-height RECIPIENT)
+    
+    (test-readonly-nft stacks-block-height)
+    (test-readonly-nft prev-stacks-block-height)
+    
+    (test-readonly-stx-account tx-sender)
+    (test-readonly-stx-account RECIPIENT)
+
+    true))
+
+(define-private (test-clarity-db-funcs)
+    (let (
+        (prev-stacks-block-height (if (> stacks-block-height (var-get start-height)) (- stacks-block-height u1) stacks-block-height))
+        (prev-burn-block-height (if (> burn-block-height u0) (- burn-block-height u1) burn-block-height))
+    )
+    (test-map prev-stacks-block-height)
+    (test-map stacks-block-height)
+
+    (test-data-var prev-stacks-block-height)
+    (test-data-var stacks-block-height)
+
+    (test-ft stacks-block-height)
+    (test-nft stacks-block-height)
+    (test-stx-account stacks-block-height)
+
+    true
+))
+
+(define-read-only (test-readonly-clarity-db-funcs-at-prev-block)
+    (let (
+        (prev-block-opt (if (> stacks-block-height (var-get start-height)) (get-stacks-block-info? id-header-hash (- stacks-block-height u1)) none))
+    )
+    (match prev-block-opt
+        prev-block
+            (at-block prev-block (test-readonly-clarity-db-funcs))
+        true)))
+
+(define-public (test-all)
+    (begin
+        (test-readonly-clarity-db-funcs)
+        (test-clarity-db-funcs)
+        (test-readonly-clarity-db-funcs-at-prev-block)
+        (ok true)))
+"#;
+
+    let contract_deploy = || {
+        let smart_contract_payload = TransactionPayload::SmartContract(
+            TransactionSmartContract {
+                name: ContractName::try_from("test-clarity-db").unwrap(),
+                code_body: StacksString::from_str(&code_body).expect("FATAL: invalid code body"),
+            },
+            None,
+        );
+
+        let auth = TransactionAuth::from_p2pkh(&private_key).unwrap();
+        let mut smart_contract =
+            StacksTransaction::new(TransactionVersion::Testnet, auth, smart_contract_payload);
+
+        smart_contract.chain_id = 0x80000000;
+        smart_contract.anchor_mode = TransactionAnchorMode::OnChainOnly;
+        smart_contract.post_condition_mode = TransactionPostConditionMode::Allow;
+        smart_contract.set_tx_fee(code_body.len() as u64);
+        smart_contract.auth.set_origin_nonce(0);
+
+        let mut tx_signer = StacksTransactionSigner::new(&smart_contract);
+        tx_signer.sign_origin(&private_key).unwrap();
+        let smart_contract_signed = tx_signer.get_tx().unwrap();
+
+        smart_contract_signed
+    };
+
+    let mut sender_nonce = 1;
+    let mut next_contract_call = || {
+        let cc_payload = TransactionPayload::ContractCall(TransactionContractCall {
+            address: addr.clone(),
+            contract_name: ContractName::try_from("test-clarity-db").unwrap(),
+            function_name: ClarityName::try_from("test-all").unwrap(),
+            function_args: vec![],
+        });
+
+        let auth = TransactionAuth::from_p2pkh(&private_key).unwrap();
+        let mut cc = StacksTransaction::new(TransactionVersion::Testnet, auth, cc_payload);
+
+        cc.chain_id = 0x80000000;
+        cc.anchor_mode = TransactionAnchorMode::OnChainOnly;
+        cc.post_condition_mode = TransactionPostConditionMode::Allow;
+        cc.set_tx_fee(1);
+        cc.auth.set_origin_nonce(sender_nonce);
+        sender_nonce += 1;
+
+        let mut tx_signer = StacksTransactionSigner::new(&cc);
+        tx_signer.sign_origin(&private_key).unwrap();
+        let cc_signed = tx_signer.get_tx().unwrap();
+
+        cc_signed
+    };
+
+    let mut boot_tenures = vec![];
+
+    // deploy
+    boot_tenures.push(NakamotoBootTenure::Sortition(vec![
+        NakamotoBootStep::Block(vec![contract_deploy(), next_contract_call()]),
+        NakamotoBootStep::Block(vec![next_contract_call()]),
+        NakamotoBootStep::Block(vec![next_contract_call(), next_contract_call()]),
+    ]));
+
+    for i in 1..10 {
+        if i % 2 == 1 {
+            boot_tenures.push(NakamotoBootTenure::NoSortition(vec![
+                NakamotoBootStep::Block(vec![next_contract_call()]),
+                NakamotoBootStep::Block(vec![next_contract_call(), next_contract_call()]),
+            ]));
+        } else {
+            boot_tenures.push(NakamotoBootTenure::Sortition(vec![
+                NakamotoBootStep::Block(vec![next_contract_call()]),
+                NakamotoBootStep::Block(vec![next_contract_call(), next_contract_call()]),
+            ]));
+        }
+    }
+
+    // make malleablized blocks
+    let (test_signers, test_stackers) = TestStacker::multi_signing_set(&[
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3,
+    ]);
+
+    let plan = NakamotoBootPlan::new(test_name)
+        .with_private_key(private_key)
+        .with_test_signers(test_signers)
+        .with_test_stackers(test_stackers)
+        .with_pox_constants(10, 5)
+        .with_extra_peers(0)
+        .with_initial_balances(initial_balances);
+
+    let (mut peer, _other_peers) = plan.boot_into_nakamoto_peers(boot_tenures, Some(&observer));
+
+    // read out all Nakamoto blocks
+    let sortdb = peer.sortdb.take().unwrap();
+    let mut stacks_node = peer.stacks_node.take().unwrap();
+    let naka_tip =
+        NakamotoChainState::get_canonical_block_header(stacks_node.chainstate.db(), &sortdb)
+            .unwrap()
+            .unwrap();
+    let tip_id = naka_tip.index_block_hash();
+
+    let sortitions = SortitionDB::get_all_snapshots(&sortdb).unwrap();
+    let mut all_nakamoto_blocks = vec![];
+
+    for sort in sortitions {
+        let nakamoto_db = stacks_node.chainstate.nakamoto_blocks_db();
+        let mut nakamoto_blocks = nakamoto_db
+            .get_all_blocks_in_tenure(&sort.consensus_hash, &tip_id)
+            .unwrap();
+        all_nakamoto_blocks.append(&mut nakamoto_blocks);
+    }
+
+    all_nakamoto_blocks
+        .sort_by(|blk1, blk2| blk1.header.chain_length.cmp(&blk2.header.chain_length));
+
+    for naka_block in all_nakamoto_blocks {
+        replay_block(&sortdb, &mut stacks_node.chainstate, naka_block, &observer);
+    }
+}
+
 #[test]
 fn prop_ephemeral_tip_height_matches_current() {
     proptest!(|(n in 1usize..=12)| {
@@ -477,5 +803,4 @@ fn prop_ephemeral_tip_height_matches_current() {
 }
 
 // Test TODO:
-// * nakamoto boot plan with Clarity smart contracts which exercise the Clarity DB thoroghly
 // * stacks 2.x test
