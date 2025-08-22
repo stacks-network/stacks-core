b1134c794c0907a6aad4d0c42009854ae9283bfa
Federico De Felici <fdf8210@gmail.com>
2025-08-22 16:11:01 +0200

    chore: remove BitcoinRPCRequest create_wallet and list_wallet, #6387

--- stacks-node/src/burnchains/bitcoin_regtest_controller.rs
+++ stacks-node/src/burnchains/bitcoin_regtest_controller.rs
@@ -2744,57 +2744,6 @@ impl BitcoinRPCRequest {
         Ok(())
     }
 
-    /// Calls `listwallets` method through RPC call and returns wallet names as a vector of Strings
-    pub fn list_wallets(config: &Config) -> RPCResult<Vec<String>> {
-        let payload = BitcoinRPCRequest {
-            method: "listwallets".to_string(),
-            params: vec![],
-            id: "stacks".to_string(),
-            jsonrpc: "2.0".to_string(),
-        };
-
-        let mut res = BitcoinRPCRequest::send(config, payload)?;
-        let mut wallets = Vec::new();
-        match res.as_object_mut() {
-            Some(ref mut object) => match object.get_mut("result") {
-                Some(serde_json::Value::Array(entries)) => {
-                    while let Some(entry) = entries.pop() {
-                        let parsed_wallet_name: String = match serde_json::from_value(entry) {
-                            Ok(wallet_name) => wallet_name,
-                            Err(err) => {
-                                warn!("Failed parsing wallet name: {err}");
-                                continue;
-                            }
-                        };
-
-                        wallets.push(parsed_wallet_name);
-                    }
-                }
-                _ => {
-                    warn!("Failed to get wallets");
-                }
-            },
-            _ => {
-                warn!("Failed to get wallets");
-            }
-        };
-
-        Ok(wallets)
-    }
-
-    /// Tries to create a wallet with the given name
-    pub fn create_wallet(config: &Config, wallet_name: &str) -> RPCResult<()> {
-        let payload = BitcoinRPCRequest {
-            method: "createwallet".to_string(),
-            params: vec![wallet_name.into(), true.into()],
-            id: "stacks".to_string(),
-            jsonrpc: "2.0".to_string(),
-        };
-
-        BitcoinRPCRequest::send(config, payload)?;
-        Ok(())
-    }
-
     pub fn stop_bitcoind(config: &Config) -> RPCResult<serde_json::Value> {
         let payload = BitcoinRPCRequest {
             method: "stop".to_string(),
