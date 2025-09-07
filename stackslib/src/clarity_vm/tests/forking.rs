f497aa6e8ecd11ec7ac81c4ceeb852e013039527
bestmike007 <i@bestmike007.com>
2025-09-07 07:10:08 +0000

    feat: improve wasm nested context error tracing

--- stackslib/src/clarity_vm/tests/forking.rs
+++ stackslib/src/clarity_vm/tests/forking.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use clarity::vm::analysis::errors::CheckErrors;
 use clarity::vm::ast::ASTRules;
 use clarity::vm::contexts::OwnedEnvironment;
 use clarity::vm::errors::{Error, InterpreterResult as Result, RuntimeErrorType};
