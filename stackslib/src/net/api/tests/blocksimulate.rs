863bafaa9e75f8b47fd90734b47663c57274fc1b
Roberto De Ioris <roberto.deioris@gmail.com>
2025-09-18 08:00:39 +0200

    added blocksimulate tests

---
+++ stackslib/src/net/api/tests/blocksimulate.rs
@@ -0,0 +1,107 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2025 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use stacks_common::types::chainstate::{ConsensusHash, StacksBlockId};
+
+use crate::chainstate::stacks::StacksBlock;
+use crate::net::api::blocksimulate;
+use crate::net::api::tests::TestRPC;
+use crate::net::connection::ConnectionOptions;
+use crate::net::httpcore::{StacksHttp, StacksHttpRequest};
+use crate::net::test::TestEventObserver;
+use crate::net::ProtocolFamily;
+
+#[test]
+fn test_try_parse_request() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+    let mut http = StacksHttp::new(addr.clone(), &ConnectionOptions::default());
+
+    let request = StacksHttpRequest::new_block_simulate(addr.into(), &StacksBlockId([0x01; 32]));
+
+    let bytes = request.try_serialize().unwrap();
+
+    debug!("Request:\n{}\n", std::str::from_utf8(&bytes).unwrap());
+
+    let (parsed_preamble, offset) = http.read_preamble(&bytes).unwrap();
+
+    let mut handler = blocksimulate::RPCNakamotoBlockSimulateRequestHandler::new();
+    let mut parsed_request = http
+        .handle_try_parse_request(
+            &mut handler,
+            &parsed_preamble.expect_request(),
+            &bytes[offset..],
+        )
+        .unwrap();
+    assert_eq!(handler.block_id, Some(StacksBlockId([0x01; 32])));
+
+    // parsed request consumes headers that would not be in a constructed request
+    parsed_request.clear_headers();
+    let (preamble, contents) = parsed_request.destruct();
+
+    assert_eq!(&preamble, request.preamble());
+}
+
+#[test]
+fn test_try_make_response() {
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 33333);
+
+    let test_observer = TestEventObserver::new();
+    let rpc_test = TestRPC::setup_nakamoto(function_name!(), &test_observer);
+
+    let nakamoto_consensus_hash = rpc_test.consensus_hash.clone();
+
+    let mut requests = vec![];
+
+    // query existing, non-empty Nakamoto block
+    let request =
+        StacksHttpRequest::new_block_simulate(addr.clone().into(), &rpc_test.canonical_tip);
+    requests.push(request);
+
+    // query non-existent block
+    let request =
+        StacksHttpRequest::new_block_simulate(addr.clone().into(), &StacksBlockId([0x01; 32]));
+    requests.push(request);
+
+    let mut responses = rpc_test.run(requests);
+
+    // got the Nakamoto tip
+    let response = responses.remove(0);
+    println!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let resp = response.decode_simulated_block().unwrap();
+
+    let tip_block = test_observer.get_blocks().last().unwrap().clone();
+
+    assert_eq!(resp.consensus_hash, nakamoto_consensus_hash);
+    assert_eq!(resp.consensus_hash, tip_block.metadata.consensus_hash);
+
+    assert_eq!(resp.transactions.len(), tip_block.receipts.len());
+
+    // got a failure
+    let response = responses.remove(0);
+    debug!(
+        "Response:\n{}\n",
+        std::str::from_utf8(&response.try_serialize().unwrap()).unwrap()
+    );
+
+    let (preamble, body) = response.destruct();
+    assert_eq!(preamble.status_code, 404);
+}
