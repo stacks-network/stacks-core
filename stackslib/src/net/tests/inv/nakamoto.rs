2ec0358e1a6487a55810b9004f668318ff50f50e
Roberto De Ioris <roberto.deioris@gmail.com>
2025-10-20 14:05:02 +0200

    explained why -2 in the test

--- stackslib/src/net/tests/inv/nakamoto.rs
+++ stackslib/src/net/tests/inv/nakamoto.rs
@@ -527,6 +527,8 @@ where
                 },
                 NakamotoBootTenure::NoSortition(boot_steps) => {
                     let boot_steps_len = boot_steps.len();
+                    // when NakamotoBootTenure::NoSortition is in place we have every NakamotoBootStep::Block
+                    // followed by NakamotoBootStep::TenureExtend (this is why with index by boot_steps_len - 2)
                     match boot_steps.get_mut(boot_steps_len - 2).unwrap() {
                         NakamotoBootStep::Block(transactions) => {
                             transactions.append(&mut tip_transactions)
