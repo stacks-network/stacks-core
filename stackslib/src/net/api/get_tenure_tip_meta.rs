911924b35280ad642e90544815cf683f3f6df545
Aaron Blankstein <235161024+aaronb-stacks@users.noreply.github.com>
2026-01-08 15:11:41 -0600

    chore: update copyright

--- stackslib/src/net/api/get_tenure_tip_meta.rs
+++ stackslib/src/net/api/get_tenure_tip_meta.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Stacks Open Internet Foundation
+// Copyright (C) 2026 Stacks Open Internet Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
