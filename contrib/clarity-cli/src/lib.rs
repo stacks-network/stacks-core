e14ce328d9f4ef92f7851b15c2611dace519e50b
Simone Orsi <241460653+simone-stacks@users.noreply.github.com>
2026-01-12 12:25:09 +0100

    avoid duplication by deferring None to -

--- contrib/clarity-cli/src/lib.rs
+++ contrib/clarity-cli/src/lib.rs
@@ -144,13 +144,7 @@ pub fn read_file_or_stdin_bytes(path: &str) -> Vec<u8> {
 pub fn read_optional_file_or_stdin(path: Option<&PathBuf>) -> String {
     match path {
         Some(p) => read_file_or_stdin(p.to_str().expect("Invalid UTF-8 in path")),
-        None => {
-            let mut buffer = String::new();
-            io::stdin()
-                .read_to_string(&mut buffer)
-                .expect("Error reading from stdin");
-            buffer
-        }
+        None => read_file_or_stdin("-"),
     }
 }
 
