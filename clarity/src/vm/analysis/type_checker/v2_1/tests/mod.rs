8e25b900ad496c803aee964e431194359dfca613
francesco <francesco-stacks@users.noreply.github.com>
2026-02-27 15:28:38 +0000

    fix unit tests for at-block

--- clarity/src/vm/analysis/type_checker/v2_1/tests/mod.rs
+++ clarity/src/vm/analysis/type_checker/v2_1/tests/mod.rs
@@ -927,13 +927,37 @@ fn test_at_block() {
     for (good_test, expected) in good.iter() {
         assert_eq!(
             expected,
-            &format!("{}", type_check_helper(good_test).unwrap())
+            &format!(
+                "{}",
+                type_check_helper_version(
+                    good_test,
+                    ClarityVersion::latest(),
+                    StacksEpochId::Epoch33
+                )
+                .unwrap()
+            )
         );
     }
 
     for (bad_test, expected) in bad.iter() {
-        assert_eq!(*expected, *type_check_helper(bad_test).unwrap_err().err);
+        assert_eq!(
+            *expected,
+            *type_check_helper_version(bad_test, ClarityVersion::latest(), StacksEpochId::Epoch33)
+                .unwrap_err()
+                .err
+        );
     }
+
+    assert_eq!(
+        StaticCheckErrorKind::AtBlockUnavailable,
+        *type_check_helper_version(
+            "(at-block (sha256 u0) u1)",
+            ClarityVersion::latest(),
+            StacksEpochId::Epoch34
+        )
+        .unwrap_err()
+        .err
+    );
 }
 
 #[apply(test_clarity_versions)]
