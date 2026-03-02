9bfb200c5ec8bcd98664c930e535a0c850e5edd1
Jacinta Ferrant <236437600+jacinta-stacks@users.noreply.github.com>
2026-03-02 15:04:36 -0800

    Add copyright
    Signed-off-by: Jacinta Ferrant <236437600+jacinta-stacks@users.noreply.github.com>

--- clarity-types/benches/utf8_data.rs
+++ clarity-types/benches/utf8_data.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2026 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+use std::hint::black_box;
+
 use clarity_types::types::Utf8Char;
 use criterion::{Criterion, criterion_group, criterion_main};
 
@@ -32,44 +48,39 @@ fn bench_utf8(c: &mut Criterion) {
         let old_multi = make_old_multibyte(size);
 
         group.bench_function(format!("new_clone_ascii_{size}"), |b| {
-            b.iter(|| std::hint::black_box(new_ascii.clone()));
+            b.iter(|| black_box(new_ascii.clone()));
         });
         group.bench_function(format!("old_clone_ascii_{size}"), |b| {
-            b.iter(|| std::hint::black_box(old_ascii.clone()));
+            b.iter(|| black_box(old_ascii.clone()));
         });
 
         group.bench_function(format!("new_clone_multibyte_{size}"), |b| {
-            b.iter(|| std::hint::black_box(new_multi.clone()));
+            b.iter(|| black_box(new_multi.clone()));
         });
         group.bench_function(format!("old_clone_multibyte_{size}"), |b| {
-            b.iter(|| std::hint::black_box(old_multi.clone()));
+            b.iter(|| black_box(old_multi.clone()));
         });
 
         // --- Construction: raw data structure comparison ---
         group.bench_function(format!("new_construct_ascii_{size}"), |b| {
             b.iter(|| {
-                let data: Vec<Utf8Char> = (0..size).map(|_| Utf8Char::from_char('A')).collect();
-                std::hint::black_box(data);
+                black_box((0..size).map(|_| Utf8Char::from_char('A')).collect::<Vec<_>>());
             });
         });
         group.bench_function(format!("old_construct_ascii_{size}"), |b| {
             b.iter(|| {
-                let data: Vec<Vec<u8>> = (0..size).map(|_| vec![b'A']).collect();
-                std::hint::black_box(data);
+                black_box((0..size).map(|_| vec![b'A']).collect::<Vec<Vec<u8>>>());
             });
         });
 
         group.bench_function(format!("new_construct_multibyte_{size}"), |b| {
             b.iter(|| {
-                let data: Vec<Utf8Char> =
-                    (0..size).map(|_| Utf8Char::from_char('\u{2603}')).collect();
-                std::hint::black_box(data);
+                black_box((0..size).map(|_| Utf8Char::from_char('\u{2603}')).collect::<Vec<_>>());
             });
         });
         group.bench_function(format!("old_construct_multibyte_{size}"), |b| {
             b.iter(|| {
-                let data: Vec<Vec<u8>> = (0..size).map(|_| vec![0xE2, 0x98, 0x83]).collect();
-                std::hint::black_box(data);
+                black_box((0..size).map(|_| vec![0xE2u8, 0x98, 0x83]).collect::<Vec<Vec<u8>>>());
             });
         });
 
@@ -83,44 +94,42 @@ fn bench_utf8(c: &mut Criterion) {
         group.bench_function(format!("new_value_construct_ascii_{size}"), |b| {
             b.iter(|| {
                 let s = std::str::from_utf8(&ascii_bytes).unwrap();
-                let data: Vec<Utf8Char> = s.chars().map(Utf8Char::from_char).collect();
-                std::hint::black_box(data);
+                black_box(s.chars().map(Utf8Char::from_char).collect::<Vec<_>>());
             });
         });
         group.bench_function(format!("old_value_construct_ascii_{size}"), |b| {
             b.iter(|| {
                 let s = std::str::from_utf8(&ascii_bytes).unwrap();
-                let data: Vec<Vec<u8>> = s
-                    .chars()
-                    .map(|c| {
-                        let mut buf = vec![0u8; c.len_utf8()];
-                        c.encode_utf8(&mut buf);
-                        buf
-                    })
-                    .collect();
-                std::hint::black_box(data);
+                black_box(
+                    s.chars()
+                        .map(|c| {
+                            let mut buf = vec![0u8; c.len_utf8()];
+                            c.encode_utf8(&mut buf);
+                            buf
+                        })
+                        .collect::<Vec<Vec<u8>>>(),
+                );
             });
         });
 
         group.bench_function(format!("new_value_construct_multibyte_{size}"), |b| {
             b.iter(|| {
                 let s = std::str::from_utf8(&multi_bytes).unwrap();
-                let data: Vec<Utf8Char> = s.chars().map(Utf8Char::from_char).collect();
-                std::hint::black_box(data);
+                black_box(s.chars().map(Utf8Char::from_char).collect::<Vec<_>>());
             });
         });
         group.bench_function(format!("old_value_construct_multibyte_{size}"), |b| {
             b.iter(|| {
                 let s = std::str::from_utf8(&multi_bytes).unwrap();
-                let data: Vec<Vec<u8>> = s
-                    .chars()
-                    .map(|c| {
-                        let mut buf = vec![0u8; c.len_utf8()];
-                        c.encode_utf8(&mut buf);
-                        buf
-                    })
-                    .collect();
-                std::hint::black_box(data);
+                black_box(
+                    s.chars()
+                        .map(|c| {
+                            let mut buf = vec![0u8; c.len_utf8()];
+                            c.encode_utf8(&mut buf);
+                            buf
+                        })
+                        .collect::<Vec<Vec<u8>>>(),
+                );
             });
         });
     }
