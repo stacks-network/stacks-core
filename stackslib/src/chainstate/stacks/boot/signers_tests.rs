7837e037e38108b31aaa30756a360af8836b6029
rob-stacks <roberto@stackslabs.com>
2026-03-18 12:11:00 +0100

    removed reward set differentiation for epoch3-5

--- stackslib/src/chainstate/stacks/boot/signers_tests.rs
+++ stackslib/src/chainstate/stacks/boot/signers_tests.rs
@@ -524,8 +524,3 @@ pub fn get_signer_index(
         })
         .expect("signer not found") as u128
 }
-
-#[test]
-fn make_signer_set_pox_5_units() {
-    assert_eq!(StacksChainState::make_signer_set_pox_5(100, &[]), None);
-}
--- stackslib/src/chainstate/stacks/tests/reward_set.rs
+++ stackslib/src/chainstate/stacks/tests/reward_set.rs
@@ -52,12 +52,8 @@ pub fn check_make_reward_set(
 
     prop_assume!(threshold > 0);
 
-    let reward_set = StacksChainState::make_reward_set(
-        threshold,
-        addresses.to_vec(),
-        StacksEpochId::latest(),
-        PoxVersions::Pox4,
-    );
+    let reward_set =
+        StacksChainState::make_reward_set(threshold, addresses.to_vec(), StacksEpochId::latest());
 
     prop_assert_eq!(Some(threshold), reward_set.pox_ustx_threshold);
 
