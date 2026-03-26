af00ec8cb9728a56421d8addbb18e153c4806e25
Hank Stoever <233293993+hstove-stacks@users.noreply.github.com>
2026-03-26 15:48:53 -0700

    feat: sample code to submit BTC to a timelock

--- stacks-node/src/tests/signer/v0/pox_5_tests.rs
+++ stacks-node/src/tests/signer/v0/pox_5_tests.rs
@@ -18,22 +18,32 @@ use std::time::Duration;
 
 use clarity::vm::types::PrincipalData;
 use stacks::address::AddressHashMode;
+use stacks::burnchains::bitcoin::address::BitcoinAddress;
+use stacks::burnchains::bitcoin::BitcoinNetworkType;
 use stacks::chainstate::stacks::address::PoxAddress;
+use stacks::codec::StacksMessageCodec;
 use stacks::core::test_util::make_contract_call;
 use stacks::core::StacksEpochId;
 use stacks::types::chainstate::{StacksAddress, StacksPublicKey};
-use stacks::util::secp256k1::Secp256k1PrivateKey;
+use stacks::types::PrivateKey;
+use stacks::util::hash::{hex_bytes, to_hex};
+use stacks::util::secp256k1::{Secp256k1PrivateKey, Secp256k1PublicKey};
 use stacks::util_lib::boot::boot_code_id;
 use stacks::util_lib::signed_structured_data::pox5::make_pox_5_signer_grant_signature;
+use stacks_common::deps_common::bitcoin::blockdata::opcodes::{self, OP_CLTV};
+use stacks_common::deps_common::bitcoin::blockdata::script::Builder;
 use stacks_signer::v0::SpawnedSigner;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{fmt, EnvFilter};
 
+use crate::burnchains::rpc::bitcoin_rpc_client::{
+    BitcoinRpcClient, ImportDescriptorsRequest, Timestamp,
+};
 use crate::tests::nakamoto_integrations::wait_for;
 use crate::tests::neon_integrations::{get_account, get_chain_info, submit_tx};
 use crate::tests::signer::SignerTest;
 use crate::tests::{self};
-use crate::BurnchainController;
+use crate::{BurnchainController, Keychain};
 
 /// Submit a `grant-signer-key` transaction so that `stacker_addr` can use `signer_sk`
 /// without a per-transaction signature.
@@ -156,6 +166,61 @@ fn stake_pox_5(signer_test: &SignerTest<SpawnedSigner>) {
     .expect("Timed out waiting for stake transactions");
 }
 
+fn submit_btc_timelocks(signer_test: &SignerTest<SpawnedSigner>) {
+    let rpc_client = BitcoinRpcClient::from_stx_config(&signer_test.running_nodes.conf).unwrap();
+    let lock_period = 12;
+    // The unlock height for a given timelock is halfway through the reward
+    // cycle where the STX are unlocked.
+    let current_cycle = signer_test.get_current_reward_cycle();
+    let unlock_cycle = current_cycle + lock_period;
+    let pox_constants = signer_test
+        .running_nodes
+        .btc_regtest_controller
+        .get_burnchain()
+        .pox_constants;
+    let cycle_length = pox_constants.reward_cycle_length + pox_constants.prepare_length;
+    let unlock_height = signer_test
+        .running_nodes
+        .btc_regtest_controller
+        .get_burnchain()
+        .reward_cycle_to_block_height(unlock_cycle)
+        + (cycle_length / 2) as u64;
+    for stacker_sk in signer_test.signer_stacks_private_keys.iter() {
+        info!("---- Locking 1000 sats for stacker ----";
+            "stacker" => %stacker_sk.to_hex(),
+        );
+        let stacker_addr = tests::to_addr(stacker_sk);
+        let stacker_cv = PrincipalData::from(stacker_addr.clone());
+        let mut stacker_cv_bytes = vec![];
+        stacker_cv
+            .consensus_serialize(&mut stacker_cv_bytes)
+            .unwrap();
+        let script = Builder::new()
+            .push_slice(&stacker_cv_bytes)
+            .push_opcode(opcodes::All::OP_DROP)
+            .push_scriptint(unlock_height.try_into().unwrap())
+            .push_opcode(OP_CLTV)
+            .push_opcode(opcodes::All::OP_DROP)
+            .push_slice(&[0x00, 0x00])
+            .into_script()
+            .to_v0_p2wsh();
+        let addr =
+            BitcoinAddress::from_scriptpubkey(BitcoinNetworkType::Regtest, script.as_bytes())
+                .expect("Failed to convert script to segwit p2wpkh address");
+        let addr_str = addr.to_string();
+        let lock_amount = 0.001;
+        let txid = rpc_client
+            .send_to_address("staking", &addr, lock_amount)
+            .expect("Failed to send to address");
+        info!("Sent BTC to timelock";
+            "timelock_addr" => addr_str,
+            "timelock_script" => to_hex(script.as_bytes()),
+            "txid" => txid.to_hex(),
+            "amount" => lock_amount,
+        );
+    }
+}
+
 #[test]
 #[ignore]
 fn test_pox_5_activation() {
@@ -174,13 +239,24 @@ fn test_pox_5_activation() {
     let send_amt = 1000;
     let send_fee = 180;
 
+    let staker_btc_sk = Secp256k1PrivateKey::from_seed("staker".as_bytes());
+    let mut staker_btc_sk_bytes = staker_btc_sk.to_bytes();
+    staker_btc_sk_bytes.insert(0, 0xef); // regtest flag
+    let staker_btc_wif = stacks_common::address::b58::check_encode_slice(&staker_btc_sk_bytes);
+    let staker_btc_pk = Secp256k1PublicKey::from_private(&staker_btc_sk);
+
+    let btc_miner_1_seed = vec![1, 1, 1, 1];
+    let btc_miner_1_pk = Keychain::default(btc_miner_1_seed.clone()).get_pub_key();
+
     let signer_test: SignerTest<SpawnedSigner> =
         SignerTest::new_with_config_modifications_and_snapshot(
             num_signers,
             vec![(sender_addr.clone(), (send_amt + send_fee) * 10)],
             |_| {},
-            |_| {},
-            None,
+            |node_config| {
+                node_config.node.seed = btc_miner_1_seed.clone();
+            },
+            Some(vec![btc_miner_1_pk, staker_btc_pk]),
             None,
             Some(function_name!()),
         );
@@ -188,12 +264,27 @@ fn test_pox_5_activation() {
     let conf = &signer_test.running_nodes.conf;
     let epochs = conf.burnchain.epochs.clone().unwrap();
     let epoch_35_start = epochs[StacksEpochId::Epoch35].start_height;
+    let rpc_client = BitcoinRpcClient::from_stx_config(&signer_test.running_nodes.conf).unwrap();
 
     info!("---- Bootstrap Snapshot ----");
     if signer_test.bootstrap_snapshot_to_height(Some(epoch_35_start - 2)) {
         signer_test.shutdown_and_snapshot();
         return;
     }
+
+    // Import the staker's BTC private key into a wallet
+    rpc_client.create_wallet("staking", Some(false)).unwrap();
+    let pkh_raw = format!("pkh({staker_btc_wif})");
+    let pkh_info = rpc_client.get_descriptor_info(&pkh_raw).unwrap();
+    let pkh_desc = ImportDescriptorsRequest {
+        descriptor: format!("{pkh_raw}#{}", pkh_info.checksum),
+        timestamp: Timestamp::Time(0),
+        internal: Some(true),
+    };
+    rpc_client
+        .import_descriptors("staking", &[&pkh_desc])
+        .expect("Failed to import descriptor");
+
     let mut bh = get_chain_info(conf).burn_block_height;
     info!("---- Starting test ----";
         "epoch_3_5" => epoch_35_start,
@@ -231,6 +322,9 @@ fn test_pox_5_activation() {
     info!("---- Got contract source ----";
         "source" => source.split('\n').collect::<Vec<&str>>()[0..3].join("\n"),
     );
+
+    info!("---- Submitting btc timelocks ----");
+    submit_btc_timelocks(&signer_test);
 
     info!("---- Staking in pox-5 ----");
 
