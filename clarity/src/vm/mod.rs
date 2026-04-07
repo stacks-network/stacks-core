061da7acd4c0eee9ecb932d954f721572e8e2d16
benjamin-stacks <246469650+benjamin-stacks@users.noreply.github.com>
2026-04-07 15:02:36 +0200

    conditional import

--- clarity/src/vm/mod.rs
+++ clarity/src/vm/mod.rs
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#[cfg(any(test, feature = "testing"))]
 use stacks_common::util::MustInto;
 pub mod diagnostic;
 pub mod errors;
