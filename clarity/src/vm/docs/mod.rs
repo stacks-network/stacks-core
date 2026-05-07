38361a9f27a47eb6d6a22559851e381357dd6610
rob-stacks <roberto@stackslabs.com>
2026-05-07 15:24:14 +0200

    fixed typo

--- clarity/src/vm/docs/mod.rs
+++ clarity/src/vm/docs/mod.rs
@@ -1422,9 +1422,9 @@ const SECP256K1DECOMPRESS_API: SpecialAPI = SpecialAPI {
     input_type: "(buff 33)",
     snippet: "secp256k1-decompress? ${1:compressed-public-key})",
     output_type: "(response (buff 65) uint)",
-    signature: "(secp256r1-decompress? public-key)",
+    signature: "(secp256k1-decompress? public-key)",
     description: "The `secp256r1-decompress?` function decompress the provided (compressed) public key.",
-    example: "(secp256r1-decompress? 
+    example: "(secp256k1-decompress? 
     0x0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352) ;; Returns (ok 0x0450863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b23522cd470243453a299fa9e77237716103abc11a1df38855ed6f2ee187e9c582ba6)",
 };
 
