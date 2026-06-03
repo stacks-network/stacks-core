dcab934ed63a4cbb073dcc3bd6c12a6ce1e5651e
benjamin-stacks <246469650+benjamin-stacks@users.noreply.github.com>
2026-06-03 14:57:49 +0200

    fix unused import warning

--- stacks-common/src/util/secp256k1/wasm.rs
+++ stacks-common/src/util/secp256k1/wasm.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#[cfg(all(any(test, feature = "testing"), not(feature = "wasm-deterministic")))]
 use ::libsecp256k1::curve::Scalar;
 pub use ::libsecp256k1::Error;
 use ::libsecp256k1::{
