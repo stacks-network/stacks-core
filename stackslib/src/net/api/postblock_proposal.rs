b72b32b315c6f78dbc699ce0dbc061ff5ca2b1ce
Federico De Felici <federico-stacks@users.noreply.github.com>
2026-08-10 15:43:15 +0200

    crc: remove unused import

--- stackslib/src/net/api/postblock_proposal.rs
+++ stackslib/src/net/api/postblock_proposal.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::hash::Hash;
 #[cfg(any(test, feature = "testing"))]
 use std::sync::LazyLock;
 use std::thread::{self, JoinHandle};
